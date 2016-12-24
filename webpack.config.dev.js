95c07cd7c9e7a5b90af196fce8cc5a171fa679e7
Clément Sauvage <clement.sauvage@me.com>
2016-12-24 10:19:01 +0100

    [Clément] Fix typo and remove unavailable commands

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,9 +4,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    debug: true,
-    noInfo: false,
-    target:'web',
+    target: 'web',
     context: path.join(__dirname, './front'),
     entry: {
         kernel: ['kernel'],
