43ab136eeb0ad536f5d90fb8c9990991d472b756
atamano <atamano@student.42.fr>
2017-01-02 21:08:32 +0100

    Fix requested changes

--- src/AppBundle/Controller/NewsletterController.php
+++ src/AppBundle/Controller/NewsletterController.php
@@ -5,31 +5,50 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Entity\NewsletterSubscription;
 use AppBundle\Form\NewsletterSubscriptionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
+/**
+ * @Route("/newsletter")
+ */
 class NewsletterController extends Controller
 {
     /**
-     * @Route("/newsletter", name="newsletter")
+     * @Route("", name="newsletter_index")
+     * @Method({"GET", "POST"})
      */
-    public function newsletterAction(Request $request)
+    public function indexAction(Request $request)
     {
         $newsletter = new NewsletterSubscription();
+
         $form = $this->createForm(NewsletterSubscriptionType::class, $newsletter);
+        $form->add('submit', SubmitType::class);
 
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $newsletter->setId(NewsletterSubscription::createUuid($newsletter->getEmail()));
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($newsletter);
             $em->flush();
-            $request->getSession()->getFlashBag()->add('info', 'Email bien enregistré !');
 
-            return $this->redirect($this->generateUrl('homepage'));
+            return $this->redirectToRoute('newsletter_thanks');
         }
 
-        return $this->render('newsletter/newsletter.html.twig', array(
+        return $this->render('newsletter/index.html.twig', [
             'form' => $form->createView(),
-        ));
+        ]);
+    }
+
+    /**
+     * @Route("/remerciement", name="newsletter_thanks")
+     * @Method("GET")
+     */
+    public function thanksAction()
+    {
+        return $this->render('newsletter/thanks.html.twig');
     }
 }
