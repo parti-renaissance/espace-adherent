e8803e194d4842c0fee2bdde9d1a27a8004b187a
Titouan Galopin <galopintitouan@gmail.com>
2017-01-28 14:27:09 +0100

    Add environment variable to disable the platform

---
+++ src/AppBundle/FeatureFlag/EnablePlatformSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\FeatureFlag;
+
+use AppBundle\Controller\AdherentController;
+use AppBundle\Controller\CommitteeController;
+use AppBundle\Controller\EventsController;
+use AppBundle\Controller\MembershipController;
+use AppBundle\Controller\Security\AdherentSecurityController;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class EnablePlatformSubscriber implements EventSubscriberInterface
+{
+    private $enablePlatform;
+
+    public function __construct($enablePlatform)
+    {
+        $this->enablePlatform = (bool) $enablePlatform;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.controller' => 'onKernelController',
+        ];
+    }
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        if ($this->enablePlatform) {
+            return;
+        }
+
+        foreach (self::$disabledControllers as $disabledController) {
+            if ($event->getController()[0] instanceof $disabledController) {
+                throw new NotFoundHttpException('The platform is disabled for the moment');
+            }
+        }
+    }
+
+    private static $disabledControllers = [
+        AdherentController::class,
+        CommitteeController::class,
+        EventsController::class,
+        MembershipController::class,
+        AdherentSecurityController::class,
+    ];
+}
