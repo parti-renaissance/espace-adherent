4543bde8fc753fcc7dfd5c7e02d4836e8244b615
Titouan Galopin <galopintitouan@gmail.com>
2017-01-29 05:03:32 +0100

    Configure proxies and htaccess (#158)

--- web/app.php
+++ web/app.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-if (!empty($_SERVER['BASIC_AUTH_USER']) && !empty($_SERVER['BASIC_AUTH_PASSWORD'])) {
+if (!empty($_SERVER['BASIC_AUTH_USER']) && !empty($_SERVER['BASIC_AUTH_PASSWORD']) && !empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
     if (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] !== $_SERVER['BASIC_AUTH_USER'] || $_SERVER['PHP_AUTH_PW'] !== $_SERVER['BASIC_AUTH_PASSWORD']) {
         header('HTTP/1.0 401 Unauthorized');
         header('WWW-Authenticate: Basic realm="Password required"');
@@ -13,6 +13,8 @@ if (!empty($_SERVER['BASIC_AUTH_USER']) && !empty($_SERVER['BASIC_AUTH_PASSWORD'
 }
 
 use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__.'/../app/trusted_proxies.php';
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
