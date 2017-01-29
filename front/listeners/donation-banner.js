63100e29325858e89a504ab8d403b85ae48af7af
Titouan Galopin <galopintitouan@gmail.com>
2017-01-29 16:34:27 +0100

    Remove FOSJsRoutingBundle

--- front/listeners/donation-banner.js
+++ front/listeners/donation-banner.js
@@ -2,12 +2,16 @@
  * Handle the click on the donation banner close button by saving the information in a cookie.
  */
 export default (di) => {
+    let banner = dom('#header-banner'),
+        bannerButton = dom('#header-banner-close-btn');
+
+    if (!banner) {
+        return;
+    }
+
     let cookies = di.get('cookies');
 
     if (typeof cookies.get('banner_donation') === 'undefined') {
-        let banner = dom('#header-banner'),
-            bannerButton = dom('#header-banner-close-btn');
-
         show(banner);
 
         on(bannerButton, 'click', () => {
