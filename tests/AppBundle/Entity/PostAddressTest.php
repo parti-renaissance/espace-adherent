3aff931a23df3a0c338945815f406e33b0e5fcef
Titouan Galopin <galopintitouan@gmail.com>
2017-01-31 18:39:38 +0100

    Add test for a foreign address in PostAdressTest (#182)

--- tests/AppBundle/Entity/PostAddressTest.php
+++ tests/AppBundle/Entity/PostAddressTest.php
@@ -21,6 +21,21 @@ class PostAddressTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('92 bld Victor Hugo, 92110 Clichy, France', $address->getGeocodableAddress());
     }
 
+    public function testCreateFullForeignAddress()
+    {
+        $address = PostAddress::createForeignAddress('US', '20500', 'Washington, DC', '1600 Pennsylvania Ave NW', 48.123456, 5.987654);
+
+        $this->assertSame('US', $address->getCountry());
+        $this->assertSame('1600 Pennsylvania Ave NW', $address->getAddress());
+        $this->assertSame('20500', $address->getPostalCode());
+        $this->assertNull($address->getCity());
+        $this->assertNull($address->getInseeCode());
+        $this->assertSame('Washington, DC', $address->getCityName());
+        $this->assertSame(48.123456, $address->getLatitude());
+        $this->assertSame(5.987654, $address->getLongitude());
+        $this->assertSame('1600 Pennsylvania Ave NW, 20500 Washington, DC, United States', $address->getGeocodableAddress());
+    }
+
     public function testEquals()
     {
         $address1 = PostAddress::createFrenchAddress('92 bld Victor Hugo', '92110-92024');
