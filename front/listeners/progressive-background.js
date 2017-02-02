d7d241eca33a7987b7ab0da3e39823e718932fe8
Titouan Galopin <galopintitouan@gmail.com>
2017-02-02 23:08:58 +0100

    Improve images compression

---
+++ front/listeners/progressive-background.js
@@ -0,0 +1,42 @@
+import stackblur from 'stackblur-canvas';
+
+/*
+ * Progressive background images
+ */
+export default () => {
+    const imgToCanvas = (img) => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
+
+        canvas.getContext('2d').drawImage(img, 0, 0);
+
+        return canvas;
+    };
+
+    findAll(document, '.progressive-background').forEach((element) => {
+        const sdSrc = element.dataset.sd;
+        const hdSrc = element.dataset.hd;
+        const bgPrefix = element.dataset.bgPrefix ? `${element.dataset.bgPrefix}, ` : '';
+
+        const sd = new Image();
+        const hd = new Image();
+
+        sd.src = sdSrc;
+
+        on(sd, 'load', () => {
+            const canvas = imgToCanvas(sd);
+
+            stackblur.canvasRGB(canvas, 0, 0, canvas.width, canvas.height, 10);
+
+            element.style['background-image'] = `${bgPrefix}url(${canvas.toDataURL('image/png')})`;
+
+            // Load high quality version after low quality one
+            hd.src = hdSrc;
+
+            on(hd, 'load', () => {
+                element.style['background-image'] = `${bgPrefix}url(${imgToCanvas(hd).toDataURL('image/png')})`;
+            });
+        });
+    });
+};
