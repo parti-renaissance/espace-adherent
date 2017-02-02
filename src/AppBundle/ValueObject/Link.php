363f31fddffd7b07e607d4c591ddcb8b006a186b
Hugo Hamon <hugo.hamon@sensiolabs.com>
2017-02-02 15:14:01 +0100

    [AppBundle] refactored social networks links.

---
+++ src/AppBundle/ValueObject/Link.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\ValueObject;
+
+final class Link
+{
+    private $url;
+    private $label;
+
+    public function __construct(string $url, string $label = null)
+    {
+        $this->url = $url;
+        $this->label = $label;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+}
