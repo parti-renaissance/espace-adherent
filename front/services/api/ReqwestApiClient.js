23073b5221f329f0d670a24603c242a58c12c825
Hugo Hamon <hugo.hamon@sensiolabs.com>
2017-02-06 16:09:43 +0100

    [AppBundle] added actions to follow and unfollow a committee.

--- front/services/api/ReqwestApiClient.js
+++ front/services/api/ReqwestApiClient.js
@@ -32,5 +32,21 @@ export default class ReqwestApiClient {
             callback(null);
         });
     }
-    
+
+    toggleCommitteeMembership(committeeUuid, committeeSlug, action, token, callback) {
+        let request = this._reqwest({
+            url: '/comites/'+committeeUuid+'/'+committeeSlug+'/'+action,
+            type: 'html',
+            method: 'post',
+            data: { 'token': token }
+        });
+
+        request.then((response) => {
+            callback(JSON.parse(response));
+        });
+
+        request.fail(() => {
+            callback(null);
+        });
+    }
 }
