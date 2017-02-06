129d855241eaaeeba65544dfb4f0690d8813038c
Titouan Galopin <galopintitouan@gmail.com>
2017-02-06 18:03:53 +0100

    Add cache invalidation for assets and add tests for Glide

--- src/AppBundle/Twig/AssetExtension.php
+++ src/AppBundle/Twig/AssetExtension.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Twig;
 
+use AppBundle\Entity\Media;
 use League\Glide\Signatures\SignatureFactory;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Bridge\Twig\Extension\AssetExtension as BaseAssetExtension;
@@ -28,18 +29,22 @@ class AssetExtension extends \Twig_Extension
         return array(
             new \Twig_SimpleFunction('webpack_asset', [$this, 'webpackAsset'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('static_asset', [$this, 'transformedStaticAsset'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction('dynamic_asset', [$this, 'transformedDynamicAsset'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('media_asset', [$this, 'transformedMediaAsset'], ['is_safe' => ['html']]),
         );
     }
 
     public function transformedStaticAsset(string $path, array $parameters = [])
     {
+        $parameters['cache'] = $this->hash;
+
         return $this->generateAssetUrl('static/'.$path, $parameters);
     }
 
-    public function transformedDynamicAsset(string $path, array $parameters = [])
+    public function transformedMediaAsset(Media $media, array $parameters = [])
     {
-        return $this->generateAssetUrl('images/'.$path, $parameters);
+        $parameters['cache'] = substr(md5($media->getUpdatedAt()->format('U')), 0, 20);
+
+        return $this->generateAssetUrl('images/'.$media->getPath(), $parameters);
     }
 
     public function webpackAsset(string $path, $packageName = null)
