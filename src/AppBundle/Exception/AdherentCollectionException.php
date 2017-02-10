ce68d4de6af9ef9251c3b59f0b3e19845c8b6e6f
Romain Pierre <romain.pierre@sensiolabs.com>
2017-02-10 10:42:35 +0100

    Implements the tests for committee members actions

---
+++ src/AppBundle/Exception/AdherentCollectionException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Exception;
+
+class AdherentCollectionException extends \BadMethodCallException
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(
+        $message = 'This method requires a collection of Adherent entities',
+        $code = 0,
+        \Exception $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
