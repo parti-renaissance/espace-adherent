c4b772e466fcf8662b28c81d41495d5b388d1e38
Titouan Galopin <galopintitouan@gmail.com>
2017-02-17 11:10:56 +0100

    Implement Google Tag Manager and improve analytics system

---
+++ front/utils/analytics.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+/*
+ * Initialize the different analytics tools
+ */
+export default () => {
+
+    /*
+     * Google Analytics
+     */
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-72646335-1', 'auto');
+    ga('send', 'pageview');
+
+    /*
+     * Google Tag Manager
+     */
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W4TQQ7T');
+
+};
