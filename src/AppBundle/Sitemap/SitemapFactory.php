515783de7dca8e3b5105c8c3272b89cf8970e58c
Titouan Galopin <galopintitouan@gmail.com>
2017-02-19 16:13:25 +0100

    Fix sitemap priorities

--- src/AppBundle/Sitemap/SitemapFactory.php
+++ src/AppBundle/Sitemap/SitemapFactory.php
@@ -30,11 +30,11 @@ class SitemapFactory
 
         if (!$main->isHit()) {
             $sitemap = new Sitemap();
-            $sitemap->add($this->generateUrl('homepage'), null, ChangeFrequency::HOURLY, 5);
-            $sitemap->add($this->generateUrl('donation_index'), null, ChangeFrequency::MONTHLY, 3);
-            $sitemap->add($this->generateUrl('app_je_marche'), null, ChangeFrequency::NEVER, 1);
-            $sitemap->add($this->generateUrl('newsletter_subscription'), null, ChangeFrequency::NEVER, 1);
-            $sitemap->add($this->generateUrl('invitation_form'), null, ChangeFrequency::NEVER, 1);
+            $sitemap->add($this->generateUrl('homepage'), null, ChangeFrequency::HOURLY, 1);
+            $sitemap->add($this->generateUrl('donation_index'), null, ChangeFrequency::MONTHLY, 0.8);
+            $sitemap->add($this->generateUrl('app_je_marche'), null, ChangeFrequency::NEVER, 0.5);
+            $sitemap->add($this->generateUrl('newsletter_subscription'), null, ChangeFrequency::NEVER, 0.5);
+            $sitemap->add($this->generateUrl('invitation_form'), null, ChangeFrequency::NEVER, 0.5);
 
             $main->set((string) $sitemap);
             $main->expiresAfter(3600);
@@ -73,23 +73,23 @@ class SitemapFactory
                 $this->generateUrl('articles_list', ['category' => $category->getSlug()]),
                 null,
                 ChangeFrequency::DAILY,
-                3
+                0.8
             );
         }
     }
 
     private function addPages(Sitemap $sitemap)
     {
-        $sitemap->add($this->generateUrl('page_emmanuel_macron'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_emmanuel_macron_revolution'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_emmanuel_macron_programme'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_le_mouvement'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_le_mouvement_notre_organisation'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_le_mouvement_la_carte'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_le_mouvement_les_comites'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_le_mouvement_les_evenements'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_le_mouvement_devenez_benevole'), null, ChangeFrequency::WEEKLY, 2);
-        $sitemap->add($this->generateUrl('page_mentions_legales'), null, ChangeFrequency::WEEKLY, 1);
+        $sitemap->add($this->generateUrl('page_emmanuel_macron'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_emmanuel_macron_revolution'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_emmanuel_macron_programme'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_le_mouvement'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_le_mouvement_notre_organisation'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_le_mouvement_la_carte'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_le_mouvement_les_comites'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_le_mouvement_les_evenements'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_le_mouvement_devenez_benevole'), null, ChangeFrequency::WEEKLY, 0.6);
+        $sitemap->add($this->generateUrl('page_mentions_legales'), null, ChangeFrequency::WEEKLY, 0.5);
     }
 
     private function addArticles(Sitemap $sitemap)
@@ -101,7 +101,7 @@ class SitemapFactory
                 $this->generateUrl('article_view', ['slug' => $article->getSlug()]),
                 $article->getUpdatedAt()->format(\DATE_ATOM),
                 ChangeFrequency::WEEKLY,
-                2
+                0.6
             );
         }
     }
