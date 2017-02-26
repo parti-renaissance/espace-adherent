0eb68a053c87d786661ccba7ef93e79097016f01
Titouan Galopin <galopintitouan@gmail.com>
2017-02-26 12:18:58 +0100

    Implement legacy redirections

---
+++ src/AppBundle/Controller/LegacyController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Committee;
+use AppBundle\Entity\Event;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class LegacyController extends Controller
+{
+    /**
+     * @Route("/espaceperso/evenement/{id}-{slug}", requirements={"id"="\d+"})
+     * @Method("GET")
+     */
+    public function redirectEventAction($id): Response
+    {
+        $event = $this->getDoctrine()->getRepository(Event::class)->find((int) $id);
+
+        if (!$event) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->redirectToRoute('app_committee_show_event', [
+            'uuid' => $event->getUuid()->toString(),
+            'slug' => $event->getSlug(),
+        ]);
+    }
+
+    /**
+     * @Route("/espaceperso/comite/{id}-{slug}", requirements={"id"="\d+"})
+     * @Method("GET")
+     */
+    public function redirectCommitteeAction($id): Response
+    {
+        $committee = $this->getDoctrine()->getRepository(Committee::class)->find((int) $id);
+
+        if (!$committee) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->redirectToRoute('app_committee_show', [
+            'uuid' => $committee->getUuid()->toString(),
+            'slug' => $committee->getSlug(),
+        ]);
+    }
+}
