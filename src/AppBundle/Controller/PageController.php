20f6b6a04157270ae9df3fd9062c41225b041223
Titouan Galopin <galopintitouan@gmail.com>
2017-03-01 09:16:11 +0100

    Revert "Add new program page"
    This reverts commit cd3f5ea1e7016355e725cd807a5c251f493a3beb.

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -51,10 +51,11 @@ class PageController extends Controller
      */
     public function emmanuelMacronProgrammeAction()
     {
-        return $this->render('page/emmanuel-macron/propositions.html.twig', [
+        return $this->render('page/emmanuel-macron/programme.html.twig', [
             'page' => $this->getDoctrine()->getRepository(Page::class)->findOneBySlug('emmanuel-macron-propositions'),
 
-            'proposals' => $this->getDoctrine()->getRepository(Proposal::class)->findAllOrderedByPosition(),
+            // TODO Enable when the proposals are ready
+            // 'proposals' => $this->getDoctrine()->getRepository(Proposal::class)->findAllOrderedByPosition(),
         ]);
     }
 
@@ -64,6 +65,8 @@ class PageController extends Controller
      */
     public function emmanuelMacronPropositionAction($slug)
     {
+        // TODO Enable when the proposals are ready
+        throw $this->createNotFoundException();
         $proposal = $this->getDoctrine()->getRepository(Proposal::class)->findOneBySlug($slug);
 
         if (!$proposal || !$proposal->isPublished()) {
