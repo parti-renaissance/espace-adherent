1d41d44f2ae1352246d4ee39064d01c9d23638e4
Titouan Galopin <galopintitouan@gmail.com>
2017-03-01 09:14:52 +0100

    Implement stats API endpoint

---
+++ tests/AppBundle/Controller/StatsControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\AppBundle\MysqlWebTestCase;
+
+class StatsControllerTest extends MysqlWebTestCase
+{
+    use ControllerTestTrait;
+
+    /**
+     * @group functionnal
+     */
+    public function testIndex()
+    {
+        $this->client->request(Request::METHOD_GET, '/api/stats');
+
+        $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
+        $this->assertJson($this->client->getResponse()->getContent());
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->init();
+    }
+
+    protected function tearDown()
+    {
+        $this->kill();
+
+        parent::tearDown();
+    }
+}
