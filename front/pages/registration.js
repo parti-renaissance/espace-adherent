7c1a6345c46a2fff74e3a374f2646e76fadd3242
YaniOz <yani.makouf@gmail.com>
2017-03-07 19:03:38 +0100

    modif register.html.twig et _membership_form.html.twig

--- front/pages/registration.js
+++ front/pages/registration.js
@@ -1,3 +1,45 @@
-/**
- * Created by yanioz on 07/03/2017.
+/*
+ * Registration
  */
+export default () => {
+
+    const passTwo = dom('#membership_request_password_second');
+    const passOne = dom('#membership_request_password_first');
+    const message = dom('#confirmMessage');
+    const okColor = "#016ea2";
+    const notOkColor = "#90100c";
+
+
+    on(passOne, 'input', (event) => {
+        if (passTwo.value !== '') {
+            if (passOne.value == passTwo.value) {
+                passOne.style.backgroundColor = okColor;
+                passTwo.style.backgroundColor = okColor;
+                message.style.color = okColor;
+                message.innerHTML = 'Les mots de passe correspondent!';
+            } else {
+                passOne.style.backgroundColor = notOkColor;
+                passTwo.style.backgroundColor = notOkColor;
+                message.style.color = notOkColor;
+                message.innerHTML = 'Les mots de passe ne correspondent pas !';
+            }
+        }
+    });
+
+    on(passTwo, 'input', (event) => {
+        if (passOne.value !== '') {
+            if (passOne.value == passTwo.value) {
+                passOne.style.backgroundColor = okColor;
+                passTwo.style.backgroundColor = okColor;
+                message.style.color = okColor;
+                message.innerHTML = 'Les mots de passe correspondent!';
+            } else {
+                passOne.style.backgroundColor = notOkColor;
+                passTwo.style.backgroundColor = notOkColor;
+                message.style.color = notOkColor;
+                message.innerHTML = 'Les mots de passe ne correspondent pas !';
+            }
+        }
+    });
+
+}
