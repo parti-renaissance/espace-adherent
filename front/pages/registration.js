0ffb98cc76421f2657e33f553c83d383b03de694
YaniOz <yani.makouf@gmail.com>
2017-03-09 18:47:57 +0100

    simplified registration.js and changed error message

--- front/pages/registration.js
+++ front/pages/registration.js
@@ -4,19 +4,13 @@
 export default () => {
     const passTwo = dom('#membership_request_password_second');
     const passOne = dom('#membership_request_password_first');
-
-    on(passOne, 'input', (event) => {
+    const checkPass = (event) => {
         if (passOne.value === passTwo.value) {
             passTwo.setCustomValidity('');
         } else {
-            passTwo.setCustomValidity('Les mots de passe ne correspondent pas !');
+            passTwo.setCustomValidity('Le mot de passe et sa confirmation sont différents !');
         }
-    });
-    on(passTwo, 'input', (event) => {
-        if (passOne.value === passTwo.value) {
-            passTwo.setCustomValidity('');
-        } else {
-            passTwo.setCustomValidity('Les mots de passe ne correspondent pas !');
-        }
-    });
+    };
+    on(passOne, 'input', checkPass);
+    on(passTwo, 'input', checkPass);
 };
