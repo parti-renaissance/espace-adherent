37083de071ff1e82b653ad9fcb23cd819884162d
Titouan Galopin <galopintitouan@gmail.com>
2017-03-13 09:06:42 +0100

    Fix front icon

--- tests/AppBundle/Controller/EventControllerTest.php
+++ tests/AppBundle/Controller/EventControllerTest.php
@@ -23,17 +23,6 @@ class EventControllerTest extends SqliteWebTestCase
     /**
      * @group functionnal
      */
-    public function testExportIcalEvent()
-    {
-        $url = '/evenements/'.LoadEventData::EVENT_1_UUID.'/'.date('Y-m-d', strtotime('tomorrow')).'-reunion-de-reflexion-parisienne/ical';
-        $this->client->request('GET', $url);
-
-        $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
-    }
-
-    /**
-     * @group functionnal
-     */
     public function testAnonymousUserCanRegisterToEvent()
     {
         $committeeUrl = sprintf('/comites/%s/en-marche-paris-8', LoadAdherentData::COMMITTEE_1_UUID);
@@ -350,6 +339,16 @@ class EventControllerTest extends SqliteWebTestCase
         ]);
 
         $this->assertClientIsRedirectedTo($membersUrl, $this->client);
+    }
+
+    /**
+     * @group functionnal
+     */
+    public function testExportIcalEvent()
+    {
+        $this->client->request('GET', '/evenements/'.LoadEventData::EVENT_1_UUID.'/'.date('Y-m-d', strtotime('tomorrow')).'-reunion-de-reflexion-parisienne/ical');
+
+        $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
     }
 
     private function seeMessageSuccesfullyCreatedFlash(Crawler $crawler, ?string $message = null)
