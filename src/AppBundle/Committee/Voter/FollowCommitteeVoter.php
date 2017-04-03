c1b681b2f7a8d4a1781a70185d764783625f97c9
Hugo Hamon <hugo.hamon@sensiolabs.com>
2017-04-03 13:40:44 +0200

    [AppBundle] committee hosts and supervisors are not allowed to leave their committees anymore.

--- src/AppBundle/Committee/Voter/FollowCommitteeVoter.php
+++ src/AppBundle/Committee/Voter/FollowCommitteeVoter.php
@@ -41,24 +41,14 @@ class FollowCommitteeVoter extends AbstractCommitteeVoter
 
     private function voteOnUnfollowCommitteeAttribute(Adherent $adherent, Committee $committee): bool
     {
-        $committeeUuid = (string) $committee->getUuid();
-
         // An adherent who isn't already following (or hosting) a committee
         // cannot unfollow (or leave) it.
-        if (!$membership = $this->repository->findMembership($adherent, $committeeUuid)) {
+        if (!$membership = $this->repository->findMembership($adherent, $committee->getUuid())) {
             return false;
         }
 
         // Any basic follower of a committee can unfollow the committee
         // at any point in time.
-        if (!$membership->isHostMember()) {
-            return true;
-        }
-
-        // If an adherent is granted a host privilege on a committee,
-        // then he\she can leave it only if there is at least another
-        // host person to manage this committee. A committee cannot be
-        // managed by zero hosts members.
-        return $this->repository->countHostMembers($committeeUuid) >= 2;
+        return $membership->isFollower();
     }
 }
