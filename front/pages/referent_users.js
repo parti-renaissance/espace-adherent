32b3d3d1082060d27827544ab2e8ac061067d9b3
Reinis GRINBERGS <rpg600@gmail.com>
2017-04-04 16:05:16 +0200

    Add a button to export the referent's user list to a csv file

--- front/pages/referent_users.js
+++ front/pages/referent_users.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { CSVLink } from 'react-csv';
 import SearchEngine from '../services/datagrid/SearchEngine';
 import DataGridFactory from '../services/datagrid/DataGridFactory';
 
@@ -11,10 +12,14 @@ export default (slugifier, columns, users) => {
     const selectedUsersCount = dom('#selected-users-count');
     const sendMailBtn = dom('#send-mail-btn');
     const selectedUsersInput = dom('#selected-users-json');
+    const headerNames = columns.map(object => object.name);
+    const headerKeys = columns.map(object => object.key);
+    const filename = `${slugifier.slugify(dom('li.active .csv-export-title').innerHTML)}.csv`;
 
     const onSelectedChange = (rows) => {
         let count = 0;
         const json = [];
+        const data = [];
 
         Object.keys(rows).forEach((key) => {
             json.push({
@@ -22,8 +27,25 @@ export default (slugifier, columns, users) => {
                 id: rows[key].id,
             });
 
+            // Construct the csv data ordered by column names
+            const dataRow = [];
+            headerKeys.map(i => dataRow.push(rows[key][i]));
+            data.push(dataRow);
+
             count += 1;
         });
+
+        render(
+            <CSVLink
+                data={[headerNames, ...data]}
+                filename={filename}
+                className={`btn btn--blue referent__btn-export-csv${0 === count ? ' disabled' : ''}`}
+                onClick={(0 === count) ? e => e.preventDefault() : ''}
+            >
+                Export csv
+            </CSVLink>,
+            dom('#csv-export')
+        );
 
         selectedUsersInput.value = JSON.stringify(json);
         selectedUsersCount.innerHTML = count;
