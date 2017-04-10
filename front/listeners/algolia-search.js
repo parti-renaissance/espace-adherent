f63e024a15595a7d45ed91fb64bf601b847c0bc0
Titouan Galopin <galopintitouan@gmail.com>
2017-04-10 23:49:59 +0200

    Implement JS integration of Algolia

--- front/listeners/algolia-search.js
+++ front/listeners/algolia-search.js
@@ -1,48 +1,40 @@
-import algoliasearch from 'algoliasearch';
+import React from 'react';
+import { render } from 'react-dom';
+import AlgoliaSearch from '../components/AlgoliaSearch';
 
 /*
  * Algolia search
  */
-export default () => {
-    /*
-    var client = algoliasearch('{{ algolia_app_id|e('js') }}', '{{ algolia_api_key|e('js') }}');
-    var articles = client.initIndex('Article_dev');
-    var pages = client.initIndex('Page_dev');
-    var proposals = client.initIndex('Proposal_dev');
-    autocomplete('#aa-search-input', {}, [
-        {
-            source: autocomplete.sources.hits(articles, {hitsPerPage: 3}),
-            displayKey: 'title',
-            templates: {
-                header: '<div class="aa-suggestions-category">Articles:</div>',
-                suggestion: function (suggestion) {
-                    return '<a href="/article/' + suggestion.slug + '">' +
-                        suggestion._highlightResult.title.value + '</a>';
-                }
-            }
-        },
-        {
-            source: autocomplete.sources.hits(pages, {hitsPerPage: 3}),
-            displayKey: 'title',
-            templates: {
-                header: '<div class="aa-suggestions-category">Pages:</div>',
-                suggestion: function (suggestion) {
-                    return '<a href="#">' +
-                        suggestion._highlightResult.title.value + '</a>';
-                }
-            }
-        },
-        {
-            source: autocomplete.sources.hits(proposals, {hitsPerPage: 3}),
-            displayKey: 'title',
-            templates: {
-                header: '<div class="aa-suggestions-category">Propositions:</div>',
-                suggestion: function (suggestion) {
-                    return '<a href="/emmanuel-macron/le-programme/' + suggestion.slug + '">' +
-                        suggestion._highlightResult.title.value + '</a>';
-                }
-            }
-        }
-    ]);
-    */
+export default (di) => {
+    const overlay = dom('#search-overlay');
+    const banner = dom('#header-banner');
+    const header = dom('header');
+    const content = dom('main');
+    const footer = dom('footer');
+
+    on(dom('#je-cherche'), 'click', () => {
+        addClass(overlay, 'g-search');
+        addClass(banner, 'hide-me');
+        addClass(header, 'hide-me');
+        addClass(content, 'hide-me');
+        addClass(footer, 'hide-me');
+
+        dom('#search-input').focus();
+    });
+
+    on(dom('#je-ferme-la-recherche'), 'click', () => {
+        removeClass(overlay, 'g-search');
+        removeClass(banner, 'hide-me');
+        removeClass(header, 'hide-me');
+        removeClass(content, 'hide-me');
+        removeClass(footer, 'hide-me');
+    });
+
+    render(
+        <AlgoliaSearch
+            appId={di.get('algoliaAppId')}
+            appKey={di.get('algoliaAppPublicKey')}
+        />,
+        dom('#search-engine')
+    );
 };
