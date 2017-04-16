558972b119f06c1aa5c3a0e6fa5b8ec0904f5b71
Titouan Galopin <galopintitouan@gmail.com>
2017-04-16 22:06:27 +0200

    Remove unique index on email address in Facebook filter

--- src/AppBundle/Entity/FacebookProfile.php
+++ src/AppBundle/Entity/FacebookProfile.php
@@ -11,8 +11,7 @@ use Ramsey\Uuid\UuidInterface;
 /**
  * @ORM\Table(name="facebook_profiles", uniqueConstraints={
  *   @ORM\UniqueConstraint(name="facebook_profile_uuid", columns="uuid"),
- *   @ORM\UniqueConstraint(name="facebook_profile_facebook_id", columns="facebook_id"),
- *   @ORM\UniqueConstraint(name="facebook_profile_email_address", columns="email_address")
+ *   @ORM\UniqueConstraint(name="facebook_profile_facebook_id", columns="facebook_id")
  * })
  * @ORM\Entity(repositoryClass="AppBundle\Repository\FacebookProfileRepository")
  *
