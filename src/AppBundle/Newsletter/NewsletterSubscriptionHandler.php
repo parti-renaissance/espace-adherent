e75cd7b67376d49752219f2899cde493796606af
Titouan Galopin <galopintitouan@gmail.com>
2017-05-04 23:38:00 +0200

    Disable donation and newsletter emails during campaign silence

--- src/AppBundle/Newsletter/NewsletterSubscriptionHandler.php
+++ src/AppBundle/Newsletter/NewsletterSubscriptionHandler.php
@@ -6,16 +6,19 @@ use AppBundle\Entity\NewsletterSubscription;
 use AppBundle\Mailjet\MailjetService;
 use AppBundle\Mailjet\Message\NewsletterSubscriptionMessage;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class NewsletterSubscriptionHandler
 {
     private $manager;
     private $mailjet;
+    private $requestStack;
 
-    public function __construct(EntityManager $manager, MailjetService $mailjet)
+    public function __construct(EntityManager $manager, MailjetService $mailjet, RequestStack $requestStack)
     {
         $this->manager = $manager;
         $this->mailjet = $mailjet;
+        $this->requestStack = $requestStack;
     }
 
     public function subscribe(NewsletterSubscription $subscription)
@@ -23,7 +26,10 @@ class NewsletterSubscriptionHandler
         $this->manager->persist($subscription);
         $this->manager->flush();
 
-        $this->mailjet->sendMessage(NewsletterSubscriptionMessage::createFromSubscription($subscription));
+        $campaignExpired = (bool) $this->requestStack->getCurrentRequest()->attributes->get('_campaign_expired', false);
+        if (!$campaignExpired) {
+            $this->mailjet->sendMessage(NewsletterSubscriptionMessage::createFromSubscription($subscription));
+        }
     }
 
     public function unsubscribe(?string $email)
