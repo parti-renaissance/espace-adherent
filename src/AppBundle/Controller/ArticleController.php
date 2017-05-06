6cc11e71e89f453bd99389ddfb38d89a4ba870a6
Titouan Galopin <galopintitouan@gmail.com>
2017-05-06 19:26:28 +0200

    Add HTTP cache on several read-only pages

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Article;
 use AppBundle\Entity\ArticleCategory;
 use Psr\Cache\CacheItemPoolInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -22,6 +23,7 @@ class ArticleController extends Controller
      *     name="articles_list"
      * )
      * @Method("GET")
+     * @Cache(maxage=60, smaxage=60)
      */
     public function actualitesAction(string $category, int $page): Response
     {
@@ -59,6 +61,7 @@ class ArticleController extends Controller
     /**
      * @Route("/article/{slug}", defaults={"_enable_campaign_silence"=true}, name="article_view")
      * @Method("GET")
+     * @Cache(maxage=60, smaxage=60)
      */
     public function articleAction($slug): Response
     {
@@ -79,6 +82,7 @@ class ArticleController extends Controller
     /**
      * @Route("/feed.xml", defaults={"_enable_campaign_silence"=true}, name="articles_feed")
      * @Method("GET")
+     * @Cache(maxage=60, smaxage=60)
      */
     public function feedAction(): Response
     {
