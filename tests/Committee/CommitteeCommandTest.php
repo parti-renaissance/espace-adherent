08c2375064fefda7aa62be3974b200542f0a1144
illusionOfParadise <olga.rotach@gmail.com>
2017-06-13 15:26:31 +0200

    [TESTS] Unit test for Committee\CommitteeCommand::createFromCommittee

---
+++ tests/Committee/CommitteeCommandTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\AppBundle\Committee;
+
+use AppBundle\Committee\CommitteeCommand;
+use AppBundle\Entity\Committee;
+use AppBundle\Entity\PostAddress;
+use Ramsey\Uuid\Uuid;
+
+class CommitteeCommandTest extends \PHPUnit_Framework_TestCase
+{
+    const CREATOR_UUID = '3966af25-2b09-407c-9283-c4d2103d0448';
+
+    public function testCreateCommitteeCommandFromCommittee()
+    {
+        $name = 'En Marche ! de Lyon 1er';
+        $description = 'Le comité En Marche ! de Lyon 1er';
+        $uuid = Committee::createUuid($name);
+        $facebook = 'https://facebook.com/en-marche';
+        $twitter = 'enMarcheLyon';
+        $googlePlus = 'https://googleplus.com/en-marche';
+
+        $committee = new Committee(
+            $uuid,
+            Uuid::fromString(self::CREATOR_UUID),
+            $name,
+            $description,
+            PostAddress::createFrenchAddress('2 Rue de la République', '69001-69381'),
+            '69001-en-marche-lyon'
+        );
+        $committee->setSocialNetworks(
+            $facebook,
+            $twitter,
+            $googlePlus
+        );
+
+        $committeeCommand = CommitteeCommand::createFromCommittee($committee);
+
+        $this->assertInstanceOf(CommitteeCommand::class, $committeeCommand);
+        $this->assertSame($uuid, $committeeCommand->getCommitteeUuid());
+        $this->assertSame($committee, $committeeCommand->getCommittee());
+        $this->assertSame($name, $committeeCommand->name);
+        $this->assertSame($description, $committeeCommand->description);
+        $this->assertSame($facebook, $committeeCommand->facebookPageUrl);
+        $this->assertSame($twitter, $committeeCommand->twitterNickname);
+        $this->assertSame($googlePlus, $committeeCommand->googlePlusPageUrl);
+    }
+}
