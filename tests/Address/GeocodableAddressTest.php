92da2e8900fe367ef8312bebd3b6bd9afde4710d
illusionOfParadise <olga.rotach@gmail.com>
2017-06-14 10:34:09 +0200

    [TESTS] Unit test for Address\GeocodableAddress

---
+++ tests/Address/GeocodableAddressTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\AppBundle\Address;
+
+use AppBundle\Address\Address;
+use AppBundle\Address\GeocodableAddress;
+
+class GeocodableAddressTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateGeocodableAddressFromAddress()
+    {
+        $addressStr = '23 rue Ernest Renan';
+        $postalCode = '94110';
+        $country = 'FR';
+
+        $address = new Address();
+        $address->setCountry($country);
+        $address->setAddress($addressStr);
+        $address->setPostalCode($postalCode);
+        $address->setCity('94110-94003');
+
+        $geocodableAddress = GeocodableAddress::createFromAddress($address);
+
+        $this->assertContains($addressStr, (string) $geocodableAddress);
+        $this->assertContains('Arcueil', (string) $geocodableAddress);
+        $this->assertContains($postalCode, (string) $geocodableAddress);
+        $this->assertContains($country, (string) $geocodableAddress);
+        $this->assertSame('23 rue Ernest Renan, 94110 Arcueil, FR', (string) $geocodableAddress);
+    }
+}
