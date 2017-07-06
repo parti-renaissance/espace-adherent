3de496c30c71fd1820fb67d56093eb3a7d25dce5
Marine Boudeau <m14868@gmail.com>
2017-07-06 06:55:39 -0400

    update helptext for skills

--- src/Form/SummaryType.php
+++ src/Form/SummaryType.php
@@ -125,7 +125,7 @@ class SummaryType extends AbstractType
                         'mapped' => false,
                         'required' => false,
                         'attr' => [
-                            'placeholder' => 'Sasissez une compétence',
+                            'placeholder' => 'Saisissez une compétence et tapez "entrer"',
                         ],
                     ])
                     ->add('skills', CollectionType::class, [
