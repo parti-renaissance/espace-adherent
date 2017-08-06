256b86026bcc8100521ca8d806da7e2e9ffd043a
Marine Boudeau <m14868@gmail.com>
2017-08-06 09:22:47 -0400

    Add isTrue class

--- src/Form/NewsletterSubscriptionType.php
+++ src/Form/NewsletterSubscriptionType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class NewsletterSubscriptionType extends AbstractType
 {
