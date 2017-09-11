a3aa565e186684fffd6caf19fd792c1b6dba1344
Rémi Gardien <remi.gardien@sensiolabs.com>
2017-09-11 14:03:48 +0200

    Remove trailing slash in urls [ch756]

---
+++ tests/Controller/RedirectingControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use AppBundle\DataFixtures\ORM\LoadArticleData;
+use AppBundle\DataFixtures\ORM\LoadPageData;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\AppBundle\SqliteWebTestCase;
+
+/**
+ * @group functional
+ */
+class RedirectingControllerTest extends SqliteWebTestCase
+{
+    use ControllerTestTrait;
+
+    /**
+     * @dataProvider provideUrlsAndRedirections
+     */
+    public function testRemoveTrailingSlashAction(string $uri, string $redirectUri)
+    {
+        $this->client->request(Request::METHOD_GET, $uri);
+
+        $this->assertResponseStatusCode(Response::HTTP_MOVED_PERMANENTLY, $this->client->getResponse());
+        $this->assertClientIsRedirectedTo($redirectUri, $this->client);
+
+        $this->client->followRedirect();
+
+        $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
+    }
+
+    public function provideUrlsAndRedirections()
+    {
+        yield 'Emmanuel Macron' => ['/emmanuel-macron/', '/emmanuel-macron'];
+        yield 'Le mouvement' => ['/le-mouvement/', '/le-mouvement'];
+        yield 'Actualités' => ['/articles/tout/', '/articles/tout'];
+        yield 'Inscription' => ['/inscription/', '/inscription'];
+        yield 'Inscription with parameters' => ['/inscription/?param1=value1&param2=value2', '/inscription?param1=value1&param2=value2'];
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->init([
+            LoadPageData::class,
+            LoadArticleData::class,
+        ]);
+    }
+
+    protected function tearDown()
+    {
+        $this->kill();
+
+        parent::tearDown();
+    }
+}
