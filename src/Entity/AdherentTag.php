012afea2084fc7d81d9a9b96731194f9d9664693
illusionOfParadise <olga.rotach@gmail.com>
2017-10-03 18:24:59 +0200

    Create adherent tags admin page

---
+++ src/Entity/AdherentTag.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Algolia\AlgoliaSearchBundle\Mapping\Annotation as Algolia;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *   name="adherent_tags",
+ *   uniqueConstraints={
+ *     @ORM\UniqueConstraint(name="adherent_tag_name_unique", columns="name")
+ *   }
+ * )
+ *
+ * @UniqueEntity("name")
+ *
+ * @Algolia\Index
+ */
+class AdherentTag
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned": true})
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(length=100, unique=true)
+     *
+     * @Assert\NotBlank
+     * @Assert\Length(max="100")
+     *
+     * @Algolia\Attribute
+     */
+    private $name = '';
+
+    public function __construct(?string $name = null)
+    {
+        if ($name) {
+            $this->name = $name;
+        }
+    }
+
+    public function __toString(): string
+    {
+        return $this->name ?: '';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
