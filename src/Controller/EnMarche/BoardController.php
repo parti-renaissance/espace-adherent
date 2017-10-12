da61c40b31dc3c5e2487b89eb53266579d0be8c1
Rémi Gardien <remi.gardien@sensiolabs.com>
2017-10-12 11:09:47 +0200

    [BOARD_MEMBER] Add filters

--- src/Controller/EnMarche/BoardController.php
+++ src/Controller/EnMarche/BoardController.php
@@ -2,10 +2,14 @@
 
 namespace AppBundle\Controller\EnMarche;
 
+use AppBundle\BoardMember\BoardMemberFilter;
+use AppBundle\Entity\Adherent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/espace-membres-conseil")
@@ -13,6 +17,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
  */
 class BoardController extends Controller
 {
+    const TOKEN_ID = 'board_search';
+
     /**
      * @Route("/", defaults={"_enable_campaign_silence"=true}, name="app_board_home")
      * @Method("GET")
@@ -28,9 +34,26 @@ class BoardController extends Controller
      * @Route("/recherche", defaults={"_enable_campaign_silence"=true}, name="app_board_search")
      * @Method("GET")
      */
-    public function searchAction()
+    public function searchAction(Request $request): Response
     {
-        return $this->render('board/search.html.twig');
+        $filter = new BoardMemberFilter();
+        $filter->handleRequest($request);
+
+        if ($filter->hasToken() && !$this->isCsrfTokenValid(self::TOKEN_ID, $filter->getToken())) {
+            return $this->redirectToRoute('app_board_search');
+        }
+
+        $repository = $this->getDoctrine()->getRepository(Adherent::class);
+        $results = $repository->searchBoardMembers($filter->hasToken() ? $filter : new BoardMemberFilter());
+
+        $filter->setToken($this->get('security.csrf.token_manager')->getToken(self::TOKEN_ID));
+
+        return $this->render('board/search.html.twig', [
+            'filter' => $filter,
+            'has_filter' => $request->query->has(BoardMemberFilter::PARAMETER_TOKEN),
+            'results_count' => $results->count(),
+            'results' => $results->getQuery()->getResult(),
+        ]);
     }
 
     /**
