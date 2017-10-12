8214bfd3efbce640a794e3ab20336bb5a86d6689
illusionOfParadise <olga.rotach@gmail.com>
2017-10-12 12:16:39 +0200

    [BOARD_MEMBER] Add board member role admin + board member to adherent admin

--- src/Entity/BoardMember/BoardMember.php
+++ src/Entity/BoardMember/BoardMember.php
@@ -4,7 +4,7 @@ namespace AppBundle\Entity\BoardMember;
 
 use Algolia\AlgoliaSearchBundle\Mapping\Annotation as Algolia;
 use AppBundle\Entity\Adherent;
-use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -19,6 +19,12 @@ class BoardMember
     const AREA_FRANCE_METROPOLITAN = 'metropolitan';
     const AREA_OVERSEAS_FRANCE = 'overseas';
     const AREA_ABROAD = 'abroad';
+
+    const AREAS_CHOICES = [
+        'board_member.area.metropolitan' => self::AREA_FRANCE_METROPOLITAN,
+        'board_member.area.overseas' => self::AREA_OVERSEAS_FRANCE,
+        'board_member.area.abroad' => self::AREA_ABROAD,
+    ];
 
     /**
      * @ORM\Column(type="integer")
@@ -60,7 +66,7 @@ class BoardMember
      *         @ORM\JoinColumn(name="role_id", referencedColumnName="id", onDelete="CASCADE")
      *     }
      * )
-     * @ORM\OrderBy({"name"="ASC"})
+     * @ORM\OrderBy({"maleName"="ASC"})
      * @Assert\NotNull
      */
     private $roles;
@@ -118,7 +124,7 @@ class BoardMember
         return $this->roles;
     }
 
-    public function setRoles(ArrayCollection $roles): void
+    public function setRoles(iterable $roles): void
     {
         $this->roles = $roles;
     }
