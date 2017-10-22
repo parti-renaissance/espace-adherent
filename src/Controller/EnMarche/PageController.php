570cfe146fb519a359c89889cd20a5e27a50d8f1
Marine Boudeau <m14868@gmail.com>
2017-10-22 21:45:15 +0200

    More tweaks to the mooc and campus pages

--- src/Controller/EnMarche/PageController.php
+++ src/Controller/EnMarche/PageController.php
@@ -20,14 +20,14 @@ class PageController extends Controller
     use CanaryControllerTrait;
 
     /**
-     * @Route("/mooc", defaults={"_enable_campaign_silence"=true}, name="page_mooc")
+     * @Route("/campus/mooc", defaults={"_enable_campaign_silence"=true}, name="page_mooc")
      * @Method("GET")
      */
     public function moocAction()
     {
         $this->disableInProduction();
 
-        return $this->render('page/mooc.html.twig');
+        return $this->render('page/campus/mooc.html.twig');
     }
 
     /**
