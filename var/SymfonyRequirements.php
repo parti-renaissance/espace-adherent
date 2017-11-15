a935e36ceff862e13c095ec36c9f9754984573af
Marine Boudeau <m14868@gmail.com>
2017-11-15 14:40:25 +0100

    Revert "Add new layout to interactive purchasing power"
    This reverts commit 887b2c5f88bc69e105ba936f4a2988e52ff0aaf4.

--- var/SymfonyRequirements.php
+++ var/SymfonyRequirements.php
@@ -260,7 +260,7 @@ class RequirementCollection implements IteratorAggregate
      *
      * @param RequirementCollection $collection A RequirementCollection instance
      */
-    public function addCollection(self $collection)
+    public function addCollection(RequirementCollection $collection)
     {
         $this->requirements = array_merge($this->requirements, $collection->all());
     }
