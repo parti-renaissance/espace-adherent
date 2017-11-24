b3bbd61848f8f544bcb02433c88bb2cc2559398a
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2017-11-24 17:31:15 +0100

    Add Citizen Project Skills

--- src/Entity/CitizenProjectCategory.php
+++ src/Entity/CitizenProjectCategory.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Algolia\AlgoliaSearchBundle\Mapping\Annotation as Algolia;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
@@ -21,4 +23,29 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  */
 class CitizenProjectCategory extends BaseEventCategory
 {
+    /**
+     * @var CitizenProjectCategorySkill[]|Collection
+     *
+     * @ORM\OneToMany(targetEntity="CitizenProjectCategorySkill", mappedBy="citizen_project_categories", indexBy="id", cascade={"all"}, orphanRemoval=true)
+     */
+    private $categorySkills;
+
+    public function __construct(?string $name = null, ?string $status = parent::DISABLED)
+    {
+        parent::__construct($name, $status);
+
+        $this->categorySkills = new ArrayCollection();
+    }
+
+    public function addSkill(CitizenProjectSkill $skill, bool $promotion = false)
+    {
+        foreach ($this->categorySkills as $categorySkill) {
+            if ($categorySkill->getSkill() === $skill && $categorySkill->getPromotion() === $promotion) {
+                return;
+            }
+        }
+
+        $categorySkill = new CitizenProjectCategorySkill($this, $skill, $promotion);
+        $this->categorySkills->add($categorySkill);
+    }
 }
