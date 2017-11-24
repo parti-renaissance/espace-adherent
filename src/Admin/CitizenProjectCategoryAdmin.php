6019aec0a9f383be1d1866fb2352c7dac7ad8231
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2017-11-24 17:31:15 +0100

    Add Citizen Project Skills

--- src/Admin/CitizenProjectCategoryAdmin.php
+++ src/Admin/CitizenProjectCategoryAdmin.php
@@ -3,10 +3,12 @@
 namespace AppBundle\Admin;
 
 use AppBundle\Entity\BaseEventCategory;
+use AppBundle\Form\CategorySkillType;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CitizenProjectCategoryAdmin extends AbstractAdmin
@@ -29,6 +31,17 @@ class CitizenProjectCategoryAdmin extends AbstractAdmin
                 'choices' => [
                     'Visible' => BaseEventCategory::ENABLED,
                     'Masqué' => BaseEventCategory::DISABLED,
+                ],
+            ])
+            ->add('categorySkills', CollectionType::class, [
+                'label' => 'Liste des compétences',
+                'entry_type' => CategorySkillType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'error_bubbling' => false,
+                'attr' => [
+                    'class' => 'category-skills-bloc',
                 ],
             ])
         ;
--- src/DataFixtures/ORM/LoadCitizenProjectCategoryData.php
+++ src/DataFixtures/ORM/LoadCitizenProjectCategoryData.php
@@ -3,6 +3,7 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\CitizenProjectCategory;
+use AppBundle\Entity\EventCategory;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,18 +11,17 @@ use Doctrine\Common\Persistence\ObjectManager;
 class LoadCitizenProjectCategoryData extends AbstractFixture implements FixtureInterface
 {
     const CITIZEN_PROJECT_CATEGORIES = [
-        'CPC001' => 'Éducation, culture et citoyenneté',
-        'CPC002' => 'Emploi et formation professionnelle',
-        'CPC003' => 'Lien social et aide aux personnes en difficulté',
-        'CPC004' => 'Lutte contre les discriminations',
-        'CPC005' => 'Numérique, innovation et entrepreneuriat',
-        'CPC006' => 'Transition écologique et solidaire',
+        'CPC001' => 'Nature et Environnement',
+        'CPC002' => 'Education, culture et citoyenneté',
+        'CPC003' => 'Culture',
+        'CPC004' => 'Lien social et aide aux personnes en difficulté',
+        'CPC005' => 'Santé',
     ];
 
     public function load(ObjectManager $manager)
     {
         foreach (self::CITIZEN_PROJECT_CATEGORIES as $code => $name) {
-            $category = new CitizenProjectCategory($name);
+            $category = new CitizenProjectCategory($name, EventCategory::ENABLED);
             $manager->persist($category);
             $this->addReference(strtolower($code), $category);
         }
