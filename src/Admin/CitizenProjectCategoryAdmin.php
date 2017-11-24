1fba628abd20d037b95fe4232614daff25625f95
Rémi Gardien <remi.gardien@sensiolabs.com>
2017-11-24 16:04:39 +0100

    Administrates citizen project categories

---
+++ src/Admin/CitizenProjectCategoryAdmin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use AppBundle\Entity\BaseEventCategory;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CitizenProjectCategoryAdmin extends AbstractAdmin
+{
+    protected $datagridValues = [
+        '_page' => 1,
+        '_per_page' => 32,
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'name',
+    ];
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => 'Visibilité',
+                'choices' => [
+                    'Visible' => BaseEventCategory::ENABLED,
+                    'Masqué' => BaseEventCategory::DISABLED,
+                ],
+            ])
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('name', null, [
+                'label' => 'Nom',
+            ])
+            ->add('status', null, [
+                'label' => 'Visibilité',
+                'template' => 'admin/citizen_project_category/list_status.html.twig',
+            ])
+            ->add('_action', null, [
+                'virtual_field' => true,
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+}
