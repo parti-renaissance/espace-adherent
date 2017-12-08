bd63b0bc4706d740f2d5097ec59fbcad98572215
HeahDude <heah@heahprod.com>
2017-12-08 18:14:07 +0100

    WIP Handled show/edit citizen action

--- src/Controller/EnMarche/CitizenActionManagerController.php
+++ src/Controller/EnMarche/CitizenActionManagerController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\EnMarche;
 use AppBundle\CitizenAction\CitizenActionCommand;
 use AppBundle\CitizenAction\CitizenActionCommandHandler;
 use AppBundle\CitizenProject\CitizenProjectManager;
+use AppBundle\Entity\CitizenAction;
 use AppBundle\Entity\CitizenProject;
 use AppBundle\Event\EventRegistrationCommand;
 use AppBundle\Form\CitizenActionCommandType;
@@ -41,12 +42,41 @@ class CitizenActionManagerController extends Controller
             $this->get('app.event.registration_handler')->handle(new EventRegistrationCommand($action, $this->getUser()));
             $this->addFlash('info', $this->get('translator')->trans('citizen_action.creation.success'));
 
-            return $this->redirectToRoute('app_event_show', [
+            return $this->redirectToRoute('app_citizen_action_show', [
                 'slug' => $action->getSlug(),
             ]);
         }
 
         return $this->render('citizen_action_manager/create.html.twig', [
+            'citizen_project' => $project,
+            'project_hosts' => $citizenProjectManager->getCitizenProjectAdministrators($project),
+            'citizen_action_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}/editer", name="app_citizen_action_manager_edit", requirements={"slug": "[A-Za-z0-9\-]+"})
+     * @Method("GET|POST")
+     * @Security("is_granted('EDIT_CITIZEN_ACTION', project)")
+     */
+    public function editAction(Request $request, CitizenProject $project, CitizenAction $action, CitizenProjectManager $citizenProjectManager): Response
+    {
+        $command = CitizenActionCommand::createFromCitizenAction($action);
+        $form = $this->createForm(CitizenActionCommandType::class, $command)
+            ->handleRequest($request)
+        ;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $action = $this->get(CitizenActionCommandHandler::class)->handleUpdate($command, $action);
+
+            $this->addFlash('info', $this->get('translator')->trans('citizen_action.update.success'));
+
+            return $this->redirectToRoute('app_citizen_action_show', [
+                'slug' => $action->getSlug(),
+            ]);
+        }
+
+        return $this->render('citizen_action_manager/edit.html.twig', [
             'citizen_project' => $project,
             'project_hosts' => $citizenProjectManager->getCitizenProjectAdministrators($project),
             'citizen_action_form' => $form->createView(),
