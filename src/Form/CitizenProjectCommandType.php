afafd1bf837821114e9ffa207bb82eadc349c2ba
Guillaume Denhez <denhezguillaume@gmail.com>
2017-12-18 10:09:53 +0100

    add some text reviews

--- src/Form/CitizenProjectCommandType.php
+++ src/Form/CitizenProjectCommandType.php
@@ -133,7 +133,7 @@ class CitizenProjectCommandType extends AbstractType
                     'required' => false,
                     'filter_emojis' => true,
                     'attr' => [
-                        'placeholder' => 'Vous avez déjà le soutient d\'un comité local ? Indiquez son nom : (Optionnel)',
+                        'placeholder' => 'Ajouter le comité local',
                     ],
                 ]);
 
