e12cd360999ea37c287f56baf3dae24bb3cda84c
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2017-12-19 11:22:36 +0100

    [CP] Added uploaded file edit form

---
+++ src/CitizenProject/CitizenProjectUpdateCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\CitizenProject;
+
+use AppBundle\Address\NullableAddress;
+use AppBundle\Entity\CitizenProject;
+use AppBundle\Entity\CitizenProjectCommitteeSupport;
+
+class CitizenProjectUpdateCommand extends CitizenProjectCommand
+{
+    public static function createFromCitizenProject(CitizenProject $citizenProject): self
+    {
+        $address = $citizenProject->getPostAddress() ? NullableAddress::createFromAddress($citizenProject->getPostAddress()) : null;
+        $dto = new self($address);
+        $dto->name = $citizenProject->getName();
+        $dto->subtitle = $citizenProject->getSubtitle();
+        $dto->category = $citizenProject->getCategory();
+        $dto->phone = $citizenProject->getPhone();
+        $dto->committeeSupports = $citizenProject->getCommitteeSupports();
+        $dto->problemDescription = $citizenProject->getProblemDescription();
+        $dto->proposedSolution = $citizenProject->getProposedSolution();
+        $dto->requiredMeans = $citizenProject->getRequiredMeans();
+        $dto->assistanceNeeded = $citizenProject->isAssistanceNeeded();
+        $dto->assistanceContent = $citizenProject->getAssistanceContent();
+        $dto->citizenProject = $citizenProject;
+        $dto->skills = $citizenProject->getSkills();
+
+        if ($citizenProject->getPostAddress()) {
+            $dto->address->setAddress(null); // Fix the Citizen Project nullable address
+        }
+
+        /** @var CitizenProjectCommitteeSupport $committeeSupport */
+        foreach ($dto->committeeSupports as $committeeSupport) {
+            $dto->addCommittee($committeeSupport->getCommittee());
+        }
+
+        return $dto;
+    }
+}
--- src/Entity/CitizenProject.php
+++ src/Entity/CitizenProject.php
@@ -478,7 +478,8 @@ class CitizenProject extends BaseGroup implements CoordinatorAreaInterface
         NullablePostAddress $address,
         PhoneNumber $phone,
         iterable $skills,
-        iterable $committees
+        iterable $committees,
+        ?UploadedFile $image
     ): void {
         $this->setName($name);
         $this->setSubtitle($subtitle);
@@ -489,6 +490,10 @@ class CitizenProject extends BaseGroup implements CoordinatorAreaInterface
         $this->setProposedSolution($proposedSolution);
         $this->setRequiredMeans($requiredMeans);
         $this->setSkills($skills);
+
+        if ($image instanceof UploadedFile) {
+            $this->setImage($image);
+        }
 
         if (null === $this->postAddress || !$this->postAddress->equals($address)) {
             $this->postAddress = $address;
