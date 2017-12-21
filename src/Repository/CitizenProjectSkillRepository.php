d9c675520245e58593dceef057ba79440b541265
Nicolas Bordes <nicolasbordes@me.com>
2017-12-21 16:51:43 +0100

    Add import command to import citizen project skills

--- src/Repository/CitizenProjectSkillRepository.php
+++ src/Repository/CitizenProjectSkillRepository.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\CitizenProjectSkill;
 use Doctrine\ORM\EntityRepository;
 
 class CitizenProjectSkillRepository extends EntityRepository
 {
+    public function findOneByName(string $name): ?CitizenProjectSkill
+    {
+        return $this->findOneBy(['name' => $name]);
+    }
 }
