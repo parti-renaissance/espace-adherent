b356b62f996bcb27981dc91e323996749ca7428f
HeahDude <heah@heahprod.com>
2018-01-07 19:06:02 +0100

    Moved voters

---
+++ tests/Security/Voter/Committee/SuperviseCommitteeVoterTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\AppBundle\Security\Voter\Committee;
+
+use AppBundle\Committee\CommitteeManager;
+use AppBundle\Committee\CommitteePermissions;
+use AppBundle\Security\Voter\Committee\SuperviseCommitteeVoter;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\User\User;
+
+class SuperviseCommitteeVoterTest extends AbstractCommitteeVoterTest
+{
+    /* @var SuperviseCommitteeVoter */
+    private $voter;
+    private $manager;
+
+    public function testUnsupportedAdherentType()
+    {
+        $committee = $this->createCommittee(self::ADHERENT_2_UUID);
+        $token = $this->createAuthenticatedToken(new User('foobar', 'password', ['ROLE_ADHERENT']));
+
+        $this->manager->expects($this->never())->method('superviseCommittee');
+
+        $this->assertSame(
+            VoterInterface::ACCESS_DENIED,
+            $this->voter->vote($token, $committee, [CommitteePermissions::SUPERVISE])
+        );
+    }
+
+    public function testUnsupportedCommitteeType()
+    {
+        $token = $this->createAuthenticatedToken($this->createAdherentFromUuidAndEmail(self::ADHERENT_2_UUID));
+
+        $this->manager->expects($this->never())->method('superviseCommittee');
+
+        $this->assertSame(
+            VoterInterface::ACCESS_ABSTAIN,
+            $this->voter->vote($token, new \stdClass(), [CommitteePermissions::SUPERVISE])
+        );
+    }
+
+    public function testUnsupportedAttribute()
+    {
+        $adherent = $this->createAdherentFromUuidAndEmail(self::ADHERENT_1_UUID);
+        $committee = $this->createCommittee(self::ADHERENT_1_UUID);
+        $token = $this->createAuthenticatedToken($adherent);
+
+        $this->manager->expects($this->never())->method('superviseCommittee');
+
+        $this->assertSame(
+            VoterInterface::ACCESS_ABSTAIN,
+            $this->voter->vote($token, $committee, [CommitteePermissions::CREATE])
+        );
+    }
+
+    public function testAnonymousIsNotGrantedToSuperviseAnApprovedCommittee()
+    {
+        $committee = $this->createCommittee(self::ADHERENT_1_UUID);
+        $committee->approved();
+
+        $token = $this->createAnonymousToken();
+
+        $this->manager->expects($this->never())->method('superviseCommittee');
+
+        $this->assertSame(
+            VoterInterface::ACCESS_DENIED,
+            $this->voter->vote($token, $committee, [CommitteePermissions::SUPERVISE])
+        );
+    }
+
+    public function testAdherentIsNotGrantedToSuperviseCommittee()
+    {
+        $adherent = $this->createAdherentFromUuidAndEmail(self::ADHERENT_1_UUID);
+
+        $committee = $this->createCommittee(self::ADHERENT_2_UUID);
+        $committee->approved();
+
+        $token = $this->createAuthenticatedToken($adherent);
+
+        $this
+            ->manager
+            ->expects($this->once())
+            ->method('superviseCommittee')
+            ->with($adherent, $committee)
+            ->willReturn(false);
+
+        $this->assertSame(
+            VoterInterface::ACCESS_DENIED,
+            $this->voter->vote($token, $committee, [CommitteePermissions::SUPERVISE])
+        );
+    }
+
+    public function testCommitteeOwnerIsGrantedToSuperviseCommittee()
+    {
+        $adherent = $this->createAdherentFromUuidAndEmail(self::ADHERENT_1_UUID);
+        $committee = $this->createCommittee($adherent->getUuid());
+        $token = $this->createAuthenticatedToken($adherent);
+
+        $this
+            ->manager
+            ->expects($this->once())
+            ->method('superviseCommittee')
+            ->with($adherent, $committee)
+            ->willReturn(true);
+
+        $this->assertSame(
+            VoterInterface::ACCESS_GRANTED,
+            $this->voter->vote($token, $committee, [CommitteePermissions::SUPERVISE])
+        );
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->manager = $this->createMock(CommitteeManager::class);
+        $this->voter = new SuperviseCommitteeVoter($this->manager);
+    }
+
+    protected function tearDown()
+    {
+        $this->manager = null;
+        $this->voter = null;
+
+        parent::tearDown();
+    }
+}
