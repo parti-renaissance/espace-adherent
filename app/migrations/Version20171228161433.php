89b8bbcf35a6f57823d41151e2e386c50b70c7e1
Rémi Gardien <remi.gardien@sensiolabs.com>
2018-01-08 20:12:25 +0100

    Update image_name during migration

--- app/migrations/Version20171228161433.php
+++ app/migrations/Version20171228161433.php
@@ -10,6 +10,7 @@ class Version20171228161433 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->addSql('ALTER TABLE citizen_projects ADD image_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('UPDATE citizen_projects SET image_name = CONCAT(uuid, \'.jpg\') WHERE image_uploaded = 1');
     }
 
     public function down(Schema $schema)
