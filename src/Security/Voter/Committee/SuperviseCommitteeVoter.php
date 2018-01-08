a493c7a00dfb31778fab6bf59fe5b98ed75912a4
Jules Pietri <heahdude@yahoo.fr>
2018-01-08 15:45:42 +0100

    Moved voters (#1975)

---
+++ src/Security/Voter/Committee/SuperviseCommitteeVoter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Security\Voter\Committee;
+
+use AppBundle\Committee\CommitteeManager;
+use AppBundle\Committee\CommitteePermissions;
+use AppBundle\Entity\Adherent;
+use AppBundle\Entity\Committee;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class SuperviseCommitteeVoter extends Voter
+{
+    private $manager;
+
+    public function __construct(CommitteeManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    protected function supports($attribute, $subject): bool
+    {
+        return CommitteePermissions::SUPERVISE === $attribute && $subject instanceof Committee;
+    }
+
+    /**
+     * @param string         $attribute
+     * @param Committee      $committee
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $committee, TokenInterface $token): bool
+    {
+        if ($token instanceof AnonymousToken) {
+            return false;
+        }
+
+        $supervisor = $token->getUser();
+
+        if (!$supervisor instanceof Adherent) {
+            return false;
+        }
+
+        return $this->manager->superviseCommittee($supervisor, $committee);
+    }
+}
