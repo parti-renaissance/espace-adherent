9b9e9f4faf8a299d7df4fd19371dae3853a48b5f
Marine Boudeau <m14868@gmail.com>
2018-01-10 17:27:44 +0100

    Add more visual tweaks to the PC creation form

--- src/Form/CitizenProjectCommandType.php
+++ src/Form/CitizenProjectCommandType.php
@@ -52,7 +52,7 @@ class CitizenProjectCommandType extends AbstractType
                 'attr' => ['maxlength' => 60],
             ])
             ->add('category', EventCategoryType::class, [
-                'placeholder' => 'Choisir une catégorie',
+                'placeholder' => 'Choisir un thème',
                 'class' => CitizenProjectCategory::class,
             ])
             ->add('image', FileType::class, [
