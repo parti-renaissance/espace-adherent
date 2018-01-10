1ebb32e5d1dfb98c7d1864226052c37e9618936b
Rémi Gardien <remi.gardien@sensiolabs.com>
2018-01-10 10:45:38 +0100

    Fix CS

--- src/Entity/EntityTranslationInterface.php
+++ src/Entity/EntityTranslationInterface.php
@@ -6,5 +6,5 @@ use A2lix\I18nDoctrineBundle\Doctrine\Interfaces\OneLocaleInterface;
 
 interface EntityTranslationInterface extends OneLocaleInterface
 {
-    //public function isEmpty(): bool;
+    public function isEmpty(): bool;
 }
