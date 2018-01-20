281be7033364907d20c41799bc53351115d6f2d6
Rémi Gardien <remi.gardien@sensiolabs.com>
2018-01-20 22:48:57 +0100

    Test TimelineSynchronizeCommand

---
+++ tests/Command/TimelineSynchronizeCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\DataFixtures\ORM\LoadTimelineData;
+use Tests\AppBundle\SqliteWebTestCase;
+
+/**
+ * @group functional
+ */
+class TimelineSynchronizeCommandTest extends SqliteWebTestCase
+{
+    public function testCommand()
+    {
+        $output = $this->runCommand('app:timeline:synchronize');
+
+        $this->assertContains('Synchronizing entity AppBundle\Entity\Timeline\Profile ... done, 5 records indexed', $output);
+        $this->assertContains('Synchronizing entity AppBundle\Entity\Timeline\Theme ... done, 5 records indexed', $output);
+        $this->assertContains('Synchronizing entity AppBundle\Entity\Timeline\Measure ... done, 17 records indexed', $output);
+        $this->assertContains('Timeline has been successfully synchronized with Algolia.', $output);
+    }
+
+    public function setUp()
+    {
+        $this->loadFixtures([
+            LoadTimelineData::class,
+        ]);
+
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        $this->loadFixtures([]);
+
+        parent::tearDown();
+    }
+}
