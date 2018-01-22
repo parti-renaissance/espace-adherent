78b40ebb9eb59acfcc06f87e37e6a6511a4da557
Benjamin Lazarecki <benjamin.lazarecki@gmail.com>
2018-01-22 18:40:52 +0100

    Nullable fields (#2045)

--- src/Entity/Adherent.php
+++ src/Entity/Adherent.php
@@ -60,7 +60,7 @@ class Adherent implements UserInterface, GeoPointInterface, EncoderAwareInterfac
     private $oldPassword;
 
     /**
-     * @ORM\Column(length=6)
+     * @ORM\Column(length=6, nullable=true)
      */
     private $gender;
 
@@ -83,7 +83,7 @@ class Adherent implements UserInterface, GeoPointInterface, EncoderAwareInterfac
     private $birthdate;
 
     /**
-     * @ORM\Column(length=20)
+     * @ORM\Column(length=20, nullable=true)
      */
     private $position;
 
@@ -114,7 +114,7 @@ class Adherent implements UserInterface, GeoPointInterface, EncoderAwareInterfac
     private $lastLoggedAt;
 
     /**
-     * @ORM\Column(type="json_array")
+     * @ORM\Column(type="json_array", nullable=true)
      */
     private $interests = [];
 
@@ -226,11 +226,11 @@ class Adherent implements UserInterface, GeoPointInterface, EncoderAwareInterfac
         UuidInterface $uuid,
         string $emailAddress,
         string $password,
-        string $gender,
+        ?string $gender,
         string $firstName,
         string $lastName,
         ?\DateTime $birthDate,
-        string $position,
+        ?string $position,
         PostAddress $postAddress,
         PhoneNumber $phone = null,
         string $status = self::DISABLED,
@@ -389,7 +389,7 @@ class Adherent implements UserInterface, GeoPointInterface, EncoderAwareInterfac
         return $this->phone;
     }
 
-    public function getGender(): string
+    public function getGender(): ?string
     {
         return $this->gender;
     }
