e25e2fed531a98264cb28e793023ff7007c8bce0
Antoine TAMANO <atamano@student.42.fr>
2018-02-02 17:22:41 +0100

    Auto deploy develop (#2127)
    * Fix logs name (#2126)
    
    * Use deployment for migration tasks (#2123)
    
    * feat: add autodeploy on develop
    
    * fix slack notifications

--- scripts/deployment_develop.sh
+++ scripts/deployment_develop.sh
@@ -8,11 +8,10 @@ sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials $GCLOUD
 # Migrates database
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/gcloud-service-key.json
 
-sudo /opt/google-cloud-sdk/bin/kubectl set image pod/staging-migrate-tasks enmarche=eu.gcr.io/$GCLOUD_PROJECT/app:$CIRCLE_SHA1
-sudo /opt/google-cloud-sdk/bin/kubectl get pod staging-migrate-tasks -o yaml | sudo /opt/google-cloud-sdk/bin/kubectl replace --force -f -
+sudo /opt/google-cloud-sdk/bin/kubectl set image deploy/develop-migrate enmarche=eu.gcr.io/$GCLOUD_PROJECT/app:$CIRCLE_SHA1
 
-# Deploy to staging
-declare -a images=("staging-app" "staging-worker-mailer-campaign" "staging-worker-mailer-transactional" "staging-worker-referent")
+# Deploy to develop
+declare -a images=("develop-app" "develop-worker-mailer-campaign" "develop-worker-mailer-transactional" "develop-worker-referent")
 
 for image in "${images[@]}"
 do
@@ -20,6 +19,6 @@ do
 done
 
 # Send result to slack
-migration_log=$(sudo /opt/google-cloud-sdk/bin/kubectl logs staging-migrate-tasks --container=enmarche || true)
+migration_log=$(sudo /opt/google-cloud-sdk/bin/kubectl logs deploy/develop-migrate --container=enmarche || true)
 json="{\"text\": \"\`\`\`$(echo $migration_log | sed 's/"//g' | sed "s/'//g" | sed 's/\\/\//g' )\`\`\`\"}"
 curl -s "Content-Type: application/json" -d "payload=$json" https://hooks.slack.com/services/$SLACK_TOKEN
---
+++ scripts/deployment_staging.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Get credentials
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials $GCLOUD_CLUSTER --project $GCLOUD_PROJECT --zone europe-west1-d
+
+# Migrates database
+export GOOGLE_APPLICATION_CREDENTIALS=$HOME/gcloud-service-key.json
+
+sudo /opt/google-cloud-sdk/bin/kubectl set image deploy/staging-migrate enmarche=eu.gcr.io/$GCLOUD_PROJECT/app:$CIRCLE_SHA1
+
+# Deploy to staging
+declare -a images=("staging-app" "staging-worker-mailer-campaign" "staging-worker-mailer-transactional" "staging-worker-referent")
+
+for image in "${images[@]}"
+do
+   sudo /opt/google-cloud-sdk/bin/kubectl set image deployment/$image enmarche=eu.gcr.io/$GCLOUD_PROJECT/app:$CIRCLE_SHA1
+done
+
+# Send result to slack
+migration_log=$(sudo /opt/google-cloud-sdk/bin/kubectl logs deploy/staging-migrate --container=enmarche || true)
+json="{\"text\": \"\`\`\`$(echo $migration_log | sed 's/"//g' | sed "s/'//g" | sed 's/\\/\//g' )\`\`\`\"}"
+curl -s "Content-Type: application/json" -d "payload=$json" https://hooks.slack.com/services/$SLACK_TOKEN
