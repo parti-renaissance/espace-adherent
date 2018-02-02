de1be5741b09a0cb666a3f06c033e8eabdc9336e
Rémi Gardien <remi@gardien.biz>
2018-02-02 12:05:59 +0100

    feat: add autodeploy on develop

--- scripts/deployment_develop.sh
+++ scripts/deployment_develop.sh
@@ -8,10 +8,10 @@ sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials $GCLOUD
 # Migrates database
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/gcloud-service-key.json
 
-sudo /opt/google-cloud-sdk/bin/kubectl set image deployment/staging-migrate enmarche=eu.gcr.io/$GCLOUD_PROJECT/app:$CIRCLE_SHA1
+sudo /opt/google-cloud-sdk/bin/kubectl set image deploy/develop-migrate enmarche=eu.gcr.io/$GCLOUD_PROJECT/app:$CIRCLE_SHA1
 
-# Deploy to staging
-declare -a images=("staging-app" "staging-worker-mailer-campaign" "staging-worker-mailer-transactional" "staging-worker-referent")
+# Deploy to develop
+declare -a images=("develop-app" "develop-worker-mailer-campaign" "develop-worker-mailer-transactional" "develop-worker-referent")
 
 for image in "${images[@]}"
 do
@@ -19,6 +19,6 @@ do
 done
 
 # Send result to slack
-migration_log=$(sudo /opt/google-cloud-sdk/bin/kubectl logs staging-migrate --container=enmarche || true)
+migration_log=$(sudo /opt/google-cloud-sdk/bin/kubectl logs develop-migrate --container=enmarche || true)
 json="{\"text\": \"\`\`\`$(echo $migration_log | sed 's/"//g' | sed "s/'//g" | sed 's/\\/\//g' )\`\`\`\"}"
 curl -s "Content-Type: application/json" -d "payload=$json" https://hooks.slack.com/services/$SLACK_TOKEN
