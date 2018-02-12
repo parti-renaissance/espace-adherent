a20e5123007bb18d5138c26fecf7ed03a9de7329
Dimitri Gritsajuk <dimitri.gritsajuk@sensiolabs.com>
2018-02-12 14:21:07 +0100

    [Image Generator] Add tract pdf generator

--- src/ImageGenerator/Command/ImageCommandInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace AppBundle\ImageGenerator\Command;
-
-interface ImageCommandInterface
-{
-    public function getImagePath(): string;
-}
---
+++ src/MediaGenerator/Image/CitizenProjectCoverGenerator.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\MediaGenerator\Image;
+
+use AppBundle\MediaGenerator\BaseSnappyGenerator;
+use AppBundle\MediaGenerator\Command\CitizenProjectImageCommand;
+use AppBundle\MediaGenerator\Command\MediaCommandInterface;
+use AppBundle\MediaGenerator\MediaContent;
+use Knp\Snappy\GeneratorInterface;
+use Symfony\Component\Templating\EngineInterface;
+
+class CitizenProjectCoverGenerator extends BaseSnappyGenerator
+{
+    private $badgeImagePath;
+
+    public function __construct(
+        GeneratorInterface $imageGenerator,
+        EngineInterface $templateEngine,
+        string $badgeImagePath
+    ) {
+        parent::__construct($imageGenerator, $templateEngine);
+
+        $this->badgeImagePath = $badgeImagePath;
+    }
+
+    public function generate(MediaCommandInterface $command): MediaContent
+    {
+        /** @var CitizenProjectImageCommand $command */
+        $html = $this->templateEngine->render(
+            'citizen_project/cover_image.html.twig',
+            [
+                'command' => $command,
+                'badge_path' => $this->badgeImagePath,
+            ]
+        );
+
+        $imageContent = $this->mediaGenerator->getOutputFromHtml(
+            $html,
+            [
+                'format' => 'png',
+                'quality' => 100,
+            ]
+        );
+
+        return new MediaContent($imageContent, 'image/png', \strlen($imageContent));
+    }
+}
