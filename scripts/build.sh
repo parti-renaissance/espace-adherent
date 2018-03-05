29de947f7a6449b8d60038e3201f6ec68af202d3
Benoit Galati <benoit.galati@gmail.com>
2018-03-05 16:47:19 +0100

    Fix - docker image name should be prefixed with the branch name not the targetted env

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -xe
 
-TARGETED_ENV="${1:-default}"
-if [[ "${TARGETED_ENV}" == "master" ]]; then
-    TARGETED_ENV="preprod"
-fi
-
+BRANCH="$1"
 DOCKER_IMAGE="eu.gcr.io/$GCLOUD_PROJECT/app"
-DOCKER_IMAGE_TAG="$TARGETED_ENV-$CIRCLE_SHA1"
+DOCKER_IMAGE_TAG="$BRANCH-$CIRCLE_SHA1"
 
 # Build the image
-gcloud docker -- pull $DOCKER_IMAGE:$TARGETED_ENV
-docker build --cache-from=$DOCKER_IMAGE:$TARGETED_ENV -t $DOCKER_IMAGE:$DOCKER_IMAGE_TAG .
+gcloud docker -- pull $DOCKER_IMAGE:$BRANCH
+docker build --cache-from=$DOCKER_IMAGE:$BRANCH -t $DOCKER_IMAGE:$DOCKER_IMAGE_TAG .
 
 # Push the images to Google Cloud
 gcloud docker -- push $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
 
 # Set image tag
-gcloud container images add-tag $DOCKER_IMAGE:$DOCKER_IMAGE_TAG $DOCKER_IMAGE:$TARGETED_ENV --quiet
+gcloud container images add-tag $DOCKER_IMAGE:$DOCKER_IMAGE_TAG $DOCKER_IMAGE:$BRANCH --quiet
