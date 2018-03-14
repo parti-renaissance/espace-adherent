265081b7cc54a363c5ef3fa9f2e60112e098ee61
Charles d'Oiron <doironcharles@gmail.com>
2018-03-14 19:43:05 +0100

    fix iphone 5 & js export

--- front/pages/grande_marche_europe.js
+++ front/pages/grande_marche_europe.js
@@ -1,17 +1,19 @@
-const splashScreen = dom('.splash-screen');
-const logo = dom('.logo');
-const form = dom('.gme__title form');
+export default () => {
+    const splashScreen = dom('.splash-screen');
+    const logo = dom('.logo');
+    const form = dom('.gme__title form');
 
-// Opening Animation
-setInterval(() => {
-    logo.style.opacity = 1;
-}, 100);
-setInterval(() => {
-    splashScreen.style.transform = 'translateY(-150%)';
-}, 3000);
-setInterval(() => {
-    form.style.display = 'flex';
-}, 3100);
-setInterval(() => {
-    form.style.transform = 'translateY(0)';
-}, 3200);
+    // Opening Animation
+    setInterval(() => {
+        logo.style.opacity = 1;
+    }, 100);
+    setInterval(() => {
+        splashScreen.style.transform = 'translateY(-150%)';
+    }, 3000);
+    setInterval(() => {
+        form.style.display = 'flex';
+    }, 3100);
+    setInterval(() => {
+        form.style.transform = 'translateY(0)';
+    }, 3200);
+};
