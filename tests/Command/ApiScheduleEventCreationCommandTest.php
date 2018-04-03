2d2e6184d25cc674e934a26b65126efd31e25bde
Rémi Gardien <remi.gardien@sensiolabs.com>
2018-04-03 10:58:40 +0200

    Test Api synchronization commands

---
+++ tests/Command/ApiScheduleEventCreationCommandTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\AppBundle\Command;
+
+use AppBundle\DataFixtures\ORM\LoadAdherentData;
+use AppBundle\DataFixtures\ORM\LoadEventData;
+use Tests\AppBundle\SqliteWebTestCase;
+
+/**
+ * @group functional
+ */
+class ApiScheduleEventCreationCommandTest extends SqliteWebTestCase
+{
+    public function testCommand(): void
+    {
+        $output = $this->runCommand('app:sync:events');
+
+        $this->assertContains('Starting synchronization.', $output);
+        $this->assertContains('15/15', $output);
+        $this->assertContains('Successfully scheduled for synchronization!', $output);
+    }
+
+    public function setUp()
+    {
+        $this->loadFixtures([
+            LoadAdherentData::class,
+            LoadEventData::class,
+        ]);
+
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        $this->loadFixtures([]);
+
+        parent::tearDown();
+    }
+}
