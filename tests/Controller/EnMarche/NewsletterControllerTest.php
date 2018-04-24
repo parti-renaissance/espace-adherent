calinzaharia/en-marche.fr

[ch3333] Add country in newsletter inscription and implement a form theme cause of multiple use
https://app.clubhouse.io/enmarche/story/3333/ajout-d-un-cta-inscription-newsletter-sur-les-pages-d-articles

tests/Controller/EnMarche/NewsletterControllerTest.php
@@ -43,6 +43,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'titouan.galopin@en-marche.fr',
             'app_newsletter_subscription[postalCode]' => '10000',
+            'app_newsletter_subscription[country]' => 'France',
         ]));
 
         // Subscription should have been saved
@@ -53,6 +54,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
 
         $this->assertSame('titouan.galopin@en-marche.fr', $subscription->getEmail());
         $this->assertSame('10000', $subscription->getPostalCode());
+        $this->assertSame('France', $subscription->getCountry());
         $this->assertResponseStatusCode(Response::HTTP_FOUND, $this->client->getResponse());
 
         // Email should have been sent
@@ -66,6 +68,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'titouan.galopin@en-marche.fr',
             'app_newsletter_subscription[postalCode]' => '20000',
+            'app_newsletter_subscription[country]' => 'France',
         ]));
 
         $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
@@ -86,6 +89,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'titouan.galopin@en-marche.fr',
             'app_newsletter_subscription[postalCode]' => '10000',
+            'app_newsletter_subscription[country]' => 'France',
         ]));
 
         // Subscription should have been saved
@@ -179,6 +183,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'abc@en-marche-dev.fr',
             'app_newsletter_subscription[postalCode]' => '59000',
+            'app_newsletter_subscription[country]' => 'France',
         ]));
 
         $this->assertResponseStatusCode(Response::HTTP_FOUND, $this->client->getResponse());

parti-renaissance/espace-adherent

tests/Controller/EnMarche/NewsletterControllerTest.php
@@ -43,6 +43,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'titouan.galopin@en-marche.fr',
             'app_newsletter_subscription[postalCode]' => '10000',
+            'app_newsletter_subscription[country]' => 'FR',
         ]));
 
         // Subscription should have been saved
@@ -53,6 +54,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
 
         $this->assertSame('titouan.galopin@en-marche.fr', $subscription->getEmail());
         $this->assertSame('10000', $subscription->getPostalCode());
+        $this->assertSame('FR', $subscription->getCountry());
         $this->assertResponseStatusCode(Response::HTTP_FOUND, $this->client->getResponse());
 
         // Email should have been sent
@@ -66,6 +68,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'titouan.galopin@en-marche.fr',
             'app_newsletter_subscription[postalCode]' => '20000',
+            'app_newsletter_subscription[country]' => 'FR',
         ]));
 
         $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
@@ -86,6 +89,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'titouan.galopin@en-marche.fr',
             'app_newsletter_subscription[postalCode]' => '10000',
+            'app_newsletter_subscription[country]' => 'FR',
         ]));
 
         // Subscription should have been saved
@@ -179,6 +183,7 @@ class NewsletterControllerTest extends SqliteWebTestCase
         $this->client->submit($crawler->filter('form[name=app_newsletter_subscription]')->form([
             'app_newsletter_subscription[email]' => 'abc@en-marche-dev.fr',
             'app_newsletter_subscription[postalCode]' => '59000',
+            'app_newsletter_subscription[country]' => 'FR',
         ]));
 
         $this->assertResponseStatusCode(Response::HTTP_FOUND, $this->client->getResponse());