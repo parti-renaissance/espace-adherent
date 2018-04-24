2b31b5ec53e0baed1de0e213c4c30675b8ac34b3
Charles d'Oiron <doironcharles@gmail.com>
2018-04-24 15:31:36 +0200

    test nav

--- react/src/js/components/Nav.js
+++ react/src/js/components/Nav.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { sse1 } from './../utils/';
 
 const Nav = props => (
-    <ul className="referent__nav">
-        <Link to="dashboard-referent">
-            <li className="active">Tableau de bord</li>
-        </Link>
-        <Link to="/event-page">
-            <li>Evénement</li>
-        </Link>
-        <Link to="/committee-page">
-            <li>Comités</li>
-        </Link>
-        <Link to="/send-a-message">
-            <li>Envoyer un message</li>
-        </Link>
-    </ul>
+    <div id="sse1">
+        <div id="sses1">
+            <ul className="referent__nav">
+                <Link to="dashboard-referent">
+                    <li>Tableau de bord</li>
+                </Link>
+                <Link to="/event-page">
+                    <li>Evénement</li>
+                </Link>
+                <Link to="/committee-page">
+                    <li>Comités</li>
+                </Link>
+                <Link to="/send-a-message">
+                    <li>Envoyer un message</li>
+                </Link>
+            </ul>
+        </div>
+    </div>
 );
 
 export default Nav;
