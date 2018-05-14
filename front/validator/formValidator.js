5ca2614d79de6aff4b4dcbf9d9086a0c6719b57c
Alexandre Vinet <alexandre.vinet@sensiolabs.com>
2018-05-14 19:45:21 +0200

    [ch3092] Instant validation: fix some js validation
    https://app.clubhouse.io/enmarche/story/3092/int%C3%A9grer-l-instant-field-validation-sur-les-formulaires-de-don-et-d-adh%C3%A9sion

--- front/validator/formValidator.js
+++ front/validator/formValidator.js
@@ -1,15 +1,20 @@
 import reqwest from 'reqwest';
 
+let idErrors = [];
+
 function displayErrors(node, id) {
     if (Object.prototype.hasOwnProperty.call(node, 'children')) {
         Object.keys(node.children).forEach((key) => {
             displayErrors(node.children[key], `${id}_${key}`);
         });
     }
+
     let htmlErrors = dom(`#${id}_errors`);
+
     if (Object.prototype.hasOwnProperty.call(node, 'errors')) {
         if (!htmlErrors) {
             const htmlInput = dom(`#${id}`);
+
             if (!htmlInput) {
                 return;
             }
@@ -19,6 +24,8 @@ function displayErrors(node, id) {
             );
             htmlErrors = dom(`#${id}_errors`);
         }
+
+        idErrors.push(`${id}_errors`);
         htmlErrors.innerHTML = '';
         node.errors.forEach((value) => {
             htmlErrors.insertAdjacentHTML('beforeend', `<li class="form__error">${value}</li>`);
@@ -32,14 +39,26 @@ export default (formType, form) => {
     formType = encodeURI(formType);
     form.addEventListener('change', () => {
         const url = `/api/form/validate/${formType}`;
-
         const formData = new FormData(form);
         const emptyFieldNames = [];
+
         for (const field of formData.entries()) {
             if (!field[1]) {
                 emptyFieldNames.push(field[0]);
+            } else {
+                const targetField = form.querySelector(`[name="${field[0]}"`);
+                const id = targetField.dataset.validatedWith;
+
+                if (id) {
+                    const linkedField = dom(`#${id}`);
+
+                    if (!linkedField.value) {
+                        emptyFieldNames.push(field[0]);
+                    }
+                }
             }
         }
+
         for (const fieldName of emptyFieldNames) {
             formData.delete(fieldName);
         }
@@ -50,6 +69,11 @@ export default (formType, form) => {
             data: formData,
             processData: false,
             success(resp) {
+                for (const idError of idErrors) {
+                    const htmlErrors = dom(`#${idError}`);
+                    htmlErrors.parentNode.removeChild(htmlErrors);
+                }
+                idErrors = [];
                 displayErrors(resp, form.name);
             },
         });
