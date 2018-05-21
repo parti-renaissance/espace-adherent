092a03ba232b1005dde128050d92d6e65525f1c1
Dimitri Gritsajuk <dimitri.gritsajuk@sensiolabs.com>
2018-05-21 17:18:46 +0200

    Add filter by adherent role

---
+++ src/Adherent/AdherentRoleEnum.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Adherent;
+
+use MyCLabs\Enum\Enum;
+
+class AdherentRoleEnum extends Enum
+{
+    public const REFERENT = 'referent';
+
+    public const COMMITTEE_SUPERVISOR = 'committee_supervisor';
+    public const COMMITTEE_HOST = 'committee_host';
+
+    public const CITIZEN_PROJECT_HOLDER = 'citizen_project_holder';
+
+    public const BOARD_MEMBER = 'board_member';
+
+    public const COORDINATOR = 'coordinator';
+
+    public const PROCURATION_MANAGER = 'procuration_manager';
+
+    public const USER = 'user';
+}
