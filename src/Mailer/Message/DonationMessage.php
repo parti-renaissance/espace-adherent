Orgoff/en-marche.fr

[ch3967] Refactor donations to register transactions.
https://app.clubhouse.io/enmarche/story/3967/etq-enmarche-je-souhaite-avoir-l-historique-de-toutes-les-transactions-paybox

src/Mailer/Message/DonationMessage.php
@@ -2,12 +2,14 @@
 
 namespace AppBundle\Mailer\Message;
 
-use AppBundle\Entity\Donation;
+use AppBundle\Entity\Transaction;
 
 final class DonationMessage extends Message
 {
-    public static function createFromDonation(Donation $donation): self
+    public static function createFromDonation(Transaction $transaction): self
     {
+        $donation = $transaction->getDonation();
+
         return new self(
             $donation->getUuid(),
             '54677',
@@ -16,7 +18,7 @@ final class DonationMessage extends Message
             'Merci pour votre engagement',
             [
                 'target_firstname' => self::escape($donation->getFirstName()),
-                'year' => (int) $donation->getDonatedAt()->format('Y') + 1,
+                'year' => (int) $transaction->getPayboxDateTime()->format('Y') + 1,
             ]
         );
     }

src/Repository/DonationRepository.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Donation\DonationStatusEnum;
 use AppBundle\Donation\PayboxPaymentSubscription;
 use AppBundle\Entity\Donation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -26,14 +27,6 @@ class DonationRepository extends ServiceEntityRepository
         return $this->findOneByValidUuid($uuid);
     }
 
-    public function findByEmailAddressOrderedByDonatedAt(string $email, string $order = 'ASC'): array
-    {
-        return $this->findBy(
-            ['emailAddress' => $email],
-            ['donatedAt' => $order]
-        );
-    }
-
     /**
      * @return Donation[]
      */
@@ -42,12 +35,14 @@ class DonationRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('donation')
             ->andWhere('donation.emailAddress = :email')
             ->andWhere('donation.duration != :duration')
+            ->andWhere('donation.status = :status')
             ->andWhere('donation.subscriptionEndedAt IS NULL')
             ->setParameters([
+                'status' => DonationStatusEnum::SUBSCRIPTION_IN_PROGRESS,
                 'email' => $email,
                 'duration' => PayboxPaymentSubscription::NONE,
             ])
-            ->orderBy('donation.donatedAt', 'ASC')
+            ->orderBy('donation.createdAt', 'ASC')
             ->getQuery()
             ->getResult()
         ;

parti-renaissance/espace-adherent

src/Mailer/Message/DonationMessage.php
@@ -2,12 +2,14 @@
 
 namespace AppBundle\Mailer\Message;
 
-use AppBundle\Entity\Donation;
+use AppBundle\Entity\Transaction;
 
 final class DonationMessage extends Message
 {
-    public static function createFromDonation(Donation $donation): self
+    public static function createFromTransaction(Transaction $transaction): self
     {
+        $donation = $transaction->getDonation();
+
         return new self(
             $donation->getUuid(),
             '54677',
@@ -16,7 +18,7 @@ final class DonationMessage extends Message
             'Merci pour votre engagement',
             [
                 'target_firstname' => self::escape($donation->getFirstName()),
-                'year' => (int) $donation->getDonatedAt()->format('Y') + 1,
+                'year' => (int) $transaction->getPayboxDateTime()->format('Y') + 1,
             ]
         );
     }

src/Repository/DonationRepository.php
@@ -29,33 +29,19 @@ class DonationRepository extends ServiceEntityRepository
     /**
      * @return Donation[]
      */
-    public function findByEmailAddressOrderedByDonatedAt(string $email): array
-    {
-        return $this->createQueryBuilder('donation')
-            ->where('donation.emailAddress = :email')
-            ->andWhere('donation.donatedAt IS NOT NULL')
-            ->setParameter('email', $email)
-            ->orderBy('donation.donatedAt', 'DESC')
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-
-    /**
-     * @return Donation[]
-     */
     public function findAllSubscribedDonationByEmail(string $email): array
     {
         return $this->createQueryBuilder('donation')
             ->andWhere('donation.emailAddress = :email')
             ->andWhere('donation.duration != :duration')
+            ->andWhere('donation.status = :status')
             ->andWhere('donation.subscriptionEndedAt IS NULL')
-            ->andWhere('donation.donatedAt IS NOT NULL')
             ->setParameters([
+                'status' => Donation::STATUS_SUBSCRIPTION_IN_PROGRESS,
                 'email' => $email,
                 'duration' => PayboxPaymentSubscription::NONE,
             ])
-            ->orderBy('donation.donatedAt', 'DESC')
+            ->orderBy('donation.createdAt', 'DESC')
             ->getQuery()
             ->getResult()
         ;