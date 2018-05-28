claaslange/en-marche.fr

Store history of join/leave of committees
https://app.clubhouse.io/enmarche/story/3659/stocker-l-historique-du-nombre-de-membres-de-comités

tests/TestHelperTrait.php
@@ -26,6 +26,7 @@ use AppBundle\Entity\ProcurationProxy;
 use AppBundle\Entity\ProcurationRequest;
 use AppBundle\Entity\PurchasingPowerChoice;
 use AppBundle\Entity\PurchasingPowerInvitation;
+use AppBundle\Entity\Reporting\CommitteeMembershipEvent;
 use AppBundle\Entity\Summary;
 use AppBundle\Entity\TonMacronChoice;
 use AppBundle\Entity\TonMacronFriendInvitation;
@@ -56,6 +57,7 @@ use AppBundle\Repository\TonMacronChoiceRepository;
 use AppBundle\Repository\TonMacronFriendInvitationRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityRepository;
 use League\Flysystem\Filesystem;
 use League\Glide\Server;
 use libphonenumber\PhoneNumber;
@@ -169,6 +171,11 @@ trait TestHelperTrait
     public function getCommitteeMembershipRepository(): CommitteeMembershipRepository
     {
         return $this->getRepository(CommitteeMembership::class);
+    }
+
+    public function getCommitteeMembershipEventRepository(): EntityRepository
+    {
+        return $this->getRepository(CommitteeMembershipEvent::class);
     }
 
     public function getEventRegistrationRepository(): EventRegistrationRepository

parti-renaissance/espace-adherent

tests/TestHelperTrait.php
@@ -27,6 +27,7 @@ use AppBundle\Entity\ProcurationProxy;
 use AppBundle\Entity\ProcurationRequest;
 use AppBundle\Entity\PurchasingPowerChoice;
 use AppBundle\Entity\PurchasingPowerInvitation;
+use AppBundle\Entity\Reporting\CommitteeMembershipHistory;
 use AppBundle\Entity\Summary;
 use AppBundle\Entity\TonMacronChoice;
 use AppBundle\Entity\TonMacronFriendInvitation;
@@ -60,6 +61,7 @@ use AppBundle\Repository\TonMacronFriendInvitationRepository;
 use AppBundle\Repository\TransactionRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityRepository;
 use League\Flysystem\Filesystem;
 use League\Glide\Server;
 use libphonenumber\PhoneNumber;
@@ -173,6 +175,11 @@ trait TestHelperTrait
     public function getCommitteeMembershipRepository(): CommitteeMembershipRepository
     {
         return $this->getRepository(CommitteeMembership::class);
+    }
+
+    public function getCommitteeMembershipHistoryRepository(): EntityRepository
+    {
+        return $this->getRepository(CommitteeMembershipHistory::class);
     }
 
     public function getEventRegistrationRepository(): EventRegistrationRepository