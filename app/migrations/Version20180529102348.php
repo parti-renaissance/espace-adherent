nadsrosun/en-marche.fr

[ch4192] Add payment method in historic donation
https://app.clubhouse.io/enmarche/story/4192/ajouter-le-moyen-de-paiement-dans-l-historique-des-dons-sur-l-onglet-mes-dons

app/migrations/Version20180529102348.php
@@ -1,0 +1,20 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20180529102348 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE transaction ADD type VARCHAR(255) NOT NULL DEFAULT \'paybox\'');
+        $this->addSql('ALTER TABLE transaction CHANGE type type VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE transaction DROP type');
+    }
+}

parti-renaissance/espace-adherent

app/migrations/Version20180529102348.php
@@ -1,0 +1,20 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20180529102348 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE donation_transactions ADD type VARCHAR(255) NOT NULL DEFAULT \'cb\'');
+        $this->addSql('ALTER TABLE donation_transactions CHANGE type type VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE donation_transactions DROP type');
+    }
+}