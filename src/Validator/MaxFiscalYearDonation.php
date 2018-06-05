nadsrosun/en-marche.fr

[ch2795] Create a donation limit per year
https://app.clubhouse.io/enmarche/story/2795/introduire-une-limite-annuelle-de-7-500-euros-de-don-par-compte-utilisateur

src/Validator/MaxFiscalYearDonation.php
@@ -1,0 +1,15 @@
+<?php
+
+namespace AppBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+class MaxFiscalYearDonation extends Constraint
+{
+    public $message = 'donation.max_fiscal_year_donation';
+    public $maxDonation = 750000; // Amount in cents
+}

tests/Repository/TransactionRepositoryTest.php
@@ -1,0 +1,45 @@
+<?php
+
+namespace Tests\AppBundle\Repository;
+
+use AppBundle\DataFixtures\ORM\LoadDonationData;
+use AppBundle\Repository\TransactionRepository;
+use Tests\AppBundle\MysqlWebTestCase;
+use Tests\AppBundle\TestHelperTrait;
+
+class TransactionRepositoryTest extends MysqlWebTestCase
+{
+    use TestHelperTrait;
+
+    /**
+     * @var TransactionRepository
+     */
+    private $transactionRepository;
+
+    public function testGetTotalAmountCurrentYearByEmail(): void
+    {
+        static::assertSame(25000, $this->transactionRepository->getTotalAmountCurrentYearByEmail('jacques.picard@en-marche.fr'));
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->loadFixtures([
+            LoadDonationData::class,
+        ]);
+
+        $this->container = $this->getContainer();
+        $this->transactionRepository = $this->getTransactionRepository();
+    }
+
+    protected function tearDown()
+    {
+        $this->loadFixtures([]);
+
+        $this->transactionRepository = null;
+        $this->container = null;
+
+        parent::tearDown();
+    }
+}

parti-renaissance/espace-adherent

src/Validator/MaxFiscalYearDonation.php
@@ -1,0 +1,15 @@
+<?php
+
+namespace AppBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+class MaxFiscalYearDonation extends Constraint
+{
+    public $message = 'donation.max_fiscal_year_donation';
+    public $maxDonationInCents = 750000; // Amount in cents
+}

tests/Repository/TransactionRepositoryTest.php
@@ -1,0 +1,48 @@
+<?php
+
+namespace Tests\AppBundle\Repository;
+
+use AppBundle\DataFixtures\ORM\LoadDonationData;
+use AppBundle\Repository\TransactionRepository;
+use Cake\Chronos\Chronos;
+use Tests\AppBundle\MysqlWebTestCase;
+use Tests\AppBundle\TestHelperTrait;
+
+class TransactionRepositoryTest extends MysqlWebTestCase
+{
+    use TestHelperTrait;
+
+    /**
+     * @var TransactionRepository
+     */
+    private $transactionRepository;
+
+    public function testGetTotalAmountCurrentYearByEmail(): void
+    {
+        Chronos::setTestNow(Chronos::createFromFormat('Y/m/d H:i:s', '2018/06/15 15:16:17'));
+
+        static::assertSame(25000, $this->transactionRepository->getTotalAmountInCentsByEmail('jacques.picard@en-marche.fr'));
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->loadFixtures([
+            LoadDonationData::class,
+        ]);
+
+        $this->container = $this->getContainer();
+        $this->transactionRepository = $this->getTransactionRepository();
+    }
+
+    protected function tearDown()
+    {
+        $this->loadFixtures([]);
+
+        $this->transactionRepository = null;
+        $this->container = null;
+
+        parent::tearDown();
+    }
+}