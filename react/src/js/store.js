f8c87a1fadee07f52bb66a495bf3f23fe0b10f2e
atamano <atamano@student.42.fr>
2018-06-08 14:25:40 +0200

    Persist state to local storage

--- react/src/js/store.js
+++ react/src/js/store.js
@@ -1,11 +1,23 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import reducers from './reducers/index.js';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createHistory from 'history/createBrowserHistory';
 import { routerMiddleware } from 'react-router-redux';
 import promise from 'redux-promise-middleware';
+import localForage from 'localforage';
+
+const persistConfig = {
+    key: 'root',
+    storage: localForage,
+};
 
 export const history = createHistory();
 const middleware = [promise(), thunk, routerMiddleware(history)];
-export const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
+export const store = createStore(
+    persistReducer(persistConfig, reducers),
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export const persistor = persistStore(store);
