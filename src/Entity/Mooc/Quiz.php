667d5f6a19664c6e7e35ed3c8d0b24a3905900c6
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2018-06-19 11:03:33 +0200

    [MOOC] Updated share elements fields

--- src/Entity/Mooc/Quiz.php
+++ src/Entity/Mooc/Quiz.php
@@ -26,13 +26,13 @@ class Quiz extends BaseMoocElement
     public function __construct(
         string $title = null,
         string $content = null,
-        string $twitterText = null,
-        string $facebokText = null,
-        string $emailObject = null,
-        string $emailBody = null,
+        string $shareTwitterText = null,
+        string $shareFacebokText = null,
+        string $shareEmailObject = null,
+        string $shareEmailBody = null,
         string $typeformUrl = null
     ) {
-        parent::__construct($title, $content, $twitterText, $facebokText, $emailObject, $emailBody);
+        parent::__construct($title, $content, $shareTwitterText, $shareFacebokText, $shareEmailObject, $shareEmailBody);
         $this->typeformUrl = $typeformUrl;
     }
 
