3c95dfcb3905a4bbc2a501932ecb813b5f30d9d2
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2018-06-22 14:17:56 +0200

    Fixed lint JS and rebase

--- front/pages/article.js
+++ front/pages/article.js
@@ -1,5 +1,4 @@
 export default () => {
-
     const inputNewsLetter = find(document, '.newsletter input');
     let isExpanded;
     let isFixed;
@@ -10,39 +9,32 @@ export default () => {
     const botArticle = article.getBoundingClientRect().bottom;
 
     function expandInput(e) {
-        if (e.target == inputNewsLetter && !isExpanded){
+        if (e.target === inputNewsLetter && !isExpanded) {
             addClass(inputNewsLetter, 'isExpanded');
-            isExpanded = true;}
-        else if (e.target != inputNewsLetter && isExpanded){
+            isExpanded = true;
+        } else if (e.target !== inputNewsLetter && isExpanded) {
             removeClass(inputNewsLetter, 'isExpanded');
             isExpanded = false;
         }
-    };
-    on(document,'click', expandInput)
+    }
+    on(document, 'click', expandInput);
 
     function socialToFixed() {
-        if ((window.scrollY >= topSocialBlock) && (window.scrollY < (botArticle - 600)) && !isFixedToBottom){
-            addClass(socialBlock, 'isFixed')
+        if ((window.scrollY >= topSocialBlock) && (window.scrollY < (botArticle - 600)) && !isFixedToBottom) {
+            addClass(socialBlock, 'isFixed');
             isFixed = true;
-            console.log('POSITION FIX')
-        }
-        else if (isFixed  &&  (window.scrollY < topSocialBlock)) {
-            removeClass(socialBlock, 'isFixed')
-            console.log('POSITION  RELATIVE')
-        }
-        else if (isFixed  &&  (window.scrollY > (botArticle - 600))) {
-            removeClass(socialBlock, 'isFixed')
-            addClass(socialBlock, 'isFixedToBottom')
+        } else if (isFixed && (window.scrollY < topSocialBlock)) {
+            removeClass(socialBlock, 'isFixed');
+        } else if (isFixed && (window.scrollY > (botArticle - 600))) {
+            removeClass(socialBlock, 'isFixed');
+            addClass(socialBlock, 'isFixedToBottom');
             isFixedToBottom = true;
-            console.log('POSITION FIX BOTTOM')
-        }
-        else if (isFixedToBottom && (window.scrollY < (botArticle - 600))){
-            addClass(socialBlock, 'isFixed')
-            removeClass(socialBlock, 'isFixedToBottom')
+        } else if (isFixedToBottom && (window.scrollY < (botArticle - 600))) {
+            addClass(socialBlock, 'isFixed');
+            removeClass(socialBlock, 'isFixedToBottom');
             isFixedToBottom = false;
             isFixed = true;
-            console.log('REMOVE POSITION FIX BOTTOM & ADD POSITION FIX')
         }
     }
-    on(document, 'scroll', socialToFixed)
+    on(document, 'scroll', socialToFixed);
 };
