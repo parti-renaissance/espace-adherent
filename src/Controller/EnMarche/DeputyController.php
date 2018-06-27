KOUISAmine/en-marche.fr

Add mail sending to adherents of deputy district
https://app.clubhouse.io/enmarche/story/4437/etq-d%C3%A9put%C3%A9-dans-la-section-envoyer-un-message-pas-de-filtre-affichage-du-nb-d-adh%C3%A9rents-recevant-le-message-dans-destinataire

src/Controller/EnMarche/DeputyController.php
@@ -1,0 +1,70 @@
+<?php
+
+namespace AppBundle\Controller\EnMarche;
+
+use AppBundle\Form\DeputyMessageType;
+use AppBundle\Deputy\DeputyMessage;
+use AppBundle\Deputy\DeputyMessageNotifier;
+use AppBundle\Repository\AdherentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/espace-depute")
+ * @Security("is_granted('ROLE_DEPUTY')")
+ */
+class DeputyController extends Controller
+{
+    /**
+     * @Route("/utilisateurs/message", name="app_deputy_users_message")
+     * @Method("GET|POST")
+     */
+    public function usersSendMessageAction(Request $request, AdherentRepository $adherentRepository): Response
+    {
+        $recipients = $adherentRepository->findAllInDistrict($this->getUser()->getManagedDistrict());
+        $message = $this->createMessage($recipients);
+
+        $form = $this->createForm(DeputyMessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->get(DeputyMessageNotifier::class)->sendMessage($message);
+            $this->addFlash('info', $this->get('translator')->trans('deputy.message.success'));
+
+            return $this->redirect($this->generateUrl('app_deputy_users_message'));
+        }
+
+        return $this->render('deputy/users_message.html.twig', [
+            'results_count' => count($recipients),
+            'message' => $message,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/evenements", name="app_deputy_events")
+     * @Method("GET")
+     */
+    public function eventsAction(): Response
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/comites", name="app_deputy_committees")
+     * @Method("GET")
+     */
+    public function committeesAction(): Response
+    {
+        return new Response();
+    }
+
+    private function createMessage(array $recipients): DeputyMessage
+    {
+        return new DeputyMessage($this->getUser(), $recipients);
+    }
+}

src/Deputy/DeputyMessageNotifier.php
@@ -1,0 +1,33 @@
+<?php
+
+namespace AppBundle\Deputy;
+
+use AppBundle\Mailer\MailerService;
+use AppBundle\Mailer\Message\DeputyMessage as Message;
+
+class DeputyMessageNotifier
+{
+    private $mailer;
+
+    public function __construct(MailerService $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendMessage(DeputyMessage $message): void
+    {
+        $chunks = array_chunk(
+            $message->getRecipients(),
+            MailerService::PAYLOAD_MAXSIZE
+        );
+
+        foreach ($chunks as $chunk) {
+            $this->mailer->sendMessage($this->createMessage($message, $chunk));
+        }
+    }
+
+    private function createMessage(DeputyMessage $message, array $recipients): Message
+    {
+        return Message::createFromModel($message, $recipients);
+    }
+}

src/Mailer/Message/DeputyMessage.php
@@ -1,0 +1,57 @@
+<?php
+
+namespace AppBundle\Mailer\Message;
+
+use AppBundle\Deputy\DeputyMessage as DeputyMessageModel;
+use AppBundle\Entity\Adherent;
+use Ramsey\Uuid\Uuid;
+
+final class DeputyMessage extends Message
+{
+    /**
+     * @param DeputyMessageModel $model
+     * @param Adherent[]         $recipients
+     *
+     * @return DeputyMessage
+     */
+    public static function createFromModel(DeputyMessageModel $model, array $recipients): self
+    {
+        if (!$recipients) {
+            throw new \InvalidArgumentException('At least one recipient is required.');
+        }
+
+        $deputy = $model->getFrom();
+        $first = array_shift($recipients);
+
+        $message = new self(
+            Uuid::uuid4(),
+            '455851',
+            $first->getEmailAddress(),
+            $first->getFullName() ?: '',
+            $model->getSubject(),
+            [
+                'deputy_fullname' => self::escape($deputy->getFullName()),
+                'circonscription_name' => self::escape($deputy->getManagedDistrict()),
+                'target_message' => $model->getContent(),
+            ],
+            [
+                'target_firstname' => self::escape($first->getFirstName() ?: ''),
+            ],
+            $deputy->getEmailAddress()
+        );
+
+        $message->setSenderName(sprintf('Votre député%s En Marche !', $deputy->isFemale() ? 'e' : ''));
+
+        foreach ($recipients as $recipient) {
+            $message->addRecipient(
+                $recipient->getEmailAddress(),
+                $recipient->getFullName() ?: '',
+                [
+                    'target_firstname' => self::escape($recipient->getFirstName() ?: ''),
+                ]
+            );
+        }
+
+        return $message;
+    }
+}

parti-renaissance/espace-adherent

src/Controller/EnMarche/DeputyController.php
@@ -1,0 +1,70 @@
+<?php
+
+namespace AppBundle\Controller\EnMarche;
+
+use AppBundle\Form\DeputyMessageType;
+use AppBundle\Deputy\DeputyMessage;
+use AppBundle\Deputy\DeputyMessageNotifier;
+use AppBundle\Repository\AdherentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/espace-depute")
+ * @Security("is_granted('ROLE_DEPUTY')")
+ */
+class DeputyController extends Controller
+{
+    /**
+     * @Route("/utilisateurs/message", name="app_deputy_users_message")
+     * @Method("GET|POST")
+     */
+    public function usersSendMessageAction(Request $request, AdherentRepository $adherentRepository): Response
+    {
+        $recipients = $adherentRepository->findAllInDistrict($this->getUser()->getManagedDistrict());
+        $message = $this->createMessage($recipients);
+
+        $form = $this->createForm(DeputyMessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->get(DeputyMessageNotifier::class)->sendMessage($message);
+            $this->addFlash('info', 'deputy.message.success');
+
+            return $this->redirectToRoute('app_deputy_users_message');
+        }
+
+        return $this->render('deputy/users_message.html.twig', [
+            'results_count' => count($recipients),
+            'message' => $message,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/evenements", name="app_deputy_events")
+     * @Method("GET")
+     */
+    public function listEventsAction(): Response
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/comites", name="app_deputy_committees")
+     * @Method("GET")
+     */
+    public function listCommitteesAction(): Response
+    {
+        return new Response();
+    }
+
+    private function createMessage(array $recipients): DeputyMessage
+    {
+        return new DeputyMessage($this->getUser(), $recipients);
+    }
+}

src/Deputy/DeputyMessageNotifier.php
@@ -1,0 +1,33 @@
+<?php
+
+namespace AppBundle\Deputy;
+
+use AppBundle\Mailer\MailerService;
+use AppBundle\Mailer\Message\DeputyMessage as Message;
+
+class DeputyMessageNotifier
+{
+    private $mailer;
+
+    public function __construct(MailerService $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendMessage(DeputyMessage $message): void
+    {
+        $chunks = array_chunk(
+            $message->getRecipients(),
+            MailerService::PAYLOAD_MAXSIZE
+        );
+
+        foreach ($chunks as $chunk) {
+            $this->mailer->sendMessage($this->createMessage($message, $chunk));
+        }
+    }
+
+    private function createMessage(DeputyMessage $message, array $recipients): Message
+    {
+        return Message::create($message, $recipients);
+    }
+}

src/Mailer/Message/DeputyMessage.php
@@ -1,0 +1,57 @@
+<?php
+
+namespace AppBundle\Mailer\Message;
+
+use AppBundle\Deputy\DeputyMessage as DeputyMessageModel;
+use AppBundle\Entity\Adherent;
+use Ramsey\Uuid\Uuid;
+
+final class DeputyMessage extends Message
+{
+    /**
+     * @param DeputyMessageModel $model
+     * @param Adherent[]         $recipients
+     *
+     * @return DeputyMessage
+     */
+    public static function create(DeputyMessageModel $model, array $recipients): self
+    {
+        if (!$recipients) {
+            throw new \InvalidArgumentException('At least one recipient is required.');
+        }
+
+        $deputy = $model->getFrom();
+        $first = array_shift($recipients);
+
+        $message = new self(
+            Uuid::uuid4(),
+            '455851',
+            $first->getEmailAddress(),
+            $first->getFullName() ?: '',
+            $model->getSubject(),
+            [
+                'deputy_fullname' => self::escape($deputy->getFullName()),
+                'circonscription_name' => self::escape($deputy->getManagedDistrict()),
+                'target_message' => $model->getContent(),
+            ],
+            [
+                'target_firstname' => self::escape($first->getFirstName() ?: ''),
+            ],
+            $deputy->getEmailAddress()
+        );
+
+        $message->setSenderName(sprintf('Votre député%s En Marche !', $deputy->isFemale() ? 'e' : ''));
+
+        foreach ($recipients as $recipient) {
+            $message->addRecipient(
+                $recipient->getEmailAddress(),
+                $recipient->getFullName() ?: '',
+                [
+                    'target_firstname' => self::escape($recipient->getFirstName() ?: ''),
+                ]
+            );
+        }
+
+        return $message;
+    }
+}