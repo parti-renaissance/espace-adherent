6c7e359c72cd58e9bb506319f03d84ceaea6da07
illusionOfParadise <olga.rotach@gmail.com>
2018-07-26 12:58:18 +0200

    Add e-mail subscription changing from external service (Mailchimp)

---
+++ src/Controller/Api/SubscriptionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use AppBundle\Subscription\SubscriptionHandler;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+/**
+ * @Route("/email-subscriptions")
+ */
+class SubscriptionController extends Controller
+{
+    /**
+     * @Route("/change", name="app_change_email_subscriptions_webhook")
+     * @Method("POST")
+     */
+    public function changeEmailSubscriptionsAction(Request $request): Response
+    {
+        if (!$request->query->has('secret') || !$secret = $request->query->get('secret')) {
+            throw new UnauthorizedHttpException('There is no secret');
+        }
+
+        if ($secret !== $this->container->getParameter('webhook.email_subscriptions_secret')) {
+            throw new AccessDeniedHttpException('Wrong secret.');
+        }
+
+        $type = $request->request->get('type');
+        $data = $request->request->get('data');
+
+        if (0 === $request->request->count() || !$type || !isset($data['email']) || !isset($data['list_id'])) {
+            throw new BadRequestHttpException('The request should contain correct data.');
+        }
+
+        $this->get(SubscriptionHandler::class)->changeSubscription($type, $data['email'], $data['list_id']);
+        $this->getDoctrine()->getManager()->flush();
+
+        return new Response('OK');
+    }
+}
