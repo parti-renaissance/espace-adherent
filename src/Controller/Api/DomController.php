5b767d19db81c5fe658455c6db0c6e3cee69bc6b
Rémi Gardien <remi@gardien.biz>
2018-07-27 14:46:19 +0200

    Create an endpoint to expose header & footer markup

---
+++ src/Controller/Api/DomController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DomController extends Controller
+{
+    /**
+     * @Route("/dom", name="app_api_dom")
+     * @Method("GET")
+     */
+    public function dom(): JsonResponse
+    {
+        return new JsonResponse([
+            'header' => $this->renderView('components/_header.html.twig'),
+            'footer' => $this->renderView('components/_footer.html.twig'),
+        ]);
+    }
+}
