915497001b774374a3a6ac54b9039b93d5dde780
Brian Whitton <me@brianwhitton.com>
2018-08-22 23:50:39 -0400

    remove unused reducers

--- react/src/js/citizen_project/reducers/index.js
+++ react/src/js/citizen_project/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
 import filter from './filter';
-import stats from './stats';
-import user from './user';
 import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
     filter,
-    stats,
-    user,
     routing: routerReducer,
 });
