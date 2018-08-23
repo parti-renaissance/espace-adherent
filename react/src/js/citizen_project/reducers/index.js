05d2798481fe6ab9d77f476660027f39a2b924f9
Brian Whitton <me@brianwhitton.com>
2018-08-22 23:50:52 -0400

    move package import to top of file

--- react/src/js/citizen_project/reducers/index.js
+++ react/src/js/citizen_project/reducers/index.js
@@ -1,6 +1,6 @@
+import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import filter from './filter';
-import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
     filter,
