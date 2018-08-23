da2509f9fd86e2d0f20163ca9ed55c64633acd4b
Brian Whitton <me@brianwhitton.com>
2018-08-22 23:49:33 -0400

    add action and reducers for categories

--- react/src/js/citizen_project/actions/citizen-projects.js
+++ react/src/js/citizen_project/actions/citizen-projects.js
@@ -1,6 +1,7 @@
 import callApi from '../../utils/api';
 
 export const CITIZEN_PROJECTS = 'CITIZEN_PROJECTS';
+export const CATEGORIES = 'CATEGORIES';
 
 const API = process.env.REACT_APP_CITIZEN_API;
 
@@ -8,5 +9,12 @@ export function getCitizenProjects() {
     return {
         type: CITIZEN_PROJECTS,
         payload: callApi(API, '?status=APPROVED'),
+    };
+}
+
+export function getCategories() {
+    return {
+        type: CATEGORIES,
+        payload: callApi(API, '/categories'),
     };
 }
