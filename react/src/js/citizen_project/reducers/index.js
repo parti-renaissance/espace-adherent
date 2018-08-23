21fd0eed7f563d7679580647a9a4b654225fa093
Brian Whitton <me@brianwhitton.com>
2018-08-22 23:51:02 -0400

    import reducers

--- react/src/js/citizen_project/reducers/index.js
+++ react/src/js/citizen_project/reducers/index.js
@@ -1,8 +1,16 @@
 import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import filter from './filter';
+import categories from './categories';
+import locales from './cities-and-countries';
+import citizen from './citizen-projects';
+import turnkey from './turnkey-projects';
 
 export default combineReducers({
     filter,
+    citizen,
+    turnkey,
+    locales,
+    categories,
     routing: routerReducer,
 });
