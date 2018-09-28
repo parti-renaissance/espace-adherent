89ab10ca9983602e1b514f39954c74fdde873671
Dimitri Gritsajuk <dimitri.gritsajuk@sensiolabs.com>
2018-09-28 18:22:50 +0200

    [MailerBundle] migration to v2 - part 2

---
+++ src/Mail/Transactional/CitizenActionCancellationMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Mail\Transactional;
+
+use AppBundle\Collection\EventRegistrationCollection;
+use AppBundle\Entity\CitizenAction;
+use AppBundle\Entity\EventRegistration;
+use AppBundle\Mail\AdherentMailTrait;
+use AppBundle\Utils\StringCleaner;
+use EnMarche\MailerBundle\Mail\ChunkableMailInterface;
+use EnMarche\MailerBundle\Mail\Recipient;
+use EnMarche\MailerBundle\Mail\TransactionalMail;
+
+final class CitizenActionCancellationMail extends TransactionalMail implements ChunkableMailInterface
+{
+    use AdherentMailTrait;
+
+    public const SUBJECT = '[Action citoyenne] Une action citoyenne à laquelle vous participez vient d\'être annulée.';
+
+    public static function createRecipientsFrom(EventRegistrationCollection $subscriptions): array
+    {
+        return $subscriptions
+            ->map(function (EventRegistration $registration) {
+                return new Recipient(
+                    $registration->getEmailAddress(),
+                    $registration->getFullName(),
+                    ['target_firstname' => StringCleaner::htmlspecialchars($registration->getFirstName())]
+                );
+            })
+            ->toArray()
+        ;
+    }
+
+    public static function createTemplateVarsFrom(CitizenAction $action, string $eventLink): array
+    {
+        return [
+            'citizen_action_name' => StringCleaner::htmlspecialchars($action->getName()),
+            'event_slug' => $eventLink,
+        ];
+    }
+}
---
+++ src/Mail/Transactional/CitizenActionRegistrationConfirmationMail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Mail\Transactional;
+
+use AppBundle\Entity\CitizenAction;
+use AppBundle\Entity\EventRegistration;
+use AppBundle\Utils\StringCleaner;
+use EnMarche\MailerBundle\Mail\Recipient;
+use EnMarche\MailerBundle\Mail\RecipientInterface;
+use EnMarche\MailerBundle\Mail\TransactionalMail;
+
+final class CitizenActionRegistrationConfirmationMail extends TransactionalMail
+{
+    public static function createRecipientFrom(EventRegistration $registration): RecipientInterface
+    {
+        return new Recipient(
+            $registration->getEmailAddress(),
+            $registration->getFirstName(),
+            ['prenom' => StringCleaner::htmlspecialchars($registration->getFirstName())]
+        );
+    }
+
+    public static function createTemplateVarsFrom(CitizenAction $action, string $link): array
+    {
+        return [
+            'citizen_action_name' => StringCleaner::htmlspecialchars($action->getName()),
+            'citizen_action_organiser' => StringCleaner::htmlspecialchars($action->getOrganizerName()),
+            'citizen_action_calendar_url' => $link,
+        ];
+    }
+}
