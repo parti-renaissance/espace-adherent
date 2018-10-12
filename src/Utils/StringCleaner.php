0deabfb603d7e38dec18f4640de88bf84fed2633
Dimitri Gritsajuk <dimitri.gritsajuk@sensiolabs.com>
2018-10-12 10:47:31 +0200

    Change composer Mailer version

--- src/Utils/StringCleaner.php
+++ src/Utils/StringCleaner.php
@@ -4,7 +4,7 @@ namespace AppBundle\Utils;
 
 class StringCleaner
 {
-    public static function htmlspecialchars(string $value, int $flags = ENT_NOQUOTES): string
+    public static function htmlspecialchars(string $value, int $flags = \ENT_NOQUOTES): string
     {
         return htmlspecialchars($value, $flags, 'UTF-8', false);
     }
