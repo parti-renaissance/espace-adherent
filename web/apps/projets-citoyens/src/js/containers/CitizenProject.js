accc3166adb0610b9b1bbc271750e02366498e31
Charles d'Oiron <doironcharles@gmail.com>
2018-10-12 17:15:04 +0200

    line length

--- web/apps/projets-citoyens/src/js/containers/CitizenProject.js
+++ web/apps/projets-citoyens/src/js/containers/CitizenProject.js
@@ -7,9 +7,7 @@ import { getPinned } from '../actions/turnkey-projects';
 import TurnkeyProjectDetail from './../components/TurnkeyProjectDetail';
 
 const ViewTurnkeys = () => (
-    <Link
-        to="/projets-citoyens/decouvrir"
-        className="turnkey__cta turnkey__cta__yellow">
+    <Link to="/projets-citoyens/decouvrir" className="turnkey__cta turnkey__cta__yellow">
         Voir tous les projets clés en main
     </Link>
 );
@@ -24,34 +22,24 @@ class CitizenProject extends Component {
     render() {
         const {
             // count,
-            pinned: { /* loading, */ project }
+            pinned: { /* loading, */ project },
         } = this.props;
         return (
             <div className="citizen__wrapper">
                 <h2 className="">Déjà 500 projets citoyens lancés !</h2>
                 <p className="citizen__blurb">
-                    Parce que transformer la France demande l'engagement de
-                    tous, des citoyens partout dans le pays se regroupent pour
-                    agir dans leurs territoires. Ils l'ont fait : pourquoi pas
-                    vous ?
+                    Parce que transformer la France demande l'engagement de tous, des citoyens partout dans le pays se
+                    regroupent pour agir dans leurs territoires. Ils l'ont fait : pourquoi pas vous ?
                 </p>
                 <div className="citizen__helplist">
-                    <h3 className="citizen__helplist-header">
-                        Un projet citoyen c'est&nbsp;quoi&nbsp;?
-                    </h3>
+                    <h3 className="citizen__helplist-header">Un projet citoyen c'est&nbsp;quoi&nbsp;?</h3>
                     <div>
                         <span className="number">1</span>
-                        <p>
-                            Un projet local porté par des citoyens, en lien avec
-                            les acteurs du territoire
-                        </p>
+                        <p>Un projet local porté par des citoyens, en lien avec les acteurs du territoire</p>
                     </div>
                     <div>
                         <span className="number">2</span>
-                        <p>
-                            Un projet destiné à changer concrètement la vie des
-                            habitants
-                        </p>
+                        <p>Un projet destiné à changer concrètement la vie des habitants</p>
                     </div>
                     <div>
                         <span className="number">3</span>
@@ -64,7 +52,8 @@ class CitizenProject extends Component {
                         href="https://storage.googleapis.com/en-marche-prod/documents/projets-citoyens/La%20Charte%20des%20Projets%20Citoyens.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="simple--link">
+                        className="simple--link"
+                    >
                         En savoir plus sur la Charte des Projets Citoyens
                     </a>
                 </div>
@@ -85,7 +74,7 @@ class CitizenProject extends Component {
 
 const mapStateToProps = state => ({
     count: state.citizen.count,
-    pinned: state.turnkey.pinned
+    pinned: state.turnkey.pinned,
 });
 
 export default connect(mapStateToProps)(CitizenProject);
