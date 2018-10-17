psa-tzouk/en-marche.fr

Update Referent API endpoints

features/api/adherents.feature
@@ -11,19 +11,27 @@ Feature:
       | LoadAdherentData                   |
       | LoadEmailSubscriptionHistoryData   |
       | LoadCommitteeMembershipHistoryData |
+      | LoadClientData                     |
 
   Scenario: As a non logged-in user I can not access the adherents count information
-    When I am on "/api/adherents/count"
+    When I am on "/api/statistics/adherents/count"
     Then the response status code should be 401
 
   Scenario: As an adherent I can not access the adherents count information
     When I am logged as "jacques.picard@en-marche.fr"
-    And I am on "/api/adherents/count"
-    Then the response status code should be 403
+    And I am on "/api/statistics/adherents/count"
+    Then the response status code should be 401
 
   Scenario: As a referent I can access the adherents count information
-    Given I am logged as "referent@en-marche-dev.fr"
-    When I am on "/api/adherents/count"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/adherents/count?email=referent@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -34,17 +42,24 @@ Feature:
     """
 
   Scenario: As a non logged-in user I can not access the managed by referent adherents count information
-    When I am on "/api/adherents/count-by-referent-area"
+    When I am on "/api/statistics/adherents/count-by-referent-area"
     Then the response status code should be 401
 
   Scenario: As an adherent I can not access the managed by referent adherents count information
     When I am logged as "jacques.picard@en-marche.fr"
-    And I am on "/api/adherents/count-by-referent-area"
-    Then the response status code should be 403
+    And I am on "/api/statistics/adherents/count-by-referent-area"
+    Then the response status code should be 401
 
   Scenario: As a referent I can access the managed by referent adherents count information
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/adherents/count-by-referent-area"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/adherents/count-by-referent-area?email=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:

features/api/events.feature
@@ -1,7 +1,7 @@
 @api
 Feature:
   In order to get events count in the
-  As a referent
+  As a client passing a referent email
   I should be able to access events API stats
 
   Background:
@@ -11,25 +11,33 @@ Feature:
       | LoadAdherentData      |
       | LoadEventData         |
       | LoadCitizenActionData |
+      | LoadClientData        |
 
   Scenario: As a non logged-in user I can not get events count in the referent managed zone
-    When I am on "/api/events/count"
+    When I am on "/api/statistics/events/count"
     Then the response status code should be 401
 
   Scenario Outline: As an adherent I can not get events count in the referent managed zone
     When I am logged as "jacques.picard@en-marche.fr"
     And I am on "<url>"
-    Then the response status code should be 403
+    Then the response status code should be 401
 
     Examples:
       | url                            |
-      | /api/events/count              |
-      | /api/events/count-by-month     |
-      | /api/events/count-participants |
+      | /api/statistics/events/count              |
+      | /api/statistics/events/count-by-month     |
+      | /api/statistics/events/count-participants |
 
-  Scenario: As a referent I can get events count in the referent managed zone
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/events/count"
+  Scenario: As a client passing a referent email I can get events count in the referent managed zone
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count?email=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -56,12 +64,19 @@ Feature:
     """
 
   Scenario: As a non logged-in user I can not get events count in the referent managed zone
-    When I am on "/api/events/count-by-month?country=fr"
+    When I am on "/api/statistics/events/count-by-month?country=fr"
     Then the response status code should be 401
 
-  Scenario: As a referent I can get events count in the referent managed zone
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/events/count-by-month"
+  Scenario:  As a client passing a referent email I can get events count in the referent managed zone
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?email=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -86,7 +101,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?country=fr"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?email=referent-75-77@en-marche-dev.fr&country=fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -111,7 +134,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?city=Paris 8e"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?email=referent-75-77@en-marche-dev.fr&city=Paris%208e"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -136,7 +167,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?city=Fontainebleau"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?email=referent-75-77@en-marche-dev.fr&city=Fontainebleau"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -161,7 +200,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?committee=515a56c0-bde8-56ef-b90c-4745b1c93818"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?email=referent-75-77@en-marche-dev.fr&committee=515a56c0-bde8-56ef-b90c-4745b1c93818"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -187,7 +234,15 @@ Feature:
     """
 
     # Test get stats for committee with scheduled events but not managed by referent
-    When I am on "/api/events/count-by-month?committee=62ea97e7-6662-427b-b90a-23429136d0dd"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?email=referent-75-77@en-marche-dev.fr&committee=62ea97e7-6662-427b-b90a-23429136d0dd"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -212,9 +267,16 @@ Feature:
     }
     """
 
-  Scenario: As a referent I can get participants count
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/events/count-participants"
+  Scenario: As a client passing a referent email I can get participants count
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-participants?email=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:

parti-renaissance/espace-adherent

features/api/adherents.feature
@@ -11,19 +11,27 @@ Feature:
       | LoadAdherentData                   |
       | LoadEmailSubscriptionHistoryData   |
       | LoadCommitteeMembershipHistoryData |
+      | LoadClientData                     |
 
   Scenario: As a non logged-in user I can not access the adherents count information
-    When I am on "/api/adherents/count"
+    When I am on "/api/statistics/adherents/count"
     Then the response status code should be 401
 
   Scenario: As an adherent I can not access the adherents count information
     When I am logged as "jacques.picard@en-marche.fr"
-    And I am on "/api/adherents/count"
-    Then the response status code should be 403
+    And I am on "/api/statistics/adherents/count"
+    Then the response status code should be 401
 
   Scenario: As a referent I can access the adherents count information
-    Given I am logged as "referent@en-marche-dev.fr"
-    When I am on "/api/adherents/count"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/adherents/count?referent=referent@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -34,17 +42,24 @@ Feature:
     """
 
   Scenario: As a non logged-in user I can not access the managed by referent adherents count information
-    When I am on "/api/adherents/count-by-referent-area"
+    When I am on "/api/statistics/adherents/count-by-referent-area"
     Then the response status code should be 401
 
   Scenario: As an adherent I can not access the managed by referent adherents count information
     When I am logged as "jacques.picard@en-marche.fr"
-    And I am on "/api/adherents/count-by-referent-area"
-    Then the response status code should be 403
+    And I am on "/api/statistics/adherents/count-by-referent-area"
+    Then the response status code should be 401
 
   Scenario: As a referent I can access the managed by referent adherents count information
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/adherents/count-by-referent-area"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/adherents/count-by-referent-area?referent=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:

features/api/events.feature
@@ -1,7 +1,7 @@
 @api
 Feature:
   In order to get events count in the
-  As a referent
+  As a client passing a referent email
   I should be able to access events API stats
 
   Background:
@@ -11,25 +11,33 @@ Feature:
       | LoadAdherentData      |
       | LoadEventData         |
       | LoadCitizenActionData |
+      | LoadClientData        |
 
   Scenario: As a non logged-in user I can not get events count in the referent managed zone
-    When I am on "/api/events/count"
+    When I am on "/api/statistics/events/count"
     Then the response status code should be 401
 
   Scenario Outline: As an adherent I can not get events count in the referent managed zone
     When I am logged as "jacques.picard@en-marche.fr"
     And I am on "<url>"
-    Then the response status code should be 403
+    Then the response status code should be 401
 
     Examples:
       | url                            |
-      | /api/events/count              |
-      | /api/events/count-by-month     |
-      | /api/events/count-participants |
+      | /api/statistics/events/count              |
+      | /api/statistics/events/count-by-month     |
+      | /api/statistics/events/count-participants |
 
-  Scenario: As a referent I can get events count in the referent managed zone
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/events/count"
+  Scenario: As a client passing a referent email I can get events count in the referent managed zone
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count?referent=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -56,12 +64,19 @@ Feature:
     """
 
   Scenario: As a non logged-in user I can not get events count in the referent managed zone
-    When I am on "/api/events/count-by-month?country=fr"
+    When I am on "/api/statistics/events/count-by-month?country=fr"
     Then the response status code should be 401
 
-  Scenario: As a referent I can get events count in the referent managed zone
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/events/count-by-month"
+  Scenario:  As a client passing a referent email I can get events count in the referent managed zone
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?referent=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -86,7 +101,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?country=fr"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?referent=referent-75-77@en-marche-dev.fr&country=fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -111,7 +134,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?city=Paris 8e"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?referent=referent-75-77@en-marche-dev.fr&city=Paris%208e"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -136,7 +167,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?city=Fontainebleau"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?referent=referent-75-77@en-marche-dev.fr&city=Fontainebleau"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -161,7 +200,15 @@ Feature:
     }
     """
 
-    When I am on "/api/events/count-by-month?committee=515a56c0-bde8-56ef-b90c-4745b1c93818"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?referent=referent-75-77@en-marche-dev.fr&committee=515a56c0-bde8-56ef-b90c-4745b1c93818"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -187,7 +234,15 @@ Feature:
     """
 
     # Test get stats for committee with scheduled events but not managed by referent
-    When I am on "/api/events/count-by-month?committee=62ea97e7-6662-427b-b90a-23429136d0dd"
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-by-month?referent=referent-75-77@en-marche-dev.fr&committee=62ea97e7-6662-427b-b90a-23429136d0dd"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to:
@@ -212,9 +267,16 @@ Feature:
     }
     """
 
-  Scenario: As a referent I can get participants count
-    When I am logged as "referent-75-77@en-marche-dev.fr"
-    And I am on "/api/events/count-participants"
+  Scenario: As a client passing a referent email I can get participants count
+    Given I add "Accept" header equal to "application/json"
+    And I send a "POST" request to "/oauth/v2/token" with parameters:
+      | key           | value                                        |
+      | client_secret | crOsk2OxtYb4CgnKoYvhb9wvO73QLYyccChiFrV9evE= |
+      | client_id     | 4f3394d4-7137-424a-8c73-27e0ad641fc9         |
+      | grant_type    | client_credentials                           |
+      | scope         | read:stats                                   |
+    And I add the access token to the Authorization header
+    When I send a "GET" request to "/api/statistics/events/count-participants?referent=referent-75-77@en-marche-dev.fr"
     Then the response status code should be 200
     And the response should be in JSON
     And the JSON should be equal to: