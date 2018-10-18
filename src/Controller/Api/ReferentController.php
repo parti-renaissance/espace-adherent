92a915def513c973cf02f248147cb9507bb599bc
illusionOfParadise <olga.rotach@gmail.com>
2018-10-18 16:55:58 +0200

    Update Referent API endpoint for autocomplete

--- src/Controller/Api/ReferentController.php
+++ src/Controller/Api/ReferentController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller\Api;
 
+use AppBundle\Entity\Adherent;
 use AppBundle\Repository\CommitteeRepository;
 use AppBundle\Repository\EventRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -14,8 +16,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
- * @Route("/referent")
- * @Security("is_granted('ROLE_REFERENT')")
+ * @Route("/statistics")
+ * @Security("is_granted('ROLE_OAUTH_SCOPE_READ:STATS')")
  */
 class ReferentController extends Controller
 {
@@ -32,8 +34,10 @@ class ReferentController extends Controller
     /**
      * @Route("/search/autocomplete", name="api_referent_space_search_autocomplete")
      * @Method("GET")
+     *
+     * @Entity("referent", expr="repository.findReferent(referent)", converter="querystring")
      */
-    public function searchAutocompleteAction(Request $request, CommitteeRepository $committeeRepository, EventRepository $eventRepository): Response
+    public function searchAutocompleteAction(Adherent $referent, Request $request, CommitteeRepository $committeeRepository, EventRepository $eventRepository): Response
     {
         if (!($type = $request->query->get('type')) || !($value = $request->query->get('value'))) {
             throw new BadRequestHttpException('The parameters "type" and "value" are required.');
@@ -42,7 +46,6 @@ class ReferentController extends Controller
         if (!\in_array($type, self::AUTOCOMPLETE_TYPES, true)) {
             throw new BadRequestHttpException(sprintf('Invalid parameter "type" "%" given.', $type));
         }
-        $referent = $this->getUser();
 
         switch ($type) {
             case self::AUTOCOMPLETE_TYPE_CITY:
