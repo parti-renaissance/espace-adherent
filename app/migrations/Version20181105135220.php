80c908f3b331ecbaf4b70ba3abf89125209db38e
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2018-11-05 15:46:55 +0100

    WIP

---
+++ app/migrations/Version20181105135220.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181105135220 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mailer_template_versions ADD updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\', ADD sync_status VARCHAR(255) NOT NULL COMMENT \'(DC2Type:string)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mailer_template_versions DROP updated_at, DROP sync_status');
+    }
+}
