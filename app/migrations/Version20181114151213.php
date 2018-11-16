8906926ef3833db8222e1f5208e365457494dc06
Jeremie Samson <jeremie.samson76@gmail.com>
2018-11-16 17:40:07 +0100

    update css

--- app/migrations/Version20181114151213.php
+++ app/migrations/Version20181114151213.php
@@ -9,11 +9,11 @@ final class Version20181114151213 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE emails_logs (id INT UNSIGNED AUTO_INCREMENT NOT NULL, sender VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, body LONGTEXT NOT NULL, recipients_number INT NOT NULL, uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE email_log (id INT UNSIGNED AUTO_INCREMENT NOT NULL, sender VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, body LONGTEXT NOT NULL, recipients_number INT NOT NULL, uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE emails_logs');
+        $this->addSql('DROP TABLE email_log');
     }
 }
