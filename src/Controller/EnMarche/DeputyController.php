a2053ea2bb38960e35fe0e18e724cd30178ef8a2
illusionOfParadise <olga.rotach@gmail.com>
2018-11-27 10:42:00 +0100

    Remove canary mode from deputy space

--- src/Controller/EnMarche/DeputyController.php
+++ src/Controller/EnMarche/DeputyController.php
@@ -32,8 +32,6 @@ class DeputyController extends Controller
      */
     public function usersSendMessageAction(Request $request, AdherentRepository $adherentRepository): Response
     {
-        $this->disableInProduction();
-
         $currentUser = $this->getUser();
 
         $message = DeputyMessage::create($this->getUser());
