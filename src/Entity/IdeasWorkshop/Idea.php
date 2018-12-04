84d6bd3596df957eceaa59f48f26a8816db5cbc6
illusionOfParadise <olga.rotach@gmail.com>
2018-12-04 15:51:28 +0100

    Add API Platform

--- src/Entity/IdeasWorkshop/Idea.php
+++ src/Entity/IdeasWorkshop/Idea.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity\IdeasWorkshop;
 
 use Algolia\AlgoliaSearchBundle\Mapping\Annotation as Algolia;
+use ApiPlatform\Core\Annotation\ApiResource;
 use AppBundle\Entity\Adherent;
 use AppBundle\Entity\Committee;
 use AppBundle\Entity\EntityIdentityTrait;
@@ -17,6 +18,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 use JMS\Serializer\Annotation as JMS;
 
 /**
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"},
+ * )
+ *
  * @ORM\Entity
  *
  * @ORM\Table(
