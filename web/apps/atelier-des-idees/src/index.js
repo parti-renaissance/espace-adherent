91f316ed13303662e2a0afc724450334e747e40e
Rémi Gardien <remi@gardien.biz>
2018-12-11 15:48:19 +0100

    Create React app for Ideas Workshop

--- web/apps/atelier-des-idees/src/index.js
+++ web/apps/atelier-des-idees/src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-
-import './App.css';
+import './index.css';
 import App from './App';
-import store from './redux/store/.';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
