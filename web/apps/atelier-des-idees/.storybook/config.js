9265e130339a833e936560208dd362df27bf60bb
Adrien Casanova <haitaar@gmail.com>
2018-12-12 11:47:48 +0100

    [ADI] storybook base config

--- web/apps/atelier-des-idees/.storybook/config.js
+++ web/apps/atelier-des-idees/.storybook/config.js
@@ -1,20 +1,9 @@
 import { configure } from '@storybook/react';
-import { addDecorator } from '@storybook/react';
-import { withTests } from '@storybook/addon-jest';
-import results from '../.jest-test-results.json';
-import '../src/App.css';
 
 // load stories dynamically
 const req = require.context('../src', true, /\.stories\.js$/);
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
-
-// load tests results dynamically
-addDecorator(
-    withTests({
-        results,
-    })
-);
 
 configure(loadStories, module);
