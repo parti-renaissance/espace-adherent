61ee0e0c269dbd45fc067d9430743f712e0a4d53
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-14 17:09:55 +0100

    [ADI] #13 add redux and redux devtools

--- web/apps/atelier-des-idees/src/index.js
+++ web/apps/atelier-des-idees/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+
 import './App.css';
 import App from './App';
+import store from './redux/store/.';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
