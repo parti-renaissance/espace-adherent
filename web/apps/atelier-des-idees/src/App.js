cdf8ba208ac7dde66293642e2ae3782e9842d25a
Rémi Gardien <remi@gardien.biz>
2018-12-17 22:24:00 +0100

    Test autodeploy

--- web/apps/atelier-des-idees/src/App.js
+++ web/apps/atelier-des-idees/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+                    To get started, edit <code>web/apps/atelier-des-idees/src/App.js</code> and save to reload.
                 </p>
             </div>
         );
