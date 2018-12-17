2cd51e6058d70f97ee6a46f268b39cc8fb902ad0
Jeremie Samson <jeremie.samson76@gmail.com>
2018-12-17 17:53:58 +0100

    Add consultation endpoint

---
+++ app/migrations/Version20181217183057.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20181217183057 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE ideas_workshop_consultation ADD enabled TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE ideas_workshop_consultation DROP enabled');
+    }
+}
