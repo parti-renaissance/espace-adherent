f788d6db54ebeeac0cd4bff7e730e0c94028a8fc
Jeremie Samson <jeremie.samson76@gmail.com>
2018-12-17 17:53:58 +0100

    Add consultation endpoint

--- src/DataFixtures/ORM/LoadIdeaConsultationReportData.php
+++ src/DataFixtures/ORM/LoadIdeaConsultationReportData.php
@@ -6,7 +6,7 @@ use AppBundle\Entity\IdeasWorkshop\ConsultationReport;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadConsultationReportData extends AbstractFixture
+class LoadIdeaConsultationReportData extends AbstractFixture
 {
     public function load(ObjectManager $manager)
     {
