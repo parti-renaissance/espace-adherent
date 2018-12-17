f14359b434d9e198c445e5e2ab94689c34cc9c8e
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-17 17:14:00 +0100

    [ADI-3]: complete button component + add base style button

--- web/apps/atelier-des-idees/src/components/Button/index.jsx
+++ web/apps/atelier-des-idees/src/components/Button/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 function Button(props) {
     return (
         <button
-            className="button"
+            className={classNames('button', props.className)}
             aria-label={props.label}
             disabled={props.disabled}
             onClick={() => props.onClick()}>
-            {props.label}
+            { props.icon &&
+                <img src={props.icon} className={classNames('button__icon', `button__icon--${props.classIcon}`)}/>
+            }
+            <span className='button__label'>{props.label}</span>
         </button>
     );
 }
@@ -18,6 +22,9 @@ Button.defaultProps = {
     icon: undefined,
     type: 'button',
     disabled: false,
+    className: undefined,
+    classIcon: undefined,
+    isLoading: false,
 };
 
 Button.propTypes = {
@@ -26,6 +33,9 @@ Button.propTypes = {
     type: PropTypes.string,
     onClick: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
+    className: PropTypes.string,
+    classIcon: PropTypes.string, // start or end
+    isLoading: PropTypes.bool,
 };
 
 export default Button;
