611acafe560f01f0c5ba176791f9c2e6e8ecbdd3
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-17 14:56:40 +0100

    [ADI-3] create 3 routes (consult, contribute, propose)

--- web/apps/atelier-des-idees/src/App.js
+++ web/apps/atelier-des-idees/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Consult from './pages/Consult.jsx';
+import Contribute from './pages/Contribute';
+import Propose from './pages/Propose';
 
 import logo from './logo.svg';
 
@@ -11,6 +14,9 @@ class App extends Component {
             <div className="App">
                 <Switch>
                     <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/consulter" component={Consult}></Route>
+                    <Route exact path="/contribuer" component={Contribute}></Route>
+                    <Route exact path="/proposer" compoenent={Propose}></Route>
                 </Switch>
             </div>
         );
