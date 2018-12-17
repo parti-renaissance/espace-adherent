f60d3504849a7e3f3eca2dbaf4cbd8011d4314d9
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-17 14:56:40 +0100

    [ADI-3] create 3 routes (consult, contribute, propose)

---
+++ web/apps/atelier-des-idees/src/pages/Contribute.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Contribute(props) {
+    return (
+        <div className="contribute-page">
+            CONTRIBUER
+        </div>
+    )
+}
+
+export default Contribute;
