ae9e450532d6e9806849a2ac69b0784a1c0fe8d3
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-19 16:18:36 +0100

    [ADI] Create Tabs component

---
+++ web/apps/atelier-des-idees/src/components/Tabs/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RcTabs, { TabPane } from 'rc-tabs';
+import TabContent from 'rc-tabs/lib/TabContent';
+import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';
+
+import 'rc-tabs/assets/index.css';
+
+// https://github.com/fis-components/rc-tabs
+
+function Tabs(props) {
+    return (
+        <RcTabs
+            className="tabs"
+            renderTabBar={ () => <ScrollableInkTabBar /> }
+            renderTabContent={ () => <TabContent /> }>
+            {props.panes.map((pane, index) => {
+                const Component = pane.component;
+                return <TabPane tab={pane.title} key={index}>{'function' === typeof Component ? <Component /> : Component}</TabPane>;
+            })}
+        </RcTabs>
+    );
+}
+
+Tabs.propTypes = {
+    panes: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        component: PropTypes.oneOfType([PropTypes.node.isRequired, PropTypes.func]),
+    })).isRequired,
+};
+
+export default Tabs;
