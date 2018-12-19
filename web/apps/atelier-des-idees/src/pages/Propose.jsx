b602704185e56559a70933e7766303ed42f46f7d
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-19 15:18:03 +0100

    [ADI] Add routes to app

---
+++ web/apps/atelier-des-idees/src/pages/Propose.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Propose(props) {
+    return (
+        <div className="propose-page">PROPOSER</div>
+    );
+}
+
+export default Propose;
