3e21710dd60684de68321de00eb48bee842ca5c2
Adrien Casanova <haitaar@gmail.com>
2018-12-19 17:18:20 +0100

    [ADI] add modal reducer

---
+++ web/apps/atelier-des-idees/src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import modal from './modal';
+
+const rootReducer = combineReducers({ modal });
+
+export default rootReducer;
--- web/apps/atelier-des-idees/src/redux/store/configStore.dev.js
+++ web/apps/atelier-des-idees/src/redux/store/configStore.dev.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
