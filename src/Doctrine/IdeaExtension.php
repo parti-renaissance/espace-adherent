26133447d4751158bc059ea75a2e0991ce543131
Jeremie Samson <jeremie.samson76@gmail.com>
2018-12-19 15:43:01 +0100

    Add author uuid filter on /ideas

--- src/Doctrine/IdeaExtension.php
+++ src/Doctrine/IdeaExtension.php
@@ -2,23 +2,25 @@
 
 namespace AppBundle\Doctrine;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
 use AppBundle\Entity\IdeasWorkshop\Idea;
 use AppBundle\Entity\IdeasWorkshop\IdeaStatusEnum;
 
-class IdeaExtension implements QueryItemExtensionInterface, QueryCollectionExtensionInterface
+class IdeaExtension implements QueryItemExtensionInterface, ContextAwareQueryCollectionExtensionInterface
 {
     public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
     {
         $this->modifyQuery($queryBuilder, $resourceClass);
     }
 
-    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null, array $context = [])
     {
-        $this->modifyQuery($queryBuilder, $resourceClass);
+        if (!isset($context['filters']['author.uuid'])) {
+            $this->modifyQuery($queryBuilder, $resourceClass);
+        }
     }
 
     private function modifyQuery(QueryBuilder $queryBuilder, string $resourceClass): void
