4ac0740c493c8d86e9781af69db02db43b32859d
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-19 16:21:22 +0100

    [ADI] Create start LatestIdeas

---
+++ web/apps/atelier-des-idees/src/components/LatestIdeas/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Tabs from '../Tabs/index';
+
+function LatestIdeas(props) {
+    const panes = [
+        {
+            title: 'Propositions finalisées',
+            component: <div/>,
+        },
+        {
+            title: 'Propositions en cours d\'élaboration',
+            component: <div/>,
+        },
+    ];
+
+    return (
+        <div className="latest-ideas">
+            <h2 className="latest-ideas__title">Consultez les dernières propostions publiées par nos adhérents</h2>
+            <Tabs panes={ panes }/>
+        </div>
+    );
+}
+
+export default LatestIdeas;
