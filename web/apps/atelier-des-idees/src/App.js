8eb617e13bad9cff2cc63be4a720dcc08391ff27
Adrien Casanova <haitaar@gmail.com>
2018-12-19 17:23:19 +0100

    [ADI] add ModalRoot to the app

--- web/apps/atelier-des-idees/src/App.js
+++ web/apps/atelier-des-idees/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+// pages
 import Home from './pages/Home';
 import Consult from './pages/Consult';
 import Contribute from './pages/Contribute';
 import Propose from './pages/Propose';
+
+// modal
+import ModalRoot from './containers/ModalRoot';
 
 import logo from './logo.svg';
 
@@ -12,6 +16,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <ModalRoot />
                 <Switch>
                     <Route exact path="/atelier-des-idees" component={Home} />
                     <Route exact path="/atelier-des-idees/consulter" component={Consult} />
