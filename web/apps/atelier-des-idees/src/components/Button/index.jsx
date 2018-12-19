43a47f42db9a8458f2c579ed00dd74e9b07599de
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-19 15:27:56 +0100

    [ADI] add component Button

---
+++ web/apps/atelier-des-idees/src/components/Button/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+function Button(props) {
+    return (
+        <button
+            className={classNames('button', props.className)}
+            aria-label={props.label}
+            disabled={props.disabled}
+            onClick={() => props.onClick()}>
+            { props.icon &&
+                <img src={props.icon} className={classNames('button__icon', `button__icon--${props.classIcon}`)}/>
+            }
+            <span className='button__label'>{props.label}</span>
+        </button>
+    );
+}
+
+Button.defaultProps = {
+    label: undefined,
+    icon: undefined,
+    type: 'button',
+    disabled: false,
+    className: undefined,
+    classIcon: undefined,
+    isLoading: false,
+};
+
+Button.propTypes = {
+    label: PropTypes.string,
+    icon: PropTypes.string,
+    type: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool,
+    className: PropTypes.string,
+    classIcon: PropTypes.oneOf(['start', 'end']),
+    isLoading: PropTypes.bool,
+};
+
+export default Button;
