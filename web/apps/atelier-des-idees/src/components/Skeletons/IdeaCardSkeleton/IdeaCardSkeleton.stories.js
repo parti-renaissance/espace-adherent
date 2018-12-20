b9fa78e2cf1bc44b5653f117a994d5f35f934949
Adrien Casanova <haitaar@gmail.com>
2018-12-20 16:37:47 +0100

    [ADI] create IdeaCardSkeleton component

---
+++ web/apps/atelier-des-idees/src/components/Skeletons/IdeaCardSkeleton/IdeaCardSkeleton.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import IdeaCardSkeleton from '.';
+
+storiesOf('IdeaCardSkeleton', module).add('default', () => <IdeaCardSkeleton />);
