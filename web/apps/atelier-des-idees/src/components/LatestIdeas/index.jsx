057cf20259fae49acd909f09e1f38f13ff68f67d
Adrien Casanova <haitaar@gmail.com>
2018-12-20 17:51:47 +0100

    [ADI] create LatestIdeas container

--- web/apps/atelier-des-idees/src/components/LatestIdeas/index.jsx
+++ web/apps/atelier-des-idees/src/components/LatestIdeas/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Tabs from '../Tabs/index';
 import LatestIdeasPane from './LatestIdeasPane';
 
 function LatestIdeas(props) {
+    const { published = {}, pending = {} } = props.ideas;
     const panes = [
         {
             title: 'Propositions finalisées',
-            component: <LatestIdeasPane link="/atelier-des-idees/consulter" />,
+            component: (
+                <LatestIdeasPane
+                    link="/atelier-des-idees/consulter"
+                    ideas={published.items}
+                    isLoading={published.isLoading}
+                />
+            ),
         },
         {
             title: 'Propositions en cours d\'élaboration',
-            component: <LatestIdeasPane link="/atelier-des-idees/contribuer" />,
+            component: (
+                <LatestIdeasPane
+                    link="/atelier-des-idees/contribuer"
+                    ideas={pending.items}
+                    isLoading={pending.isLoading}
+                />
+            ),
         },
     ];
 
@@ -23,5 +37,16 @@ function LatestIdeas(props) {
         </article>
     );
 }
+
+LatestIdeas.defaultProps = {
+    ideas: {},
+};
+
+LatestIdeas.propTypes = {
+    ideas: PropTypes.shape({
+        published: PropTypes.shape({ isLoading: PropTypes.bool, items: PropTypes.array }),
+        pending: PropTypes.shape({ isLoading: PropTypes.bool, items: PropTypes.array }),
+    }),
+};
 
 export default LatestIdeas;
