d84588dda033019ac1dcd9bb3fa3d0d643ff593d
Adrien Casanova <haitaar@gmail.com>
2018-12-20 17:21:02 +0100

    [ADI] handle loading for FETCH_IDEAS

--- web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
+++ web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
@@ -1,7 +1,10 @@
+import { createRequestTypes } from '../helpers/actions';
+
 // modal
 export const SHOW_MODAL = 'SHOW_MODAL';
 export const HIDE_MODAL = 'HIDE_MODAL';
 
 // ideas
+export const FETCH_IDEAS = createRequestTypes('FETCH_IDEAS');
 export const SET_IDEAS = 'SET_IDEAS';
 export const ADD_IDEAS = 'ADD_IDEAS';
--- web/apps/atelier-des-idees/src/redux/thunk/ideas.js
+++ web/apps/atelier-des-idees/src/redux/thunk/ideas.js
@@ -1,11 +1,17 @@
+import { FETCH_IDEAS } from '../constants/actionTypes';
+import { createRequest, createRequestSuccess, createRequestFailure } from '../actions/loading';
 import { addIdeas } from '../actions/ideas';
 
-const HOST = process.env.REACT_APP_EM_API_HOST;
-
 export function fetchIdeas(status, params = {}) {
-    return (dispatch, getState, axios) =>
-        axios
+    return (dispatch, getState, axios) => {
+        dispatch(createRequest(FETCH_IDEAS, status));
+        return axios
             .get('/api/ideas', { params: { status, ...params } })
             .then(res => res.data)
-            .then(ideas => dispatch(addIdeas(ideas)));
+            .then((ideas) => {
+                dispatch(addIdeas(ideas));
+                dispatch(createRequestSuccess(FETCH_IDEAS, status));
+            })
+            .catch(error => dispatch(createRequestFailure(FETCH_IDEAS, status)));
+    };
 }
