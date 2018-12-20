26ec919934a1c8f1078c9556ea539848e72a2fd2
Adrien Casanova <haitaar@gmail.com>
2018-12-20 15:55:27 +0100

    [ADI] add params to fetchIdeas thunk

--- web/apps/atelier-des-idees/src/redux/thunk/ideas.js
+++ web/apps/atelier-des-idees/src/redux/thunk/ideas.js
@@ -2,10 +2,10 @@ import { addIdeas } from '../actions/ideas';
 
 const HOST = process.env.REACT_APP_EM_API_HOST;
 
-export function fetchIdeas(status) {
+export function fetchIdeas(status, params = {}) {
     return (dispatch, getState, axios) =>
         axios
-            .get('/api/ideas', { params: { status } })
+            .get('/api/ideas', { params: { status, ...params } })
             .then(res => res.data)
             .then(ideas => dispatch(addIdeas(ideas)));
 }
