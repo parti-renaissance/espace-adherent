6e355b6ea5c6a516a8c23d753aba8a44deaabfc1
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-20 14:30:07 +0100

    [ADI] Add Reports Modal

---
+++ web/apps/atelier-des-idees/src/components/Modal/ReportsModal/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ReportsModal(props) {
+    // TODO: Complete reports components
+    return <div className="reports-modal">RAPPORTS</div>;
+}
+
+export default ReportsModal;
