bd0d46cc0ff2be84d4a7302f6901aaf193e71bfc
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-20 17:51:11 +0100

    [ADI] add layout en marche

--- web/apps/atelier-des-idees/src/components/Reports/index.jsx
+++ web/apps/atelier-des-idees/src/components/Reports/index.jsx
@@ -7,14 +7,12 @@ import { REPORTS_MODAL } from '../../constants/modalTypes';
 class Reports extends React.PureComponent {
     render() {
         return (
-            <div className="reports">
+            <div className="l__wrapper reports">
                 <div className="reports__first-section">
                     <h3 className="reports__first-section__title">
 						Nous n’avons pas attendu l’Atelier des Idées pour vous consulter
                     </h3>
-                    <p className="reports__first-section__text">
-						Consultez les rapports de consultations terminées
-                    </p>
+                    <p className="reports__first-section__text">Consultez les rapports de consultations terminées</p>
                     <button
                         className="reports__first-section__button button--primary"
                         onClick={() => this.props.showModal(REPORTS_MODAL)}
