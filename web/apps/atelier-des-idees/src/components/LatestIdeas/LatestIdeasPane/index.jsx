eda23f4e3102a470db085773e2dc5a48f98b64b4
Adrien Casanova <haitaar@gmail.com>
2018-12-20 17:51:01 +0100

    [ADI][WIP] add isLoading props to LatestIdeasPane component

--- web/apps/atelier-des-idees/src/components/LatestIdeas/LatestIdeasPane/index.jsx
+++ web/apps/atelier-des-idees/src/components/LatestIdeas/LatestIdeasPane/index.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const LatestIdeasPane = props => (
     <div className="latest-ideas__pane">
         {/* IdeasCardList */}
+        {props.isLoading && 'Loading...'}
         <div className="latest-ideas__pane__footer">
             <Link to={props.link} className="button button--tertiary latest-ideas__pane__footer__btn">
                 Voir toutes les propositions
@@ -15,10 +16,12 @@ const LatestIdeasPane = props => (
 
 LatestIdeasPane.defaultProps = {
     ideas: [],
+    isLoading: false,
 };
 
 LatestIdeasPane.propTypes = {
     ideas: PropTypes.array,
+    isLoading: PropTypes.bool,
     link: PropTypes.string.isRequired,
 };
 
