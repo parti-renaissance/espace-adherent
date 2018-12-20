9421107e415f2827a872f1b82e68cda7d79fd636
Adrien Casanova <haitaar@gmail.com>
2018-12-20 12:23:04 +0100

    [ADI] add axios as thunk argument

--- web/apps/atelier-des-idees/src/redux/store/configStore.prod.js
+++ web/apps/atelier-des-idees/src/redux/store/configStore.prod.js
@@ -1,7 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import axios from 'axios';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// axios config
+const baseURL = 'undefined' === typeof window.config ? process.env.REACT_APP_EM_API_HOST : window.config.em_api_host;
+const axiosInstance = axios.create({
+    baseURL,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
 
 export default store;
