9e0743d5100b5508f2d4d37a491edf15681acc11
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-20 10:29:05 +0100

    [ADI] jsx refactory

--- web/apps/atelier-des-idees/src/components/Banner/index.jsx
+++ web/apps/atelier-des-idees/src/components/Banner/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Mobile, NotMobile } from '../../helpers/responsive';
 
 class Banner extends React.PureComponent {
     constructor(props) {
@@ -10,40 +9,23 @@ class Banner extends React.PureComponent {
     render() {
         return (
             <div className="banner">
-                <NotMobile>
-                    <div className="banner__container__titles">
-                        <h3 className="banner__container__titles__title">{this.props.title}</h3>
-                        <h4 className="banner__container__titles__subtitle">{this.props.subtitle}</h4>
-                    </div>
-                    <div className="banner__container">
-                        <button
-                            className="banner__container__close button--transparent"
-                            onClick={() => this.props.onClose()}
-                        >
-                            <img src="/assets/img/icn_close-white.svg" />
-                        </button>
-                        <a href={this.props.link} className="banner__container__link button--secondary" target="_blank">
-                            {this.props.linkLabel}
-                        </a>
-                    </div>
-                </NotMobile>
-                <Mobile>
-                    <div className="banner__container__titles">
-                        <button
-                            className="banner__container__close button--transparent"
-                            onClick={() => this.props.onClose()}
-                        >
-                            <img src="/assets/img/icn_close-white.svg" />
-                        </button>
-                        <h3 className="banner__container__titles__title">{this.props.title}</h3>
-                        <h4 className="banner__container__titles__subtitle">{this.props.subtitle}</h4>
-                    </div>
-                    <div className="banner__container">
-                        <a href={this.props.link} className="banner__container__link button--secondary" target="_blank">
-                            {this.props.linkLabel}
-                        </a>
-                    </div>
-                </Mobile>
+                <div className="banner__container__close">
+                    <button
+                        className="banner__container__close button--transparent"
+                        onClick={() => this.props.onClose()}
+                    >
+                        <img src="/assets/img/icn_close-white.svg" />
+                    </button>
+                </div>
+                <div className="banner__container__titles">
+                    <h3 className="banner__container__titles__title">{this.props.title}</h3>
+                    <h4 className="banner__container__titles__subtitle">{this.props.subtitle}</h4>
+                </div>
+                <div className="banner__container">
+                    <a href={this.props.link} className="banner__container__link button--secondary" target="_blank">
+                        {this.props.linkLabel}
+                    </a>
+                </div>
             </div>
         );
     }
