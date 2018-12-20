8b1a1b7bc0e568e3fda7c08ea089df169629b73b
Adrien Casanova <haitaar@gmail.com>
2018-12-20 17:19:51 +0100

    [ADI] add loading reducer

--- web/apps/atelier-des-idees/src/redux/reducers/index.js
+++ web/apps/atelier-des-idees/src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import loading from './loading';
 import modal from './modal';
 import ideas from './ideas';
 
-const rootReducer = combineReducers({ modal, ideas });
+const rootReducer = combineReducers({ loading, modal, ideas });
 
 export default rootReducer;
