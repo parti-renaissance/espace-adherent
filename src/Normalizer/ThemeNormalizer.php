8d1fe7119fc02e38f79e5ee6950d15c8a0a9ef6a
Jeremie Samson <jeremie.samson76@gmail.com>
2018-12-21 14:34:46 +0100

    Add thumbnail on ideas

---
+++ src/Normalizer/ThemeNormalizer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Normalizer;
+
+use AppBundle\Entity\IdeasWorkshop\Theme;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ThemeNormalizer implements NormalizerInterface
+{
+    private $normalizer;
+    private $urlGenerator;
+
+    public function __construct(NormalizerInterface $normalizer, UrlGeneratorInterface $urlGenerator)
+    {
+        $this->normalizer = $normalizer;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        $data['thumbnail'] = $object->getImageName()
+            ? $this->urlGenerator->generate(
+                'asset_url',
+                ['path' => $object->getImagePath()],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            )
+            : null
+        ;
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof Theme;
+    }
+}
