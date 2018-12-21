ae26fe294e0da2d6d5511db21ea5a8e5f49ebfa8
Adrien Casanova <haitaar@gmail.com>
2018-12-21 19:03:16 +0100

    [ADI] use IdeaCard component in IdeaCardList

--- web/apps/atelier-des-idees/src/components/IdeaCardList/index.jsx
+++ web/apps/atelier-des-idees/src/components/IdeaCardList/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import IdeaCardSkeletonList from '../Skeletons/IdeaCardSkeletonList';
+import IdeaCard from '../IdeaCard';
 
 const IdeaCardList = (props) => {
     if (props.isLoading) {
@@ -8,9 +9,8 @@ const IdeaCardList = (props) => {
     }
     return (
         <div className="idea-card-list">
-            {/* TODO: use IdeaCard */}
             {props.ideas.map(idea => (
-                <div>{idea.name}</div>
+                <IdeaCard {...idea} />
             ))}
         </div>
     );
@@ -21,19 +21,7 @@ IdeaCardList.defaultProps = {
 };
 
 IdeaCardList.propTypes = {
-    ideas: PropTypes.arrayOf(
-        PropTypes.shape({
-            author: PropTypes.string.isRequired,
-            author_category: PropTypes.string.isRequired,
-            date: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired,
-            id: PropTypes.string.isRequired,
-            status: PropTypes.string.isRequired,
-            theme: PropTypes.array.isRequired,
-            thumbnail: PropTypes.string.isRequired,
-            title: PropTypes.string.isRequired,
-        })
-    ),
+    ideas: PropTypes.array.isRequired,
     isLoading: PropTypes.bool,
 };
 
