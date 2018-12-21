0c9cad21cae8e4c3dbeff61f3e13da71dc113ffb
Adrien Casanova <haitaar@gmail.com>
2018-12-21 19:02:38 +0100

    [ADI] adapt IdeaCard component to new ideas data structure

--- web/apps/atelier-des-idees/src/components/IdeaCard/index.jsx
+++ web/apps/atelier-des-idees/src/components/IdeaCard/index.jsx
@@ -1,40 +1,49 @@
 import React from 'react';
+import { ideaStatus } from '../../constants/api';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import VotingFooter from './VotingFooter';
 import ContributingFooter from './ContributingFooter';
 
+const AUTHOR_CATEGORY_NAMES = {
+    ADHERENT: 'Adhérent',
+    COMMITTEE: 'Comité',
+    QG: 'LaREM',
+};
+
 function IdeaCard(props) {
     return (
         <div className="idea-card">
             <div className="idea-card__main">
                 <div className="idea-card__content">
-                    <p className="idea-card__content__title">{props.title}</p>
+                    <p className="idea-card__content__title">{props.name}</p>
                     <div className="idea-card__content__infos">
                         <span className="idea-card__content__infos__author">
                             <span className="idea-card__content__infos__meta">Par</span>
-                            <span className="idea-card__content__infos__author__name">{props.author.name}</span>
+                            <span className="idea-card__content__infos__author__name">{`${props.author.first_name} ${
+                                props.author.last_name
+                            }`}</span>
                             <span className="idea-card__content__infos__author__separator" />
                             <span
                                 className={classnames(
                                     'idea-card__content__infos__author__type',
-                                    `idea-card__content__infos__author__type--${props.author.type.id}`
+                                    `idea-card__content__infos__author__type--${props.author_category}`
                                 )}
                             >
-                                {props.author.type.text}
+                                {AUTHOR_CATEGORY_NAMES[props.author_category]}
                             </span>
                         </span>
-                        <span className="idea-card__content__infos__date">
-                            {new Date(props.createdAt).toLocaleDateString()}
-                        </span>
+                        {/* <span className="idea-card__content__infos__date">
+                            {new Date(props.created_at).toLocaleDateString()}
+                                </span>*/}
                         <span className="idea-card__content__infos__contributors">
                             <img
                                 className="idea-card__content__infos__contributors__icon"
                                 src="/assets/img/icn_20px_contributors.svg"
                             />
                             <span className="idea-card__content__infos__contributors__text">
-                                {props.nbContributors}
+                                {props.contributors_count}
                             </span>
                         </span>
                         <span className="idea-card__content__infos__comments">
@@ -42,14 +51,15 @@ function IdeaCard(props) {
                                 className="idea-card__content__infos__contributors__icon"
                                 src="/assets/img/icn_20px_comments.svg"
                             />
-                            <span className="idea-card__content__infos__contributors__text">{props.nbComments}</span>
+                            <span className="idea-card__content__infos__contributors__text">
+                                {props.comments_count}
+                            </span>
                         </span>
                     </div>
                     <p className="idea-card__content__description">{props.description}</p>
                     <ul className="idea-card__content__tags">
-                        {props.tags.map(tag => (
-                            <li className="idea-card__content__tags__item">{tag}</li>
-                        ))}
+                        <li className="idea-card__content__tags__item">{props.category.name}</li>
+                        <li className="idea-card__content__tags__item">{props.theme.name}</li>
                     </ul>
                 </div>
                 <div className="idea-card__container">
@@ -63,26 +73,25 @@ function IdeaCard(props) {
 }
 
 IdeaCard.defaultProps = {
-    nbComments: 0,
-    nbContributors: 0,
+    comments_count: 0,
+    contributors_count: 0,
 };
 
 IdeaCard.propTypes = {
     author: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        type: PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            text: PropTypes.string.isRequired,
-        }),
+        first_name: PropTypes.string.isRequired,
+        last_name: PropTypes.string.isRequired,
     }).isRequired,
+    author_category: PropTypes.string.isRequired,
     thumbnail: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired, // ISO UTC
+    created_at: PropTypes.string.isRequired, // ISO UTC
     description: PropTypes.string.isRequired,
-    nbComments: PropTypes.number,
-    nbContributors: PropTypes.number,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    comments_count: PropTypes.number,
+    contributors_count: PropTypes.number,
     tags: PropTypes.arrayOf(PropTypes.string), // array of ids
-    title: PropTypes.string.isRequired,
-    status: PropTypes.oneOf(['pending', 'approuved']).isRequired,
+    status: PropTypes.oneOf(ideaStatus).isRequired,
 };
 
 export default IdeaCard;
