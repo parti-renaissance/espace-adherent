11ad2dee79dec3144e0fa1c0cdcd658ae37c93b9
Adrien Casanova <haitaar@gmail.com>
2018-12-21 19:03:16 +0100

    [ADI] use IdeaCard component in IdeaCardList

--- web/apps/atelier-des-idees/src/components/Reports/index.jsx
+++ web/apps/atelier-des-idees/src/components/Reports/index.jsx
@@ -10,14 +10,14 @@ class Reports extends React.PureComponent {
             <div className="l__wrapper reports">
                 <div className="reports__first-section">
                     <h3 className="reports__first-section__title">
-						Nous n’avons pas attendu l’Atelier des Idées pour vous consulter
+                        Nous n’avons pas attendu l’Atelier des Idées pour vous consulter
                     </h3>
                     <p className="reports__first-section__text">Consultez les rapports de consultations terminées</p>
                     <button
-                        className="reports__first-section__button button--primary"
+                        className="reports__first-section__button button button--primary"
                         onClick={() => this.props.showModal(REPORTS_MODAL)}
                     >
-						Je lis les rapports
+                        Je lis les rapports
                     </button>
                 </div>
                 <div className="reports__second-section">
