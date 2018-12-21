dcccc1b6520dee9922b46f11a3559dab71f2f0b9
Adrien Casanova <haitaar@gmail.com>
2018-12-21 16:27:50 +0100

    [ADI] create Header component

---
+++ web/apps/atelier-des-idees/src/components/Header/Header.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Header from '.';
+
+storiesOf('Header', module).add('default', () => <Header />);
---
+++ web/apps/atelier-des-idees/src/components/Header/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import { NavLink } from 'react-router-dom';
+import { NotMobile, Mobile } from '../../helpers/responsive';
+import Button from '../Button';
+
+class Header extends React.PureComponent {
+    render() {
+        // const menuItems = [<NavLink to="/atelier-des-idees">Vue d'ensemble</NavLink>];
+        const menuItems = [
+            <NavLink className="header__item" to="/atelier-des-idees">
+                Vue d'ensemble
+            </NavLink>,
+            <button className="header__item header__button">Mes notes</button>,
+            <button className="header__item header__button">Mes contributions</button>,
+        ];
+        return (
+            <section className="header">
+                <div className="header__inner l__wrapper">
+                    <NotMobile>
+                        <div className="header__nav">{menuItems}</div>
+                        <Button className="button--primary" label="Je rédige mon idée" />
+                    </NotMobile>
+                    {/* <Mobile>
+                <ScrollMenu
+                    alignCenter={false}
+                    arrowRight={<div>{'>'}</div>}
+                    data={menuItems}
+                    hideArrows={true}
+                    hideSingleArrow={true}
+                />
+            </Mobile>*/}
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Header;
