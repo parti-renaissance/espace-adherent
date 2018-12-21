fc3098f9821aee2f7b5dff8a494520e55d9a5baa
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-21 16:33:46 +0100

    [ADI] update IdeaCard for the footer

--- web/apps/atelier-des-idees/src/components/IdeaCard/IdeaCard.stories.js
+++ web/apps/atelier-des-idees/src/components/IdeaCard/IdeaCard.stories.js
@@ -15,9 +15,10 @@ const props = {
     nbComments: 122,
     nbContributors: 4,
     description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie sapien eu orci congue sollicitudin. Sed malesuada nisi sed diam tristique, ullamcorper fermentum massa euismod. Vivamus a augue vitae nibh scelerisque…',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie sapien eu orci congue sollicitudin. Sed malesuada nisi sed diam tristique, ullamcorper fermentum massa euismod. Vivamus a augue vitae nibh scelerisque…',
     tags: ['education', 'civil-rights'],
     title: 'Réduire le nombre d’élèves dans les classes dans les quartiers défavorisés',
+    status: 'approved',
 };
 
 storiesOf('IdeaCard', module)
