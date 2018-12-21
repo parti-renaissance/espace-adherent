87309b4def08199ee5fe662b34705722f0a2e157
Adrien Casanova <haitaar@gmail.com>
2018-12-21 11:45:57 +0100

    [ADI] add IdeaCardSkeletonList to LatestIdeasPane when loading

--- web/apps/atelier-des-idees/src/components/LatestIdeas/LatestIdeasPane/index.jsx
+++ web/apps/atelier-des-idees/src/components/LatestIdeas/LatestIdeasPane/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import IdeaCardSkeletonList from '../../Skeletons/IdeaCardSkeletonList';
 
 const LatestIdeasPane = props => (
     <div className="latest-ideas__pane">
         {/* IdeasCardList */}
-        {props.isLoading && 'Loading...'}
+        {props.isLoading && <IdeaCardSkeletonList nbItems={5} />}
         <div className="latest-ideas__pane__footer">
             <Link to={props.link} className="button button--tertiary latest-ideas__pane__footer__btn">
                 Voir toutes les propositions
