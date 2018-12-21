5728290892d3984b356229da49b6cb0b740f4237
Adrien Casanova <haitaar@gmail.com>
2018-12-21 11:51:03 +0100

    [ADI] adapt axios use to api

--- web/apps/atelier-des-idees/src/redux/store/configStore.prod.js
+++ web/apps/atelier-des-idees/src/redux/store/configStore.prod.js
@@ -7,6 +7,7 @@ import rootReducer from '../reducers';
 const baseURL = 'undefined' === typeof window.config ? process.env.REACT_APP_EM_API_HOST : window.config.em_api_host;
 const axiosInstance = axios.create({
     baseURL,
+    headers: { Accept: 'application/json' },
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
