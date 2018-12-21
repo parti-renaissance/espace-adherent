d621c0a9379589622ece4da83461b689eb7cda20
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-21 19:48:21 +0100

    [ADI] Create container for reports

--- web/apps/atelier-des-idees/src/components/Reports/index.jsx
+++ web/apps/atelier-des-idees/src/components/Reports/index.jsx
@@ -1,23 +1,53 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { showModal } from '../../redux/actions/modal';
 
 import { REPORTS_MODAL } from '../../constants/modalTypes';
 
+// TODO: api call reports
+const mockReports = {
+    reports: [
+        {
+            file: '/',
+            fileName: 'document-5.pdf',
+            size: '1.2 Mb',
+        },
+        {
+            file: '/',
+            fileName: 'document-5.pdf',
+            size: '1.2 Mb',
+        },
+        {
+            file: '/',
+            fileName: 'document-5.pdf',
+            size: '1.2 Mb',
+        },
+        {
+            file: '/',
+            fileName: 'document-5.pdf',
+            size: '1.2 Mb',
+        },
+        {
+            file: '/',
+            fileName: 'document-5.pdf',
+            size: '1.2 Mb',
+        },
+    ],
+};
 class Reports extends React.PureComponent {
     render() {
         return (
             <div className="l__wrapper reports">
                 <div className="reports__first-section">
                     <h3 className="reports__first-section__title">
-                        Nous n’avons pas attendu l’Atelier des Idées pour vous consulter
+						Nous n’avons pas attendu l’Atelier des Idées pour vous consulter
                     </h3>
-                    <p className="reports__first-section__text">Consultez les rapports de consultations terminées</p>
+                    <p className="reports__first-section__text">
+						Consultez les rapports de consultations terminées
+                    </p>
                     <button
                         className="reports__first-section__button button button--primary"
                         onClick={() => this.props.showModal(REPORTS_MODAL)}
                     >
-                        Je lis les rapports
+						Je lis les rapports
                     </button>
                 </div>
                 <div className="reports__second-section">
@@ -29,7 +59,4 @@ class Reports extends React.PureComponent {
     }
 }
 
-export default connect(
-    null,
-    { showModal }
-)(Reports);
+export default Reports;
