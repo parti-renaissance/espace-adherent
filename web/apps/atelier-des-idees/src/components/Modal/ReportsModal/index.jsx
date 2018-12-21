2b97d7b6467e655b5c4ddded3ce10100ff3fe4e3
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-21 19:02:09 +0100

    [ADI] Complete reports modal

--- web/apps/atelier-des-idees/src/components/Modal/ReportsModal/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/ReportsModal/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ReportsModal(props) {
-    // TODO: Complete reports components
-    return <div className="reports-modal">RAPPORTS</div>;
+    return (
+        <div className="reports-modal">
+            <h2 className="reports-modal__title">Je lis les {props.reports.length} rapports</h2>
+            <p className="reports-modal__subtitle">Nous n’avons pas attendu l’Atelier des Idées pour vous consulter</p>
+            {props.reports.map((report, index) => (
+                <a className="reports-modal__report" key={index} href={report.file} download>
+                    <img className="reports-modal__report__pdf-preview" src="/assets/img/pdf_preview.svg" />
+                    <div className="reports-modal__report__content">
+                        <span className="reports-modal__report__content__file"> {report.fileName}</span>
+                        <span className="reports-modal__report__content__size"> {report.size}</span>
+                    </div>
+                </a>
+            ))}
+        </div>
+    );
 }
+
+ReportsModal.propTypes = {
+    reports: PropTypes.arrayOf(
+        PropTypes.shape({
+            file: PropTypes.string.isRequired,
+            fileName: PropTypes.string.isRequired,
+            size: PropTypes.string.isRequired,
+        }).isRequired
+    ).isRequired,
+};
 
 export default ReportsModal;
