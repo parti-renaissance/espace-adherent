bf5bcc79d41ce2188b28111be5a79c2ade5b1643
Adrien Casanova <haitaar@gmail.com>
2018-12-24 16:04:36 +0100

    [ADI] add metadate to ideas reducer

--- web/apps/atelier-des-idees/src/redux/actions/ideas.js
+++ web/apps/atelier-des-idees/src/redux/actions/ideas.js
@@ -1,5 +1,5 @@
 import { action } from '../helpers/actions';
 import { SET_IDEAS, ADD_IDEAS } from '../constants/actionTypes';
 
-export const setIdeas = (ideas = []) => action(SET_IDEAS, { ideas });
-export const addIdeas = (ideas = []) => action(ADD_IDEAS, { ideas });
+export const setIdeas = (items = [], metadata = {}) => action(SET_IDEAS, { items, metadata });
+export const addIdeas = (items = [], metadata = {}) => action(ADD_IDEAS, { items, metadata });
