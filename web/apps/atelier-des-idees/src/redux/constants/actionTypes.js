8b5f77e709adc6bf6d15e5c3f5d84ecc7e9b4a0b
Adrien Casanova <haitaar@gmail.com>
2018-12-24 18:07:46 +0100

    [ADI] rename forgotten action type

--- web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
+++ web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
@@ -10,11 +10,9 @@ export const SET_IDEAS = 'SET_IDEAS';
 export const ADD_IDEAS = 'ADD_IDEAS';
 
 // pinned
-export const FETCH_CONSULTATION_PINNED = createRequestTypes(
-    'FETCH_CONSULTATION_PINNED'
-);
+export const FETCH_CONSULTATION_PINNED = createRequestTypes('FETCH_CONSULTATION_PINNED');
 export const SHOW_CONSULTATION_PINNED = 'SHOW_CONSULTATION_PINNED';
 export const HIDE_CONSULTATION_PINNED = 'HIDE_CONSULTATION_PINNED';
 // reports
 export const FETCH_REPORTS = createRequestTypes('FETCH_REPORTS');
-export const SET_REPORTS = 'ADD_REPORTS';
+export const SET_REPORTS = 'SET_REPORTS';
