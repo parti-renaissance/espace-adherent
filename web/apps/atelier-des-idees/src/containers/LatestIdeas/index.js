dd2410bb6d643463113bbe6cfc39ba0fed730c68
Adrien Casanova <haitaar@gmail.com>
2018-12-24 09:41:26 +0100

    [ADI] sort ideas by creation date in LatestIdeas container

--- web/apps/atelier-des-idees/src/containers/LatestIdeas/index.js
+++ web/apps/atelier-des-idees/src/containers/LatestIdeas/index.js
@@ -4,6 +4,22 @@ import LatestIdeas from '../../components/LatestIdeas';
 import { selectLoadingState } from '../../redux/selectors/loading';
 import { selectIdeasWithStatus } from '../../redux/selectors/ideas';
 
+/**
+ * Sort ideas by creation date DESC
+ * @param {array} ideas Ideas to be sorted
+ */
+function sortIdeasByDate(ideas = []) {
+    return ideas.sort((a, b) => {
+        if (a.created_at < b.created_at) {
+            return 1;
+        }
+        if (a.created_at > b.created_at) {
+            return -1;
+        }
+        return 0;
+    });
+}
+
 const mapStateToProps = (state) => {
     const isLoadingFinalizedIdeas = selectLoadingState(state, 'FETCH_IDEAS_FINALIZED');
     const isLoadingPendingIdeas = selectLoadingState(state, 'FETCH_IDEAS_PENDING');
@@ -12,8 +28,8 @@ const mapStateToProps = (state) => {
     const pendingIdeas = selectIdeasWithStatus(state, ideaStatus.PENDING);
     return {
         ideas: {
-            finalized: { isLoading: isLoadingFinalizedIdeas, items: finalizedIdeas },
-            pending: { isLoading: isLoadingPendingIdeas, items: pendingIdeas },
+            finalized: { isLoading: isLoadingFinalizedIdeas, items: sortIdeasByDate(finalizedIdeas) },
+            pending: { isLoading: isLoadingPendingIdeas, items: sortIdeasByDate(pendingIdeas) },
         },
     };
 };
