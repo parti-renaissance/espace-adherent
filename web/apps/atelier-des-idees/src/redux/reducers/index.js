3c64457339312ef5f4f01b4021ccf46f3f6d0e49
Adrien Casanova <haitaar@gmail.com>
2018-12-24 10:15:13 +0100

    [ADI] create pinned reducer and associated action creators

--- web/apps/atelier-des-idees/src/redux/reducers/index.js
+++ web/apps/atelier-des-idees/src/redux/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import loading from './loading';
 import modal from './modal';
 import ideas from './ideas';
+import pinned from './pinned';
 
-const rootReducer = combineReducers({ loading, modal, ideas });
+const rootReducer = combineReducers({ loading, modal, ideas, pinned });
 
 export default rootReducer;
