c75bde2cd03e6001ce80dda0397508af6808deb5
Adrien Casanova <haitaar@gmail.com>
2018-12-24 10:51:17 +0100

    [ADI] add extraInfo prop to Banner component

--- web/apps/atelier-des-idees/src/components/Banner/Banner.stories.js
+++ web/apps/atelier-des-idees/src/components/Banner/Banner.stories.js
@@ -6,10 +6,11 @@ import Banner from '.';
 const props = {
     title: 'Répondez à notre consultation sur les retraites !',
     subtitle: 'Du 5 aout 2018 au 29 septembre 2018',
-    linkLabel: 'Je participe (2 min)',
+    linkLabel: 'Je participe',
     link: 'http://google.fr',
 };
 
 storiesOf('Banner', module)
     .addParameters({ jest: ['Banner'] })
-    .add('default', () => <Banner {...props} onClose={action('close')} />);
+    .add('default', () => <Banner {...props} onClose={action('close')} />)
+    .add('with extra info', () => <Banner {...props} extraInfo="2 min." onClose={action('close')} />);
