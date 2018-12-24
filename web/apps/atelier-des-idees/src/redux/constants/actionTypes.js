11c33644a855319fce707203020c6c28a8c36bfd
Adrien Casanova <haitaar@gmail.com>
2018-12-24 10:15:13 +0100

    [ADI] create pinned reducer and associated action creators

--- web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
+++ web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
@@ -8,3 +8,8 @@ export const HIDE_MODAL = 'HIDE_MODAL';
 export const FETCH_IDEAS = createRequestTypes('FETCH_IDEAS');
 export const SET_IDEAS = 'SET_IDEAS';
 export const ADD_IDEAS = 'ADD_IDEAS';
+
+// pinned
+export const FETCH_CONSULTATION_PINNED = createRequestTypes('FETCH_CONSULTATION_PINNED');
+export const SHOW_CONSULTATION_PINNED = 'SHOW_CONSULTATION_PINNED';
+export const HIDE_CONSULTATION_PINNED = 'HIDE_CONSULTATION_PINNED';
