e73adf1021fa9586ad78c29e5fed98cac69bd3ad
Adrien Casanova <haitaar@gmail.com>
2018-12-24 19:16:28 +0100

    [ADI] add Consult page

--- web/apps/atelier-des-idees/src/redux/thunk/navigation.js
+++ web/apps/atelier-des-idees/src/redux/thunk/navigation.js
@@ -19,5 +19,10 @@ export function initHomePage() {
 
 export function initContributePage() {
     const params = { limit: 10, 'order[\'created_at\']': 'DESC' };
+    return dispatch => dispatch(fetchIdeas(ideaStatus.PENDING, params, true));
+}
+
+export function initConsultPage() {
+    const params = { limit: 10, 'order[\'created_at\']': 'DESC' };
     return dispatch => dispatch(fetchIdeas(ideaStatus.FINALIZED, params, true));
 }
