65e2b8e2ee6f5d55a005b6fc5834eb9a32dbd758
Adrien Casanova <haitaar@gmail.com>
2018-12-24 10:49:21 +0100

    [ADI] add pinned selectors

---
+++ web/apps/atelier-des-idees/src/redux/selectors/pinned.js
@@ -0,0 +1,4 @@
+import { getShowConsultationPinned, getConsultationPinned } from '../reducers/pinned';
+
+export const selectShowConsultationPinned = state => getShowConsultationPinned(state.pinned);
+export const selectConsultationPinned = state => getConsultationPinned(state.pinned);
