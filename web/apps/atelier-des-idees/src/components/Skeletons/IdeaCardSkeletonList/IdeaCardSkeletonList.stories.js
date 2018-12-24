e0878e53f8e75fe604d0092dcf49356bc2e9a234
Adrien Casanova <haitaar@gmail.com>
2018-12-24 17:52:19 +0100

    [ADI] add mode prop to IdeaCardSkeletonList

--- web/apps/atelier-des-idees/src/components/Skeletons/IdeaCardSkeletonList/IdeaCardSkeletonList.stories.js
+++ web/apps/atelier-des-idees/src/components/Skeletons/IdeaCardSkeletonList/IdeaCardSkeletonList.stories.js
@@ -5,4 +5,5 @@ import IdeaCardSkeletonList from '.';
 storiesOf('IdeaCardSkeletonList', module)
     .addDecorator(story => <div style={{ background: '#fafbfc' }}>{story()}</div>)
     .add('default', () => <IdeaCardSkeletonList />)
-    .add('with nbItems', () => <IdeaCardSkeletonList nbItems={5} />);
+    .add('with nbItems', () => <IdeaCardSkeletonList nbItems={5} />)
+    .add('grid mode', () => <IdeaCardSkeletonList nbItems={5} mode="grid" />);
