bdc2cfa24a0aad0902c01d35dda27401c7d8e7e9
Adrien Casanova <haitaar@gmail.com>
2018-12-24 10:52:08 +0100

    [ADI] create fetchConsultationPinned thunk

--- web/apps/atelier-des-idees/src/redux/thunk/navigation.js
+++ web/apps/atelier-des-idees/src/redux/thunk/navigation.js
@@ -1,10 +1,14 @@
 import { fetchIdeas } from './ideas';
+import { fetchConsultationPinned } from './pinned';
 import { ideaStatus } from '../../constants/api';
 
 export function initHomePage() {
     const params = { limit: 5, 'order[\'created_at\']': 'DESC' };
     return dispatch =>
         Promise.all([
+            // consultation pinned
+            dispatch(fetchConsultationPinned()),
+            // ideas
             dispatch(fetchIdeas(ideaStatus.FINALIZED, params)),
             dispatch(fetchIdeas(ideaStatus.PENDING, params)),
         ]);
