35d4e02785910da9885bc946e72f4176ba820d2e
Rémi Gardien <remi@gardien.biz>
2018-12-26 17:25:00 +0100

    Add swagger support

---
+++ src/Swagger/PaginatorSwagger.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Swagger;
+
+class PaginatorSwagger
+{
+    public const DEFINITION = [
+        'type' => 'object',
+        'properties' => [
+            'total_items' => [
+                'type' => 'integer',
+            ],
+            'items_per_page' => [
+                'type' => 'integer',
+            ],
+            'count' => [
+                'type' => 'integer',
+            ],
+            'current_page' => [
+                'type' => 'integer',
+            ],
+            'last_page' => [
+                'type' => 'integer',
+            ],
+        ],
+    ];
+
+    public static function getPaginatedResponseFor(string $definition): array
+    {
+        $model = substr($definition, 0, strpos($definition, '-'));
+
+        return [
+            'description' => "Paginated $model collection response",
+            'schema' => [
+                'type' => 'object',
+                'properties' => [
+                    'metadatas' => self::DEFINITION,
+                    'items' => [
+                        'type' => 'array',
+                        'items' => [
+                            '$ref' => "#/definitions/$definition",
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
