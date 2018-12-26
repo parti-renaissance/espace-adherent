07053480bbb2a069aaf42252505f0378a9acb0e5
Adrien Casanova <haitaar@gmail.com>
2018-12-26 18:45:30 +0100

    [ADI] add TextEditor to question blocks

--- web/apps/atelier-des-idees/src/pages/CreateIdea/index.jsx
+++ web/apps/atelier-des-idees/src/pages/CreateIdea/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TextEditor from '../../components/TextEditor';
 
-const QUESTIONS = [
+const FIRST_QUESTIONS = [
     {
         label: 'Constat',
         question: 'Quel problème souhaitez vous résoudre ?',
         canCollapse: false,
+        placeholder: 'Expliquez le problème que vous identifiez et espérez pouvoir remédier.',
     },
     {
         label: 'Solution',
         question: 'Quelle réponse votre idée apporte-t-elle ?',
         canCollapse: false,
+        placeholder: 'Expliquez comment votre proposition répond concrètement au problème.',
     },
     {
         label: 'Comparaison',
@@ -24,6 +27,31 @@ const QUESTIONS = [
     },
 ];
 
+const SECOND_QUESTIONS = [
+    {
+        label: 'Droit',
+        question: 'Votre idée suppose t-elle de changer le droit ?',
+        canCollapse: false,
+        placeholder:
+            'Expliquez si votre idée nécessite - ou non - de changer le droit en vigueur. Si oui, idéalement, précisez ce qu’il faudrait changer.',
+    },
+    {
+        label: 'Budget',
+        question: 'Votre idée a-t-elle un impact financier ?',
+        canCollapse: false,
+    },
+    {
+        label: 'Environnement',
+        question: 'Votre idée a t-elle un impact écologique ?',
+        canCollapse: true,
+    },
+    {
+        label: 'Égalité hommes-femmes',
+        question: 'Votre idée a t-elle un impact sur l’égalité entre les femmes et les hommes ?',
+        canCollapse: true,
+    },
+];
+
 function QuestionBlockHeader({ label, question, nbQuestion }) {
     return (
         <h3 className="question-block-header">
@@ -33,10 +61,15 @@ function QuestionBlockHeader({ label, question, nbQuestion }) {
     );
 }
 
-function renderQuestionBlock({ label, question }, nbQ) {
+function renderQuestionBlock({ label, question, placeholder }, nbQ) {
     return (
         <div className="question-block">
             <QuestionBlockHeader label={label} question={question} nbQuestion={nbQ} />
+            <TextEditor
+                maxLength={1700}
+                onChange={htmlContent => console.warn(htmlContent)}
+                placeholder={placeholder}
+            />
         </div>
     );
 }
@@ -49,10 +82,13 @@ function CreateIdeaPage(props) {
                 <div className="create-idea-page__content__main l__wrapper--medium">
                     <section className="create-idea-page__start-section">
                         <h2>Quelles sont les caractéristiques principales de votre idée ?</h2>
-                        {QUESTIONS.map((question, index) => renderQuestionBlock(question, index + 1))}
+                        {FIRST_QUESTIONS.map((question, index) => renderQuestionBlock(question, index + 1))}
                     </section>
                     <section className="create-idea-page__continue-section">
                         <h2>Votre idée peut-elle être mise en oeuvre ?</h2>
+                        {SECOND_QUESTIONS.map((question, index) =>
+                            renderQuestionBlock(question, FIRST_QUESTIONS.length + index + 1)
+                        )}
                     </section>
                     <div className="create-idea-page__content__footer">FOOTER</div>
                 </div>
