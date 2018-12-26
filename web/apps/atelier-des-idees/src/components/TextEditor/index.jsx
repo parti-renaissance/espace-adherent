40b8816730b67bcd2ad4306225193a053ce67b96
Adrien Casanova <haitaar@gmail.com>
2018-12-26 15:05:22 +0100

    [ADI] add onChange prop to TextEditor component

--- web/apps/atelier-des-idees/src/components/TextEditor/index.jsx
+++ web/apps/atelier-des-idees/src/components/TextEditor/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Editor } from 'react-draft-wysiwyg';
 import { EditorState } from 'draft-js';
+import { stateToHTML } from 'draft-js-export-html';
 
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
@@ -21,12 +22,25 @@ const toolbar = {
     },
 };
 
+/**
+ * Custom WYSIWYG editor based on react-draft-wysiwyg and draft-js
+ * See more https://jpuri.github.io/react-draft-wysiwyg
+ */
 class TextEditor extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             editorState: EditorState.createEmpty(),
         };
+        this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    }
+
+    onEditorStateChange(editorState) {
+        // update state
+        this.setState({ editorState });
+        // convert content state to html and send
+        const htmlContent = stateToHTML(editorState.getCurrentContent());
+        this.props.onChange(htmlContent);
     }
 
     render() {
@@ -38,9 +52,7 @@ class TextEditor extends React.Component {
                 editorClassName="text-editor__content"
                 toolbarClassName="text-editor__toolbar"
                 wrapperClassName="text-editor"
-                onEditorStateChange={(editorState) => {
-                    this.setState({ editorState });
-                }}
+                onEditorStateChange={this.onEditorStateChange}
             />
         );
     }
@@ -51,6 +63,7 @@ TextEditor.defaultProp = {
 };
 
 TextEditor.propTypes = {
+    onChange: PropTypes.func.isRequired,
     placeholder: PropTypes.string,
 };
 
