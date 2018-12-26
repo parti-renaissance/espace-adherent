816c4462f00bc5ef55200a88227cd9e72a15f2b2
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-26 15:24:26 +0100

    [ADI] Proposal criteria integration

--- web/apps/atelier-des-idees/src/pages/Propose/index.jsx
+++ web/apps/atelier-des-idees/src/pages/Propose/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ProposalSteps from './ProposalSteps';
+import ProposalCriteria from './ProposalCriteria';
 
 function Propose(props) {
     return (
         <div className="propose-page">
             <div className="l__wrapper">
                 <ProposalSteps />
+                <ProposalCriteria />
             </div>
         </div>
     );
