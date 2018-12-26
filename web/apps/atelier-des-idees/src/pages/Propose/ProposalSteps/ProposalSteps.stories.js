e036c7da6403f8a4e7ee7ec44a6ba5fb225536e6
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-26 15:24:26 +0100

    [ADI] Proposal criteria integration

--- web/apps/atelier-des-idees/src/pages/Propose/ProposalSteps/ProposalSteps.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ProposalSteps from '.';
-
-storiesOf('ProposalSteps', module)
-    .addParameters({ jest: ['ProposalSteps'] })
-    .add('default', () => <ProposalSteps />);
