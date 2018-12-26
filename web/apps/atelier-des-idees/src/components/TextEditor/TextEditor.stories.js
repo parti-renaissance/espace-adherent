bbe38e1b6d9131fa4322d20cd19bf4157c5cf014
Adrien Casanova <haitaar@gmail.com>
2018-12-26 15:12:27 +0100

    [ADI] add initialContent prop

--- web/apps/atelier-des-idees/src/components/TextEditor/TextEditor.stories.js
+++ web/apps/atelier-des-idees/src/components/TextEditor/TextEditor.stories.js
@@ -3,7 +3,10 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import TextEditor from '.';
 
+const content = '<p><strong>Mes courses :</strong></p><ul><li>Tomates</li><li>Riz</li><li>Mousse au chocolat</li></ul>';
+
 storiesOf('TextEditor', module)
     .addParameters({ jest: ['TextEditor'] })
     .add('default', () => <TextEditor onChange={action('editor change')} />)
-    .add('with placeholder', () => <TextEditor placeholder="Coucou toi" onChange={action('editor change')} />);
+    .add('with placeholder', () => <TextEditor placeholder="Coucou toi" onChange={action('editor change')} />)
+    .add('with initial content', () => <TextEditor initialContent={content} onChange={action('editor change')} />);
