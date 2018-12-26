0b2c080b94a7f4aca208be791ad99b3922ba5a47
Adrien Casanova <haitaar@gmail.com>
2018-12-26 15:05:22 +0100

    [ADI] add onChange prop to TextEditor component

--- web/apps/atelier-des-idees/src/components/TextEditor/TextEditor.stories.js
+++ web/apps/atelier-des-idees/src/components/TextEditor/TextEditor.stories.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import TextEditor from '.';
 
 storiesOf('TextEditor', module)
     .addParameters({ jest: ['TextEditor'] })
-    .add('default', () => <TextEditor />)
-    .add('with placeholder', () => <TextEditor placeholder="Coucou toi" />);
+    .add('default', () => <TextEditor onChange={action('editor change')} />)
+    .add('with placeholder', () => <TextEditor placeholder="Coucou toi" onChange={action('editor change')} />);
