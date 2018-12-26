d22fbee4644ff2c856ec189833589aaa67251c71
Adrien Casanova <haitaar@gmail.com>
2018-12-26 17:59:07 +0100

    [ADI] create CreateIdea page

---
+++ web/apps/atelier-des-idees/src/pages/CreateIdea/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CreateIdeaPage(props) {
+    return (
+        <div className="create-idea-page">
+            <div className="create-idea-page__header l__wrapper">HEADER</div>
+            <div className="create-idea-page__content">
+                <div className="l__wrapper--medium">CONTENT</div>
+            </div>
+        </div>
+    );
+}
+
+export default CreateIdeaPage;
