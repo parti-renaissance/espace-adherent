9be709ed8c736eec7e4c7f16419559403b22f50c
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-27 17:28:02 +0100

    [ADI] structure firstform

--- web/apps/atelier-des-idees/src/components/Modal/FormSubmit/FirstForm/FirstForm.stories.js
+++ web/apps/atelier-des-idees/src/components/Modal/FormSubmit/FirstForm/FirstForm.stories.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import FirstForm from '.';
 
+const props = {
+    themeOptions: [
+        { value: 'agriculture', label: 'Agriculture' },
+        { value: 'education', label: 'Education' },
+        { value: 'culture', label: 'Culture' },
+        { value: 'defense', label: 'Défense' },
+        { value: 'parity', label: 'Parité' },
+    ],
+    localityOptions: [
+        { value: 'national', label: 'National' },
+        { value: 'european', label: 'Européen' },
+    ],
+}
+
 storiesOf('FirstForm', module)
     .addParameters({ jest: ['FirstForm'] })
-    .add('default', () => <FirstForm />);
+    .add('default', () => <FirstForm {...props}/>);
