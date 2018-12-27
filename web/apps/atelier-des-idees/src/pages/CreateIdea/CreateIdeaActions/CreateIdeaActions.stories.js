b801b553ec74196293ad213ecd63c189b4a2082f
Adrien Casanova <haitaar@gmail.com>
2018-12-27 11:26:11 +0100

    [ADI] create CreateIdeaActions component

---
+++ web/apps/atelier-des-idees/src/pages/CreateIdea/CreateIdeaActions/CreateIdeaActions.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import CreateIdeaActions from '.';
+
+storiesOf('CreateIdeaActions', module)
+    .addParameters({ jest: ['CreateIdeaActions'] })
+    .add('default', () => (
+        <CreateIdeaActions
+            onDeleteClicked={action('delete clicked')}
+            onPublishClicked={action('publish clicked')}
+            onSaveClicked={action('save clicked')}
+        />
+    ));
