bd2f0321d5d743c50aec2a42b78c912631b1ba27
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-27 10:49:31 +0100

    [ADI] feature collapse + style

--- web/apps/atelier-des-idees/src/components/Collapse/index.jsx
+++ web/apps/atelier-des-idees/src/components/Collapse/index.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import PropTyes from 'prop-types';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-function Collapse(props) {
-    return <div className="collapse">Collapse</div>;
+class Collapse extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isCollapsed: false,
+        };
+    }
+
+    render() {
+        const Component = this.props.title.component;
+        return (
+            <div
+                className={classNames('collapse', {
+                    'collapse--close': !this.state.isCollapsed,
+                    'collapse--open': this.state.isCollapsed,
+                })}
+            >
+                <div className="collapse__container">
+                    {'function' === typeof Component ? <Component /> : Component}
+                    {this.state.isCollapsed ? (
+                        <button
+                            className="button collapse__container__btn--open"
+                            onClick={() =>
+                                this.setState({ isCollapsed: !this.state.isCollapsed })
+                            }
+                        >
+							-
+                        </button>
+                    ) : (
+                        <button
+                            className="button collapse__container__btn--close"
+                            onClick={() =>
+                                this.setState({ isCollapsed: !this.state.isCollapsed })
+                            }
+                        >
+							+
+                        </button>
+                    )}
+                </div>
+                {this.props.children && this.state.isCollapsed && (
+                    <div>{this.props.children}</div>
+                )}
+            </div>
+        );
+    }
 }
+
+Collapse.propTypes = {
+    title: PropTypes.shape({
+        component: PropTypes.oneOfType([PropTypes.node.isRequired, PropTypes.func]),
+    }),
+};
 
 export default Collapse;
