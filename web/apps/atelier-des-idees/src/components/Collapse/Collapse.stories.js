b41591c25e3dcf1be62210c1b3064ed54d4f0aaf
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-27 10:49:31 +0100

    [ADI] feature collapse + style

--- web/apps/atelier-des-idees/src/components/Collapse/Collapse.stories.js
+++ web/apps/atelier-des-idees/src/components/Collapse/Collapse.stories.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Collapse from '.';
 
+const props = {
+    title: {
+        component: () => (
+            <div>5. Droit : Votre idée suppose t-elle de changer le droit ? </div>
+        ),
+    },
+};
+
 storiesOf('Collapse', module)
     .addParameters({ jest: ['Collapse'] })
-    .add('default', () => <Collapse />);
+    .add('default', () => <Collapse {...props} />)
+    .add('with children', () => (
+        <Collapse {...props}>
+            <div>TEST</div>
+        </Collapse>
+    ));
