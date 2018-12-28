0f04fe3c4881289331009d4f949fbca0ee1f0399
Adrien Casanova <haitaar@gmail.com>
2018-12-28 15:18:32 +0100

    [ADI] create IdeaPageTitle component

--- web/apps/atelier-des-idees/src/components/IdeaReader/index.jsx
+++ web/apps/atelier-des-idees/src/components/IdeaReader/index.jsx
@@ -5,17 +5,6 @@ class IdeaReader extends React.PureComponent {
     render() {
         return (
             <div className="idea-reader">
-                <h1 className="idea-reader__title">{this.props.title}</h1>
-                <div className="idea-reader__info">
-                    {this.props.authorName && (
-                        <span className="idea-reader__info__author">
-                            Par <span className="idea-reader__info__author-name">{this.props.authorName}</span>
-                        </span>
-                    )}
-                    {this.props.createdAt && (
-                        <span className="idea-reader__info__date"> le {this.props.createdAt}</span>
-                    )}
-                </div>
                 <div className="idea-reader__content">
                     {this.props.paragraphs.map(paragraph => (
                         <div
@@ -30,15 +19,10 @@ class IdeaReader extends React.PureComponent {
 }
 
 IdeaReader.defaultProps = {
-    authorName: '',
-    createdAt: '',
     paragraphs: [],
 };
 
 IdeaReader.propTypes = {
-    authorName: PropTypes.string,
-    createdAt: PropTypes.string,
-    title: PropTypes.string.isRequired,
     paragraphs: PropTypes.arrayOf(PropTypes.string),
 };
 
