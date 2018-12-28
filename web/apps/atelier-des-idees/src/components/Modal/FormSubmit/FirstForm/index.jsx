6d6ee931f707ad7953e6a35a7780d1a528509bf4
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-28 11:10:39 +0100

    [ADI] First form add verifErrors for array and modify TextArea style

--- web/apps/atelier-des-idees/src/components/Modal/FormSubmit/FirstForm/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/FormSubmit/FirstForm/index.jsx
@@ -10,8 +10,8 @@ class FirstForm extends React.Component {
         this.state = {
             inputs: {
                 description: '',
-                theme: '',
-                locality: '',
+                theme: [],
+                locality: [],
             },
             errors: {
                 form: '',
@@ -28,7 +28,7 @@ class FirstForm extends React.Component {
     handleErrors() {
         let canSubmit = true;
         const verifErrors = Object.keys(this.state.inputs).reduce((acc, curr) => {
-            if ('' === this.state.inputs[curr]) {
+            if (!this.state.inputs[curr] || !this.state.inputs[curr].length) {
                 // TODO: modify error msg
                 acc[curr] = 'Information manquante';
                 if (!acc.form) {
