2457fc1cc2161a27d0f764e33ef94be64ee7875b
Aurélie Largent <aurelie.largent@abbeal.com>
2018-12-28 11:40:57 +0100

    [ADI] FormSubmit -> PublishIdeaForm

--- web/apps/atelier-des-idees/src/components/Modal/FormSubmit/FormSubmit.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import FormSubmit from '.';
-
-storiesOf('FormSubmit', module)
-    .addParameters({ jest: ['FormSubmit'] })
-    .add('default', () => <FormSubmit />);
