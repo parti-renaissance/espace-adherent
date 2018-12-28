7bece285c39e285154c6498bcc27193c4a3de7bd
Jeremie Samson <jeremie.samson76@gmail.com>
2018-12-28 11:57:25 +0100

    Switch threadn and comment identifier to uuid

--- src/Controller/Api/ThreadController.php
+++ src/Controller/Api/ThreadController.php
@@ -4,9 +4,13 @@ namespace AppBundle\Controller\Api;
 
 use AppBundle\Entity\IdeasWorkshop\Thread;
 use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class ThreadController
 {
+    /**
+     * @ParamConverter("thread", options={"mapping": {"id": "uuid"}})
+     */
     public function approveAction(Thread $thread, ObjectManager $manager): Thread
     {
         $thread->approve();
@@ -15,6 +19,9 @@ class ThreadController
         return $thread;
     }
 
+    /**
+     * @ParamConverter("thread", options={"mapping": {"id": "uuid"}})
+     */
     public function reportAction(Thread $thread, ObjectManager $manager): Thread
     {
         $thread->report();
