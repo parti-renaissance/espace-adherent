a69b8253f2aa28b933aee3a42ed5d881f703f960
Adrien Casanova <haitaar@gmail.com>
2018-12-28 14:07:43 +0100

    [ADI] use IdeaReader component in CreateIdea page

--- web/apps/atelier-des-idees/src/components/IdeaReader/index.jsx
+++ web/apps/atelier-des-idees/src/components/IdeaReader/index.jsx
@@ -7,10 +7,14 @@ class IdeaReader extends React.PureComponent {
             <div className="idea-reader">
                 <h1 className="idea-reader__title">{this.props.title}</h1>
                 <div className="idea-reader__info">
-                    <span className="idea-reader__info__author">
-                        Par <span className="idea-reader__info__author-name">{this.props.authorName}</span>
-                    </span>
-                    <span className="idea-reader__info__date"> le {this.props.createdAt}</span>
+                    {this.props.authorName && (
+                        <span className="idea-reader__info__author">
+                            Par <span className="idea-reader__info__author-name">{this.props.authorName}</span>
+                        </span>
+                    )}
+                    {this.props.createdAt && (
+                        <span className="idea-reader__info__date"> le {this.props.createdAt}</span>
+                    )}
                 </div>
                 <div className="idea-reader__content">
                     {this.props.paragraphs.map(paragraph => (
@@ -26,12 +30,14 @@ class IdeaReader extends React.PureComponent {
 }
 
 IdeaReader.defaultProps = {
+    authorName: '',
+    createdAt: '',
     paragraphs: [],
 };
 
 IdeaReader.propTypes = {
-    authorName: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
+    authorName: PropTypes.string,
+    createdAt: PropTypes.string,
     title: PropTypes.string.isRequired,
     paragraphs: PropTypes.arrayOf(PropTypes.string),
 };
