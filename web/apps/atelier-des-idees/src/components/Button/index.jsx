2e05b4c1d4c444159f2ec3a93618437bb8108e0b
Adrien Casanova <haitaar@gmail.com>
2018-12-31 12:58:57 +0100

    [ADI] add loading state to Button component

--- web/apps/atelier-des-idees/src/components/Button/index.jsx
+++ web/apps/atelier-des-idees/src/components/Button/index.jsx
@@ -3,42 +3,67 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 function Button(props) {
-    return (
-        <button
-            className={classNames('button', `button--${props.mode}`, props.className)}
-            aria-label={props.label}
-            disabled={props.disabled}
-            onClick={() => props.onClick()}
-        >
-            {props.icon && (
-                <img src={props.icon} className={classNames('button__icon', `button__icon--${props.classIcon}`)} />
-            )}
-            <span className="button__label">{props.label}</span>
-        </button>
-    );
+  return (
+    <button
+      className={classNames(
+        'button',
+        `button--${props.mode}`,
+        props.className,
+        {
+          'button--loading': props.isLoading
+        }
+      )}
+      aria-label={props.label}
+      disabled={props.disabled}
+      onClick={() => props.onClick()}
+    >
+      {props.isLoading ? (
+        <div className="button__loader">
+          <span className="button__loader__ball" />
+          <span className="button__loader__ball" />
+          <span className="button__loader__ball" />
+        </div>
+      ) : (
+        <React.Fragment>
+          {props.icon && (
+            <img
+              src={props.icon}
+              className={classNames(
+                'button__icon',
+                `button__icon--${props.classIcon}`
+              )}
+            />
+          )}
+          <span className="button__label">{props.label}</span>
+        </React.Fragment>
+      )}
+    </button>
+  );
 }
 
 Button.defaultProps = {
-    label: undefined,
-    mode: 'primary',
-    icon: undefined,
-    type: 'button',
-    disabled: false,
-    className: undefined,
-    classIcon: undefined,
-    isLoading: false,
+  label: undefined,
+  mode: 'primary',
+  icon: undefined,
+  isLoading: false,
+  type: 'button',
+  disabled: false,
+  className: undefined,
+  classIcon: undefined,
+  isLoading: false
 };
 
 Button.propTypes = {
-    label: PropTypes.string,
-    mode: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
-    icon: PropTypes.string,
-    type: PropTypes.string,
-    onClick: PropTypes.func.isRequired,
-    disabled: PropTypes.bool,
-    className: PropTypes.string,
-    classIcon: PropTypes.oneOf(['start', 'end']),
-    isLoading: PropTypes.bool,
+  label: PropTypes.string,
+  mode: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
+  icon: PropTypes.string,
+  isLoading: PropTypes.bool,
+  type: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  classIcon: PropTypes.oneOf(['start', 'end']),
+  isLoading: PropTypes.bool
 };
 
 export default Button;
