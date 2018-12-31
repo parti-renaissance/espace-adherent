2d1e098fbe2028b8426c6d7a37094530fc175174
Adrien Casanova <haitaar@gmail.com>
2018-12-31 11:55:58 +0100

    [ADI] add initial dispatch

--- web/apps/atelier-des-idees/src/redux/store/configStore.dev.js
+++ web/apps/atelier-des-idees/src/redux/store/configStore.dev.js
@@ -1,16 +1,28 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {
+    createStore,
+    applyMiddleware,
+    compose
+} from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
 import rootReducer from '../reducers';
+import {
+    initApp
+} from '../thunk/navigation'
 
 // axios config
 const baseURL = process.env.REACT_APP_ADI_BASE_URL || null;
 const axiosInstance = axios.create({
     baseURL,
-    headers: { Accept: 'application/json' },
+    headers: {
+        Accept: 'application/json'
+    },
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk.withExtraArgument(axiosInstance))));
+
+// initial dispatch
+store.dispatch(initApp())
 
 export default store;
