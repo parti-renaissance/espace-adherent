1930bf7e9ccc6df5ccccb1dcd7966257ba08aa80
Adrien Casanova <haitaar@gmail.com>
2018-12-31 15:16:14 +0100

    [ADI] refactor TextArea style

--- web/apps/atelier-des-idees/src/components/TextArea/index.jsx
+++ web/apps/atelier-des-idees/src/components/TextArea/index.jsx
@@ -5,33 +5,30 @@ import classNames from 'classnames';
 function TextArea(props) {
     return (
         <div className="text-area">
-            <textarea
-                className={classNames('text-area__input', {
-                    'text-area__input--error': props.error,
-                })}
-                disabled={props.disabled}
-                id={props.id}
-                maxLength={props.maxlength}
-                name={props.name}
-                onChange={(e) => {
-                    const { value } = e.target;
-                    if (
-                        !props.maxLength ||
-						(props.maxLength && value.length <= props.maxLength)
-                    ) {
-                        props.onChange(e.target.value);
-                    }
-                }}
-                placeholder={props.placeholder}
-                value={props.value}
-            >
-                {props.value}
-            </textarea>
-            {props.maxLength && (
-                <div className="text-area__counter">{`${props.value.length}/${
-                    props.maxLength
-                }`}</div>
-            )}
+            <div className="text-area__input-wrapper">
+                <textarea
+                    className={classNames('text-area__input', {
+                        'text-area__input--error': props.error,
+                    })}
+                    disabled={props.disabled}
+                    id={props.id}
+                    maxLength={props.maxlength}
+                    name={props.name}
+                    onChange={(e) => {
+                        const { value } = e.target;
+                        if (!props.maxLength || (props.maxLength && value.length <= props.maxLength)) {
+                            props.onChange(e.target.value);
+                        }
+                    }}
+                    placeholder={props.placeholder}
+                    value={props.value}
+                >
+                    {props.value}
+                </textarea>
+                {props.maxLength && (
+                    <div className="text-area__counter">{`${props.value.length}/${props.maxLength}`}</div>
+                )}
+            </div>
             {props.error && <p className="text-area__error">{props.error}</p>}
         </div>
     );
