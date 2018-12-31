01ecb3a56c42414157628e27a2ac0811a97972ad
Adrien Casanova <haitaar@gmail.com>
2018-12-31 12:14:19 +0100

    [ADI] create withAuth HoC

---
+++ web/apps/atelier-des-idees/src/hocs/withAuth.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { FETCH_AUTH_USER } from '../redux/constants/actionTypes';
+import { selectLoadingState } from '../redux/selectors/loading';
+import { selectIsAuthenticated } from '../redux/selectors/auth';
+
+export default ChildComponent => {
+  const withAuth = ({ isAuthLoading, isAuthenticated, ...props }) => {
+    if (!isAuthLoading && !isAuthenticated) {
+      return <Redirect to="/connexion" />;
+    }
+    return isAuthLoading ? null : <ChildComponent {...props} />;
+  };
+
+  withAuth.propTypes = {
+    isAuthLoading: PropTypes.bool.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
+  };
+
+  function mapStateToProps(state) {
+    const isAuthLoading = selectLoadingState(state, FETCH_AUTH_USER);
+    const isAuthenticated = selectIsAuthenticated(state);
+    return {
+      isAuthLoading,
+      isAuthenticated
+    };
+  }
+
+  return connect(
+    mapStateToProps,
+    {}
+  )(withAuth);
+};
