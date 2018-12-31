ae4a7e36992f1bd78fb500508ef1e75c6c5b06fb
Adrien Casanova <haitaar@gmail.com>
2018-12-31 12:58:57 +0100

    [ADI] add loading state to Button component

--- web/apps/atelier-des-idees/src/components/Button/Button.stories.js
+++ web/apps/atelier-des-idees/src/components/Button/Button.stories.js
@@ -4,13 +4,36 @@ import { action } from '@storybook/addon-actions';
 import Button from '.';
 
 const props = {
-    label: 'Je propose',
-    icon: '/assets/img/icn_20px_comments.svg',
-    classIcon: 'start', // start or end
+  label: 'Je propose',
+  icon: '/assets/img/icn_20px_comments.svg',
+  classIcon: 'start' // start or end
 };
 
 storiesOf('Button', module)
-    .addParameters({ jest: ['Button'] })
-    .add('default/primary', () => <Button {...props} onClick={action('click')} />)
-    .add('secondary', () => <Button {...props} mode="secondary" onClick={action('click')} />)
-    .add('tertiary', () => <Button {...props} mode="tertiary" onClick={action('click')} />);
+  .addParameters({ jest: ['Button'] })
+  .add('default/primary', () => <Button {...props} onClick={action('click')} />)
+  .add('secondary', () => (
+    <Button {...props} mode="secondary" onClick={action('click')} />
+  ))
+  .add('tertiary', () => (
+    <Button {...props} mode="tertiary" onClick={action('click')} />
+  ))
+  .add('primary:loading', () => (
+    <Button {...props} onClick={action('click')} isLoading={true} />
+  ))
+  .add('secondary:loading', () => (
+    <Button
+      {...props}
+      mode="secondary"
+      onClick={action('click')}
+      isLoading={true}
+    />
+  ))
+  .add('tertiary:loading', () => (
+    <Button
+      {...props}
+      mode="tertiary"
+      onClick={action('click')}
+      isLoading={true}
+    />
+  ));
