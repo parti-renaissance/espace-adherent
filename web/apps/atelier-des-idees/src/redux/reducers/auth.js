8161ae46e01103e145536086953d1f174e8f8336
Adrien Casanova <haitaar@gmail.com>
2018-12-31 15:28:50 +0100

    [ADI] link auth to CreateIdeaPage container

--- web/apps/atelier-des-idees/src/redux/reducers/auth.js
+++ web/apps/atelier-des-idees/src/redux/reducers/auth.js
@@ -1,29 +1,31 @@
-import {
-  SET_AUTH_USER
-} from '../constants/actionTypes'
+import { SET_AUTH_USER } from '../constants/actionTypes';
 
+// TODO: uncomment when linked to users api
+// const initialState = {
+//     isAuthenticated: false,
+//     user: null,
+// };
+
+// TODO: remove when linked to users api
 const initialState = {
-  isAuthenticated: false,
-  user: null
-}
+    isAuthenticated: true,
+    user: { name: 'Jean-Pierre F.' },
+};
 
 function authReducer(state = initialState, action) {
-  const {
-    type,
-    payload
-  } = action
-  switch (type) {
+    const { type, payload } = action;
+    switch (type) {
     case SET_AUTH_USER:
-      return {
-        isAuthenticated: true,
-        user: payload.user
-      }
+        return {
+            isAuthenticated: true,
+            user: payload.user,
+        };
     default:
-      return state
-  }
+        return state;
+    }
 }
 
-export default authReducer
+export default authReducer;
 
-export const getIsAuthenticated = state => state.isAuthenticated
-export const getAuthUser = state => state.user
+export const getIsAuthenticated = state => state.isAuthenticated;
+export const getAuthUser = state => state.user;
