924ccec4a1a09dbe892cecd9d0e79bc026b8c50f
Adrien Casanova <haitaar@gmail.com>
2018-12-31 15:02:14 +0100

    [ADI] create CreateIdeaPage

--- web/apps/atelier-des-idees/src/App.js
+++ web/apps/atelier-des-idees/src/App.js
@@ -5,7 +5,7 @@ import withAuth from './hocs/withAuth';
 // pages
 import Home from './pages/Home';
 import ThreeTabs from './pages/ThreeTabs';
-import CreateIdea from './pages/CreateIdea/containers/CreateIdeaPage';
+import CreateIdea from './pages/CreateIdeaPage';
 
 // modal
 import ModalRoot from './containers/ModalRoot';
@@ -13,36 +13,20 @@ import ModalRoot from './containers/ModalRoot';
 import logo from './logo.svg';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ModalRoot />
-        <Switch>
-          <Route exact path="/atelier-des-idees" component={Home} />
-          <Route
-            exact
-            path="/atelier-des-idees/consulter"
-            component={ThreeTabs}
-          />
-          <Route
-            exact
-            path="/atelier-des-idees/contribuer"
-            component={ThreeTabs}
-          />
-          <Route
-            exact
-            path="/atelier-des-idees/proposer"
-            component={ThreeTabs}
-          />
-          <Route
-            exact
-            path="/atelier-des-idees/creer-ma-note"
-            component={withAuth(CreateIdea)}
-          />
-        </Switch>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ModalRoot />
+                <Switch>
+                    <Route exact path="/atelier-des-idees" component={Home} />{' '}
+                    <Route exact path="/atelier-des-idees/consulter" component={ThreeTabs} />{' '}
+                    <Route exact path="/atelier-des-idees/contribuer" component={ThreeTabs} />{' '}
+                    <Route exact path="/atelier-des-idees/proposer" component={ThreeTabs} />{' '}
+                    <Route exact path="/atelier-des-idees/creer-ma-note" component={withAuth(CreateIdea)} />{' '}
+                </Switch>{' '}
+            </div>
+        );
+    }
 }
 
 export default App;
