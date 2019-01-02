ce866781732e47d9157e929d7e766f77ba610235
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-02 10:37:11 +0100

    [ADI] Create Success and Fail form component

---
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/SuccessForm/SuccessForm.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import SucessForm from '.';
+
+storiesOf('SucessForm', module)
+    .addParameters({ jest: ['SucessForm'] })
+    .add('default', () => <SucessForm />);
---
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/SuccessForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SucessForm extends React.PureComponent {
+    render() {
+        return <div className="sucess-form">SucessFORM</div>;
+    }
+}
+
+export default SucessForm;
