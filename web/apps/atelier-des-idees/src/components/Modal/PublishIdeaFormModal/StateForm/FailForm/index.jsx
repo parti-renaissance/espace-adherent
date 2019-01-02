63161863e87db9edcc7068a0e28cb47ba49e07ef
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-02 11:14:09 +0100

    [ADI] Integration Success and Fail form

--- web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/FailForm/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/FailForm/index.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 class FailForm extends React.PureComponent {
     render() {
-        return <div className="fail-form">FAILFORM</div>;
+        return (
+            <div className="fail-form">
+                <img src="/assets/img/icn_state_fail.svg" />
+                <h3 className="fail-form__title">OUPS</h3>
+                <p className="fail-form__subtitle">Quelque chose s'est mal passé</p>
+                <button className="button--secondary">Réessayer</button>
+            </div>
+        );
     }
 }
 
