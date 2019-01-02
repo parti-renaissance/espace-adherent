7db56541e19d309aefb009e57ea9cc06292b0331
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-02 11:14:09 +0100

    [ADI] Integration Success and Fail form

--- web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/SuccessForm/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/SuccessForm/index.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 class SucessForm extends React.PureComponent {
     render() {
-        return <div className="sucess-form">SucessFORM</div>;
+        return (
+            <div className="success-form">
+                <img src="/assets/img/icn_state_success.svg" />
+                <h3 className="success-form__title">Votre note a bien été publiée</h3>
+                <p className="success-form__subtitle">
+					Votre idée va maintenant être enrichie par des adhérents pendant 3
+					semaines. Vous pouvez leur montrer que vous prenez en compte leurs
+					commentaires en cliquant sur{' '}
+                    <span className="success-form__subtitle__approved">Approuver</span>.
+					Vous devrez ensuite intégrer vous-même leurs contributions à votre
+					partie.
+                </p>
+                <button className="success-form__button button--secondary">
+					Voir la page
+                </button>
+                <img src="assets/img/how-to-approve.svg" />
+            </div>
+        );
     }
 }
 
