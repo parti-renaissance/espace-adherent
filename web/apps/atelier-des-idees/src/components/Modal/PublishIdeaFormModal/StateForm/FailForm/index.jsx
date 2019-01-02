41cb14df4d60402395cae2d7de5e57bf5ae9c0ee
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-02 10:37:11 +0100

    [ADI] Create Success and Fail form component

---
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/FailForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class FailForm extends React.PureComponent {
+    render() {
+        return <div className="fail-form">FAILFORM</div>;
+    }
+}
+
+export default FailForm;
