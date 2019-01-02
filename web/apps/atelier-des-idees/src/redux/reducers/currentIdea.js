b832bb06612903dbd7b546689212a5ee073a81de
Adrien Casanova <haitaar@gmail.com>
2019-01-02 19:02:17 +0100

    [ADI] create thunk fetchGuidelines

--- web/apps/atelier-des-idees/src/redux/reducers/currentIdea.js
+++ web/apps/atelier-des-idees/src/redux/reducers/currentIdea.js
@@ -1,15 +1,18 @@
-import { SET_CURRENT_IDEA, UPDATE_CURRENT_IDEA } from '../constants/actionTypes';
+import { SET_CURRENT_IDEA, UPDATE_CURRENT_IDEA, SET_GUIDELINES } from '../constants/actionTypes';
 
-const initialState = {};
+const initialState = { idea: {}, guidelines: [] };
 
 function currentIdeaReducer(state = initialState, action) {
     const { type, payload } = action;
     switch (type) {
     case SET_CURRENT_IDEA: {
-        return { ...payload.data };
+        return { ...state, idea: payload.data };
     }
     case UPDATE_CURRENT_IDEA: {
-        return { ...state, ...payload.data };
+        return { ...state, idea: { ...state.idea, ...payload.data } };
+    }
+    case SET_GUIDELINES: {
+        return { ...state, guidelines: payload.data };
     }
     default:
         return state;
@@ -18,4 +21,5 @@ function currentIdeaReducer(state = initialState, action) {
 
 export default currentIdeaReducer;
 
-export const getCurrentIdea = state => state;
+export const getCurrentIdea = state => state.idea;
+export const getGuidelines = state => state.guidelines;
