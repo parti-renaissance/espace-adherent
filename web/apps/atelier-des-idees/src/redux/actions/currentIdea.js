a69b0e6a1b14a11ae1a4ec5c10ad5ca9fe54b6bf
Adrien Casanova <haitaar@gmail.com>
2019-01-02 19:02:17 +0100

    [ADI] create thunk fetchGuidelines

--- web/apps/atelier-des-idees/src/redux/actions/currentIdea.js
+++ web/apps/atelier-des-idees/src/redux/actions/currentIdea.js
@@ -1,5 +1,6 @@
 import { action } from '../helpers/actions';
-import { SET_CURRENT_IDEA, UPDATE_CURRENT_IDEA } from '../constants/actionTypes';
+import { SET_CURRENT_IDEA, UPDATE_CURRENT_IDEA, SET_GUIDELINES } from '../constants/actionTypes';
 
 export const setCurrentIdea = data => action(SET_CURRENT_IDEA, { data });
 export const updateCurrentIdea = data => action(UPDATE_CURRENT_IDEA, { data });
+export const setGuidelines = data => action(SET_GUIDELINES, { data });
