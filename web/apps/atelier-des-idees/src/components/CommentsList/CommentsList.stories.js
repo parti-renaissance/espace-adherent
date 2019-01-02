e1277f972044a7948478870c0589ccaf9f34c73a
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-02 15:26:11 +0100

    [ADI] Style CommentsList

--- web/apps/atelier-des-idees/src/components/CommentsList/CommentsList.stories.js
+++ web/apps/atelier-des-idees/src/components/CommentsList/CommentsList.stories.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import CommentsList from '.';
+
+const props = {
+    comments: [
+        {
+            id: '0000',
+            content: 'Commentaire 1',
+            author: { id: 'u1', name: 'Jean-Charles F.' },
+            createdAt: '29/11/2018 à 16h02',
+            verified: true,
+            replies: [
+                {
+                    id: '2222',
+                    content: 'Réponse 1',
+                    author: { id: 'u2', name: 'Micheline B.' },
+                    createdAt: '29/11/2018 à 18h04',
+                    verified: false,
+                },
+            ],
+        },
+        {
+            id: '1111',
+            content: 'Commentaire 2',
+            author: { id: 'u2', name: 'Micheline B.' },
+            createdAt: '29/11/2018 à 18h04',
+            verified: false,
+            replies: [],
+        },
+    ],
+};
 
 storiesOf('CommentsList', module)
     .addParameters({ jest: ['CommentsList'] })
-    .add('default', () => <CommentsList />);
+    .add('default', () => (
+        <CommentsList
+            comments={props.comments}
+            onSendComment={(comment, parentId = undefined) =>
+                action(comment, parentId)
+            }
+            onDeleteComment={commentId => action('delete comment', commentId)}
+            ownerId="u1"
+        />
+    ));
--- web/apps/atelier-des-idees/src/components/CommentsList/index.jsx
+++ web/apps/atelier-des-idees/src/components/CommentsList/index.jsx
@@ -1,10 +1,134 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Comment from './Comment';
+import TextArea from '../TextArea';
 
 class CommentsList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comment: '',
+            replyingTo: undefined,
+            showComments: true,
+        };
+    }
+
     render() {
-        return <div>CommentsList</div>;
+        return (
+            <div className="comments-list">
+                <button
+                    className="comments-list__collapse-button"
+                    onClick={() =>
+                        this.setState(prevState => ({
+                            showComments: !prevState.showComments,
+                        }))
+                    }
+                >
+                    <img
+                        className="comments-list__collapse-button__icon-replies"
+                        src="/assets/img/icn_20px_replies.svg"
+                    />
+                    <span className="comments-list__collapse-button__label">
+                        {this.props.comments.length} {this.props.collapseLabel}
+                        {1 < this.props.comments.length && 's'}
+                    </span>
+                    <img
+                        className={classNames(
+                            'comments-list__collapse-button__icon-toggle',
+                            {
+                                'comments-list__collapse-button__icon-toggle--rotate': !this
+                                    .state.showComments,
+                            }
+                        )}
+                        src="/assets/img/icn_toggle_content.svg"
+                    />
+                </button>
+                {this.state.showComments && (
+                    <div>
+                        {this.props.comments.length
+                            ? this.props.comments.map(comment => (
+                                <React.Fragment>
+                                    <Comment
+                                        {...comment}
+                                        ownerId={this.props.ownerId}
+                                        hasActions={!this.props.parentId}
+                                        isAuthor={this.props.ownerId === comment.author.id}
+                                        onReply={() => this.setState({ replyingTo: comment.id })}
+                                        onDelete={() => this.props.onDeleteComment(comment.id)}
+                                    />
+                                    {((comment.replies && !!comment.replies.length) ||
+											this.state.replyingTo === comment.id) && (
+                                            <div className="comments-list__replies">
+                                                <CommentsList
+                                                    comments={comment.replies}
+                                                    onSendComment={value =>
+                                                        // send parent comment id as (optional) second parameter
+                                                        this.props.onSendComment(value, comment.id)
+                                                    }
+                                                    parentId={comment.id}
+                                                    showForm={this.props.showForm}
+                                                    collapseLabel="réponse"
+                                                    placeholder="Écrivez votre réponse"
+                                                    emptyLabel={null}
+                                                />
+                                            </div>
+                                        )}
+                                </React.Fragment>
+							  ))
+                            : this.props.emptyLabel && <p>{this.props.emptyLabel}</p>}
+                    </div>
+                )}
+                {this.props.showForm && (
+                    <form
+                        className="comments-list__form"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            this.props.onSendComment(this.state.comment);
+                        }}
+                    >
+                        <TextArea
+                            value={this.state.comment}
+                            onChange={e => this.setState({ comment: e.target.value })}
+                            placeholder={this.props.placeholder}
+                        />
+                        <button className="comments-list__form__button button--primary">
+							Envoyer
+                        </button>
+                    </form>
+                )}
+            </div>
+        );
     }
 }
+
+CommentsList.defaultProps = {
+    comments: [],
+    showForm: true,
+    parentId: undefined,
+    emptyLabel: 'Soyez le premier à contribuer sur cette partie',
+    placeholder: 'Ajoutez votre contribution',
+    collapseLabel: 'commentaire',
+};
+
+CommentsList.propTypes = {
+    comments: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            author: PropTypes.object.isRequired,
+            content: PropTypes.string.isRequired,
+            createdAt: PropTypes.string.isRequired, // iso date
+            replies: PropTypes.array,
+            verified: PropTypes.bool,
+        })
+    ),
+    onSendComment: PropTypes.func.isRequired,
+    onDeleteComment: PropTypes.func.isRequired,
+    ownerId: PropTypes.string.isRequired,
+    showForm: PropTypes.bool,
+    parentId: PropTypes.string,
+    emptyLabel: PropTypes.string,
+    placeholder: PropTypes.string,
+};
 
 export default CommentsList;
