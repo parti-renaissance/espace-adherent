5d0fddd7f1ac99eefde86a5fe8a5ecfbe1024272
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-02 11:52:18 +0100

    [ADI ]Add success and fail form to final form

--- web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/FailForm/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/FailForm/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class FailForm extends React.PureComponent {
     render() {
@@ -7,10 +8,19 @@ class FailForm extends React.PureComponent {
                 <img src="/assets/img/icn_state_fail.svg" />
                 <h3 className="fail-form__title">OUPS</h3>
                 <p className="fail-form__subtitle">Quelque chose s'est mal passé</p>
-                <button className="button--secondary">Réessayer</button>
+                <button
+                    className="button--secondary"
+                    onClick={() => this.props.submitAgain()}
+                >
+					Réessayer
+                </button>
             </div>
         );
     }
 }
+
+FailForm.propTypes = {
+    submitAgain: PropTypes.func.isRequired,
+};
 
 export default FailForm;
