b49550d790fddd17f9be81e1536155ab37e9f816
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-03 17:31:28 +0100

    [ADI] remove mock from pinned

--- web/apps/atelier-des-idees/src/containers/ConsultationPinned/index.jsx
+++ web/apps/atelier-des-idees/src/containers/ConsultationPinned/index.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { selectShowConsultationPinned, selectConsultationPinned } from '../../redux/selectors/pinned';
+import {
+    selectShowConsultationPinned,
+    selectConsultationPinned,
+} from '../../redux/selectors/pinned';
 import { hideConsultationPinned } from '../../redux/actions/pinned';
 import Banner from '../../components/Banner';
 
 class BannerContainer extends React.Component {
     shouldComponentUpdate(nextProps, nextState) {
         // only render if hide or show banner
-        return this.props.showConsultationPinned !== nextProps.showConsultationPinned;
+        return (
+            this.props.showConsultationPinned !== nextProps.showConsultationPinned
+        );
     }
 
     render() {
         const { data, showConsultationPinned, hideBanner } = this.props;
-        const { title, link, calendar, duration } = data;
-        return showConsultationPinned ? <Banner {...data} onClose={hideBanner} /> : null;
+        const { name, url, started_at, ended_at, response_time } = data;
+        return showConsultationPinned ? (
+            <Banner {...data} onClose={hideBanner} />
+        ) : null;
     }
 }
 
@@ -28,8 +35,23 @@ function mapStateToProps(state) {
     const showConsultationPinned = selectShowConsultationPinned(state);
     const consultationPinnedData = selectConsultationPinned(state);
     // format data to match Banner props
-    const { title, link, calendar, duration } = consultationPinnedData;
-    return { showConsultationPinned, data: { title, link, subtitle: calendar, extraInfo: duration } };
+    const {
+        name,
+        url,
+        started_at,
+        ended_at,
+        response_time,
+    } = consultationPinnedData;
+    return {
+        showConsultationPinned,
+        data: {
+            name,
+            url,
+            started_at,
+            ended_at,
+            response_time,
+        },
+    };
 }
 
 export default connect(
--- web/apps/atelier-des-idees/src/redux/thunk/reports.js
+++ web/apps/atelier-des-idees/src/redux/thunk/reports.js
@@ -6,47 +6,18 @@ import {
     createRequestFailure,
 } from '../actions/loading';
 
-const reportsMock = {
-    metadata: {
-        total_items: 2,
-        items_per_page: 2,
-        count: 2,
-        current_page: 1,
-        last_page: 1,
-    },
-    items: [
-        {
-            url:
-				'https://storage.googleapis.com/en-marche-prod/documents/adherents/1-charte-et-manifeste/charte_des_valeurs.pdf',
-            position: 1,
-            name: 'Rapport sur les énergies renouvables',
-        },
-        {
-            url:
-				'https://storage.googleapis.com/en-marche-prod/documents/adherents/1-charte-et-manifeste/regles_de_fonctionnement_LaREM.pdf',
-            position: 2,
-            name: 'Rapport sur la politique du logement',
-        },
-    ],
-};
-
 export function fetchReports() {
     return (dispatch, getState, axios) => {
         dispatch(createRequest(FETCH_REPORTS));
-        return (
-            axios
-        // TODO: Replace by real api
-                .get('/api/consultation_reports')
-                .then(res => res.data)
-                .then(({ items, metadata }) => {
-                    dispatch(setReports(items));
-                    dispatch(createRequestSuccess(FETCH_REPORTS));
-                })
-                .catch((error) => {
-                    dispatch(createRequestFailure(FETCH_REPORTS));
-                })
-        // TODO: remove finally when endpoint is up
-                .finally(() => dispatch(setReports(reportsMock.items)))
-        );
+        return axios
+            .get('/api/consultation_reports')
+            .then(res => res.data)
+            .then(({ items, metadata }) => {
+                dispatch(setReports(items));
+                dispatch(createRequestSuccess(FETCH_REPORTS));
+            })
+            .catch((error) => {
+                dispatch(createRequestFailure(FETCH_REPORTS));
+            });
     };
 }
