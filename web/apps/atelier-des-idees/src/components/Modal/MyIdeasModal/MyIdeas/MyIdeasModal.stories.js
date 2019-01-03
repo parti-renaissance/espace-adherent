20642eb733ad82362335f809b3aff1f9110359a8
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-03 10:09:43 +0100

    [ADI] Integration of my contributions

--- web/apps/atelier-des-idees/src/components/Modal/MyIdeasModal/MyIdeas/MyIdeasModal.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import MyIdeas from '.';
-
-storiesOf('MyIdeas', module)
-    .addParameters({ jest: ['MyIdeas'] })
-    .add('default', () => <MyIdeas />);
