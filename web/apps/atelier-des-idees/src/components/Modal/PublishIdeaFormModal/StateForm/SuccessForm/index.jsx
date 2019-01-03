f869e8f297fce4c29fc14314de70509820e4b246
Adrien Casanova <haitaar@gmail.com>
2019-01-03 13:52:44 +0100

    [ADI] PublishIdeaForm: add id prop

--- web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/SuccessForm/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/StateForm/SuccessForm/index.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-class SucessForm extends React.PureComponent {
+class SuccessForm extends React.PureComponent {
     render() {
         return (
             <div className="success-form">
                 <img src="/assets/img/icn_state_success.svg" />
                 <h3 className="success-form__title">Votre note a bien été publiée</h3>
                 <p className="success-form__subtitle">
-					Votre idée va maintenant être enrichie par des adhérents pendant 3
-					semaines. Vous pouvez leur montrer que vous prenez en compte leurs
-					commentaires en cliquant sur{' '}
-                    <span className="success-form__subtitle__approved">Approuver</span>.
-					Vous devrez ensuite intégrer vous-même leurs contributions à votre
-					partie.
+                    Votre idée va maintenant être enrichie par des adhérents pendant 3 semaines. Vous pouvez leur
+                    montrer que vous prenez en compte leurs commentaires en cliquant sur{' '}
+                    <span className="success-form__subtitle__approved">Approuver</span>. Vous devrez ensuite intégrer
+                    vous-même leurs contributions à votre partie.
                 </p>
-                {/* TODO: Lien vers la nouvelle note créée ? */}
                 <Link
-                    to="/atelier-des-idees/contribuer"
+                    to={`/atelier-des-idees/note/${this.props.id}`}
                     className="success-form__button button--secondary"
                 >
-					Voir la page
+                    Voir la page
                 </Link>
                 <img src="assets/img/how-to-approve.svg" />
             </div>
@@ -28,4 +26,8 @@ class SucessForm extends React.PureComponent {
     }
 }
 
-export default SucessForm;
+SuccessForm.propTypes = {
+    id: PropTypes.string.isRequired,
+};
+
+export default SuccessForm;
