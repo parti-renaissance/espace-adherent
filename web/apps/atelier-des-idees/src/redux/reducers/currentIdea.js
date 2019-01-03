cd4122d53ed84d9ebabf29b193d203d1ea17f2c4
Adrien Casanova <haitaar@gmail.com>
2019-01-03 14:47:36 +0100

    [ADI] refactor current idea reducer

--- web/apps/atelier-des-idees/src/redux/reducers/currentIdea.js
+++ web/apps/atelier-des-idees/src/redux/reducers/currentIdea.js
@@ -1,25 +1,34 @@
+import { combineReducers } from 'redux';
 import { SET_CURRENT_IDEA, UPDATE_CURRENT_IDEA, SET_GUIDELINES } from '../constants/actionTypes';
 
 const initialState = { idea: {}, guidelines: [] };
 
-function currentIdeaReducer(state = initialState, action) {
+function ideaReducer(state = initialState.idea, action) {
     const { type, payload } = action;
     switch (type) {
     case SET_CURRENT_IDEA: {
-        return { ...state, idea: payload.data };
+        return { ...payload.data };
     }
     case UPDATE_CURRENT_IDEA: {
-        return { ...state, idea: { ...state.idea, ...payload.data } };
-    }
-    case SET_GUIDELINES: {
-        return { ...state, guidelines: payload.data };
+        return { ...state, ...payload.data };
     }
     default:
         return state;
     }
 }
 
-export default currentIdeaReducer;
+function guidelinesReducer(state = initialState.guidelines, action) {
+    const { type, payload } = action;
+    switch (type) {
+    case SET_GUIDELINES: {
+        return [...payload.data];
+    }
+    default:
+        return state;
+    }
+}
+
+export default combineReducers({ idea: ideaReducer, guidelines: guidelinesReducer });
 
 export const getCurrentIdea = state => state.idea;
 export const getGuidelines = state => state.guidelines;
