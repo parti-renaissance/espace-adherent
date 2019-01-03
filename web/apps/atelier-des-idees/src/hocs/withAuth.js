3e91565d0755f44d4ae10a7bfa4afc4182578e02
Adrien Casanova <haitaar@gmail.com>
2019-01-03 14:07:22 +0100

    [ADI] loading reducer: refactor reducer and selector

--- web/apps/atelier-des-idees/src/hocs/withAuth.js
+++ web/apps/atelier-des-idees/src/hocs/withAuth.js
@@ -5,30 +5,30 @@ import { Redirect } from 'react-router-dom';
 import { selectLoadingState } from '../redux/selectors/loading';
 import { selectIsAuthenticated } from '../redux/selectors/auth';
 
-export default ChildComponent => {
-  const withAuth = ({ isAuthLoading, isAuthenticated, ...props }) => {
-    if (!isAuthLoading && !isAuthenticated) {
-      return <Redirect to="/connexion" />;
-    }
-    return isAuthLoading ? null : <ChildComponent {...props} />;
-  };
-
-  withAuth.propTypes = {
-    isAuthLoading: PropTypes.bool.isRequired,
-    isAuthenticated: PropTypes.bool.isRequired
-  };
-
-  function mapStateToProps(state) {
-    const isAuthLoading = selectLoadingState(state, 'FETCH_AUTH_USER');
-    const isAuthenticated = selectIsAuthenticated(state);
-    return {
-      isAuthLoading,
-      isAuthenticated
+export default (ChildComponent) => {
+    const withAuth = ({ isAuthLoading, isAuthenticated, ...props }) => {
+        if (!isAuthLoading && !isAuthenticated) {
+            return <Redirect to="/connexion" />;
+        }
+        return isAuthLoading ? null : <ChildComponent {...props} />;
     };
-  }
 
-  return connect(
-    mapStateToProps,
-    {}
-  )(withAuth);
+    withAuth.propTypes = {
+        isAuthLoading: PropTypes.bool.isRequired,
+        isAuthenticated: PropTypes.bool.isRequired,
+    };
+
+    function mapStateToProps(state) {
+        const { isFetching } = selectLoadingState(state, 'FETCH_AUTH_USER');
+        const isAuthenticated = selectIsAuthenticated(state);
+        return {
+            isAuthLoading: isFetching,
+            isAuthenticated,
+        };
+    }
+
+    return connect(
+        mapStateToProps,
+        {}
+    )(withAuth);
 };
