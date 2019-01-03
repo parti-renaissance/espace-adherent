962853aff220e1a613b96ee463503099766d20f9
Adrien Casanova <haitaar@gmail.com>
2019-01-03 15:22:50 +0100

    [ADI] PublishIdeaForm: use static data

--- web/apps/atelier-des-idees/src/containers/PublishIdeaFormModal/index.js
+++ web/apps/atelier-des-idees/src/containers/PublishIdeaFormModal/index.js
@@ -1,25 +1,26 @@
 import { connect } from 'react-redux';
 import PublishIdeaFormModal from '../../components/Modal/PublishIdeaFormModal';
 import { selectLoadingState } from '../../redux/selectors/loading';
+import { selectStatic } from '../../redux/selectors/static';
+
+function formatStaticData(data) {
+    return data.map(({ id, name }) => ({ value: id, label: name }));
+}
 
 function mapStateToProps(state, { id }) {
     // get request status
     const { isFetching, isSuccess, isError } = selectLoadingState(state, 'PUBLISH_IDEA', id);
-    // TODO: fetch static data
+    // get static data
+    const { themes, needs, categories, committees } = selectStatic(state);
+    const formattedCommittees = committees.map(({ uuid, name }) => ({ value: uuid, label: name }));
     return {
         isSubmitSuccess: isSuccess,
         isSubmitError: isError,
-        themeOptions: [
-            { value: 'agriculture', label: 'Agriculture' },
-            { value: 'education', label: 'Education' },
-            { value: 'culture', label: 'Culture' },
-            { value: 'defense', label: 'Défense' },
-            { value: 'parity', label: 'Parité' },
-        ],
-        localityOptions: [{ value: 'national', label: 'National' }, { value: 'european', label: 'Européen' }],
+        themeOptions: formatStaticData(themes),
+        localityOptions: formatStaticData(categories),
+        difficultiesOptions: formatStaticData(needs),
+        committeeOptions: formattedCommittees,
         authorOptions: [{ value: 'alone', label: 'Seul' }, { value: 'committee', label: 'Mon comité' }],
-        committeeOptions: [{ value: 'comittee_1', label: 'Comité 1' }, { value: 'comittee_2', label: 'Comité 2' }],
-        difficultiesOptions: [{ value: 'juridique', label: 'Juridique' }, { value: 'finance', label: 'Finance' }],
     };
 }
 
