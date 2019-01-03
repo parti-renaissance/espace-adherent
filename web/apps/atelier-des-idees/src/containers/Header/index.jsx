e9707991de42de022b1eb582356e28a8ecbbd50c
Adrien Casanova <haitaar@gmail.com>
2019-01-03 17:33:26 +0100

    [ADI] fetch user ideas and contributions on header mount

--- web/apps/atelier-des-idees/src/containers/Header/index.jsx
+++ web/apps/atelier-des-idees/src/containers/Header/index.jsx
@@ -1,10 +1,35 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { showModal } from '../../redux/actions/modal';
+import { fetchUserIdeas, fetchUserContributions } from '../../redux/thunk/ideas';
 import Header from '../../components/Header';
 
 import { MY_IDEAS_MODAL } from '../../constants/modalTypes';
 
+class HeaderContainer extends React.Component {
+    componentDidMount() {
+        this.props.initHeader();
+    }
+
+    render() {
+        const { initHeader, ...otherProps } = this.props;
+        return <Header {...otherProps} />;
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        initHeader: () => {
+            // user ideas
+            dispatch(fetchUserIdeas());
+            // user contributions
+            dispatch(fetchUserContributions());
+        },
+        onMyIdeasBtnClicked: tabActive => dispatch(showModal(MY_IDEAS_MODAL, { tabActive })),
+    };
+}
+
 export default connect(
     null,
-    { onMyIdeasBtnClicked: tabActive => showModal(MY_IDEAS_MODAL, { tabActive }) }
-)(Header);
+    mapDispatchToProps
+)(HeaderContainer);
