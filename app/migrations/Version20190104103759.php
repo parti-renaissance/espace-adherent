450c75b0db3417f9317223a12eeea978616e2baa
Jeremie Samson <jeremie.samson76@gmail.com>
2019-01-04 10:52:25 +0100

    [ADI] add unique constraint on consultation enabled

---
+++ app/migrations/Version20190104103759.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20190104103759 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE ideas_workshop_consultation CHANGE enabled enabled TINYINT(1) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX consultation_enabled_unique ON ideas_workshop_consultation (enabled)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX consultation_enabled_unique ON ideas_workshop_consultation');
+        $this->addSql('ALTER TABLE ideas_workshop_consultation CHANGE enabled enabled TINYINT(1) NOT NULL');
+    }
+}
