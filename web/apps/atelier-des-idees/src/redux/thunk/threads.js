70f789de86d5b6ddf7a35b36bc219f63ec774686
Adrien Casanova <haitaar@gmail.com>
2019-01-04 16:12:07 +0100

    [ADI] add threads thunks

--- web/apps/atelier-des-idees/src/redux/thunk/threads.js
+++ web/apps/atelier-des-idees/src/redux/thunk/threads.js
@@ -1,4 +1,3 @@
-import { FETCH_IDEA_THREADS } from '../constants/actionTypes';
 import { setCurrentIdeaThreads } from '../actions/currentIdea';
 
 export function fetchIdeaThreads(id) {
@@ -7,4 +6,47 @@ export function fetchIdeaThreads(id) {
             .get(`/api/threads/${id}/comments`)
             .then(res => res.data)
             .then(data => dispatch(setCurrentIdeaThreads(data.items)));
+}
+
+export function approveComment(id, parentId = '') {
+    return (dispatch, getState, axios) => {
+        let type = 'threads';
+        if (parentId) {
+            type = 'thread_comments';
+        }
+        return axios.put(`/api/${type}/${id}/approve`);
+    };
+}
+
+export function deleteComment(id, parentId = '') {
+    return (dispatch, getState, axios) => {
+        let type = 'threads';
+        if (parentId) {
+            type = 'thread_comments';
+        }
+        return axios.delete(`/api/${type}/${id}`);
+    };
+}
+
+export function postComment(content, answerId, parentId = '') {
+    return (dispatch, getState, axios) => {
+        // TODO: use answerId
+        let type = 'threads';
+        const body = { content };
+        if (parentId) {
+            type = 'thread_comments';
+            body.thread = parentId;
+        }
+        return axios.post(`/api/${type}`, body);
+    };
+}
+
+export function reportComment(id, parentId = '') {
+    return (dispatch, getState, axios) => {
+        let type = 'threads';
+        if (parentId) {
+            type = 'thread_comments';
+        }
+        return axios.put(`/api/${type}/${id}/report`);
+    };
 }
