2662a28d775f1521725f4e3a6bd83357de0e6e12
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-04 11:07:55 +0100

    [ADI] create flag modal component

---
+++ web/apps/atelier-des-idees/src/components/Modal/FlagModal/FlagModal.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import FlagModal from '.';
+
+const props = {
+    reasons: [
+        {
+            value: 'en_marche_values',
+            label: 'Ce que je vois ne correspond pas aux valeurs du Mouvement',
+        },
+        { value: 'inappropriate', label: 'Ce n\'est pas du contenu approprié' },
+        { value: 'commercial_content', label: 'Il s\'agit de contenu commercial' },
+        { value: 'other', label: 'Autre' },
+    ],
+};
+
+storiesOf('FlagModal', module)
+    .addParameters({ jest: ['FlagModal'] })
+    .add('default', () => <FlagModal {...props} onSubmit={action('Flag')} />);
