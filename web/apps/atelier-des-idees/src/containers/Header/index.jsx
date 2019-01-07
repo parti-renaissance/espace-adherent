093a28335505eec6e28fd47fc5d36f14c1b3a62d
Adrien Casanova <haitaar@gmail.com>
2019-01-07 11:16:38 +0100

    [ADI] hide modal when header unmount

--- web/apps/atelier-des-idees/src/containers/Header/index.jsx
+++ web/apps/atelier-des-idees/src/containers/Header/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { showModal } from '../../redux/actions/modal';
+import { showModal, hideModal } from '../../redux/actions/modal';
 import { selectIsAuthenticated } from '../../redux/selectors/auth';
 import Header from '../../components/Header';
 
 import { MY_IDEAS_MODAL } from '../../constants/modalTypes';
 
 class HeaderContainer extends React.Component {
+    componentWillUnmount() {
+        this.props.unmountHeader();
+    }
+
     render() {
         const { initHeader, ...otherProps } = this.props;
         return <Header {...otherProps} />;
@@ -20,6 +24,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
+        unmountHeader: () => dispatch(hideModal()),
         onMyIdeasBtnClicked: tabActive => dispatch(showModal(MY_IDEAS_MODAL, { tabActive })),
     };
 }
