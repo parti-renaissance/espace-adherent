dab27d5e330fd2ed84242a87aca648f0be957798
Adrien Casanova <haitaar@gmail.com>
2019-01-07 11:07:57 +0100

    [ADI] publish from my ideas drawer

--- web/apps/atelier-des-idees/src/components/Modal/MyIdeasModal/MyIdeasModal.stories.js
+++ web/apps/atelier-des-idees/src/components/Modal/MyIdeasModal/MyIdeasModal.stories.js
@@ -5,27 +5,31 @@ import MyIdeasModal from '.';
 const props = {
     my_ideas: [
         {
+            uuid: '0000',
             created_at: new Date().toISOString(),
-            name:
-				'Réduire le nombre d’élèves dans les classes dans les quartiers défavorisés',
+            name: 'Réduire le nombre d’élèves dans les classes dans les quartiers défavorisés',
             status: 'PENDING',
         },
         {
+            uuid: '0000',
             created_at: new Date().toISOString(),
             name: 'Ceci est une deuxième idée',
             status: 'PENDING',
         },
         {
+            uuid: '0000',
             created_at: new Date().toISOString(),
             name: 'Ceci est une troisième idée',
             status: 'PENDING',
         },
         {
+            uuid: '0000',
             created_at: new Date().toISOString(),
             name: 'Ceci est une quatrième idée',
             status: 'FINALIZED',
         },
         {
+            uuid: '0000',
             created_at: new Date().toISOString(),
             name: 'Ceci est une cinquième idée',
             status: 'DRAFT',
@@ -34,8 +38,7 @@ const props = {
     my_contribs: [
         {
             created_at: new Date().toISOString(),
-            name:
-				'Réduire le nombre d’élèves dans les classes dans les quartiers défavorisés',
+            name: 'Réduire le nombre d’élèves dans les classes dans les quartiers défavorisés',
         },
         {
             created_at: new Date().toISOString(),
