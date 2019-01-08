e35df98b352d61ff73dae9ab0b9557258243fa08
Adrien Casanova <haitaar@gmail.com>
2019-01-08 11:25:51 +0100

    [ADI] IdeaThread: can (un)approve thread

--- web/apps/atelier-des-idees/src/redux/actions/threads.js
+++ web/apps/atelier-des-idees/src/redux/actions/threads.js
@@ -1,6 +1,7 @@
 import { action } from '../helpers/actions';
-import { SET_THREADS, ADD_THREAD, REMOVE_THREAD } from '../constants/actionTypes';
+import { SET_THREADS, ADD_THREAD, REMOVE_THREAD, TOGGLE_APPROVE_THREAD } from '../constants/actionTypes';
 
 export const setThreads = data => action(SET_THREADS, { data });
 export const addThread = data => action(ADD_THREAD, { data });
 export const removeThread = id => action(REMOVE_THREAD, { id });
+export const toggleApproveThread = id => action(TOGGLE_APPROVE_THREAD, { id });
