9d29ce9a85966d3085111bf837c887730df85ea8
Adrien Casanova <haitaar@gmail.com>
2019-01-08 11:25:51 +0100

    [ADI] IdeaThread: can (un)approve thread

--- web/apps/atelier-des-idees/src/redux/reducers/threads.js
+++ web/apps/atelier-des-idees/src/redux/reducers/threads.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { SET_THREADS, ADD_THREAD, REMOVE_THREAD } from '../constants/actionTypes';
+import { SET_THREADS, ADD_THREAD, REMOVE_THREAD, TOGGLE_APPROVE_THREAD } from '../constants/actionTypes';
 
 const initialState = { threads: { items: [], metadata: {} }, comments: { items: [], metadata: {} } };
 
@@ -13,6 +13,16 @@ function threadsReducer(state = initialState.threads, action) {
     case REMOVE_THREAD: {
         const { id } = payload;
         return { ...state, items: state.items.filter(thread => thread.uuid !== id) };
+    }
+    case TOGGLE_APPROVE_THREAD: {
+        const { id } = payload;
+        const threads = state.items.map((thread) => {
+            if (thread.uuid === id) {
+                return { ...thread, approuved: !thread.approuved };
+            }
+            return thread;
+        });
+        return { ...state, items: threads };
     }
     default:
         return state;
