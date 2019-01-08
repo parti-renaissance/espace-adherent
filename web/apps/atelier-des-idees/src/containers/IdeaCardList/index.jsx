2025898e8ed1d3addb32b0f29bee7ec1fe71d0b0
Adrien Casanova <haitaar@gmail.com>
2019-01-08 15:01:59 +0100

    [ADI] IdeaCardList: add empty state

--- web/apps/atelier-des-idees/src/containers/IdeaCardList/index.jsx
+++ web/apps/atelier-des-idees/src/containers/IdeaCardList/index.jsx
@@ -3,16 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ideaStatus } from '../../constants/api';
 import { selectLoadingState } from '../../redux/selectors/loading';
-import {
-    selectIdeasWithStatus,
-    selectIdeasMetadata,
-} from '../../redux/selectors/ideas';
+import { selectIdeasWithStatus, selectIdeasMetadata } from '../../redux/selectors/ideas';
 import { fetchNextIdeas, voteIdea } from '../../redux/thunk/ideas';
 import Button from '../../components/Button';
 import IdeaCardList from '../../components/IdeaCardList';
 
 function IdeaCardListContainer(props) {
-    return (
+    return props.ideas.length ? (
         <React.Fragment>
             <IdeaCardList
                 ideas={props.ideas}
@@ -22,14 +19,15 @@ function IdeaCardListContainer(props) {
             />
             {props.withPaging && (
                 <div className="idea-card-list__paging">
-                    <Button
-                        label="Plus d'idées"
-                        mode="tertiary"
-                        onClick={props.onMoreClicked}
-                    />
+                    <Button label="Plus d'idées" mode="tertiary" onClick={props.onMoreClicked} />
                 </div>
             )}
         </React.Fragment>
+    ) : (
+        <div className="idea-card-list__empty">
+            <img className="idea-card-list__empty__img" src="/assets/img/no-idea-result.svg" />
+            <p>Il n'y a pas d'idée correspondant à votre recherche</p>
+        </div>
     );
 }
 
@@ -45,18 +43,13 @@ IdeaCardListContainer.propTypes = {
 };
 
 function mapStateToProps(state, ownProps) {
-    const { isFetching } = selectLoadingState(
-        state,
-        'FETCH_IDEAS',
-        ownProps.status
-    );
+    const { isFetching } = selectLoadingState(state, 'FETCH_IDEAS', ownProps.status);
     const ideas = selectIdeasWithStatus(state, ownProps.status);
     /* paging data */
     const { current_page, last_page } = selectIdeasMetadata(state);
     // show paging if props says so and is not loading and is not at the end of the list
-    const withPaging =
-		ownProps.withPaging && current_page < last_page && !isFetching;
-    return { ideas, isLoading: isFetching && !ideas.length, withPaging };
+    const withPaging = ownProps.withPaging && current_page < last_page && !isFetching;
+    return { ideas: [], isLoading: isFetching && !ideas.length, withPaging };
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
