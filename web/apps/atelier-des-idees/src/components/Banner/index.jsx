eeacc89dce574ce09cdc3ab01209aafd3526baa9
Adrien Casanova <haitaar@gmail.com>
2019-01-09 14:18:50 +0100

    [ADI] refactor homepage style

--- web/apps/atelier-des-idees/src/components/Banner/index.jsx
+++ web/apps/atelier-des-idees/src/components/Banner/index.jsx
@@ -10,36 +10,39 @@ class Banner extends React.PureComponent {
     render() {
         return (
             <div className="banner">
-                <div className="banner__container__close">
-                    <button
-                        className="banner__container__close button--transparent"
-                        onClick={() => this.props.onClose()}>
-                        <img src={icn_close} />
-                    </button>
-                </div>
-                <div className="banner__container__titles">
-                    <h3 className="banner__container__titles__title">{this.props.name}</h3>
-                    <h4 className="banner__container__titles__subtitle">
-                        Du{' '}
-                        {new Date(this.props.started_at).toLocaleDateString('fr-fr', {
-                            day: 'numeric',
-                            month: 'long',
-                            year: 'numeric',
-                        })}{' '}
-                        au{' '}
-                        {new Date(this.props.ended_at).toLocaleDateString('fr-fr', {
-                            day: 'numeric',
-                            month: 'long',
-                            year: 'numeric',
-                        })}
-                    </h4>
-                </div>
-                <div className="banner__container">
-                    <a href={this.props.url} className="banner__container__link button--secondary" target="_blank">
-                        {`${this.props.linkLabel}${
-                            this.props.response_time ? ` (${this.props.response_time}MIN)` : ''
-                        }`}
-                    </a>
+                <div className="banner__main l__wrapper">
+                    <div className="banner__container__close">
+                        <button
+                            className="banner__container__close button--transparent"
+                            onClick={() => this.props.onClose()}
+                        >
+                            <img src="/assets/img/icn_close-white.svg" />
+                        </button>
+                    </div>
+                    <div className="banner__container__titles">
+                        <h3 className="banner__container__titles__title">{this.props.name}</h3>
+                        <h4 className="banner__container__titles__subtitle">
+                            Du{' '}
+                            {new Date(this.props.started_at).toLocaleDateString('fr-fr', {
+                                day: 'numeric',
+                                month: 'long',
+                                year: 'numeric',
+                            })}{' '}
+                            au{' '}
+                            {new Date(this.props.ended_at).toLocaleDateString('fr-fr', {
+                                day: 'numeric',
+                                month: 'long',
+                                year: 'numeric',
+                            })}
+                        </h4>
+                    </div>
+                    <div className="banner__container">
+                        <a href={this.props.url} className="banner__container__link button--secondary" target="_blank">
+                            {`${this.props.linkLabel}${
+                                this.props.response_time ? ` (${this.props.response_time}MIN)` : ''
+                            }`}
+                        </a>
+                    </div>
                 </div>
             </div>
         );
