47d4e3e286c62f4ed0d25c99502ec89cf2f76ec9
illusionOfParadise <olga.rotach@gmail.com>
2019-01-11 14:42:06 +0100

    [ADI] Change idea status management

---
+++ src/Doctrine/IdeaExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use AppBundle\Entity\IdeasWorkshop\Idea;
+use Doctrine\ORM\QueryBuilder;
+
+class IdeaExtension implements ContextAwareQueryCollectionExtensionInterface
+{
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null, array $context = [])
+    {
+        if (Idea::class === $resourceClass && !isset($context['filters']['author.uuid'])) {
+            $queryBuilder
+                ->andWhere(sprintf('%s.publishedAt IS NOT NULL', $queryBuilder->getRootAliases()[0]))
+            ;
+        }
+    }
+}
