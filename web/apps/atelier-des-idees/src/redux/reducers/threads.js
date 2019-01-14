e1631a798addfbc7da8d7a4f68bed6bef34970bf
Adrien Casanova <haitaar@gmail.com>
2019-01-14 11:06:28 +0100

    [ADI] add thread comments actions

--- web/apps/atelier-des-idees/src/redux/reducers/threads.js
+++ web/apps/atelier-des-idees/src/redux/reducers/threads.js
@@ -5,9 +5,12 @@ import {
     REMOVE_THREAD,
     TOGGLE_APPROVE_THREAD,
     SET_ANSWER_THREADS_PAGING,
+    SET_THREAD_COMMENTS,
+    ADD_THREAD_COMMENTS,
+    REMOVE_THREAD_COMMENT,
 } from '../constants/actionTypes';
 
-const initialState = { threads: [], comments: { items: [], metadata: {} }, paging: {} };
+const initialState = { threads: [], comments: [], paging: {} };
 
 function threadsReducer(state = initialState.threads, action) {
     const { type, payload } = action;
@@ -38,6 +41,14 @@ function threadsReducer(state = initialState.threads, action) {
 function commentsReducer(state = initialState.comments, action) {
     const { type, payload } = action;
     switch (type) {
+    case SET_THREAD_COMMENTS:
+        return payload.data;
+    case ADD_THREAD_COMMENTS:
+        return [...state, ...payload.data];
+    case REMOVE_THREAD_COMMENT: {
+        const { id } = payload;
+        return state.filter(comment => comment.uuid !== id);
+    }
     default:
         return state;
     }
