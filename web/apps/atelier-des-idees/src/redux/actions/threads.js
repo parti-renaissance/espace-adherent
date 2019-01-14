8f2a883158c2c0b3054d5c0cf49dd508ae36c14c
Adrien Casanova <haitaar@gmail.com>
2019-01-14 11:06:28 +0100

    [ADI] add thread comments actions

--- web/apps/atelier-des-idees/src/redux/actions/threads.js
+++ web/apps/atelier-des-idees/src/redux/actions/threads.js
@@ -5,6 +5,9 @@ import {
     REMOVE_THREAD,
     TOGGLE_APPROVE_THREAD,
     SET_ANSWER_THREADS_PAGING,
+    SET_THREAD_COMMENTS,
+    ADD_THREAD_COMMENTS,
+    REMOVE_THREAD_COMMENT,
 } from '../constants/actionTypes';
 
 export const setThreads = data => action(SET_THREADS, { data });
@@ -12,3 +15,6 @@ export const addThreads = data => action(ADD_THREADS, { data });
 export const removeThread = id => action(REMOVE_THREAD, { id });
 export const toggleApproveThread = id => action(TOGGLE_APPROVE_THREAD, { id });
 export const setAnswerThreadsPaging = (answerId, data) => action(SET_ANSWER_THREADS_PAGING, { answerId, data });
+export const setThreadComments = data => action(SET_THREAD_COMMENTS, { data });
+export const addThreadComments = data => action(ADD_THREAD_COMMENTS, { data });
+export const removeThreadComment = id => action(REMOVE_THREAD_COMMENT, { id });
