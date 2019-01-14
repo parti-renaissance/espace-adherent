ff20cefd9fb1dc03c13ba0841d5244124596b9b8
Adrien Casanova <haitaar@gmail.com>
2019-01-14 12:11:05 +0100

    [ADI] update thread total comments number when sending a new thread comment

--- web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
+++ web/apps/atelier-des-idees/src/redux/constants/actionTypes.js
@@ -29,6 +29,7 @@ export const POST_THREAD = createRequestTypes('POST_THREAD');
 export const POST_THREAD_COMMENT = createRequestTypes('POST_THREAD_COMMENT');
 export const SET_THREADS = 'SET_THREADS';
 export const ADD_THREADS = 'ADD_THREADS';
+export const UPDATE_THREAD = 'UPDATE_THREAD';
 export const REMOVE_THREAD = 'REMOVE_THREAD';
 export const TOGGLE_APPROVE_THREAD = 'TOGGLE_APPROVE_THREAD';
 export const SET_ANSWER_THREADS_PAGING = 'SET_ANSWER_THREADS_PAGING';
