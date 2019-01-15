347bd9ea5edd2c36a07d81b8604730bb75d88546
Aurélie Largent <aurelie.largent@abbeal.com>
2019-01-15 18:06:49 +0100

    [ADI] fix set ideas before fetchIdeas

--- web/apps/atelier-des-idees/src/redux/thunk/navigation.js
+++ web/apps/atelier-des-idees/src/redux/thunk/navigation.js
@@ -1,5 +1,11 @@
 import { ideaStatus } from '../../constants/api';
-import { fetchIdeas, fetchIdea, fetchUserIdeas, fetchUserContributions } from './ideas';
+import {
+	fetchIdeas,
+	fetchIdea,
+	fetchUserIdeas,
+	fetchUserContributions
+} from './ideas';
+import { setIdeas } from '../actions/ideas';
 import { fetchConsultationPinned } from './pinned';
 import { fetchReports } from './reports';
 import { fetchAuthUser } from './auth';
@@ -8,61 +14,64 @@ import { fetchStaticData } from './static';
 import { setCurrentIdea } from '../actions/currentIdea';
 
 export function initApp() {
-    return dispatch =>
-        Promise.all([
-            dispatch(fetchAuthUser()).then(() => dispatch(fetchUserIdeas())),
-            dispatch(fetchStaticData()),
-            dispatch(fetchUserContributions()),
-        ]);
+	return dispatch =>
+		Promise.all([
+			dispatch(fetchAuthUser()).then(() => dispatch(fetchUserIdeas())),
+			dispatch(fetchStaticData()),
+			dispatch(fetchUserContributions())
+		]);
 }
 
 export function initHomePage() {
-    const params = {
-        limit: 5,
-        "order['publishedAt']": 'DESC',
-    };
-    return dispatch =>
-        Promise.all([
-            // consultation pinned
-            dispatch(fetchConsultationPinned()),
-            // ideas
-            dispatch(fetchIdeas(ideaStatus.FINALIZED, params)),
-            dispatch(fetchIdeas(ideaStatus.PENDING, params)),
-            // reports
-            dispatch(fetchReports()),
-        ]);
+	const params = {
+		limit: 5,
+		"order['publishedAt']": 'DESC'
+	};
+	return dispatch =>
+		Promise.all([
+			// consultation pinned
+			dispatch(fetchConsultationPinned()),
+			// ideas
+			dispatch(setIdeas()),
+			dispatch(fetchIdeas(ideaStatus.FINALIZED, params)),
+			dispatch(fetchIdeas(ideaStatus.PENDING, params)),
+			// reports
+			dispatch(fetchReports())
+		]);
 }
 
 export function initContributePage() {
-    const params = {
-        limit: 10,
-        "order['publishedAt']": 'DESC',
-    };
-    return dispatch => dispatch(fetchIdeas(ideaStatus.PENDING, params, { setMode: true }));
+	const params = {
+		limit: 10,
+		"order['publishedAt']": 'DESC'
+	};
+	return dispatch =>
+		dispatch(fetchIdeas(ideaStatus.PENDING, params, { setMode: true }));
 }
 
 export function initConsultPage() {
-    const params = {
-        limit: 10,
-        "order['publishedAt']": 'DESC',
-    };
-    return dispatch => dispatch(fetchIdeas(ideaStatus.FINALIZED, params, { setMode: true }));
+	const params = {
+		limit: 10,
+		"order['publishedAt']": 'DESC'
+	};
+	return dispatch =>
+		dispatch(fetchIdeas(ideaStatus.FINALIZED, params, { setMode: true }));
 }
 
 export function initIdeaPageBase() {
-    return dispatch => dispatch(fetchGuidelines());
+	return dispatch => dispatch(fetchGuidelines());
 }
 
 export function initIdeaPage(id) {
-    return async dispatch => {
-        await dispatch(fetchIdea(id));
-        dispatch(initIdeaPageBase());
-    };
+	return async dispatch => {
+		await dispatch(fetchIdea(id));
+		dispatch(initIdeaPageBase());
+	};
 }
 
 export function initCreateIdeaPage() {
-    return dispatch => {
-        dispatch(initIdeaPageBase());
-        dispatch(setCurrentIdea());
-    };
+	return dispatch => {
+		dispatch(initIdeaPageBase());
+		dispatch(setCurrentIdea());
+	};
 }
