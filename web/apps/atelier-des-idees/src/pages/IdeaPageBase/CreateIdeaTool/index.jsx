ec6782a0172915aff812a5bd45fd0cf63c514fcd
Adrien Casanova <haitaar@gmail.com>
2019-01-15 11:08:23 +0100

    [ADI] CreateIdeaTool: fix questions numerotation

--- web/apps/atelier-des-idees/src/pages/IdeaPageBase/CreateIdeaTool/index.jsx
+++ web/apps/atelier-des-idees/src/pages/IdeaPageBase/CreateIdeaTool/index.jsx
@@ -22,31 +22,36 @@ class CreateIdeaTool extends React.Component {
     render() {
         return (
             <article className="create-idea-tool">
-                {this.props.guidelines.map((guideline, idx) => (
-                    <section key={`guideline_${idx}`} className="create-idea-tool__start-section">
-                        <div className="create-idea-tool__section-title">
-                            {/* TODO: uncomment */}
-                            {/* <p className="create-idea-tool__section-subtitle">{guideline.category_name}</p>*/}
-                            <h2 className="create-idea-tool__section-title__main">{guideline.name}</h2>
-                        </div>
-                        {guideline.questions.map(({ id, name, required, placeholder }, index) => (
-                            <QuestionBlock
-                                isAuthor={this.props.isAuthor}
-                                canCollapse={!required}
-                                initialContent={this.props.values[id]}
-                                key={id}
-                                mode={this.props.isDraft ? 'edit' : 'contribute'}
-                                question={name}
-                                questionId={id}
-                                placeholder={placeholder}
-                                nbQuestion={index + 1}
-                                onTextChange={(htmlContent, save = false) =>
-                                    this.props.onQuestionTextChange(id, htmlContent, save)
-                                }
-                            />
-                        ))}
-                    </section>
-                ))}
+                {this.props.guidelines.map((guideline, idx) => {
+                    const nbQOffset = this.props.guidelines[idx - 1]
+                        ? this.props.guidelines[idx - 1].questions.length
+                        : 0;
+                    return (
+                        <section key={`guideline_${idx}`} className="create-idea-tool__start-section">
+                            <div className="create-idea-tool__section-title">
+                                {/* TODO: uncomment */}
+                                {/* <p className="create-idea-tool__section-subtitle">{guideline.category_name}</p>*/}
+                                <h2 className="create-idea-tool__section-title__main">{guideline.name}</h2>
+                            </div>
+                            {guideline.questions.map(({ id, name, required, placeholder }, index) => (
+                                <QuestionBlock
+                                    isAuthor={this.props.isAuthor}
+                                    canCollapse={!required}
+                                    initialContent={this.props.values[id]}
+                                    key={id}
+                                    mode={this.props.isDraft ? 'edit' : 'contribute'}
+                                    question={name}
+                                    questionId={id}
+                                    placeholder={placeholder}
+                                    nbQuestion={nbQOffset + index + 1}
+                                    onTextChange={(htmlContent, save = false) =>
+                                        this.props.onQuestionTextChange(id, htmlContent, save)
+                                    }
+                                />
+                            ))}
+                        </section>
+                    );
+                })}
             </article>
         );
     }
