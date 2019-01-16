3771a8c66c2d70f30955758453641758a140b675
Adrien Casanova <haitaar@gmail.com>
2019-01-16 11:27:43 +0100

    [ADI] IdeaPage: reset loading states on unmount

--- web/apps/atelier-des-idees/src/redux/actions/loading.js
+++ web/apps/atelier-des-idees/src/redux/actions/loading.js
@@ -1,5 +1,7 @@
+import { RESET_LOADING } from '../constants/actionTypes';
 import { action } from '../helpers/actions';
 
 export const createRequest = (requestName, id = '') => action(requestName.REQUEST, { id });
 export const createRequestSuccess = (requestName, id = '') => action(requestName.SUCCESS, { id });
 export const createRequestFailure = (requestName, id = '') => action(requestName.FAILURE, { id });
+export const resetLoading = () => action(RESET_LOADING);
