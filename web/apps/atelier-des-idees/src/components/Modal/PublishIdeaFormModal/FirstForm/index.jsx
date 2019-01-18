d23c8dd2619d728aa726a3633822470b42dc56e8
Adrien Casanova <haitaar@gmail.com>
2019-01-18 17:20:32 +0100

    [ADI] PublishIdeaForm: add needs default values

--- web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/FirstForm/index.jsx
+++ web/apps/atelier-des-idees/src/components/Modal/PublishIdeaFormModal/FirstForm/index.jsx
@@ -104,7 +104,7 @@ class FirstForm extends React.Component {
                         )}
                         error={this.state.errors.locality}
                         onSelected={value => this.handleChange('locality', value)}
-                        defaultValue={this.state.inputs.locality}
+                        defaultValue={this.state.inputs.locality.length ? this.state.inputs.locality : undefined}
                     />
                 </div>
                 <button type="submit" className="first-form__button button--secondary">
