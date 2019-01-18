d87e09c28b07933e4c1544e595033869b6039c9a
Adrien Casanova <haitaar@gmail.com>
2019-01-18 15:46:26 +0100

    [ADI] Select: improve multi select style

--- web/apps/atelier-des-idees/src/components/Select/index.jsx
+++ web/apps/atelier-des-idees/src/components/Select/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import SelectComponent, { components } from 'react-select';
 
 const DropdownIndicator = props =>
@@ -26,15 +27,14 @@ class Select extends React.Component {
     // @param {object, array} selectedOption object by default and array if multi-select
     handleChange(selectedOption) {
         // check if max options selected
-        const isMaxOptionsSelected =
-			selectedOption && selectedOption.length > this.props.maxOptionsSelected;
+        const isMaxOptionsSelected = selectedOption && selectedOption.length > this.props.maxOptionsSelected;
 
         if (isMaxOptionsSelected) {
             // set the error
             this.setState({
-                errorMaxOptions: `Vous ne pouvez sélectionner que ${
-                    this.props.maxOptionsSelected
-                } option${1 < this.props.maxOptionsSelected ? 's' : ''}`,
+                errorMaxOptions: `Vous ne pouvez sélectionner que ${this.props.maxOptionsSelected} option${
+                    1 < this.props.maxOptionsSelected ? 's' : ''
+                }`,
             });
             // remove the option that is too much
             selectedOption.pop();
@@ -47,9 +47,7 @@ class Select extends React.Component {
 
         this.setState({ selectedOption });
 
-        const formatSelectedOption = this.props.isMulti
-            ? selectedOption
-            : [selectedOption];
+        const formatSelectedOption = this.props.isMulti ? selectedOption : [selectedOption];
 
         this.props.onSelected(formatSelectedOption);
     }
@@ -59,7 +57,7 @@ class Select extends React.Component {
         return (
             <div className="select">
                 <SelectComponent
-                    className="select__input"
+                    className={classNames('select__input', { 'select__input--multi': this.props.isMulti })}
                     classNamePrefix="select__input"
                     value={this.state.selectedOption}
                     components={{ DropdownIndicator, MultiValueLabel, MultiValueRemove }}
@@ -76,10 +74,7 @@ class Select extends React.Component {
                         }),
                         control: base => ({
                             ...base,
-                            borderColor:
-								this.props.error || this.state.errorMaxOptions
-								    ? '#ff4a22'
-								    : '#e5e5e5',
+                            borderColor: this.props.error || this.state.errorMaxOptions ? '#ff4a22' : '#e5e5e5',
                         }),
                     }}
                     onChange={this.handleChange}
@@ -93,17 +88,11 @@ class Select extends React.Component {
                     noOptionsMessage={() => 'Pas de résultat'}
                 />
                 {this.props.subtitle && (
-                    <div className="select__subtitle">
-                        {'function' === typeof SubTitle ? <SubTitle /> : SubTitle}
-                    </div>
+                    <div className="select__subtitle">{'function' === typeof SubTitle ? <SubTitle /> : SubTitle}</div>
                 )}
 
-                {this.props.error && (
-                    <p className="select__error">{this.props.error}</p>
-                )}
-                {this.state.errorMaxOptions && (
-                    <p className="select__error">{this.state.errorMaxOptions}</p>
-                )}
+                {this.props.error && <p className="select__error">{this.props.error}</p>}
+                {this.state.errorMaxOptions && <p className="select__error">{this.state.errorMaxOptions}</p>}
             </div>
         );
     }
@@ -122,11 +111,7 @@ Select.defaultProps = {
 };
 
 Select.propTypes = {
-    subtitle: PropTypes.oneOfType([
-        PropTypes.node.isRequired,
-        PropTypes.func,
-        PropTypes.string,
-    ]),
+    subtitle: PropTypes.oneOfType([PropTypes.node.isRequired, PropTypes.func, PropTypes.string]),
     error: PropTypes.string,
     options: PropTypes.arrayOf(
         PropTypes.shape({
