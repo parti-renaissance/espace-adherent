c7d04086eb33b877dbee71db52e9b44325c62b31
Adrien Casanova <haitaar@gmail.com>
2019-01-24 11:41:04 +0100

    [ADI] remove app story file

--- web/apps/atelier-des-idees/src/App.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import App from './App';
-
-storiesOf('App', module)
-    .addParameters({ jest: ['App'] })
-    .add('default', () => <App />);
