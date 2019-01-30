0ad8eecea870cfde30d580ce544fe980cf78934c
Adrien Casanova <haitaar@gmail.com>
2019-01-30 17:43:01 +0100

    [ADI] create MyNicknameModal

---
+++ web/apps/atelier-des-idees/src/components/Modal/MyNicknameModal/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyNicknameModal(props) {
+    return (
+        <form className="my-nickname-modal">
+            <h2 className="my-nickname-modal__title">Mon pseudo</h2>
+        </form>
+    );
+}
+
+export default MyNicknameModal;
