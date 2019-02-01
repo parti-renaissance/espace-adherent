02b3d9d193e48061679f2d753623cbcdad7c4f52
Adrien Casanova <haitaar@gmail.com>
2019-02-01 11:07:29 +0100

    [ADI] Input: add maxLength prop and counter

--- web/apps/atelier-des-idees/src/components/Input/index.jsx
+++ web/apps/atelier-des-idees/src/components/Input/index.jsx
@@ -2,25 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-function Input(props) {
-    return (
-        <div
-            className={classNames('text-input', props.className, {
-                'text-input--error': !!props.error,
-            })}
-        >
-            <input
-                className="text-input__input"
-                disabled={props.disabled}
-                id={props.id}
-                name={props.name}
-                onChange={e => props.onChange(e.target.value)}
-                placeholder={props.placeholder}
-                value={props.value}
-            />
-            {props.error && <p className="text-input__error">{props.error}</p>}
-        </div>
-    );
+class Input extends React.Component {
+    handleChange(value) {
+        this.props.onChange(value);
+    }
+
+    render() {
+        return (
+            <div
+                className={classNames('text-input', this.props.className, {
+                    'text-input--error': !!this.props.error,
+                    'text-input--count': !!this.props.maxLength,
+                })}
+            >
+                <div className="text-input__input-wrapper">
+                    <input
+                        className="text-input__input"
+                        disabled={this.props.disabled}
+                        id={this.props.id}
+                        maxLength={this.props.maxLength}
+                        name={this.props.name}
+                        onChange={e => this.handleChange(e.target.value)}
+                        placeholder={this.props.placeholder}
+                        value={this.props.value}
+                    />
+                    {this.props.maxLength && (
+                        <div className="text-input__counter">{`${this.props.value.length} / ${
+                            this.props.maxLength
+                        }`}</div>
+                    )}
+                </div>
+                {this.props.error && <p className="text-input__error">{this.props.error}</p>}
+            </div>
+        );
+    }
 }
 
 Input.defaultProps = {
@@ -28,6 +43,7 @@ Input.defaultProps = {
     className: '',
     error: '',
     id: '',
+    maxLength: undefined,
     name: '',
     placeholder: '',
 };
@@ -37,6 +53,7 @@ Input.propTypes = {
     disabled: PropTypes.bool,
     error: PropTypes.string,
     id: PropTypes.string,
+    maxLength: PropTypes.number,
     name: PropTypes.string,
     onChange: PropTypes.func.isRequired,
     placeholder: PropTypes.string,
