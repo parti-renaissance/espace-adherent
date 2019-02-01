150cbc21f7ed4895112b4556879581e651e0682c
Adrien Casanova <haitaar@gmail.com>
2019-02-01 18:01:03 +0100

    [ADI] IdeaPageBase: create IdeaPageHeader

---
+++ web/apps/atelier-des-idees/src/pages/IdeaPageBase/IdeaPageHeader/index.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ideaStatus } from '../../../constants/api';
+import CreateIdeaActions from '../CreateIdeaActions';
+import Switch from '../../../components/Switch';
+import icn_close from '../../../img/icn_close.svg';
+
+class IdeaPageHeader extends React.Component {
+    render() {
+        return (
+            <section className="create-idea-page__header">
+                <div className="create-idea-page__header__inner l__wrapper">
+                    {this.props.showContent && (
+                        <React.Fragment>
+                            <button className="button create-idea-actions__back" onClick={this.props.onBackClicked}>
+                                ← Retour
+                            </button>
+                            {this.props.status !== ideaStatus.FINALIZED && this.props.canToggleReadingMode && (
+                                <Switch onChange={this.props.toggleReadingMode} label="Passer en mode lecture" />
+                            )}
+                            {this.props.isAuthenticated &&
+                                (this.props.isAuthor ? (
+                                    <CreateIdeaActions
+                                        onDeleteClicked={this.props.onDeleteClicked}
+                                        onPublishClicked={this.props.onPublishClicked}
+                                        onSaveClicked={this.props.onSaveClicked}
+                                        isDraft={this.props.status === ideaStatus.DRAFT}
+                                        canPublish={
+                                            this.props.status === ideaStatus.DRAFT ||
+                                            this.props.status === ideaStatus.PENDING
+                                        }
+                                    />
+                                ) : (
+                                    <button
+                                        className="button create-idea-actions__report"
+                                        onClick={this.props.onReportClicked}
+                                    >
+                                        Signaler la proposition
+                                    </button>
+                                ))}
+                        </React.Fragment>
+                    )}
+                </div>
+                {this.props.showSaveBanner && (
+                    <div className="create-idea-page__success-banner">
+                        <span>
+                            {'DRAFT' === this.props.status
+                                ? 'Votre brouillon a bien été enregistré'
+                                : 'Vos modifications ont bien été enregistrées'}
+                        </span>
+                        <button
+                            className="create-idea-page__success-banner__close"
+                            onClick={() => {
+                                this.props.closeSaveBanner();
+                                clearTimeout(this.saveBannerTimer);
+                            }}
+                        >
+                            <img src={icn_close} />
+                        </button>
+                    </div>
+                )}
+            </section>
+        );
+    }
+}
+
+IdeaPageHeader.defaultProps = {
+    canToggleReadingMode: false,
+    isAuthenticated: false,
+    isAuthor: false,
+    showContent: true,
+    showSaveBanner: false,
+};
+
+IdeaPageHeader.propTypes = {
+    canToggleReadingMode: PropTypes.bool,
+    closeSaveBanner: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+    isAuthor: PropTypes.bool,
+    onBackClicked: PropTypes.func.isRequired,
+    onDeleteClicked: PropTypes.func.isRequired,
+    onPublishClicked: PropTypes.func.isRequired,
+    onReportClicked: PropTypes.func.isRequired,
+    onSaveClicked: PropTypes.func.isRequired,
+    showContent: PropTypes.bool,
+    showSaveBanner: PropTypes.bool,
+    status: PropTypes.string,
+    toggleReadingMode: PropTypes.func.isRequired,
+};
+
+export default IdeaPageHeader;
