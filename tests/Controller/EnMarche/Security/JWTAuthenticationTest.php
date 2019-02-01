50b74e9b754873624e2249c11686cbe85d6290c3
Rémi Gardien <remi@gardien.biz>
2019-02-01 19:42:55 +0100

    Removing debug statement

--- tests/Controller/EnMarche/Security/JWTAuthenticationTest.php
+++ tests/Controller/EnMarche/Security/JWTAuthenticationTest.php
@@ -26,8 +26,6 @@ class JWTAuthenticationTest extends WebTestCase
             'password' => 'secret!12345',
         ]));
         $response = $this->client->getResponse();
-        dump($response->getContent());
-        die;
         $this->isSuccessful($response);
         $this->assertJson($content = $response->getContent());
 
