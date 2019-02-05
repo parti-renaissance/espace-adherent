ca339fb9ac360b6cfd6d719a196281ff7165bfb3
Adrien Casanova <haitaar@gmail.com>
2019-02-05 16:52:48 +0100

    [ADI] IdeaCard: improve title style

--- web/apps/atelier-des-idees/src/components/IdeaCard/VotingFooter/index.jsx
+++ web/apps/atelier-des-idees/src/components/IdeaCard/VotingFooter/index.jsx
@@ -80,11 +80,11 @@ class VotingFooter extends React.Component {
                     </button>
 
                     {!this.state.toggleVotes &&
-						(0 < this.props.totalVotes ? (
-						    <p className="voting-footer__total-votes">{this.props.totalVotes} votes</p>
-						) : (
-						    <p />
-						))}
+                        (0 < this.props.totalVotes ? (
+                            <p className="voting-footer__total-votes">{this.props.totalVotes} votes</p>
+                        ) : (
+                            <p />
+                        ))}
                     {!this.state.toggleVotes ? (
                         <div className="voting-footer__container__action-vote">
                             <button
@@ -97,7 +97,7 @@ class VotingFooter extends React.Component {
                                 }
                             >
                                 <img className="voting-footer__container__action-vote__icon" src={icn_20px_thumb} />
-								Cette proposition est
+                                Je vote
                             </button>
                         </div>
                     ) : (
@@ -107,21 +107,21 @@ class VotingFooter extends React.Component {
 
                 {/* VOTES BUTTONS */}
                 {this.state.toggleVotes &&
-					this.props.votes.map((vote, index) => (
-					    <button
-					        key={vote.id}
-					        className={classnames('voting-footer__vote', `voting-footer__vote-${index}`, {
-					            'voting-footer__vote--selected': vote.isSelected,
-					        })}
-					        onClick={() => {
-					            this.props.onSelected(vote.id);
-					            this.resetTimeout();
-					        }}
-					    >
-					        <span className="voting-footer__vote__name">{vote.name}</span>
-					        <span className="voting-footer__vote__count">{vote.count}</span>
-					    </button>
-					))}
+                    this.props.votes.map((vote, index) => (
+                        <button
+                            key={vote.id}
+                            className={classnames('voting-footer__vote', `voting-footer__vote-${index}`, {
+                                'voting-footer__vote--selected': vote.isSelected,
+                            })}
+                            onClick={() => {
+                                this.props.onSelected(vote.id);
+                                this.resetTimeout();
+                            }}
+                        >
+                            <span className="voting-footer__vote__name">{vote.name}</span>
+                            <span className="voting-footer__vote__count">{vote.count}</span>
+                        </button>
+                    ))}
             </div>
         );
     }
