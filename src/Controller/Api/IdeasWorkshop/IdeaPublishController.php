63a02bf62b8f880116b23ba6ff13ec7abf43efe7
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-02-07 17:16:26 +0100

    [Logger] Do not log BadRequest exception

--- src/Controller/Api/IdeasWorkshop/IdeaPublishController.php
+++ src/Controller/Api/IdeasWorkshop/IdeaPublishController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller\Api;
+namespace AppBundle\Controller\Api\IdeasWorkshop;
 
 use AppBundle\Entity\IdeasWorkshop\Idea;
 use Symfony\Component\HttpFoundation\Request;
---
+++ tests/Controller/Api/IdeasWorkshop/IdeaPublishControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\AppBundle\Controller\Api\IdeasWorkshop;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Tests\AppBundle\Controller\ControllerTestTrait;
+
+class IdeaPublishControllerTest extends WebTestCase
+{
+    use ControllerTestTrait;
+
+    public function testPublishDraftIdeaGeneratesBadResponse(): void
+    {
+        $this->authenticateAsAdherent($this->client, 'jacques.picard@en-marche.fr');
+
+        $this->client->request('PUT', '/api/ideas-workshop/ideas/c14937d6-fd42-465c-8419-ced37f3e6194/publish');
+
+        self::assertSame(400, $this->client->getResponse()->getStatusCode());
+    }
+
+    protected function setUp()
+    {
+        $this->init();
+        parent::setUp();
+    }
+}
