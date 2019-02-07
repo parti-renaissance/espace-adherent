5af11cadebf8e3acd4b8ebf806a2ec781efb980c
Brian Whitton <me@brianwhitton.com>
2019-02-07 17:34:31 -0500

    update my ideas reducer to namespace ideas based on status

--- web/apps/atelier-des-idees/src/redux/reducers/myIdeas.js
+++ web/apps/atelier-des-idees/src/redux/reducers/myIdeas.js
@@ -1,13 +1,36 @@
 import { SET_MY_IDEAS, REMOVE_MY_IDEA } from '../constants/actionTypes';
+import { ideaStatus } from '../../constants/api';
 
-export const initialState = { items: [], metadata: {} };
+const {
+    FINALIZED,
+    DRAFT,
+    PENDING,
+} = ideaStatus;
+
+export const initialState = {
+    [DRAFT]: {
+        items: [],
+        metadata: {},
+    },
+    [PENDING]: {
+        items: [],
+        metadata: {},
+    },
+    [FINALIZED]: {
+        items: [],
+        metadata: {},
+    },
+};
 
 const ideasReducer = (state = initialState, action) => {
     const { type, payload } = action;
     switch (type) {
     case SET_MY_IDEAS: {
-        const { items, metadata } = payload;
-        return { items, metadata };
+        const { items, metadata, namespace } = payload;
+        return {
+            ...state,
+            [namespace]: { items, metadata },
+        };
     }
     case REMOVE_MY_IDEA: {
         return { ...state, items: state.items.filter(idea => idea.uuid !== payload.id) };
