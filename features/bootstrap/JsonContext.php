48d31ee0aeba96935a13ee3de408df238f9c3b18
Jeremie Samson <jeremie.samson76@gmail.com>
2019-02-12 17:02:02 +0100

    [ADI] Refacto tests

--- features/bootstrap/JsonContext.php
+++ features/bootstrap/JsonContext.php
@@ -4,6 +4,7 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Mink\Exception\ExpectationException;
 use Behatch\Context\JsonContext as BehatchJsonContext;
 use Coduo\PHPMatcher\PHPMatcher;
+use Behat\Gherkin\Node\TableNode;
 
 class JsonContext extends BehatchJsonContext
 {
@@ -14,9 +15,37 @@ class JsonContext extends BehatchJsonContext
 
     public function assertJson(string $expected, string $actual): void
     {
-        // Remove all useless whitespaces
         $expected = preg_replace('/\s(?=([^"]*"[^"]*")*[^"]*$)/', '', $expected);
 
+        $this->match($expected, $actual);
+    }
+
+    /**
+     * Checks, that given JSON nodes match values
+     *
+     * @Then the JSON nodes should match:
+     */
+    public function theJsonNodesShouldMatch(TableNode $nodes)
+    {
+        foreach ($nodes->getRowsHash() as $node => $text) {
+            $this->theJsonNodeShouldMatch($node, $text);
+        }
+    }
+
+    /**
+     * Checks, that given JSON node matches given value
+     *
+     * @Then the JSON node :node should match :text
+     */
+    public function theJsonNodeShouldMatch($node, $text)
+    {
+        $actual = $this->inspector->evaluate($this->getJson(), $node);
+
+        $this->match($text, \is_bool($actual) ? json_encode($actual) : (string) $actual);
+    }
+
+    protected function match($expected, $actual)
+    {
         if (!PHPMatcher::match($actual, $expected, $error)) {
             throw new ExpectationException($error, $this->getSession());
         }
