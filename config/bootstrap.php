76ee594a39b6ad007b92145e691571d3b7fe1ceb
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-02-14 20:52:38 +0100

    [Flex] Rename web dir to public

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,7 +5,6 @@ use Symfony\Component\Dotenv\Dotenv;
 require \dirname(__DIR__).'/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
-// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (\is_array($env = @include \dirname(__DIR__).'/.env.local.php')) {
     $_SERVER += $env;
     $_ENV += $env;
