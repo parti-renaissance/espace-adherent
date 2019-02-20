947f9ef0241115a6266670c583841af951bacaeb
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2019-02-20 17:54:19 +0100

    [Jecoute] Fixed typo

--- src/Validator/DataSurveyConstraint.php
+++ src/Validator/DataSurveyConstraint.php
@@ -12,7 +12,7 @@ class DataSurveyConstraint extends Constraint
 {
     public $agreedToStayInContactRequired = 'data.survey.agreed_to_stay_in_contact.required';
 
-    public $emailAddresseRequired = 'data.survey.email_address.required';
+    public $emailAddressRequired = 'data.survey.email_address.required';
 
     public $genderChoiceOtherNotSelectedMessage = 'data.survey.gender_choice_other.not_selected';
     public $genderOtherEmptyMessage = 'data.survey.gender_other.empty';
