6cd829ebff708f8ef41f4a5e89bff3e7f2563cb8
Charles d'Oiron <doironcharles@gmail.com>
2019-02-26 13:15:00 +0100

    Fix error after delete my idea"

--- web/apps/atelier-des-idees/src/redux/reducers/myIdeas.js
+++ web/apps/atelier-des-idees/src/redux/reducers/myIdeas.js
@@ -1,11 +1,7 @@
 import { SET_MY_IDEAS, REMOVE_MY_IDEA } from '../constants/actionTypes';
 import { ideaStatus } from '../../constants/api';
 
-const {
-    FINALIZED,
-    DRAFT,
-    PENDING,
-} = ideaStatus;
+const { FINALIZED, DRAFT, PENDING } = ideaStatus;
 
 export const initialState = {
     [DRAFT]: {
@@ -33,7 +29,7 @@ const ideasReducer = (state = initialState, action) => {
         };
     }
     case REMOVE_MY_IDEA: {
-        return { ...state, items: state.items.filter(idea => idea.uuid !== payload.id) };
+        return { ...state, items: state.items && state.items.filter(idea => idea.uuid !== payload.id) };
     }
     default:
         return state;
--- web/apps/atelier-des-idees/src/redux/store/index.js
+++ web/apps/atelier-des-idees/src/redux/store/index.js
@@ -1,5 +1,12 @@
-if (process.env.NODE_ENV  === 'production') {
+// Incomment before Merge
+
+if ('production' === process.env.NODE_ENV) {
     module.exports = require('./configStore.prod');
 } else {
     module.exports = require('./configStore.dev');
 }
+
+// Comment before Merge
+// Activate REDUX_DEVTOOLS_EXTENSION_COMPOSE in symfony dev env
+
+// module.exports = require('./configStore.dev');
