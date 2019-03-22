aafca9be4adeb7e48cff9f0cb27cbdb6f3cd171d
Jeremie Samson <jeremie.samson76@gmail.com>
2019-03-22 11:34:48 +0100

    [Assessor] Add entity and migration

---
+++ src/Entity/AssessorOfficeEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use MyCLabs\Enum\Enum;
+
+class AssessorOfficeEnum extends Enum
+{
+    public const HOLDER = 'Titulaire';
+    public const SUBSTITUTE = 'Suppléant';
+}
