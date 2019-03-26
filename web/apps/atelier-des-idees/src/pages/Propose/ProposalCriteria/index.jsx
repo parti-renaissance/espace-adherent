ac16767f96804af5df2955b9f8c6a5144742032e
Charles d'Oiron <doironcharles@gmail.com>
2019-03-26 16:41:16 +0100

    add change how it work page

--- web/apps/atelier-des-idees/src/pages/Propose/ProposalCriteria/index.jsx
+++ web/apps/atelier-des-idees/src/pages/Propose/ProposalCriteria/index.jsx
@@ -10,64 +10,60 @@ class ProposalCriteria extends React.PureComponent {
                 <div className="proposal-criteria">
                     <div className="proposal-criteria__container">
                         <h2 className="proposal-criteria__container__title">
-              Vous voulez vous lancer&nbsp;? Voici quelques conseils avant de commencer
+                            Vous voulez vous lancer&nbsp;? Voici quelques conseils avant de commencer
                         </h2>
-                        <a
-                            href="/atelier-des-idees/creer-ma-proposition?anonymous_authentication_intention=/connexion"
-                            className="button button--primary proposal-criteria__container__link"
-                        >
-              J'ai une proposition
-                        </a>
                     </div>
                     <div className="proposal-criteria__container">
                         <div className="proposal-criteria__container__item">
                             <p className="proposal-criteria__container__item__text">
                                 <span className="proposal-criteria__container__item__text__main">
-                  Votre proposition apporte une réponse :
+                                    Votre proposition apporte une réponse :
                                 </span>{' '}
-                elle doit dépasser le constat.
+                                elle doit dépasser le constat.
                             </p>
                         </div>
                         <div className="proposal-criteria__container__item">
                             <p className="proposal-criteria__container__item__text">
                                 <span className="proposal-criteria__container__item__text__main">
-                  Votre proposition est généralisable :
+                                    Votre proposition est généralisable :
                                 </span>{' '}
-                votre idée peut s’appliquer sur tout le territoire français ou européen. Pour les idées qui concernent
-                spécifiquement votre territoire, rapprochez vous de votre comité local ou référent !
+                                votre idée peut s’appliquer sur tout le territoire français ou européen. Pour les idées
+                                qui concernent spécifiquement votre territoire, rapprochez vous de votre comité local ou
+                                référent !
                             </p>
                         </div>
                         <div className="proposal-criteria__container__item">
                             <p className="proposal-criteria__container__item__text">
-                                <span className="proposal-criteria__container__item__text__main">Votre proposition est nouvelle :</span>{' '}
-                il ne s’agit pas d’une idée déjà publiée ou mise en œuvre. Pensez à consulter les&nbsp;
+                                <span className="proposal-criteria__container__item__text__main">
+                                    Votre proposition est nouvelle :
+                                </span>{' '}
+                                il ne s’agit pas d’une idée déjà publiée ou mise en œuvre. Pensez à consulter les&nbsp;
                                 <a href="/atelier-des-idees/contribuer" target="_blank">
-                  idées en cours
+                                    idées en cours
                                 </a>
-                &nbsp;ou déjà&nbsp;
+                                &nbsp;ou déjà&nbsp;
                                 <a href="/atelier-des-idees/soutenir" target="_blank">
-                  finalisées
+                                    finalisées
                                 </a>
-                &nbsp; ainsi que le programme présidentiel.
+                                &nbsp; ainsi que le programme présidentiel.
                             </p>
                         </div>
                         <div className="proposal-criteria__container__item">
                             <p className="proposal-criteria__container__item__text">
                                 <span className="proposal-criteria__container__item__text__main">
-                  Votre proposition est en accord avec notre{' '}
+                                    Votre proposition est en accord avec notre{' '}
                                     <a
                                         href="https://storage.googleapis.com/en-marche-prod/documents/adherents/1-charte-et-manifeste/charte_des_valeurs.pdf"
                                         target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                    Charte des valeurs
+                                        rel="noopener noreferrer">
+                                        Charte des valeurs
                                     </a>
                                 </span>{' '}
-                et respecte nos&nbsp;
+                                et respecte nos&nbsp;
                                 <a href="/atelier-des-idees/conditions-generales-utilisation" target="_blank">
-                  Conditions Générales d'Utilisation
+                                    Conditions Générales d'Utilisation
                                 </a>
-                .{' '}
+                                .{' '}
                             </p>
                         </div>
                     </div>
