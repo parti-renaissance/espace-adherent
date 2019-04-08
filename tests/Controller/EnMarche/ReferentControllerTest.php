03ec9b94f4177687510d9dc31b521af6e2bb6137
illusionOfParadise <olga.rotach@gmail.com>
2019-04-08 17:37:41 +0200

    [REFERENT SPACE] Add citizen projects to referent space

--- tests/Controller/EnMarche/ReferentControllerTest.php
+++ tests/Controller/EnMarche/ReferentControllerTest.php
@@ -584,6 +584,7 @@ class ReferentControllerTest extends WebTestCase
             ['/espace-referent/utilisateurs'],
             ['/espace-referent/evenements'],
             ['/espace-referent/comites'],
+            ['/espace-referent/projets-citoyens'],
             ['/espace-referent/evenements/creer'],
         ];
     }
