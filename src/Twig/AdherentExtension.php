f26455b631bd89f64a5818390aeb50532f24fb18
illusionOfParadise <olga.rotach@gmail.com>
2019-04-15 18:00:46 +0200

    [REFERENT SPACE] Show previous connection date in the referent space

--- src/Twig/AdherentExtension.php
+++ src/Twig/AdherentExtension.php
@@ -2,59 +2,17 @@
 
 namespace AppBundle\Twig;
 
-use AppBundle\Entity\Adherent;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class AdherentExtension extends AbstractExtension
 {
-    private $memberInterests;
-
-    public function __construct(array $interests)
-    {
-        $this->memberInterests = $interests;
-    }
-
     public function getFunctions()
     {
         return [
-            new TwigFunction('member_interest_label', [$this, 'getMemberInterestLabel']),
-            new TwigFunction('get_user_level_label', [$this, 'getUserLevelLabel']),
+            new TwigFunction('member_interest_label', [AdherentRuntime::class, 'getMemberInterestLabel']),
+            new TwigFunction('get_user_level_label', [AdherentRuntime::class, 'getUserLevelLabel']),
+            new TwigFunction('get_referent_previous_visit_date', [AdherentRuntime::class, 'getReferentPreviousVisitDate']),
         ];
-    }
-
-    public function getMemberInterestLabel(string $interest)
-    {
-        if (!isset($this->memberInterests[$interest])) {
-            return '';
-        }
-
-        return $this->memberInterests[$interest];
-    }
-
-    public function getUserLevelLabel(Adherent $adherent): string
-    {
-        if (!$adherent->isAdherent()) {
-            return 'Non-adhérent(e)';
-        }
-
-        if ($adherent->isReferent()) {
-            return $adherent->isFemale() ? 'Référente 🥇' : 'Référent 🥇';
-        }
-
-        if ($adherent->isDeputy()) {
-            return $adherent->isFemale() ? 'Députée 🏛' : 'Député 🏛';
-        }
-
-        if ($adherent->isSupervisor()) {
-            return $adherent->isFemale() ? 'Animatrice 🏅' : 'Animateur 🏅';
-        }
-
-        if ($adherent->isHost()) {
-            return $adherent->isFemale() ? 'Co-animatrice 🏅' : 'Co-animateur 🏅';
-        }
-
-        // It means the user is an adherent
-        return $adherent->isFemale() ? 'Adhérente 😍' : 'Adhérent 😍';
     }
 }
