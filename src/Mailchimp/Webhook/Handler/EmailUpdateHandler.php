00ae1bea291b3dfda949aeefd4a918add40a33a2
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-05-14 09:29:17 +0200

    [Mailchimp] add webhook controller

---
+++ src/Mailchimp/Webhook/Handler/EmailUpdateHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Mailchimp\Webhook\Handler;
+
+use AppBundle\Mailchimp\Webhook\EventTypeEnum;
+use AppBundle\Membership\AdherentChangeEmailHandler;
+use AppBundle\Repository\AdherentRepository;
+
+class EmailUpdateHandler implements WebhookHandlerInterface
+{
+    private $adherentRepository;
+    private $adherentChangeEmailHandler;
+    private $listId;
+
+    public function __construct(
+        AdherentRepository $adherentRepository,
+        AdherentChangeEmailHandler $adherentChangeEmailHandler,
+        string $listId
+    ) {
+        $this->adherentRepository = $adherentRepository;
+        $this->adherentChangeEmailHandler = $adherentChangeEmailHandler;
+        $this->listId = $listId;
+    }
+
+    public function handle(array $data): void
+    {
+        if ($data['list_id'] !== $this->listId) {
+            return;
+        }
+
+        if ($adherent = $this->adherentRepository->findOneByEmail($data['old_email'])) {
+            $this->adherentChangeEmailHandler->handleRequest($adherent, $data['new_email']);
+        }
+    }
+
+    public function support(string $type): bool
+    {
+        return EventTypeEnum::UPDATE_EMAIL === $type;
+    }
+}
