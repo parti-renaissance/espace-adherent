e948e42f8c5d2427749b55d7ebd1c72224787523
Rémi Gardien <remi@gardien.biz>
2019-05-24 15:35:15 +0200

    Autocomplete address

--- front/pages/application_request.js
+++ front/pages/application_request.js
@@ -3,17 +3,16 @@ import formValidator from '../validator/formValidator';
 import AutocompletedAddressForm from '../services/address/AutocompletedAddressForm';
 import AddressObject from '../services/address/AddressObject';
 
-export default (formType) => {
+export default (volunteerFormType, runningMateFormType) => {
+    /**
+     * Volunteer request form
+     */
     const volunteerForm = dom('form[name="volunteer_request"]');
     const volunteerRootIdField = `#${volunteerForm.name}`;
 
     const volunteerCountryField = dom(`${volunteerRootIdField}_address_country`);
     const volunteerCityNameField = dom(`${volunteerRootIdField}_address_cityName`);
     const volunteerZipCodeField = dom(`${volunteerRootIdField}_address_postalCode`);
-
-    if (null === volunteerCountryField.value || 'FR' === volunteerCountryField.value) {
-        hide(regionField);
-    }
 
     const volunteerAddress = new AddressObject(
         dom(`${volunteerRootIdField}_address_address`),
@@ -41,8 +40,48 @@ export default (formType) => {
 
     volunteerAutocompleteAddressForm.buildWidget();
 
-
     volunteerZipCodeField.dispatchEvent(new Event('input'));
 
-    formValidator(formType, volunteerForm);
+    formValidator(volunteerFormType, volunteerForm);
+
+
+    /**
+     * Running mate request form
+     */
+    const runningMateForm = dom('form[name="running_mate_request"]');
+    const runningMateRootIdField = `#${runningMateForm.name}`;
+
+    const runningMateCountryField = dom(`${runningMateRootIdField}_address_country`);
+    const runningMateCityNameField = dom(`${runningMateRootIdField}_address_cityName`);
+    const runningMateZipCodeField = dom(`${runningMateRootIdField}_address_postalCode`);
+
+    const runningMateAddress = new AddressObject(
+        dom(`${runningMateRootIdField}_address_address`),
+        runningMateZipCodeField,
+        runningMateCityNameField,
+        null,
+        runningMateCountryField
+    );
+
+    const runningMateAutocompleteAddressForm = new AutocompletedAddressForm(
+        dom('form[name="running_mate_request"] .address-autocomplete'),
+        dom('form[name="running_mate_request"] .address-block'),
+        runningMateAddress
+    );
+
+    runningMateAutocompleteAddressForm.once('changed', () => {
+        runningMateCountryField.dispatchEvent(new CustomEvent('change', {
+            target: runningMateCountryField,
+            detail: {
+                zipCode: runningMateZipCodeField.value,
+                cityName: runningMateCityNameField.value,
+            },
+        }));
+    });
+
+    runningMateAutocompleteAddressForm.buildWidget();
+
+    runningMateZipCodeField.dispatchEvent(new Event('input'));
+
+    formValidator(runningMateFormType, runningMateForm);
 };
