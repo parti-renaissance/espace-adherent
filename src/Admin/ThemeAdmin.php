f7cc9f61ef789f5103973db64d5176c4edcbee4c
THERAGE Kévin <kevin.therage@sensiolabs.com>
2019-05-28 10:19:15 +0200

    Adding administration interface for Theme entity

---
+++ src/Admin/ThemeAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ThemeAdmin extends AbstractAdmin
+{
+    protected $datagridValues = [
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'name',
+    ];
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('name', null, [
+                'label' => 'Nom',
+            ])
+            ->add('_action', null, [
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+        ;
+    }
+
+}
