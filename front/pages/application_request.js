ec88bc4b5015daa521e6e3618080b41126254059
Rémi Gardien <remi@gardien.biz>
2019-05-29 15:31:31 +0200

    Fix javascript CS

--- front/pages/application_request.js
+++ front/pages/application_request.js
@@ -86,67 +86,69 @@ export default (volunteerFormType, runningMateFormType) => {
     runningMateZipCodeField.dispatchEvent(new Event('input'));
 
     formValidator(runningMateFormType, runningMateForm);
+
+    // Display uploaded file name
+    (function($, window, document, undefined)
+    {
+        $('.em-form__file--area').each(function() {
+            var $input	 = $(this),
+                $label	 = $input.siblings('.em-form__file--label'),
+                labelVal = $label.html(),
+                $fileName   = $input.siblings('.em-form__file--name'),
+                $fileDefault   = $fileName.html();
+
+            $input.on('change', function(e) {
+                var fileValue = '';
+
+                if (this.files && this.files.length > 1) {
+                    fileValue = (this.getAttribute('data-multiple-caption') || '')
+                        .replace('{count}', this.files.length);
+                } else if(e.target.value) {
+                    fileValue = e.target.value.split('\\').pop();
+                }
+
+                if (fileValue) {
+                    $label.html('Modifier la pièce jointe');
+                    $fileName.html(fileValue);
+                } else {
+                    $label.html(labelVal);
+                    $fileName.html(fileDefault);
+                }
+            });
+        });
+    })( jQuery, window, document );
+
+    // Form swaper
+    $(document).ready(function(){
+        $('#volunteer-form').hide();
+        $('#js-rolePicker .pick-btn').click(function(){
+            var $this = $(this);
+            $this.addClass('selected');
+            $this.siblings().removeClass('selected');
+
+            if ($this.attr('id') === 'js-RunningMate') {
+                $('#volunteer-form').fadeOut();
+                setTimeout(function(){
+                    $('#running-mate-form').fadeIn();
+                }, 400);
+            }
+
+            if ($this.attr('id') === 'js-Volunteer') {
+                $('#running-mate-form').fadeOut();
+                setTimeout(function(){
+                    $('#volunteer-form').fadeIn();
+                }, 400);
+            }
+        });
+
+        $('#js-rolePicker .pick-btn').hover(
+            function() {
+                $(this).siblings().addClass('fade');
+            },
+            function() {
+                $(this).siblings().removeClass('fade');
+            }
+        );
+    });
+
 };
-
-// Display uploaded file name
-'use strict';
-
-;(function($, window, document, undefined)
-{
-    $('.em-form__file--area').each(function() {
-		var $input	 = $(this),
-			$label	 = $input.siblings('.em-form__file--label'),
-            labelVal = $label.html(),
-            $fileName   = $input.siblings('.em-form__file--name'),
-            $fileDefault   = $fileName.html();
-
-		$input.on('change', function(e) {
-			var fileValue = '';
-
-			if( this.files && this.files.length > 1)
-				fileValue = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-			else if(e.target.value)
-				fileValue = e.target.value.split('\\').pop();
-
-			if(fileValue)
-				$label.html('Modifier la pièce jointe'),
-                $fileName.html(fileValue);
-			else
-				$label.html(labelVal),
-                $fileName.html(fileDefault);
-    	});
-    });
-})( jQuery, window, document );
-
-// Form swaper
-$(document).ready(function(){
-
-    $('#volunteer-form').hide();
-    $('#js-rolePicker .pick-btn').click(function(){
-        var $this = $(this);
-        $this.addClass('selected');
-        $this.siblings().removeClass('selected');
-
-        if ($this.attr('id') == 'js-RunningMate') {
-            $('#volunteer-form').fadeOut();
-            setTimeout(function(){
-                $('#running-mate-form').fadeIn();
-            }, 400);
-        }
-
-        if ($this.attr('id') === 'js-Volunteer') {
-            $('#running-mate-form').fadeOut();
-            setTimeout(function(){
-                $('#volunteer-form').fadeIn();
-            }, 400);
-        }
-    });
-
-    $('#js-rolePicker .pick-btn').hover(
-        function() {
-            $(this).siblings().addClass('fade');
-        }, function() {
-        $(this).siblings().removeClass('fade');
-    });
-
-});
