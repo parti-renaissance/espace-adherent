b7fc2f6aa91224701c659857b85c98cdae4b1d4f
Rémi Gardien <remi@gardien.biz>
2019-06-21 12:35:15 +0200

    Add command to update application requests with missing referent tags

---
+++ src/Command/ApplicationRequestsUpdateMissingTags.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\ApplicationRequest\ApplicationRequest;
+use AppBundle\Entity\ApplicationRequest\RunningMateRequest;
+use AppBundle\Entity\ApplicationRequest\VolunteerRequest;
+use AppBundle\Referent\ReferentTagManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ApplicationRequestsUpdateMissingTags extends Command
+{
+    protected static $defaultName = 'app:application-requests:update-missing-tags';
+
+    private $entityManager;
+    private $referentTagManager;
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $io;
+
+    public function __construct(EntityManagerInterface $entityManager, ReferentTagManager $referentTagManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->referentTagManager = $referentTagManager;
+
+        parent::__construct();
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->io->text('Start updating referent tags of application requests.');
+
+        $this->updateApplicationRequests(RunningMateRequest::class);
+        $this->updateApplicationRequests(VolunteerRequest::class);
+
+        $this->io->success('Done');
+    }
+
+    private function updateApplicationRequests(string $entityClass): void
+    {
+        /** @var ApplicationRequest[]|array $applicationRequests */
+        $applicationRequests = $this->entityManager->getRepository($entityClass)->findAll();
+
+        $this->io->progressStart(\count($applicationRequests));
+
+        foreach ($applicationRequests as $applicationRequest) {
+            $this->io->progressAdvance();
+
+            if (!$applicationRequest->getReferentTags()->isEmpty()) {
+                continue;
+            }
+
+            $this->referentTagManager->assignApplicationRequestReferentTags($applicationRequest);
+        }
+
+        $this->entityManager->flush();
+
+        $this->io->progressFinish();
+    }
+}
