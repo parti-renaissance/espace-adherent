10bc50e03a893f977198cf89b554b81c386082bf
Rémi Gardien <remi@gardien.biz>
2019-06-26 16:51:14 +0200

    Add 'latest' tags to master images

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ perl -pi -e "s/default/$CIRCLE_SHA1/g" ./app/config/app_version.yml
 VERSION=${CIRCLE_TAG:-$CIRCLE_BRANCH}
 DOCKER_IMAGE_TAG="eu.gcr.io/$GCLOUD_PROJECT/app:$VERSION-$CIRCLE_SHA1"
 DOCKER_IMAGE_CACHE_TAG="eu.gcr.io/$GCLOUD_PROJECT/app:master"
+DOCKER_IMAGE_LATEST_TAG="eu.gcr.io/$GCLOUD_PROJECT/app:latest"
 
 # Build the image
 gcloud docker -- pull $DOCKER_IMAGE_CACHE_TAG
@@ -17,5 +18,5 @@ gcloud docker -- push $DOCKER_IMAGE_TAG
 
 # Set image tag only for master
 if [ "$VERSION" = "master" ]; then
-    gcloud container images add-tag $DOCKER_IMAGE_TAG $DOCKER_IMAGE_CACHE_TAG --quiet
+    gcloud container images add-tag $DOCKER_IMAGE_TAG $DOCKER_IMAGE_CACHE_TAG $DOCKER_IMAGE_LATEST_TAG --quiet
 fi
