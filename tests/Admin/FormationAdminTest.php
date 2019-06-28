8a2272bfc6fbbbf04184d152ec90ea78cf15c274
nicoweb <weblefevre@gmail.com>
2019-06-28 14:36:51 +0200

    fix creation axes by admin

---
+++ tests/Admin/FormationAdminTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\AppBundle\Admin;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\AppBundle\Controller\ControllerTestTrait;
+
+class FormationAdminTest extends WebTestCase
+{
+    use ControllerTestTrait;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->init();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->kill();
+    }
+
+    /**
+     * @dataProvider uriProvider
+     */
+    public function testSuperAdminCanAccessFormationAdmin(string $uri): void
+    {
+        $this->authenticateAsAdmin($this->client, 'superadmin@en-marche-dev.fr');
+        $this->client->request(Request::METHOD_GET, $uri);
+
+        $this->assertResponseStatusCode(Response::HTTP_OK, $this->client->getResponse());
+    }
+
+    public function uriProvider(): iterable
+    {
+        yield ['/admin/app/formation-path/list'];
+        yield ['/admin/app/formation-path/create'];
+        yield ['/admin/app/formation-axe/list'];
+        yield ['/admin/app/formation-axe/create'];
+        yield['/admin/app/formation-module/list'];
+        yield['/admin/app/formation-module/create'];
+    }
+}
