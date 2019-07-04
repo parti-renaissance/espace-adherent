b8b991bbb41c2363b2a70d36a5aeac7493fd5b66
Rémi Gardien <remi@gardien.biz>
2019-07-04 03:39:30 +0200

    [ADMIN] keep track of exports

---
+++ tests/EventListener/AdministratorExportHistoryListenerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\AppBundle\Repository;
+
+use AppBundle\Entity\Reporting\AdministratorExportHistory;
+use Doctrine\ORM\EntityRepository;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Tests\AppBundle\Controller\ControllerTestTrait;
+
+/**
+ * @group functional
+ */
+class AdministratorExportHistoryListenerTest extends WebTestCase
+{
+    /**
+     * @var EntityRepository
+     */
+    private $historyRepository;
+
+    use ControllerTestTrait;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->init();
+
+        $this->historyRepository = $this->getRepository(AdministratorExportHistory::class);
+    }
+
+    protected function tearDown()
+    {
+        $this->kill();
+
+        $this->historyRepository = null;
+
+        parent::tearDown();
+    }
+
+    public function testCreateHistory()
+    {
+        self::assertEmpty($this->historyRepository->findAll());
+
+        $this->authenticateAsAdmin($this->client, 'admin@en-marche-dev.fr');
+
+        $parameters = [
+            'filter' => [
+                'city' => [
+                    'value' => 'Bordeaux',
+                ],
+            ],
+            'format' => 'csv',
+        ];
+
+        $this->client->request('GET', '/admin/app/adherent/export', $parameters);
+
+        $histories = $this->historyRepository->findAll();
+        self::assertCount(1, $histories);
+
+        /** @var AdministratorExportHistory $history */
+        $history = reset($histories);
+        self::assertSame('admin@en-marche-dev.fr', $history->getAdministrator()->getEmailAddress());
+        self::assertSame('admin_app_adherent_export', $history->getRouteName());
+        self::assertSame($parameters, $history->getParameters());
+    }
+}
