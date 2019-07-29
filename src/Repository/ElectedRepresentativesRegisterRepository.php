f8b3a3a06502fb8930c575edd9b5e49ce4598ec9
Rémi Gardien <remi@gardien.biz>
2019-07-29 16:14:29 +0200

    [ADMIN] add filter to elected representative register

--- src/Repository/ElectedRepresentativesRegisterRepository.php
+++ src/Repository/ElectedRepresentativesRegisterRepository.php
@@ -12,4 +12,30 @@ class ElectedRepresentativesRegisterRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ElectedRepresentativesRegister::class);
     }
+
+    public function findAllNuancePolitiqueValues(): array
+    {
+        return $this->findAllValues('nuancePolitique');
+    }
+
+    public function findAllTypeEluValues(): array
+    {
+        return $this->findAllValues('typeElu');
+    }
+
+    public function findAllNomFonctionValues(): array
+    {
+        return $this->findAllValues('nomFonction');
+    }
+
+    private function findAllValues(string $columnName): array
+    {
+        $results = $this->createQueryBuilder('e')
+            ->select("DISTINCT e.$columnName")
+            ->getQuery()
+            ->getArrayResult()
+        ;
+
+        return \array_column($results, $columnName);
+    }
 }
