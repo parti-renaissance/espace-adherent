eaca7ed710c1ca827fca3442e6e8e079281d0099
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-08-22 10:39:19 +0200

    [RepublicanSilence] block municipal space pages

--- src/RepublicanSilence/TagExtractor/ReferentTagExtractorInterface.php
+++ src/RepublicanSilence/TagExtractor/ReferentTagExtractorInterface.php
@@ -10,6 +10,7 @@ interface ReferentTagExtractorInterface
     public const ADHERENT_TYPE_CITIZEN_PROJECT_ADMINISTRATOR = 1; // Host
     public const ADHERENT_TYPE_COMMITTEE_ADMINISTRATOR = 2; // Supervisor or Host
     public const ADHERENT_TYPE_DEPUTY = 3; // Deputy
+    public const ADHERENT_TYPE_MUNICIPAL_CHIEF = 4; // Municipal chief
 
     public function extractTags(Adherent $adherent, ?string $slug): array;
 }
