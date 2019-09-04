71bcbdc6b4a32cc05bd4cdf7e3f7925be4c1c58c
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-09-04 10:47:36 +0200

    [MunicipalMessage] Send city name as content section

--- src/Mailchimp/Campaign/ContentSection/DeputyMessageSectionBuilder.php
+++ src/Mailchimp/Campaign/ContentSection/DeputyMessageSectionBuilder.php
@@ -2,14 +2,16 @@
 
 namespace AppBundle\Mailchimp\Campaign\ContentSection;
 
-use AppBundle\Entity\AdherentMessage\AdherentMessageInterface;
+use AppBundle\Entity\AdherentMessage\MailchimpCampaign;
 use AppBundle\Mailchimp\Campaign\Request\EditCampaignContentRequest;
 use AppBundle\Utils\StringCleaner;
 
 class DeputyMessageSectionBuilder implements ContentSectionBuilderInterface
 {
-    public function build(AdherentMessageInterface $message, EditCampaignContentRequest $request): void
+    public function build(MailchimpCampaign $campaign, EditCampaignContentRequest $request): void
     {
+        $message = $campaign->getMessage();
+
         $request
             ->addSection('full_name', StringCleaner::htmlspecialchars($message->getAuthor()->getFullName()))
             ->addSection('first_name', StringCleaner::htmlspecialchars($message->getAuthor()->getFirstName()))
--- src/Mailchimp/Campaign/ContentSection/ReferentMessageSectionBuilder.php
+++ src/Mailchimp/Campaign/ContentSection/ReferentMessageSectionBuilder.php
@@ -2,14 +2,16 @@
 
 namespace AppBundle\Mailchimp\Campaign\ContentSection;
 
-use AppBundle\Entity\AdherentMessage\AdherentMessageInterface;
+use AppBundle\Entity\AdherentMessage\MailchimpCampaign;
 use AppBundle\Mailchimp\Campaign\Request\EditCampaignContentRequest;
 use AppBundle\Utils\StringCleaner;
 
 class ReferentMessageSectionBuilder implements ContentSectionBuilderInterface
 {
-    public function build(AdherentMessageInterface $message, EditCampaignContentRequest $request): void
+    public function build(MailchimpCampaign $campaign, EditCampaignContentRequest $request): void
     {
+        $message = $campaign->getMessage();
+
         $request
             ->addSection('first_name', StringCleaner::htmlspecialchars($message->getAuthor()->getFirstName()))
             ->addSection('reply_to_link', sprintf(
