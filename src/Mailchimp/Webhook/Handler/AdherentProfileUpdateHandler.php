4e9e053729611653981053b2e9ff2e33339cb315
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-09-19 12:37:15 +0200

    [MailchimpWebhook] Update interest & notif on subscribe call

--- src/Mailchimp/Webhook/Handler/AdherentProfileUpdateHandler.php
+++ src/Mailchimp/Webhook/Handler/AdherentProfileUpdateHandler.php
@@ -4,11 +4,14 @@ namespace AppBundle\Mailchimp\Webhook\Handler;
 
 use AppBundle\Entity\Adherent;
 use AppBundle\Mailchimp\Webhook\EventTypeEnum;
+use AppBundle\Membership\UserEvent;
+use AppBundle\Membership\UserEvents;
 use AppBundle\Subscription\SubscriptionHandler;
 use Doctrine\Common\Persistence\ObjectManager;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class AdherentProfileUpdateHandler extends AbstractAdherentHandler implements LoggerAwareInterface
 {
@@ -17,17 +20,20 @@ class AdherentProfileUpdateHandler extends AbstractAdherentHandler implements Lo
     private $interests;
     private $manager;
     private $subscriptionHandler;
+    private $dispatcher;
 
     public function __construct(
         ObjectManager $manager,
         SubscriptionHandler $subscriptionHandler,
         array $interests,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        EventDispatcherInterface $dispatcher
     ) {
         $this->manager = $manager;
         $this->subscriptionHandler = $subscriptionHandler;
         $this->interests = $interests;
         $this->logger = $logger;
+        $this->dispatcher = $dispatcher;
     }
 
     public function handle(array $data): void
@@ -37,14 +43,23 @@ class AdherentProfileUpdateHandler extends AbstractAdherentHandler implements Lo
 
             $groups = $mergeFields['GROUPINGS'] ?? [];
 
+            $this->dispatcher->dispatch(UserEvents::USER_BEFORE_UPDATE, new UserEvent($adherent));
+
+            $oldSubscriptionTypes = $adherent->getSubscriptionTypes();
+
             $this->updateSubscriptions($adherent, $this->findGroupById($groups, $this->mailchimpObjectIdMapping->getSubscriptionTypeInterestGroupId()));
             $this->updateInterests($adherent, $this->findGroupById($groups, $this->mailchimpObjectIdMapping->getMemberInterestInterestGroupId()));
+
+            $this->dispatcher->dispatch(UserEvents::USER_UPDATE_SUBSCRIPTIONS, new UserEvent($adherent, null, null, $oldSubscriptionTypes));
         }
     }
 
     public function support(string $type, string $listId): bool
     {
-        return EventTypeEnum::UPDATE_PROFILE === $type && parent::support($type, $listId);
+        return
+            \in_array($type, [EventTypeEnum::UPDATE_PROFILE, EventTypeEnum::SUBSCRIBE], true)
+            && parent::support($type, $listId)
+        ;
     }
 
     private function updateInterests(Adherent $adherent, array $data): void
