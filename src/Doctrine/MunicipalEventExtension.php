d0b209a2c55f375bbc6cc000b711b2b34853e838
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-10-28 17:44:55 +0100

    [MunicipalEvent] enable search by categorie name

---
+++ src/Doctrine/MunicipalEventExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use AppBundle\Entity\MunicipalEvent;
+use Doctrine\ORM\QueryBuilder;
+
+class MunicipalEventExtension implements QueryItemExtensionInterface, QueryCollectionExtensionInterface
+{
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        string $operationName = null,
+        array $context = []
+    ) {
+        $this->modifyQuery($queryBuilder, $resourceClass);
+    }
+
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        string $operationName = null
+    ) {
+        $this->modifyQuery($queryBuilder, $resourceClass);
+    }
+
+    private function modifyQuery(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (MunicipalEvent::class === $resourceClass) {
+            $alias = $queryBuilder->getRootAliases()[0];
+
+            $queryBuilder
+                ->andWhere("$alias.status = :status")
+                ->setParameter('status', MunicipalEvent::STATUS_SCHEDULED)
+                ->andWhere("$alias.finishAt > :now")
+                ->setParameter('now', new \DateTime())
+            ;
+        }
+    }
+}
