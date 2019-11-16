a7b56053c2b3a1c66dbc25cd253446b075bb2ed8
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-11-16 18:20:46 +0100

    Update php7.3

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -12,8 +12,8 @@ if [ $uid == 0 ] && [ $gid == 0 ]; then
     fi
 fi
 
-sed -i "s/user = www-data/user = enmarche/g" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = enmarche/g" /etc/php/7.1/fpm/pool.d/www.conf
+sed -i "s/user = www-data/user = enmarche/g" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = enmarche/g" /etc/php/7.3/fpm/pool.d/www.conf
 sed -i -r "s/enmarche:x:[0-9]+:[0-9]+:/enmarche:x:$uid:$gid:/g" /etc/passwd
 sed -i -r "s/enmarche:x:[0-9]+:/enmarche:x:$gid:/g" /etc/group
 
