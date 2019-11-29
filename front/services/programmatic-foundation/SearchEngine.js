04caa3792401cee4274c00299312fbd8183734f0
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-11-29 11:12:19 +0100

    Remove Measure numbers

--- front/services/programmatic-foundation/SearchEngine.js
+++ front/services/programmatic-foundation/SearchEngine.js
@@ -3,13 +3,7 @@ import _ from 'lodash';
 export default class SearchEngine {
     static search(approaches, filters) {
         let measures = _.flatMap(approaches, (approach) => {
-            return _.flatMap(approach.sub_approaches, (subApproach) => {
-                return _.flatMap(subApproach.measures, (measure) => {
-                    measure.parentSectionIdentifierParts = [approach.position, subApproach.position];
-
-                    return measure;
-                });
-            });
+            return _.flatMap(approach.sub_approaches, (subApproach) => _.flatMap(subApproach.measures));
         });
 
         let projects = _.flatMap(measures, (measure) => _.flatMap(measure.projects));
