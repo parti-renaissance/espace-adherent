680e09e18d77a54644af3f9c2761c11defed2552
Rémi Gardien <remi@gardien.biz>
2019-12-12 14:49:22 +0100

    Introduce retirement simulator page

---
+++ src/Controller/EnMarche/RetirementSimulatorController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller\EnMarche;
+
+use AppBundle\Form\RetirementSimulatorType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/simulateur-retraite")
+ */
+class RetirementSimulatorController extends Controller
+{
+    /**
+     * @Route(name="app_retirement_simulator_home", methods={"GET"})
+     */
+    public function approachesAction(): Response
+    {
+        $form = $this->createForm(RetirementSimulatorType::class);
+
+        return $this->render('retirement_simulator/home.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
