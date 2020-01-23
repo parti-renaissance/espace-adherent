fccce93911b94aa1fccb66ecff7d738a2169a0f2
Benjamin ZASLAVSKY <benjamin.zaslavsky@sensiolabs.com>
2020-01-23 10:47:32 +0100

    ConsularRole - Remove Collection
    - Remove useless ArrayCollection

--- src/Entity/ConsularManagedArea.php
+++ src/Entity/ConsularManagedArea.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Entity;
 
 use Algolia\AlgoliaSearchBundle\Mapping\Annotation as Algolia;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -23,38 +22,24 @@ class ConsularManagedArea
     private $id;
 
     /**
-     * @var ConsularDistrict[]|ArrayCollection
+     * @var ConsularDistrict
      *
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\ConsularDistrict")
      */
     private $consularDistrict;
-
-    public function __construct()
-    {
-        $this->consularDistrict = new ArrayCollection();
-    }
 
     public function getId(): int
     {
         return $this->id;
     }
 
-    public function getConsularDistricts(): ArrayCollection
+    public function getConsularDistricts(): ConsularDistrict
     {
         return $this->consularDistrict;
     }
 
-    public function addConsularDistrict(ReferentTag $tag): void
+    public function setConsularDistrict(ConsularDistrict $consularDistrict): void
     {
-        if (!$this->consularDistrict->contains($tag)) {
-            $this->consularDistrict->add($tag);
-        }
-    }
-
-    public function removeConsularDistrict(ReferentTag $tag): void
-    {
-        if ($this->consularDistrict->contains($tag)) {
-            $this->consularDistrict->remove($tag);
-        }
+        $this->consularDistrict = $consularDistrict;
     }
 }
