982892e814d56b899ae7a0c3adbe59bb8ddc0c07
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-03-14 14:54:43 +0100

    Fix blameableListener for MinistryVoteResult

--- src/Election/Listener/VoteResultBlameableListener.php
+++ src/Election/Listener/VoteResultBlameableListener.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Election\Listener;
 
+use AppBundle\Entity\Adherent;
+use AppBundle\Entity\Election\BaseVoteResult;
 use AppBundle\Entity\Election\ListTotalResult;
-use AppBundle\Entity\Election\VotePlaceResult;
+use AppBundle\Entity\Election\MinistryListTotalResult;
 use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Events;
+use Doctrine\ORM\UnitOfWork;
 use Symfony\Component\Security\Core\Security;
 
 class VoteResultBlameableListener implements EventSubscriber
@@ -31,24 +35,61 @@ class VoteResultBlameableListener implements EventSubscriber
             return;
         }
 
-        $uow = $args->getEntityManager()->getUnitOfWork();
+        $entityManager = $args->getEntityManager();
+        $uow = $entityManager->getUnitOfWork();
 
         foreach ($uow->getScheduledEntityUpdates() as $listTotalResult) {
-            if (!$listTotalResult instanceof ListTotalResult) {
+            if (!\in_array(\get_class($listTotalResult), [ListTotalResult::class, MinistryListTotalResult::class], true)) {
                 continue;
             }
 
-            if ($voteResult = $listTotalResult->getVoteResult()) {
-                $voteResult->setUpdatedBy($user);
-                $voteResult->setUpdatedAt(new \DateTime());
-
-                $uow->recomputeSingleEntityChangeSet(
-                    $args->getEntityManager()->getClassMetadata(VotePlaceResult::class),
-                    $voteResult
-                );
-
+            if ($this->updateBlameableData($listTotalResult, $user, $uow, $entityManager)) {
                 return;
             }
         }
+
+        foreach (array_merge($uow->getScheduledEntityDeletions(), $uow->getScheduledEntityInsertions()) as $listTotalResult) {
+            if (!$listTotalResult instanceof MinistryListTotalResult) {
+                continue;
+            }
+
+            if ($this->updateBlameableData($listTotalResult, $user, $uow, $entityManager)) {
+                return;
+            }
+        }
+    }
+
+    private function updateBlameableData(
+        $listTotalResult,
+        Adherent $user,
+        UnitOfWork $uow,
+        EntityManagerInterface $entityManager
+    ): bool {
+        if ($voteResult = $this->getVoteResult($listTotalResult)) {
+            $voteResult->setUpdatedBy($user);
+            $voteResult->setUpdatedAt(new \DateTime());
+
+            $uow->recomputeSingleEntityChangeSet(
+                $entityManager->getClassMetadata(\get_class($voteResult)),
+                $voteResult
+            );
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getVoteResult($listTotalResult): ?BaseVoteResult
+    {
+        if ($listTotalResult instanceof ListTotalResult) {
+            return $listTotalResult->getVoteResult();
+        }
+
+        if ($listTotalResult instanceof MinistryListTotalResult) {
+            return $listTotalResult->getMinistryVoteResult();
+        }
+
+        return null;
     }
 }
