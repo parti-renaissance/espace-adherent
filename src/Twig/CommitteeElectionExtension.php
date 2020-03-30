0fc83919a569bd1fd9d8a829030494624b4ac5e6
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-03-30 10:55:04 +0200

    [CandidacyBlock] add woman/man parity warning

--- src/Twig/CommitteeElectionExtension.php
+++ src/Twig/CommitteeElectionExtension.php
@@ -10,8 +10,8 @@ class CommitteeElectionExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new TwigFunction('get_committee_candidacy_membership', [CommitteeRuntime::class, 'getCommitteeCandidacyMembership']),
             new TwigFunction('is_candidate', [CommitteeRuntime::class, 'isCandidate']),
+            new TwigFunction('count_committee_candidates', [CommitteeRuntime::class, 'countCommitteeCandidates']),
         ];
     }
 }
