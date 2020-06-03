f717737fb598fb2e1444552774e2315d5844ffbb
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-06-03 19:11:04 +0200

    [EM-440] Add Session TTL with Remember me feature

--- src/Security/AdherentLoginTimestampRecorder.php
+++ src/Security/AdherentLoginTimestampRecorder.php
@@ -4,9 +4,7 @@ namespace App\Security;
 
 use App\Entity\Adherent;
 use Doctrine\Common\Persistence\ObjectManager;
-use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\JWTUserToken;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\SecurityEvents;
@@ -33,13 +31,6 @@ class AdherentLoginTimestampRecorder implements EventSubscriberInterface
         // OAuth calls are not login attempts
         if ($token instanceof PostAuthenticationGuardToken && 'api_oauth' === $token->getProviderKey()) {
             return;
-        }
-
-        if (!$token instanceof PostAuthenticationGuardToken
-            && !$token instanceof JWTUserToken
-            && !$token instanceof UsernamePasswordToken
-        ) {
-            throw new \RuntimeException(sprintf('Authentication token must be a %s or %s or %s instance. (%s given)', PostAuthenticationGuardToken::class, JWTUserToken::class, UsernamePasswordToken::class, \get_class($token)));
         }
 
         $user->recordLastLoginTime();
