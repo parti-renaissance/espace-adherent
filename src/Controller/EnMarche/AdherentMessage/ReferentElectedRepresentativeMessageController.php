0b8705a284fab4de55cf37ee9ac765eb7efacd0a
Rémi Gardien <remi@gardien.biz>
2020-08-17 13:45:08 +0200

    Remove canary on elected representative messages

--- src/Controller/EnMarche/AdherentMessage/ReferentElectedRepresentativeMessageController.php
+++ src/Controller/EnMarche/AdherentMessage/ReferentElectedRepresentativeMessageController.php
@@ -17,9 +17,4 @@ class ReferentElectedRepresentativeMessageController extends AbstractMessageCont
     {
         return AdherentMessageTypeEnum::REFERENT_ELECTED_REPRESENTATIVE;
     }
-
-    protected function isCanary(): bool
-    {
-        return true;
-    }
 }
