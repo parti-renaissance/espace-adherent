6b44314e1d5c86ca57a9473babfda0d6ae12b9b4
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-09-03 17:06:54 +0200

    [Referent] update designation form validation rules

--- src/Validator/TerritorialCouncil/TerritorialCouncilDesignation.php
+++ src/Validator/TerritorialCouncil/TerritorialCouncilDesignation.php
@@ -11,7 +11,7 @@ class TerritorialCouncilDesignation extends Constraint
 {
     public $messageVoteStartDateTooClose = 'La convocation doit avoir lieu au minimum 7 jours avant la tenue du vote.';
     public $messageVoteEndDateInvalid = 'La durée du vote doit être comprise entre 5h et 7 jours.';
-    public $messageMeetingStartDateInvalid = 'La date de début de la réunion doit être la même que la date de début du vote.';
+    public $messageVoteStartDateInvalid = 'La date de début du vote n\'est pas le même jour que la date du Conseil territorial';
     public $messageMeetingStartDateTooFarAway = 'La date de début de la réunion ne doit pas dépasser le {{date}}';
     public $messageMeetingEndDateInvalid = 'La durée de la réunion ne doit pas dépasser 12 heures.';
     public $messageElectionPollChoiceInvalid = 'Les choix du sondage sont invalides.';
