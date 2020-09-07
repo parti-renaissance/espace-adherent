89d4267ff96b26e5401aad88a7238db98effb732
Rémi Gardien <remi@gardien.biz>
2020-09-07 10:57:26 +0200

    Fix command class name

--- src/Command/RemindActivatedMembershipCommand.php
+++ src/Command/RemindActivatedMembershipCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RemindUnconfirmedMembershipCommand extends Command
+class RemindActivatedMembershipCommand extends Command
 {
     protected static $defaultName = 'app:membership:remind-activated';
 
