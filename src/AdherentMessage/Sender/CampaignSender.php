091cdd27915e15e1fb05c7069da5b00c65b1e205
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-09-11 16:37:08 +0200

    [AdherentMessage] fix test message

--- src/AdherentMessage/Sender/CampaignSender.php
+++ src/AdherentMessage/Sender/CampaignSender.php
@@ -2,6 +2,7 @@
 
 namespace App\AdherentMessage\Sender;
 
+use App\Entity\Adherent;
 use App\Entity\AdherentMessage\AdherentMessageInterface;
 use App\Entity\AdherentMessage\CampaignAdherentMessageInterface;
 use App\Mailchimp\Manager;
@@ -27,7 +28,9 @@ class CampaignSender implements SenderInterface
 
     public function sendTest(AdherentMessageInterface $message, array $recipients = []): bool
     {
-        return $this->manager->sendTestCampaign($message, $recipients);
+        return $this->manager->sendTestCampaign($message, array_map(function (Adherent $adherent): string {
+            return $adherent->getEmailAddress();
+        }, $recipients));
     }
 
     public function renderMessage(AdherentMessageInterface $message, array $recipients = []): string
