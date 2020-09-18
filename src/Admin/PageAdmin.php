568d97fcf6ae9b3ed41eb35dce41796e0c659bfa
Rémi Gardien <remi@gardien.biz>
2020-09-18 12:45:46 +0200

    add help labels & translate layouts

--- src/Admin/PageAdmin.php
+++ src/Admin/PageAdmin.php
@@ -38,6 +38,7 @@ class PageAdmin extends AbstractAdmin
                 ->add('title', TextType::class, [
                     'label' => 'Titre',
                     'filter_emojis' => true,
+                    'help' => 'Le titre ne sera visible que sur les layouts qui affichent le titre',
                 ])
             ->end()
             ->with('Contenu', ['class' => 'col-md-8'])
@@ -53,7 +54,9 @@ class PageAdmin extends AbstractAdmin
                 ->add('layout', ChoiceType::class, [
                     'label' => 'Layout',
                     'choices' => Page::LAYOUTS,
-                    'choice_label' => function (?string $choice) { return $choice; },
+                    'choice_label' => function (?string $choice) {
+                        return "page.layout.$choice";
+                    },
                 ])
                 ->add('content', TextareaType::class, [
                     'label' => 'Contenu',
@@ -74,6 +77,7 @@ class PageAdmin extends AbstractAdmin
             ->add('headerMedia', null, [
                 'label' => 'Image d\'en-tête',
                 'required' => false,
+                'help' => 'Cette image ne sera visible que sur les layouts qui affichent l\'image d\'en-tête',
             ])
             ->end()
         ;
