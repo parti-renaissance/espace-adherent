59cb96fbd97e2cd97bfe96dd18a49a91eab25047
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-09-23 17:44:25 +0200

    Remove algolia/algolia-search-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -34,7 +34,6 @@ return [
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Algolia\AlgoliaSearchBundle\AlgoliaAlgoliaSearchBundle::class => ['prod' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true, 'dev_front' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'dev_front' => true],
     steevanb\DoctrineStats\Bridge\DoctrineStatsBundle\DoctrineStatsBundle::class => ['dev' => true, 'test' => true, 'dev_front' => true],
