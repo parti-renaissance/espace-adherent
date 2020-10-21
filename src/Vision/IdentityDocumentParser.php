92696a8d00694491f920ad756d4558c903511502
Rémi Gardien <remi@gardien.biz>
2020-10-21 18:59:04 +0200

    Do not catch useless regex group

--- src/Vision/IdentityDocumentParser.php
+++ src/Vision/IdentityDocumentParser.php
@@ -35,7 +35,7 @@ class IdentityDocumentParser
     public function hasFirstName(ImageAnnotations $imageAnnotations, string $firstName): bool
     {
         if ($imageAnnotations->isFrenchNationalIdentityCard()) {
-            preg_match('/Pr(e|é)nom[^:]{0,5}:?\s?(?<first_names>.+)\\n/', $imageAnnotations->getText(), $matches);
+            preg_match('/Pr(?:e|é)nom[^:]{0,5}:?\s?(?<first_names>.+)\\n/', $imageAnnotations->getText(), $matches);
 
             $firstNames = array_map(function (string $firstName) {
                 return $this->normalize($firstName);
