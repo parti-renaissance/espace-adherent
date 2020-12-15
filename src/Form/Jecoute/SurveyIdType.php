108988fbbf689f42153ad986b4c6c95cf4d83122
Rémi Gardien <remi@gardien.biz>
2020-12-15 10:49:18 +0100

    Use custom type for survey field

---
+++ src/Form/Jecoute/SurveyIdType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form\Jecoute;
+
+use App\Form\DataTransformer\SurveyToIdTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SurveyIdType extends AbstractType
+{
+    private $transformer;
+
+    public function __construct(SurveyToIdTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer($this->transformer);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'invalid_message' => 'survey.unknown_id',
+        ]);
+    }
+
+    public function getParent()
+    {
+        return IntegerType::class;
+    }
+}
