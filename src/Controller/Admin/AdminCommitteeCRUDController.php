Diwann/en-marche.fr

[Admin] Change committee actions + remove no more needed fields

src/Controller/Admin/AdminCommitteeCRUDController.php
@@ -1,0 +1,97 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Address\PostAddressFactory;
+use App\Committee\ApproveCommitteeCommand;
+use App\Committee\CommitteeManagementAuthority;
+use App\Committee\MultipleReferentsFoundException;
+use App\Entity\Adherent;
+use App\Entity\Committee;
+use App\Exception\BaseGroupException;
+use App\Form\Admin\ApproveCommitteeCommandType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminCommitteeCRUDController extends CRUDController
+{
+    /**
+     * Approves the committee.
+     *
+     * @Route("/{id}/approve", name="app_admin_committee_approve", methods={"GET"})
+     * @Security("has_role('ROLE_ADMIN_COMMITTEES')")
+     */
+    public function approveAction(
+        Request $request,
+        Committee $committee,
+        CommitteeManagementAuthority $committeeManagementAuthority,
+        PostAddressFactory $addressFactory
+    ): Response {
+        $this->admin->checkAccess('approve');
+
+        /** @var ApproveCommitteeCommand $command */
+        $command = ApproveCommitteeCommand::constructFromCommittee($committee);
+        $form = $this->createForm(ApproveCommitteeCommandType::class, $command, [
+            'validation_groups' => ['Default', 'with_provisional_supervisors'],
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('confirm')->isClicked()) {
+                try {
+                    $committee->setName($command->getName());
+                    $committee->setDescription($command->getDescription());
+                    $committee->setNameLocked($command->isNameLocked());
+                    $address = $addressFactory->createFromAddress($command->getAddress());
+                    if (!$committee->getPostAddress()->equals($address)) {
+                        $committee->updatePostAddress($address);
+                    }
+                    $committee->setNameLocked($command->isNameLocked());
+                    if ($adherentPSF = $command->getProvisionalSupervisorFemale()) {
+                        $committee->updateProvisionalSupervisor($adherentPSF);
+                    }
+
+                    if ($adherentPSM = $command->getProvisionalSupervisorMale()) {
+                        $committee->updateProvisionalSupervisor($adherentPSM);
+                    }
+                    $committeeManagementAuthority->approve($committee);
+                    $this->addFlash('sonata_flash_success', sprintf('Le comité « %s » a été approuvé avec succès.', $committee->getName()));
+                } catch (BaseGroupException $exception) {
+                    throw $this->createNotFoundException(sprintf('Committee %u must not be approved in order to be approved.', $committee->getId()), $exception);
+                }
+
+                try {
+                    $committeeManagementAuthority->notifyReferentsForApproval($committee);
+                } catch (MultipleReferentsFoundException $exception) {
+                    $this->addFlash('warning', sprintf(
+                        'Attention, plusieurs référents (%s) ont été trouvés dans le département de ce nouveau comité. 
+                                Aucun mail de notification pour la validation de ce comité ne leur a été envoyé. 
+                                Nommez un seul référent pour permettre les notifications de ce type.',
+                        implode(', ', array_map(function (Adherent $referent) {
+                            return $referent->getEmailAddress();
+                        }, $exception->getReferents()->toArray()))
+                    ));
+                }
+
+                return $this->redirectToList();
+            }
+
+            return $this->renderWithExtraParams('admin/committee/approve/confirm.html.twig', [
+                'form' => $form->createView(),
+                'action' => 'approve',
+                'object' => $command,
+                'elements' => $this->admin->getShow(),
+            ]);
+        }
+
+        return $this->renderWithExtraParams('admin/committee/approve/edit.html.twig', [
+            'form' => $form->createView(),
+            'action' => 'approve',
+            'object' => $command,
+            'elements' => $this->admin->getShow(),
+        ]);
+    }
+}

src/Form/Admin/ApproveCommitteeCommandType.php
@@ -1,0 +1,61 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Address\Address;
+use App\Committee\ApproveCommitteeCommand;
+use App\Committee\CommitteeCommand;
+use App\Form\AddressType;
+use App\Form\AdherentIdType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApproveCommitteeCommandType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $command = $builder->getData();
+        $committee = $command instanceof CommitteeCommand ? $command->getCommittee() : null;
+        $builder
+            ->add('name', TextType::class, [
+                'filter_emojis' => true,
+                'format_title_case' => true,
+            ])
+            ->add('slug', TextType::class, [
+                'filter_emojis' => true,
+                'help' => 'Généré automatiquement depuis le titre.',
+            ])
+            ->add('description', TextareaType::class, [
+                'with_character_count' => true,
+                'attr' => ['maxlength' => 140],
+                'filter_emojis' => true,
+            ])
+            ->add('address', AddressType::class, [
+                'disable_fields' => $committee ? $committee->isApproved() : false,
+                'data' => $builder->getData() ? Address::createFromAddress($builder->getData()->getAddress()) : null,
+            ])
+            ->add('nameLocked', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('provisionalSupervisorMale', AdherentIdType::class, [
+                'required' => false,
+            ])
+            ->add('provisionalSupervisorFemale', AdherentIdType::class, [
+                'required' => false,
+            ])
+            ->add('confirm', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ApproveCommitteeCommand::class,
+        ]);
+    }
+}

parti-renaissance/espace-adherent

src/Controller/Admin/AdminCommitteeCRUDController.php
@@ -1,0 +1,97 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Address\PostAddressFactory;
+use App\Committee\CommitteeCommand;
+use App\Committee\CommitteeManagementAuthority;
+use App\Committee\MultipleReferentsFoundException;
+use App\Entity\Adherent;
+use App\Entity\Committee;
+use App\Exception\BaseGroupException;
+use App\Form\Admin\ApproveCommitteeCommandType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminCommitteeCRUDController extends CRUDController
+{
+    /**
+     * Approves the committee.
+     *
+     * @Route("/{id}/approve", name="app_admin_committee_approve", methods={"GET"})
+     * @Security("has_role('ROLE_ADMIN_COMMITTEES')")
+     */
+    public function approveAction(
+        Request $request,
+        Committee $committee,
+        CommitteeManagementAuthority $committeeManagementAuthority,
+        PostAddressFactory $addressFactory
+    ): Response {
+        $this->admin->checkAccess('approve');
+
+        /** @var CommitteeCommand $command */
+        $command = CommitteeCommand::createFromCommittee($committee);
+        $form = $this->createForm(ApproveCommitteeCommandType::class, $command, [
+            'validation_groups' => ['Default', 'with_provisional_supervisors'],
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('confirm')->isClicked()) {
+                try {
+                    $committee->setName($command->getName());
+                    $committee->setDescription($command->getDescription());
+                    $committee->setNameLocked($command->isNameLocked());
+                    $address = $addressFactory->createFromAddress($command->getAddress());
+                    if (!$committee->getPostAddress()->equals($address)) {
+                        $committee->updatePostAddress($address);
+                    }
+                    $committee->setNameLocked($command->isNameLocked());
+                    if ($adherentPSF = $command->getProvisionalSupervisorFemale()) {
+                        $committee->updateProvisionalSupervisor($adherentPSF);
+                    }
+
+                    if ($adherentPSM = $command->getProvisionalSupervisorMale()) {
+                        $committee->updateProvisionalSupervisor($adherentPSM);
+                    }
+                    $committeeManagementAuthority->approve($committee);
+                    $this->addFlash('sonata_flash_success', sprintf('Le comité « %s » a été approuvé avec succès.', $committee->getName()));
+                } catch (BaseGroupException $exception) {
+                    throw $this->createNotFoundException(sprintf('Committee %u must not be approved in order to be approved.', $committee->getId()), $exception);
+                }
+
+                try {
+                    $committeeManagementAuthority->notifyReferentsForApproval($committee);
+                } catch (MultipleReferentsFoundException $exception) {
+                    $this->addFlash('warning', sprintf(
+                        'Attention, plusieurs référents (%s) ont été trouvés dans le département de ce nouveau comité. 
+                                Aucun mail de notification pour la validation de ce comité ne leur a été envoyé. 
+                                Nommez un seul référent pour permettre les notifications de ce type.',
+                        implode(', ', array_map(function (Adherent $referent) {
+                            return $referent->getEmailAddress();
+                        }, $exception->getReferents()->toArray()))
+                    ));
+                }
+
+                return $this->redirectToList();
+            }
+
+            return $this->renderWithExtraParams('admin/committee/approve/confirm.html.twig', [
+                'form' => $form->createView(),
+                'action' => 'approve',
+                'object' => $command,
+                'elements' => $this->admin->getShow(),
+            ]);
+        }
+
+        return $this->renderWithExtraParams('admin/committee/approve/edit.html.twig', [
+            'form' => $form->createView(),
+            'action' => 'approve',
+            'object' => $command,
+            'elements' => $this->admin->getShow(),
+        ]);
+    }
+}

src/Form/Admin/ApproveCommitteeCommandType.php
@@ -1,0 +1,60 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Address\Address;
+use App\Committee\CommitteeCommand;
+use App\Form\AddressType;
+use App\Form\AdherentIdType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApproveCommitteeCommandType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $command = $builder->getData();
+        $committee = $command instanceof CommitteeCommand ? $command->getCommittee() : null;
+        $builder
+            ->add('name', TextType::class, [
+                'filter_emojis' => true,
+                'format_title_case' => true,
+            ])
+            ->add('slug', TextType::class, [
+                'filter_emojis' => true,
+                'help' => 'Généré automatiquement depuis le titre.',
+            ])
+            ->add('description', TextareaType::class, [
+                'with_character_count' => true,
+                'attr' => ['maxlength' => 140],
+                'filter_emojis' => true,
+            ])
+            ->add('address', AddressType::class, [
+                'disable_fields' => $committee ? $committee->isApproved() : false,
+                'data' => $builder->getData() ? Address::createFromAddress($builder->getData()->getAddress()) : null,
+            ])
+            ->add('nameLocked', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('provisionalSupervisorMale', AdherentIdType::class, [
+                'required' => false,
+            ])
+            ->add('provisionalSupervisorFemale', AdherentIdType::class, [
+                'required' => false,
+            ])
+            ->add('confirm', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CommitteeCommand::class,
+        ]);
+    }
+}