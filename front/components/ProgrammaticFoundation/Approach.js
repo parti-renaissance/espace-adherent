72000a9152f11413fcac967e8d76736b21b48649
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-03-11 12:30:27 +0100

    Some more PropTypes new syntax correction

--- front/components/ProgrammaticFoundation/Approach.js
+++ front/components/ProgrammaticFoundation/Approach.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import SubApproach from './SubApproach';
 
 export default class Approach extends React.Component {
