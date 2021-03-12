694eee0429fb16dc35fdf8852c025935ca1b0efc
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-03-12 17:33:14 +0100

    Fix merge issue

--- webpack.development.js
+++ webpack.development.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
