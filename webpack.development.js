f968a494c2804b6eaa703b5ff2ba90d0ce56c06d
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-03-12 10:35:30 +0100

    Update webpack-merge

--- webpack.development.js
+++ webpack.development.js
@@ -1,9 +1,10 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'inline-source-map',
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name].css',
--- webpack.production.js
+++ webpack.production.js
@@ -3,7 +3,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
