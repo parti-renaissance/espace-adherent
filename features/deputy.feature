7eb345565a454d16aafa86da9f2ae5a4894d0929
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2021-05-05 16:23:58 +0200

    [Test] update oauth token ttl (#5695)

--- features/deputy.feature
+++ features/deputy.feature
@@ -64,8 +64,8 @@ Feature:
   Scenario: As deputy of 1st Paris district I can see committees.
     Given I am logged as "deputy@en-marche-dev.fr"
     When I am on "/espace-depute/comites"
+    And I wait 3 second until I see "En Marche Paris 8"
     Then I should see 1 "table.managed__list__table tbody tr" elements
-    And I should see "En Marche Paris 8"
 
   Scenario: As deputy of 1st Paris district I can see events.
     Given I am logged as "deputy@en-marche-dev.fr"
--- src/DataFixtures/ORM/LoadOAuthTokenData.php
+++ src/DataFixtures/ORM/LoadOAuthTokenData.php
@@ -42,7 +42,7 @@ class LoadOAuthTokenData extends Fixture implements DependentFixtureInterface
             'aa56a0ab28aade7ef4a554adc02b297ebd4d5bfe587c6b847512b5f46c59cad26ce53766f8766248',
             LoadAdherentData::ADHERENT_1_UUID,
             LoadClientData::CLIENT_01_UUID,
-            '+10 minutes'
+            '+30 minutes'
         );
         $authCode2->revoke();
 
@@ -50,7 +50,7 @@ class LoadOAuthTokenData extends Fixture implements DependentFixtureInterface
             '0c33b1711015b5e3d930f65b5dc87c398bfb3b29401028ee119c882bdf87cf9dcbf9a562629535e5',
             LoadAdherentData::ADHERENT_1_UUID,
             LoadClientData::CLIENT_02_UUID,
-            '+10 minutes'
+            '+30 minutes'
         );
 
         $authCode4 = $this->createAuthorizationCode(
@@ -71,7 +71,7 @@ class LoadOAuthTokenData extends Fixture implements DependentFixtureInterface
             '4c843038f3d1ba017e6c835420efeefd03c024d9f413ecf96bc70acbdcb79e8ae0598a1579364190',
             LoadAdherentData::ADHERENT_1_UUID,
             LoadClientData::CLIENT_01_UUID,
-            '+10 minutes'
+            '+30 minutes'
         );
         $accessToken2->revoke();
 
@@ -91,7 +91,7 @@ class LoadOAuthTokenData extends Fixture implements DependentFixtureInterface
         $refreshToken2 = $this->createRefreshToken(
             $accessToken1,
             'b3b29401028ee119c882bdf87cf9dcbf9a560c33b1711015b5e3d930f65b5dc87c398bf2629535e5',
-            '+10 minutes'
+            '+30 minutes'
         );
         $refreshToken2->revoke();
 
