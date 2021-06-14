0b8898bdf159a35cc071e72b99d0b814c26186aa
Rémi Gardien <remi@gardien.biz>
2021-06-14 17:47:59 +0200

    Remove useless method

--- src/PushToken/PushTokenManager.php
+++ src/PushToken/PushTokenManager.php
@@ -22,35 +22,8 @@ class PushTokenManager
         $this->committeeMembershipRepository = $committeeMembershipRepository;
     }
 
-    public function findIdentifiersForEventCreation(BaseEvent $event): array
+    public function findIdentifiersForEventCreation(CommitteeEvent $event): array
     {
-        if ($event instanceof CommitteeEvent) {
-            return $this->committeeMembershipRepository->findPushTokenIdentifiers($event->getCommittee());
-        }
-
-        if ($event instanceof DefaultEvent) {
-            if (!$zone = $this->findZoneToNotifyForEventCreation($event)) {
-                return [];
-            }
-
-            return $this->pushTokenRepository->findIdentifiersForZones([$zone]);
-        }
-
-        throw new \InvalidArgumentException(sprintf('Event type "%s" is not handled for notifications', $event->getType()));
-    }
-
-    private function findZoneToNotifyForEventCreation(DefaultEvent $event): ?Zone
-    {
-        $boroughs = $event->getZonesOfType(Zone::BOROUGH);
-        if (!empty($boroughs)) {
-            return $boroughs[0];
-        }
-
-        $departments = $event->getParentZonesOfType(Zone::DEPARTMENT);
-        if (!empty($departments)) {
-            return $departments[0];
-        }
-
-        return null;
+        return $this->committeeMembershipRepository->findPushTokenIdentifiers($event->getCommittee());
     }
 }
