9c0a3235fc7ec76c10cb6fc9562cc0ebea353f14
Rémi Gardien <remi@gardien.biz>
2021-06-16 12:20:16 +0200

    Adjust command for event modes

--- src/Command/RemindEventCommand.php
+++ src/Command/RemindEventCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Entity\Event\BaseEvent;
 use App\Event\EventReminderHandler;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -25,17 +26,26 @@ class RemindEventCommand extends Command
     {
         $this
             ->setDescription('This command finds upcoming events and send reminders')
-            ->addArgument('start-after', InputArgument::REQUIRED, 'Minimum number of minutes before the events begin')
-            ->addArgument('start-before', InputArgument::REQUIRED, 'Maximum number of minutes before the events begin')
-            ->addArgument('mode', InputArgument::OPTIONAL, 'Events mode to filter ("online" or "meeting")')
+            ->addArgument('mode', InputArgument::REQUIRED, 'Events mode to filter ("online" or "meeting")')
+            ->addArgument('online-start-after', InputArgument::OPTIONAL, 'Minimum number of minutes before the online events begin')
+            ->addArgument('online-start-before', InputArgument::OPTIONAL, 'Maximum number of minutes before the online events begin')
+            ->addArgument('meeting-delay', InputArgument::OPTIONAL, 'Number of days before reminder is sent to meeting events.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $startAfter = (new \DateTime())->modify(sprintf('+%d minutes', (int) $input->getArgument('start-after')));
-        $startBefore = (new \DateTime())->modify(sprintf('+%d minutes', (int) $input->getArgument('start-before')));
         $mode = $input->getArgument('mode');
+
+        if (BaseEvent::MODE_MEETING === $mode) {
+            $startAfter = (new \DateTime())->modify(sprintf('+%d days', (int) $input->getArgument('meeting-delay')))->setTime(0, 0, 0);
+            $startBefore = (clone $startAfter)->modify('+1 day');
+        } elseif (BaseEvent::MODE_ONLINE === $mode) {
+            $startAfter = (new \DateTime())->modify(sprintf('+%d minutes', (int) $input->getArgument('online-start-after')));
+            $startBefore = (new \DateTime())->modify(sprintf('+%d minutes', (int) $input->getArgument('online-start-before')));
+        } else {
+            throw new \InvalidArgumentException(sprintf('Event mode "%s" is not defined.', $mode));
+        }
 
         foreach ($this->handler->findEventsToRemind($startAfter, $startBefore, $mode) as $event) {
             $this->handler->scheduleReminder($event);
