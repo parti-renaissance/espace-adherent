6ba95bc4efbd9df99ece1053373b5409b66c7c1f
Rémi Gardien <remi@gardien.biz>
2021-06-23 19:43:46 +0200

    Revert "Add command & handler to migrate adherent interests"
    This reverts commit 8068d3a0aac65a905740daac352f0090934cf52d.

--- src/Adherent/Command/AdherentMigrateInterestsCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Adherent\Command;
-
-use App\Messenger\Message\AbstractUuidAsynchronousMessage;
-
-class AdherentMigrateInterestsCommand extends AbstractUuidAsynchronousMessage
-{
-}
