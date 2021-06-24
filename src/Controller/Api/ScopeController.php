3aa341a553c452b4a08f9f327606114de0fdc29f
illusionOfParadise <olga.rotach@gmail.com>
2021-06-24 13:37:30 +0200

    [API] Get scopes of the adherent

---
+++ src/Controller/Api/ScopeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Scope\GeneralScopeGenerator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ScopeController extends AbstractController
+{
+    /**
+     * @Route("/v3/scopes", name="api_scopes", methods={"GET"})
+     */
+    public function __invoke(GeneralScopeGenerator $scopeGenerator): JsonResponse
+    {
+        return $this->json(
+            $scopeGenerator->generateScopes($this->getUser()),
+            Response::HTTP_OK,
+            [],
+            ['groups' => ['scopes']]
+        );
+    }
+}
--- src/Entity/Geo/Zone.php
+++ src/Entity/Geo/Zone.php
@@ -80,7 +80,7 @@ class Zone implements GeoInterface
      *
      * @ORM\Column(type="uuid")
      *
-     * @SymfonySerializer\Groups({"zone_read"})
+     * @SymfonySerializer\Groups({"zone_read", "scopes"})
      *
      * @ApiProperty(
      *     identifier=true,
