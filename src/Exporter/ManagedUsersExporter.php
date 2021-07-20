9917759d7c26ab15b1b64d09185580a6a6755788
Rémi Gardien <remi@gardien.biz>
2021-07-20 12:06:46 +0200

    Add address field to deputy export (#6034)

--- src/Exporter/ManagedUsersExporter.php
+++ src/Exporter/ManagedUsersExporter.php
@@ -3,6 +3,7 @@
 namespace App\Exporter;
 
 use App\Controller\EnMarche\ManagedUsers\CandidateManagedUsersController;
+use App\Controller\EnMarche\ManagedUsers\DeputyManagedUsersController;
 use App\Entity\Projection\ManagedUser;
 use App\ManagedUsers\ManagedUsersFilter;
 use App\Repository\Projection\ManagedUserRepository;
@@ -35,6 +36,24 @@ class ManagedUsersExporter
                         'Commune' => $managedUser->getCity(),
                         'Code postal' => $managedUser->getPostalCode(),
                         'Pays' => $managedUser->getCountry(),
+                    ];
+                };
+
+                break;
+            case DeputyManagedUsersController::SPACE_NAME:
+                $callback = function (ManagedUser $managedUser) {
+                    return [
+                        'Prénom' => $managedUser->getFirstName(),
+                        'Nom' => $managedUser->getLastName(),
+                        'Âge' => $managedUser->getAge(),
+                        'Genre' => $managedUser->getGenderLabel(),
+                        'Rôle' => $managedUser->getUserRoleLabels(),
+                        'Adresse postale' => $managedUser->getAddress(),
+                        'Commune' => $managedUser->getCity(),
+                        'Code postal' => $managedUser->getPostalCode(),
+                        'Pays' => $managedUser->getCountry(),
+                        'Adhésion le' => $managedUser->getCreatedAt()->format('d/m/Y H:i'),
+                        'Comités' => $managedUser->getCommitteesAsString(),
                     ];
                 };
 
