6dd99ba528106447993292169665d81a7e550dc1
illusionOfParadise <olga.rotach@gmail.com>
2021-08-19 16:09:15 +0200

    [API][Events] Impossible to subscribe to a cancelled event

--- tests/Controller/Api/Event/SubscribeAsAnonymousControllerTest.php
+++ tests/Controller/Api/Event/SubscribeAsAnonymousControllerTest.php
@@ -6,6 +6,7 @@ use App\DataFixtures\ORM\LoadCauseEventData;
 use App\DataFixtures\ORM\LoadCoalitionEventData;
 use App\DataFixtures\ORM\LoadCommitteeEventData;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\App\AbstractWebCaseTest as WebTestCase;
 use Tests\App\Controller\ApiControllerTestTrait;
 use Tests\App\Controller\ControllerTestTrait;
@@ -42,10 +43,30 @@ class SubscribeAsAnonymousControllerTest extends WebTestCase
         $this->assertSame('Hey', $registration->getLastName());
     }
 
+    /** @dataProvider provideCancelledEvents */
+    public function testAnonymousCannotSubscribeOnCancelledEvent(string $eventUuid, string $messageClass)
+    {
+        $this->client->request(Request::METHOD_POST, sprintf('/api/events/%s/subscribe', $eventUuid), [], [], [], json_encode([
+            'first_name' => 'Joe',
+            'last_name' => 'Hey',
+            'email_address' => 'j.hey@en-marche-dev.fr',
+        ]));
+
+        $this->assertStatusCode(Response::HTTP_NOT_FOUND, $this->client);
+        $this->assertCountMails(0, $messageClass, 'j.hey@en-marche-dev.fr');
+    }
+
     public function provideEvents(): iterable
     {
         yield [LoadCoalitionEventData::EVENT_1_UUID, 'CoalitionsEventRegistrationConfirmationMessage'];
         yield [LoadCauseEventData::EVENT_1_UUID, 'CoalitionsEventRegistrationConfirmationMessage'];
         yield [LoadCommitteeEventData::EVENT_1_UUID, 'EventRegistrationConfirmationMessage'];
+    }
+
+    public function provideCancelledEvents(): iterable
+    {
+        yield [LoadCoalitionEventData::EVENT_6_UUID, 'CoalitionsEventRegistrationConfirmationMessage'];
+        yield [LoadCauseEventData::EVENT_4_UUID, 'CoalitionsEventRegistrationConfirmationMessage'];
+        yield [LoadCommitteeEventData::EVENT_6_UUID, 'EventRegistrationConfirmationMessage'];
     }
 }
