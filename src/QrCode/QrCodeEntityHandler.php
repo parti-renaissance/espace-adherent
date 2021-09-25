e12e74186c44e34028e2f07f03ed38bd6d344e6d
Remg <remi@gardien.biz>
2021-09-26 01:50:36 +0200

    Force download QRCodes as SVG only

--- src/QrCode/QrCodeEntityHandler.php
+++ src/QrCode/QrCodeEntityHandler.php
@@ -17,7 +17,7 @@ class QrCodeEntityHandler
         $this->urlGenerator = $urlGenerator;
     }
 
-    public function generateQrCode(QrCode $qrCode, string $writerByName): QrCodeResponse
+    public function generateQrCode(QrCode $qrCode, string $writerByName, bool $download = false): QrCodeResponse
     {
         $redirectUrl = $this->urlGenerator->generate(
             'app_qr_code',
@@ -27,6 +27,6 @@ class QrCodeEntityHandler
             UrlGeneratorInterface::ABSOLUTE_URL
         );
 
-        return $this->qrCodeFactory->createResponse($redirectUrl, $qrCode->getName(), $writerByName);
+        return $this->qrCodeFactory->createResponse($redirectUrl, $qrCode->getName(), $writerByName, $download);
     }
 }
