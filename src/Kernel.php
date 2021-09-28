693c8de485bfea22d4e96df83010609bb59f8870
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2021-09-28 13:07:06 +0200

    [PHP] Update Symfony to 4.4.32

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,7 +9,6 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Messenger\DependencyInjection\MessengerPass;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
@@ -67,7 +66,6 @@ class Kernel extends BaseKernel
     {
         $container
             ->addCompilerPass(new StatisticsCalculatorPass())
-            ->addCompilerPass(new MessengerPass())
             ->addCompilerPass(new SecurityPass())
         ;
     }
