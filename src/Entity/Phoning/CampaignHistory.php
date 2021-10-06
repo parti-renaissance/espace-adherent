52b676908b8f4ed4adc48721276e63f9e42e89fb
Olga <olga.rotach@gmail.com>
2021-10-06 18:03:55 +0200

    Add some tests and some fixes (#6332)

--- src/Entity/Phoning/CampaignHistory.php
+++ src/Entity/Phoning/CampaignHistory.php
@@ -90,7 +90,7 @@ class CampaignHistory implements DataSurveyAwareInterface
     private $type;
 
     /**
-     * @var string
+     * @var string|null
      *
      * @ORM\Column(length=25)
      *
@@ -150,7 +150,7 @@ class CampaignHistory implements DataSurveyAwareInterface
      *
      * @Assert\Choice(
      *     callback={"App\Phoning\CampaignHistoryEngagementEnum", "toArray"},
-     *     message="phoning.campaign_history.type.invalid_choice",
+     *     message="phoning.campaign_history.engagement.invalid_choice",
      *     strict=true
      * )
      *
@@ -248,7 +248,7 @@ class CampaignHistory implements DataSurveyAwareInterface
         return $this->status;
     }
 
-    public function setStatus(string $status): void
+    public function setStatus(?string $status): void
     {
         $this->status = $status;
     }
