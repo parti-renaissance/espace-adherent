d0e2a9f3a59b23a84c2eeb05f0387fb67d0e5baa
illusionOfParadise <olga.rotach@gmail.com>
2021-10-07 17:26:12 +0200

    [Admin] Add survey results page

--- src/Entity/Jecoute/DataSurveyAwareTrait.php
+++ src/Entity/Jecoute/DataSurveyAwareTrait.php
@@ -2,21 +2,8 @@
 
 namespace App\Entity\Jecoute;
 
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
-
 trait DataSurveyAwareTrait
 {
-    /**
-     * @var DataSurvey|null
-     *
-     * @ORM\OneToOne(targetEntity="App\Entity\Jecoute\DataSurvey", cascade={"persist"}, orphanRemoval=true)
-     * @ORM\JoinColumn(onDelete="SET NULL")
-     *
-     * @Assert\Valid
-     */
-    private $dataSurvey;
-
     public function getDataSurvey(): ?DataSurvey
     {
         return $this->dataSurvey;
