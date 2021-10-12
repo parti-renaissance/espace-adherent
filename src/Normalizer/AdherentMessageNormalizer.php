c5d6fec7f39fd01608b7d1342e876dd6e59498ba
Rémi Gardien <remi@gardien.biz>
2021-10-12 16:21:01 +0200

    Add custom normalizer

---
+++ src/Normalizer/AdherentMessageNormalizer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Normalizer;
+
+use App\AdherentMessage\AdherentMessageTypeEnum;
+use App\AdherentMessage\StatisticsAggregator;
+use App\Entity\AdherentMessage\AbstractAdherentMessage;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class AdherentMessageNormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private StatisticsAggregator $statisticsAggregator;
+
+    public function __construct(StatisticsAggregator $statisticsAggregator)
+    {
+        $this->statisticsAggregator = $statisticsAggregator;
+    }
+
+    private const ALREADY_CALLED = 'ADHERENT_MESSAGE_NORMALIZER_ALREADY_CALLED';
+
+    /** @var AbstractAdherentMessage */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $context[static::ALREADY_CALLED] = true;
+
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        $groups = $context['groups'] ?? [];
+
+        if (\in_array('message_read_list', $groups, true)) {
+            $data['statistics'] = $this->statisticsAggregator->aggregateData($object);
+        }
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null, array $context = [])
+    {
+        return
+            empty($context[self::ALREADY_CALLED])
+            && $data instanceof AbstractAdherentMessage
+        ;
+    }
+
+    private function getZonesFromMessage(AbstractAdherentMessage $message): array
+    {
+        switch ($message->getType()) {
+            case AdherentMessageTypeEnum::REFERENT:
+
+        }
+
+        foreach ($message->getMailchimpCampaigns())
+    }
+}
