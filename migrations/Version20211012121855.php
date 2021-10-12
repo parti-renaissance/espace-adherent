27dd06469dc70c094a88e7259b095520681f0c3b
kevin <kevindoh26@gmail.com>
2021-10-12 17:23:12 +0200

    [EM-1260] fix pr feedbacks

--- migrations/Version20211012121855.php
+++ migrations/Version20211012121855.php
@@ -7,11 +7,6 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20211012121855 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE adherents ADD team_phoning_national_manager_role TINYINT(1) DEFAULT \'0\' NOT NULL');
