6abb8e1a4da145d6034005ebd94402542030b532
Rémi Gardien <remi@gardien.biz>
2021-10-26 12:03:25 +0200

    Add new Pap\Address & Pap\Voter entities (#6450)
    * Add new Pap\Address & Pap\Voter entities
    
    * Set default datetime to null

---
+++ src/Repository/Pap/AddressRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository\Pap;
+
+use App\Entity\Pap\Address;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class AddressRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Address::class);
+    }
+}
---
+++ src/Repository/Pap/VoterRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository\Pap;
+
+use App\Entity\Pap\Voter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class VoterRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Voter::class);
+    }
+}
