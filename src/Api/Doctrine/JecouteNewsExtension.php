d1ac6f6c815935c2b8ce79d5c4188d67755603be
kevin <kevindoh26@gmail.com>
2021-11-10 12:03:43 +0100

    [API][News] fix feedbacks

--- src/Api/Doctrine/JecouteNewsExtension.php
+++ src/Api/Doctrine/JecouteNewsExtension.php
@@ -5,22 +5,16 @@ namespace App\Api\Doctrine;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Jecoute\News;
-use App\Repository\Geo\ZoneRepository;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Security;
 
 class JecouteNewsExtension implements ContextAwareQueryCollectionExtensionInterface
 {
     private Security $security;
-    private ZoneRepository $zoneRepository;
-    private RequestStack $requestStack;
 
-    public function __construct(Security $security, ZoneRepository $zoneRepository, RequestStack $requestStack)
+    public function __construct(Security $security)
     {
         $this->security = $security;
-        $this->zoneRepository = $zoneRepository;
-        $this->requestStack = $requestStack;
     }
 
     public function applyToCollection(
