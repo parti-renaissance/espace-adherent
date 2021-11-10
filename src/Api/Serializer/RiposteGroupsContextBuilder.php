d102ec143e7939d1c95150f7e4045ff0d1b2e1d0
kevin <kevindoh26@gmail.com>
2021-11-10 14:21:43 +0100

    [API][News] clean code

--- src/Api/Serializer/RiposteGroupsContextBuilder.php
+++ src/Api/Serializer/RiposteGroupsContextBuilder.php
@@ -2,14 +2,29 @@
 
 namespace App\Api\Serializer;
 
+use ApiPlatform\Core\Serializer\SerializerContextBuilderInterface;
 use App\Entity\Jecoute\Riposte;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
-class RiposteGroupsContextBuilder extends AbstractGroupsContextBuilder
+class RiposteGroupsContextBuilder implements SerializerContextBuilderInterface
 {
-    protected function updateContext(array $context, Request $request): array
+    private SerializerContextBuilderInterface $decorated;
+    private AuthorizationCheckerInterface $authorizationChecker;
+
+    public function __construct(
+        SerializerContextBuilderInterface $decorated,
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
+        $this->decorated = $decorated;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public function createFromRequest(Request $request, bool $normalization, array $extractedAttributes = null): array
     {
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
         $resourceClass = $context['resource_class'] ?? null;
+
         if (Riposte::class === $resourceClass
             && !$this->authorizationChecker->isGranted('ROLE_OAUTH_SCOPE_JEMARCHE_APP')
         ) {
