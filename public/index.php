5299c38a4060fb04f4139d8860e6e39875d90722
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2021-11-23 09:07:16 +0100

    [Config] Use Nginx location for health check (#6570)

--- public/index.php
+++ public/index.php
@@ -10,26 +10,9 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ('prod' === $_SERVER['APP_ENV']) {
-    if (!empty($_SERVER['BASIC_AUTH_USER']) && !empty($_SERVER['BASIC_AUTH_PASSWORD']) && !empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-        if (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] !== $_SERVER['BASIC_AUTH_USER'] || $_SERVER['PHP_AUTH_PW'] !== $_SERVER['BASIC_AUTH_PASSWORD']) {
-            header('HTTP/1.0 401 Unauthorized');
-            header('WWW-Authenticate: Basic realm="Password required"');
-
-            echo 'Unauthorized';
-            exit;
-        }
-    }
-
-    if (!isset($_SERVER['HTTP_CF_RAY'])) {
-        echo 'GoogleHC healthy';
-        exit;
-    }
-
-    if ((bool) $_SERVER['ENABLE_MAINTENANCE']) {
-        include __DIR__.'/maintenance.html';
-        exit;
-    }
+if ($_SERVER['ENABLE_MAINTENANCE']) {
+    include __DIR__.'/maintenance.html';
+    exit;
 }
 
 require __DIR__.'/../config/trusted_proxies.php';
