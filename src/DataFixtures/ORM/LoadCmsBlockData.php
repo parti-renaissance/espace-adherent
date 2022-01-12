87efe9fd137a03f2a13126d91c93873dc1166cb7
illusionOfParadise <olga.rotach@gmail.com>
2022-01-12 18:21:08 +0100

    [API] Add Jemengage RGPD endpoint

--- src/DataFixtures/ORM/LoadCmsBlockData.php
+++ src/DataFixtures/ORM/LoadCmsBlockData.php
@@ -39,6 +39,16 @@ MARKDOWN;
             '**Texte de la charte** pour la *campagne* de PAP avec le Markdown'
         ));
 
+        $manager->persist($this->createCmsBlock(
+            'je-mengage-rgpd',
+            'Affiché en bas du formulaire d\'inscription',
+            'Les données recueillies sur ce formulaire sont traitées par LaREM afin de gérer les informations relatives aux inscriptions aux évènements de LaREM et de permettre à LaREM de vous envoyer des communications politiques. 
+Si vous êtes élu(e) ou ancien(ne) élu(e), nous traitons également vos données dans le cadre de l’animation de notre réseau d’élu(e)s et vos données peuvent être transférer à La République Ensemble ou à l’institut de formation Tous Politiques, conformément à la politique de protection des données des élu(e)s. Toutes les informations sont obligatoires, sauf celles marquées "Optionnel". L’absence de réponse dans ces champs ne permettra pas à LaREM de traiter votre demande. 
+Conformément à la règlementation, vous disposez d’un droit d’opposition et d’un droit à la limitation du traitement de données vous concernant, ainsi que d’un droit d’accès, de rectification, de portabilité et d’effacement de vos données. 
+Vous disposez également de la faculté de donner des directives sur le sort de vos données après votre décès. 
+Vous pouvez consulter notre Politique de protection des données (si vous êtes élu(e)s, la Politique de protection des données des élu(e)s) et exercer vos droits en nous adressant votre demande accompagnée d’une copie de votre pièce d’identité à l’adresse postale : La République En Marche, 68 rue du Rocher, 75008 Paris, France ou électronique suivante : **mes-donnees@en-marche.fr **ou encore en contactant notre DPO à l’adresse : **dpo@en-marche.fr**.'
+        ));
+
         $manager->flush();
     }
 
