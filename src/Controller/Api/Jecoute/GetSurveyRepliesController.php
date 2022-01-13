e48b050943afda7f656edbdd6f9c30d9cbc989ee
kevin <kevindoh26@gmail.com>
2022-01-13 19:31:43 +0100

    [EM-1971] get only national survey replies of my zone as a referent user

--- src/Controller/Api/Jecoute/GetSurveyRepliesController.php
+++ src/Controller/Api/Jecoute/GetSurveyRepliesController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\Api\Jecoute;
 
+use App\Controller\EnMarche\AccessDelegatorTrait;
+use App\Entity\Adherent;
+use App\Entity\Geo\Zone;
 use App\Entity\Jecoute\Survey;
 use App\Exporter\SurveyExporter;
+use App\Repository\Geo\ZoneRepository;
 use App\Repository\Jecoute\DataSurveyRepository;
+use App\Scope\AuthorizationChecker;
+use App\Scope\ScopeEnum;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +30,17 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class GetSurveyRepliesController extends AbstractController
 {
+    use AccessDelegatorTrait;
+
+    private AuthorizationChecker $authorizationChecker;
+    private ZoneRepository $zoneRepository;
+
+    public function __construct(AuthorizationChecker $authorizationChecker, ZoneRepository $zoneRepository)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->zoneRepository = $zoneRepository;
+    }
+
     public function __invoke(
         Request $request,
         Survey $survey,
@@ -31,14 +48,41 @@ class GetSurveyRepliesController extends AbstractController
         DataSurveyRepository $dataSurveyRepository,
         SurveyExporter $exporter
     ): Response {
+        $user = $this->getMainUser($request->getSession());
+        $scopeGenerator = $this->authorizationChecker->getScopeGenerator(
+            $request,
+            $user
+        );
+
+        $zoneCodes = [];
+        if (ScopeEnum::REFERENT === $scopeGenerator->getCode() && $survey->isNational()) {
+            $zones = $this->getZones($user);
+
+            /** @var Zone $zone */
+            foreach ($zones as $zone) {
+                switch ($zone->getType()) {
+                    case Zone::DEPARTMENT:
+                        $zoneCodes[] = $zone->getCode();
+                        break;
+                    case Zone::BOROUGH:
+                        $department = current($zone->getParentsOfType(Zone::DEPARTMENT));
+                        $zoneCodes[] = $department->getCode();
+                        break;
+                }
+            }
+
+            $zoneCodes = array_unique($zoneCodes);
+        }
+
         if ('json' !== $_format) {
-            return $exporter->export($survey, $_format);
+            return $exporter->export($survey, $_format, false, [], $zoneCodes);
         }
 
         return $this->json(
             $dataSurveyRepository->findDataSurveyForSurvey(
                 $survey,
                 [],
+                $zoneCodes,
                 $request->query->getInt('page', 1),
                 $request->query->getInt('page_size', 30)
             ),
@@ -46,5 +90,10 @@ class GetSurveyRepliesController extends AbstractController
             [],
             ['groups' => ['survey_replies_list']]
         );
+    }
+
+    protected function getZones(Adherent $adherent): array
+    {
+        return $this->zoneRepository->findForJecouteByReferentTags($adherent->getManagedArea()->getTags()->toArray());
     }
 }
