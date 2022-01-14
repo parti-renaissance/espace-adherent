3ab2e86cea75ebb0eb39cdf7c84ecc35c7fc9b6a
Olga <olga.rotach@gmail.com>
2022-01-14 16:29:13 +0100

    [API] Make Jemengage RGPD endpoint public (#6760)

--- src/Controller/Api/JeMengage/GetRgpdController.php
+++ src/Controller/Api/JeMengage/GetRgpdController.php
@@ -3,15 +3,12 @@
 namespace App\Controller\Api\JeMengage;
 
 use App\CmsBlock\CmsBlockManager;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/v3/je-mengage/rgpd", name="api_get_je_mengage_rgpd", methods={"GET"})
- *
- * @Security("is_granted('ROLE_OAUTH_SCOPE_JEMARCHE_APP') or is_granted('ROLE_OAUTH_SCOPE_JEMENGAGE_ADMIN')")
+ * @Route("/je-mengage/rgpd", name="api_get_je_mengage_rgpd", methods={"GET"})
  */
 class GetRgpdController extends AbstractController
 {
