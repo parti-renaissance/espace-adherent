57b8f131029f1f7980d34b01fd6d8da248325efe
kevin <kevindoh26@gmail.com>
2022-01-18 17:14:10 +0100

    [EM-2010] add endpoint to retrieve cgu and data policy static page

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -7,7 +7,6 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
