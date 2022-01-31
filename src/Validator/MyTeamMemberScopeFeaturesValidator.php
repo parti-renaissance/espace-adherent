854d6249a7ccdd70807dfe5aa5481152d11888c6
Olga <olga.rotach@gmail.com>
2022-01-31 11:49:22 +0100

    [API][MyTeam] Add scope features validation allowing only delegator's features (#6832)

---
+++ src/Validator/MyTeamMemberScopeFeaturesValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Validator;
+
+use App\Scope\ScopeGeneratorResolver;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class MyTeamMemberScopeFeaturesValidator extends ConstraintValidator
+{
+    private ScopeGeneratorResolver $scopeGeneratorResolver;
+
+    public function __construct(ScopeGeneratorResolver $scopeGeneratorResolver)
+    {
+        $this->scopeGeneratorResolver = $scopeGeneratorResolver;
+    }
+
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof MyTeamMemberScopeFeatures) {
+            throw new UnexpectedTypeException($constraint, MyTeamMemberScopeFeatures::class);
+        }
+
+        if (!\is_array($value)) {
+            return;
+        }
+
+        if (!($scope = $this->scopeGeneratorResolver->generate())
+            || !($features = $scope->getFeatures())) {
+            return;
+        }
+
+        if ([] !== array_diff($value, $features)) {
+            $this
+                ->context
+                ->buildViolation($constraint->message)
+                ->addViolation()
+            ;
+        }
+    }
+}
