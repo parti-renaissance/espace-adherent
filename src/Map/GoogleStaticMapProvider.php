14176d1bef86c0b820d9c78a85f3c5e5a1f7f9ce
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-02-05 16:42:56 +0100

    Step 2 - Google Maps client

--- src/Map/GoogleStaticMapProvider.php
+++ src/Map/GoogleStaticMapProvider.php
@@ -3,9 +3,9 @@
 namespace App\Map;
 
 use App\Geocoder\Coordinates;
-use GuzzleHttp\Client;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GoogleStaticMapProvider implements StaticMapProviderInterface
 {
@@ -13,12 +13,12 @@ class GoogleStaticMapProvider implements StaticMapProviderInterface
     public const MAPS_API_ENDPOINT = '/maps/api/staticmap';
     public const CACHE_KEY_PREFIX = 'maps_static_';
 
-    private $client;
+    private HttpClientInterface $client;
     private $cache;
     private $logger;
     private $key;
 
-    public function __construct(Client $client, AdapterInterface $cache, LoggerInterface $logger, string $key)
+    public function __construct(HttpClientInterface $client, AdapterInterface $cache, LoggerInterface $logger, string $key)
     {
         $this->client = $client;
         $this->cache = $cache;
@@ -54,7 +54,7 @@ class GoogleStaticMapProvider implements StaticMapProviderInterface
         ], null, '&', \PHP_QUERY_RFC3986);
 
         try {
-            return $this->client->request('GET', self::MAPS_API_ENDPOINT.'?'.$parameters)->getBody()->getContents();
+            return $this->client->request('GET', self::MAPS_API_ENDPOINT.'?'.$parameters)->getContent();
         } catch (\Exception $e) {
             $this->logger->warning('Unable to retrieve the map', ['exception' => $e]);
         }
