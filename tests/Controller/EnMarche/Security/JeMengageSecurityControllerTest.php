4eab7de0f176008683eafb2aa2407e15b70426d2
illusionOfParadise <olga.rotach@gmail.com>
2022-02-07 16:23:08 +0100

    [MyTeam] Put is_granted('ROLE_OAUTH_SCOPE_JEMENGAGE_ADMIN') to MyTeam endpoints

--- tests/Controller/EnMarche/Security/JeMengageSecurityControllerTest.php
+++ tests/Controller/EnMarche/Security/JeMengageSecurityControllerTest.php
@@ -41,7 +41,7 @@ class JeMengageSecurityControllerTest extends WebTestCase
 
     public function testAdherentWithGoodRoleCanConnectToJeMengageApp(): void
     {
-        $this->client->request(Request::METHOD_GET, 'http://login.jemengage.code/oauth/v2/auth?response_type=code&client_id=4498e44f-f214-110d-8b76-98a83f9d2b0c');
+        $this->client->request(Request::METHOD_GET, 'http://login.jemengage.code/oauth/v2/auth?response_type=code&client_id=4498e44f-f214-110d-8b76-98a83f9d2b0c&scope=jemengage_admin');
         $crawler = $this->client->followRedirect();
 
         $this->client->submit($crawler->selectButton('Connexion')->form([
@@ -50,7 +50,7 @@ class JeMengageSecurityControllerTest extends WebTestCase
         ]));
 
         $this->assertResponseStatusCode(Response::HTTP_FOUND, $this->client->getResponse());
-        $this->assertClientIsRedirectedTo('http://login.jemengage.code/oauth/v2/auth?client_id=4498e44f-f214-110d-8b76-98a83f9d2b0c&response_type=code', $this->client);
+        $this->assertClientIsRedirectedTo('http://login.jemengage.code/oauth/v2/auth?client_id=4498e44f-f214-110d-8b76-98a83f9d2b0c&response_type=code&scope=jemengage_admin', $this->client);
 
         $this->client->followRedirect();
 
