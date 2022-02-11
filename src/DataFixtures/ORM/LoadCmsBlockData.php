e7d40f7528a8bf8e26745d651a6fddd287e45ed4
Rémi Gardien <gardien.remi@gmail.com>
2022-02-11 15:24:35 +0100

    Add more cms blocks

--- src/DataFixtures/ORM/LoadCmsBlockData.php
+++ src/DataFixtures/ORM/LoadCmsBlockData.php
@@ -77,7 +77,7 @@ TXT
            'Texte affichant les informations de contact au support',
            <<<'TXT'
 En cas de question, n'hésitez pas à nous contacter au <a href="tel:+33186950286">01 86 95 02 86</a>
-ou à <a href="mailto:contact@en-marche.fr">contact@en-marche.fr</a>.
+ou à <a href="mailto:mobilisation@em-2022.fr">mobilisation@em-2022.fr</a>.
 TXT
         ));
 
@@ -115,6 +115,80 @@ en France) ou dans une ambassade ou consulat de France (si vous résidez à l'é
 TXT
         ));
 
+        $manager->persist($this->createCmsBlock(
+            'procuration-proposal-cta',
+            'Texte affiché avant le call-to-action "Je me porte mandataire"',
+            'Je me propose pour voter au nom d’un(e) citoyen(e) près de chez moi'
+        ));
+
+        $manager->persist($this->createCmsBlock(
+            'procuration-request-cta',
+            'Texte affiché avant le call-to-action "Je donne procuration"',
+            'Je souhaite qu’un(e) citoyen(e) vote en mon nom'
+        ));
+
+        $manager->persist($this->createCmsBlock(
+            'procuration-proposal-form-header',
+            'Texte affiché au début du formulaire de proposition de procuration',
+            'Portez la voix d’un citoyen près de chez vous'
+        ));
+
+        $manager->persist($this->createCmsBlock(
+            'procuration-proxy-thanks-content',
+            'Texte affiché pour la page de confirmation de proposition de procuration',
+            <<<'TXT'
+<h1 class="text--large">
+    Merci! Votre proposition est bien prise en compte.
+</h1>
+<p class="text--body">
+    Quelles sont les prochaines étapes?
+    <ol>
+        <li>Si nous recevons une demande dans votre commune ou dans l’une de celles où vous nous avez notifié pouvoir vous rendre, nous réaliserons un «match» entre vous.</li>
+        <li>Vous recevrez un mail identique vous indiquant vos coordonnées respectives.</li>
+        <li>Prenez contact l’un avec l’autre et confirmez la mission entre vous.</li>
+        <li>Attendez que le mandant réalise les démarches. N’hésitez pas à prendre des nouvelles régulièrement si vous n’en avez pas.</li>
+        <li>Vous pourrez voter le jour J en présentant simplement votre propre pièce d’identité (vous n’avez besoin d’aucun document de la part de mandant). Assurez-vous de bien connaitre l’adresse du bureau de vote à l’avance.</li>
+    </ol>
+</p>
+TXT
+        ));
+
+        $manager->persist($this->createCmsBlock(
+            'procuration-request-thanks-content',
+            'Texte affiché pour la page de confirmation de demande de procuration',
+            <<<'TXT'
+<h1 class="text--large">
+    Merci! Votre demande est bien prise en compte.
+</h1>
+<p class="text--body">
+    Quelles sont les prochaines étapes?
+    <ol>
+        <li>Nous vous cherchons un mandataire</li>
+        <li>Quand nous l’avons trouvé, nous vous envoyons à tous les deux le même mail avec vos coordonnées respectives.</li>
+        <li>Prenez contact l’un avec l’autre et confirmez la mission entre vous.</li>
+        <li>
+            Vous, le mandant, devez-vous rendre dans un commissariat ou une gendarmerie (si vous habitez en France) ou dans un consulat de France (si vous résidez à l'étranger) pour établir la procuration. Vous pouvez aussi gagner du temps dans la démarche et faire l’opération en ligne sur https://www.maprocuration.gouv.fr : 
+            
+            Notez que vous devrez quand même vous rendre dans les lieux précédemment cités pour vérifier votre identité et valider la procédure.
+            
+            Notez aussi que vous devrez être en possession de votre numéro d’électeur pour faire la procédure sur le site https://www.maprocuration.gouv.fr ou au commissariat / gendarmerie.
+        </li>
+        <li>N’oubliez pas d’informer votre mandataire quand la procuration est bien acceptée. Donnez lui toutes les indications nécessaires pour son déplacement (adresse du bureau de vote, etc.). Vous n’avez aucun document particulier à lui donner, il votera en votre nom en présentant sa propre pièce d’identité.</li>
+    </ol>
+</p>
+TXT
+        ));
+
+        $manager->persist($this->createCmsBlock(
+            'procuration-request-form-intro',
+            'Texte affiché avant le formulaire de demande de procuration',
+            <<<'TXT'
+Nous pouvons vous trouver quelqu’un qui portera votre voix le jour de l’élection. 
+Remplissez simplement le formulaire. 
+Attention, la procuration doit ensuite parvenir à votre bureau de vote, ce qui prend plusieurs jours. 
+Effectuez donc votre démarche rapidement !
+TXT
+        ));
         $manager->flush();
     }
 
