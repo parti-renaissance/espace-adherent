21e528c496b2500d3a743c1c103a641a1f6bba1d
kevin <kevindoh26@gmail.com>
2022-02-18 10:35:43 +0100

    [LPN-282] rename some EM BO menu and add new role

--- src/DataFixtures/ORM/LoadAdminData.php
+++ src/DataFixtures/ORM/LoadAdminData.php
@@ -72,6 +72,7 @@ class LoadAdminData extends Fixture
             'ROLE_ADMIN_PAP_CAMPAIGNS',
             'ROLE_ADMIN_QR_CODES',
             'ROLE_ADMIN_CMS_BLOCKS',
+            'ROLE_ADMIN_JEMENGAGE_COM',
         ];
 
         $writerRoles = [
