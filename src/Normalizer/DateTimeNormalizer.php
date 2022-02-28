4a1082956be9ba1debbb1e50668faa4791528e40
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-03-01 00:53:58 +0100

    [API] Fix empty date denormalization error

---
+++ src/Normalizer/DateTimeNormalizer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Normalizer;
+
+use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer as BaseDateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * Workaround for https://github.com/symfony/symfony/issues/27824
+ */
+class DateTimeNormalizer implements DenormalizerInterface, NormalizerInterface
+{
+    private BaseDateTimeNormalizer $decorated;
+
+    public function __construct(BaseDateTimeNormalizer $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        return $this->decorated->normalize($object, $format, $context);
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        try {
+            return $this->decorated->denormalize($data, $class, $format, $context);
+        } catch (NotNormalizableValueException $e) {
+            return null;
+        }
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return $this->decorated->supportsDenormalization($data, $type, $format);
+    }
+}
