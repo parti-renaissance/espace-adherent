a6d681687e147a90c1a7f221259db42409c863ae
Rémi Gardien <gardien.remi@gmail.com>
2022-03-07 11:16:07 +0100

    Remove relation on adherent unregistration

---
+++ migrations/Version20220307111506.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220307111506 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE contact DROP FOREIGN KEY FK_4C62E63825F06C53');
+        $this->addSql('ALTER TABLE
+          contact
+        ADD
+          CONSTRAINT FK_4C62E63825F06C53 FOREIGN KEY (adherent_id) REFERENCES adherents (id) ON DELETE
+        SET
+          NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE contact DROP FOREIGN KEY FK_4C62E63825F06C53');
+        $this->addSql('ALTER TABLE
+          contact
+        ADD
+          CONSTRAINT FK_4C62E63825F06C53 FOREIGN KEY (adherent_id) REFERENCES adherents (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+    }
+}
