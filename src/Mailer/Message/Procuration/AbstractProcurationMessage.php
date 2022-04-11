9f744b8be962d6cd884fc8d7f1d88b356d0d9564
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-04-11 12:04:14 +0200

    [Procuration] Update replyTo email (#7160)

--- src/Mailer/Message/Procuration/AbstractProcurationMessage.php
+++ src/Mailer/Message/Procuration/AbstractProcurationMessage.php
@@ -8,7 +8,7 @@ abstract class AbstractProcurationMessage extends Message
 {
     protected static function updateSenderInfo(Message $message): Message
     {
-        $message->setSenderEmail('contact@avecvous.fr');
+        $message->setSenderEmail('ne-pas-repondre@avecvous.fr');
         $message->setSenderName('Procuration avec vous');
 
         return $message;
