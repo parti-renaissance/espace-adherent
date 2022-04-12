97bd3d4b4b6820f200e130713844562c16c4209a
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-04-12 10:27:32 +0200

    [PAP] improve vote place list endpoint (#7164)

--- src/Api/Filter/PapVotePlaceScopeFilter.php
+++ src/Api/Filter/PapVotePlaceScopeFilter.php
@@ -4,11 +4,20 @@ namespace App\Api\Filter;
 
 use App\Entity\Adherent;
 use App\Entity\Pap\VotePlace;
+use App\Repository\Pap\VotePlaceRepository;
 use App\Scope\Generator\ScopeGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
 
 final class PapVotePlaceScopeFilter extends AbstractScopeFilter
 {
+    private ?VotePlaceRepository $repository = null;
+
+    /** @required */
+    public function setVotePlaceRepository(VotePlaceRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     protected function needApplyFilter(string $property, string $resourceClass, string $operationName = null): bool
     {
         return is_a($resourceClass, VotePlace::class, true);
@@ -21,13 +30,12 @@ final class PapVotePlaceScopeFilter extends AbstractScopeFilter
     ): void {
         $alias = $queryBuilder->getRootAliases()[0];
         $scope = $scopeGenerator->generate($currentUser);
-        if (!$scope->isNational()) {
-            $queryBuilder
-                ->innerJoin($alias.'.zone', 'zone')
-                ->leftJoin('zone.parents', 'parent_zone')
-                ->andWhere('zone IN (:zones) OR parent_zone IN (:zones)')
-                ->setParameter('zones', $scope->getZones())
-            ;
+
+        if ($scope->isNational()) {
+            // for national scope return 0 vote place instead of all 60K
+            $queryBuilder->setMaxResults(0);
+        } else {
+            $this->repository->withZones($queryBuilder, $scope->getZones(), $alias);
         }
     }
 }
