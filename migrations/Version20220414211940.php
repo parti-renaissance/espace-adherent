998d784ba8071ad3a9a3534dd09b0fb666d81da0
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-04-14 21:22:09 +0200

    [ManagedUser] improve status index

---
+++ migrations/Version20220414211940.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220414211940 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX projection_managed_users_search ON projection_managed_users');
+        $this->addSql('CREATE INDEX IDX_90A7D6567B00651C ON projection_managed_users (status)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX IDX_90A7D6567B00651C ON projection_managed_users');
+        $this->addSql('CREATE INDEX projection_managed_users_search ON projection_managed_users (status, postal_code, country)');
+    }
+}
