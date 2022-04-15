34765704865210b8f64c4b6e06fdbe7d6218cef0
Rémi Gardien <gardien.remi@gmail.com>
2022-04-15 09:43:40 +0200

    Fix typo

--- src/Validator/PapCampaignVotePlaces.php
+++ src/Validator/PapCampaignVotePlaces.php
@@ -9,8 +9,8 @@ use Symfony\Component\Validator\Constraint;
  */
 class PapCampaignVotePlaces extends Constraint
 {
-    public $messageAnotherCampaign = 'Un ou plusieurs bureaux de votes que vous avez choisi sont déjà dans une autre campagne.';
-    public $messageNotInManagedZone = 'Un ou plusieurs bureaux de votes ne sont pas dans la zone gérée';
+    public $messageAnotherCampaign = 'Un ou plusieurs bureaux de vote que vous avez choisi sont déjà dans une autre campagne.';
+    public $messageNotInManagedZone = 'Un ou plusieurs bureaux de vote ne sont pas dans la zone gérée';
     public $messageAnotherCampaignByZone = 'Il existe déjà une campagne dans la zone que vous gérez pour cette période.';
 
     public function getTargets()
