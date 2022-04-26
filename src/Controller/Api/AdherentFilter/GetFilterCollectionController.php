c2bca3ab4356655cd019e80fc2f9d2a93dfcc816
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-04-26 18:32:35 +0200

    [Filters] add audienceTypes field for message feature filters (#7235)

--- src/Controller/Api/AdherentFilter/GetFilterCollectionController.php
+++ src/Controller/Api/AdherentFilter/GetFilterCollectionController.php
@@ -3,47 +3,37 @@
 namespace App\Controller\Api\AdherentFilter;
 
 use App\AdherentFilter\AdherentFiltersGenerator;
-use App\Scope\AuthorizationChecker;
-use App\Scope\Exception\InvalidScopeException;
-use App\Scope\Exception\ScopeExceptionInterface;
-use App\Scope\Exception\ScopeQueryParamMissingException;
-use App\Scope\FeatureEnum;
+use App\Scope\ScopeGeneratorResolver;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/v3/adherents/filters", name="app_adherents_filters_get", methods={"GET"})
  *
- * @Security("is_granted('IS_AUTHENTICATED_FULLY') and is_granted('REQUEST_SCOPE_GRANTED')")
+ * @Security("is_granted('IS_FEATURE_GRANTED', ['contacts', 'messages'])")
  */
 class GetFilterCollectionController extends AbstractController
 {
-    private AuthorizationChecker $authorizationChecker;
+    private ScopeGeneratorResolver $scopeGeneratorResolver;
     private AdherentFiltersGenerator $builder;
 
-    public function __construct(AuthorizationChecker $authorizationChecker, AdherentFiltersGenerator $builder)
+    public function __construct(ScopeGeneratorResolver $scopeGeneratorResolver, AdherentFiltersGenerator $builder)
     {
-        $this->authorizationChecker = $authorizationChecker;
+        $this->scopeGeneratorResolver = $scopeGeneratorResolver;
         $this->builder = $builder;
     }
 
     public function __invoke(Request $request, UserInterface $user): Response
     {
-        try {
-            $this->authorizationChecker->isFeatureGranted($request, $user, [FeatureEnum::CONTACTS]);
-        } catch (InvalidScopeException|ScopeQueryParamMissingException $e) {
-            throw new BadRequestHttpException();
-        } catch (ScopeExceptionInterface $e) {
-            throw $this->createAccessDeniedException();
-        }
-
         return $this->json(
-            $this->builder->generate($this->authorizationChecker->getScopeGenerator($request, $user)->getCode(), $request->query->get('feature')),
+            $this->builder->generate(
+                $this->scopeGeneratorResolver->generate()->getMainCode(),
+                $request->query->get('feature')
+            ),
             Response::HTTP_OK,
             [],
             ['groups' => ['filter:read']]
---
+++ src/Mailchimp/Campaign/AudienceTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Mailchimp\Campaign;
+
+class AudienceTypeEnum
+{
+    public const ADHERENT = 'adherent';
+    public const LEGISLATIVE_CANDIDATE_NEWSLETTER = 'legislative_candidate_newsletter';
+}
