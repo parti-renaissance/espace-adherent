8d85f99248fa541f98083473f2882c87dee933c0
kevin <kevindoh26@gmail.com>
2022-04-29 11:58:18 +0200

    [Events][LPN-652] apply feedbacks

--- src/Entity/Event/BaseEvent.php
+++ src/Entity/Event/BaseEvent.php
@@ -89,6 +89,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiResource(
  *     attributes={
  *         "order": {"beginAt": "ASC"},
+ *         "denormalization_context": {"groups": {"event_write"}},
  *         "normalization_context": {
  *             "groups": {"event_read", "image_owner_exposed"}
  *         },
@@ -108,7 +109,6 @@ use Symfony\Component\Validator\Constraints as Assert;
  *             "path": "/v3/events/{id}",
  *             "access_control": "is_granted('CAN_MANAGE_EVENT', object)",
  *             "requirements": {"id": "%pattern_uuid%"},
- *             "denormalization_context": {"groups": {"event_write"}},
  *         },
  *         "delete": {
  *             "path": "/v3/events/{id}",
@@ -175,9 +175,6 @@ use Symfony\Component\Validator\Constraints as Assert;
  *             "access_control": "is_granted('ROLE_USER')",
  *             "path": "/v3/events",
  *             "validation_groups": {"Default", "api_put_validation", "event_creation"},
- *             "denormalization_context": {
- *                 "groups": {"event_write"}
- *             },
  *         },
  *     },
  * )
