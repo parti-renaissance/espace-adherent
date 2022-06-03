bc2cde1904ea2145913cd71ebc703f5ff7894536
kevin <kevindoh26@gmail.com>
2022-06-03 16:59:36 +0200

    [LPN-865] change keys

--- src/Controller/Api/AdherentMessage/GetAdherentMessageKpiController.php
+++ src/Controller/Api/AdherentMessage/GetAdherentMessageKpiController.php
@@ -5,21 +5,25 @@ namespace App\Controller\Api\AdherentMessage;
 use App\Entity\Geo\Zone;
 use App\Repository\MailchimpCampaignReportRepository;
 use App\Scope\ScopeGeneratorResolver;
+use Symfony\Component\HttpFoundation\Request;
 
 class GetAdherentMessageKpiController
 {
     public function __invoke(
+        Request $request,
         MailchimpCampaignReportRepository $mailchimpCampaignReportRepository,
         ScopeGeneratorResolver $resolver
     ): array {
+        $maxHistory = (int) $request->query->get('max_history', 30);
         $scope = $resolver->generate();
 
         return [
+            'local' => $mailchimpCampaignReportRepository->findLocalReportRation($scope->getMainCode(), $scope->getZones(), $maxHistory),
+            'national' => $mailchimpCampaignReportRepository->findNationalReportRatio($scope->getMainCode(), $maxHistory),
             'zones' => array_map(function (Zone $zone): string {
                 return $zone->getName();
             }, $scope->getZones()),
-            'local' => $mailchimpCampaignReportRepository->findLocalReportRation($scope->getMainCode(), $scope->getZones()),
-            'national' => $mailchimpCampaignReportRepository->findNationalReportRatio($scope->getMainCode()),
+            'since' => new \DateTime("-$maxHistory days"),
         ];
     }
 }
--- src/Repository/MailchimpCampaignReportRepository.php
+++ src/Repository/MailchimpCampaignReportRepository.php
@@ -20,36 +20,36 @@ class MailchimpCampaignReportRepository extends ServiceEntityRepository
         parent::__construct($registry, MailchimpCampaignReport::class);
     }
 
-    public function findNationalReportRatio(string $messageType): array
+    public function findNationalReportRatio(string $messageType, int $maxHistory = 30): array
     {
-        return $this->createReportRationQueryBuilder($messageType)
+        return $this->createReportRationQueryBuilder($messageType, $maxHistory)
             ->getQuery()
             ->getSingleResult()
         ;
     }
 
-    public function findLocalReportRation(string $messageType, array $zones): array
+    public function findLocalReportRation(string $messageType, array $zones, int $maxHistory = 30): array
     {
-        return $this->createReportRationQueryBuilder($messageType)
+        return $this->createReportRationQueryBuilder($messageType, $maxHistory)
             ->innerJoin(AudienceFilter::class, 'filter', Join::WITH, 'message.filter = filter')
             ->innerJoin('filter.zone', 'zone', Join::WITH, 'zone IN (:zones)')
-            ->addSelect('COUNT(mc) as nb_campagnes')
+            ->addSelect('COUNT(mc) as nb_campaigns')
             ->setParameter('zones', $zones)
             ->getQuery()
             ->getSingleResult()
         ;
     }
 
-    private function createReportRationQueryBuilder(string $messageType): QueryBuilder
+    private function createReportRationQueryBuilder(string $messageType, int $maxHistory): QueryBuilder
     {
         $qb = $this->createQueryBuilder('mcr')
             ->innerJoin(MailchimpCampaign::class, 'mc', Join::WITH, 'mcr = mc.report')
             ->innerJoin(AbstractAdherentMessage::class, 'message', Join::WITH, 'message = mc.message')
-            ->select('COALESCE(ROUND(SUM(mcr.openUnique) / SUM(mcr.emailSent), 4), 0) AS tx_ouverture')
-            ->addSelect('COALESCE(ROUND(SUM(mcr.clickUnique) / SUM(mcr.emailSent), 4), 0) AS tx_clique')
-            ->addSelect('COALESCE(ROUND(SUM(mcr.unsubscribed) / SUM(mcr.emailSent), 4), 0) AS tx_desabonnement')
+            ->select('COALESCE(ROUND(SUM(mcr.openUnique) / SUM(mcr.emailSent), 4), 0) AS opened_rate')
+            ->addSelect('COALESCE(ROUND(SUM(mcr.clickUnique) / SUM(mcr.emailSent), 4), 0) AS clicked_rate')
+            ->addSelect('COALESCE(ROUND(SUM(mcr.unsubscribed) / SUM(mcr.emailSent), 4), 0) AS unsubscribed_rate')
             ->where('message.sentAt >= :last_month')
-            ->setParameter('last_month', new \DateTime('-30 days'))
+            ->setParameter('last_month', new \DateTime("-$maxHistory days"))
         ;
 
         if (!AdherentMessageTypeEnum::isValid($messageType)) {
