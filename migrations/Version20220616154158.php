baac2aef73f7e06c58f919a38deb67829fdbe6ab
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-06-16 17:05:19 +0200

    Add rememberMe token table

---
+++ migrations/Version20220616154158.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220616154158 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        if ($schema->hasTable('rememberme_token')) {
+            return;
+        }
+
+        $this->addSql('CREATE TABLE `rememberme_token` (
+  `series` char(88) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` char(88) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastUsed` datetime NOT NULL,
+  `class` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`series`),
+  UNIQUE KEY `series` (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci');
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
