7c441cd7d4a8575fccbc6cb30047946b8f0d6152
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-06-29 15:58:12 +0200

    Fix CS

--- src/Entity/RepublicanSilence.php
+++ src/Entity/RepublicanSilence.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
