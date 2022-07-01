d4a2f0a2b9374f8414efff0706034f2c82e349cc
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-07-01 10:48:05 +0200

    Named import for merge

--- webpack.development.js
+++ webpack.development.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common');
 
