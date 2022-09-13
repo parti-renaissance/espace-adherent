b9052df9ae4f563e9241adb2cf8acc16d3585f42
Rémi Gardien <gardien.remi@gmail.com>
2022-09-13 21:21:43 +0200

    Handle Renaissance QRCodes

--- src/QrCode/QrCodeHostEnum.php
+++ src/QrCode/QrCodeHostEnum.php
@@ -6,9 +6,11 @@ class QrCodeHostEnum
 {
     public const HOST_ENMARCHE = 'enmarche';
     public const HOST_AVECVOUS = 'avecvous';
+    public const HOST_RENAISSANCE = 'renaissance';
 
     public const ALL = [
         self::HOST_ENMARCHE,
         self::HOST_AVECVOUS,
+        self::HOST_RENAISSANCE,
     ];
 }
