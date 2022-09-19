736d45d75354772c7998f043308eb7ec41fb178e
kevin <kevindoh26@gmail.com>
2022-09-19 15:05:03 +0200

    add conditions checkbox and some modification on donation funnel

--- src/Form/Renaissance/NewsletterSubscriptionType.php
+++ src/Form/Renaissance/NewsletterSubscriptionType.php
@@ -4,6 +4,7 @@ namespace App\Form\Renaissance;
 
 use App\Renaissance\Newsletter\SubscriptionRequest;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,6 +18,8 @@ class NewsletterSubscriptionType extends AbstractType
             ->add('firstName', TextType::class)
             ->add('zipCode', TextType::class)
             ->add('email', EmailType::class)
+            ->add('conditions', CheckboxType::class)
+            ->add('cguAccepted', CheckboxType::class)
         ;
     }
 
