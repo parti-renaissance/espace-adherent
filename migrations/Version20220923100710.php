300fe50d76649732897aa3eff1adfef306e8caf7
Rémi Gardien <gardien.remi@gmail.com>
2022-09-23 10:11:03 +0200

    Do not revert doctrine migrations

---
+++ migrations/Version20220923100710.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220923100710 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE renaissance_newsletter_subscription DROP country');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE
+          renaissance_newsletter_subscription
+        ADD
+          country VARCHAR(2) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
