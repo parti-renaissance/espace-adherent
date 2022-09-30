d360650f003cd22c0eb7315acd7dfb2e9790fc3a
Rémi Gardien <gardien.remi@gmail.com>
2022-09-30 15:21:21 +0200

    Add active membership filter on referent messages

---
+++ src/AdherentFilter/FilterBuilder/ActiveMembershipFilterBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\AdherentFilter\FilterBuilder;
+
+use App\Filter\FilterCollectionBuilder;
+use App\Scope\FeatureEnum;
+use App\Scope\ScopeEnum;
+
+class ActiveMembershipFilterBuilder implements AdherentFilterBuilderInterface
+{
+    public function supports(string $scope, string $feature = null): bool
+    {
+        return \in_array($scope, [ScopeEnum::REFERENT], true)
+            && FeatureEnum::MESSAGES === $feature
+        ;
+    }
+
+    public function build(string $scope, string $feature = null): array
+    {
+        return (new FilterCollectionBuilder())
+            ->createBooleanSelect('isActiveMembership', 'Cotisation active')
+            ->getFilters()
+        ;
+    }
+}
