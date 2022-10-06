ca7a75e92ed1a25edbc53891aa314b254f751713
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-10-06 17:07:28 +0200

    Clean migration

--- migrations/Version20221006142902.php
+++ migrations/Version20221006142902.php
@@ -7,11 +7,6 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20221006142902 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE adherent_request ADD password VARCHAR(255) DEFAULT NULL');
