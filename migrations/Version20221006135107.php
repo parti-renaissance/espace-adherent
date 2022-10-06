f961be02f9fdf6b04343c49ea1c718e27b06f7e0
Rémi Gardien <gardien.remi@gmail.com>
2022-10-06 17:46:13 +0200

    Audience add re filter (#7624)
    * Add RE filter on audience
    
    * Adjust tests
    
    * Adjust phoning campaign tests

---
+++ migrations/Version20221006135107.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221006135107 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE audience ADD is_renaissance_membership TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE audience_snapshot ADD is_renaissance_membership TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE audience DROP is_renaissance_membership');
+        $this->addSql('ALTER TABLE audience_snapshot DROP is_renaissance_membership');
+    }
+}
