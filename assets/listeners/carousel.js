cb10f6a84f1b91d33a97b036832b548fa4511200
Arthur Monney <monneylobe@gmail.com>
2022-10-27 17:04:40 +0100

    fix CodeFactor syntax error

--- assets/listeners/carousel.js
+++ assets/listeners/carousel.js
@@ -4,6 +4,6 @@ export default () => {
     findAll(document, '.carousel-widget').forEach((element) => new Carousel(element, {
         slidesVisible: 3,
         slidesToScroll: 3,
-        loop: true
+        loop: true,
     }));
 };
