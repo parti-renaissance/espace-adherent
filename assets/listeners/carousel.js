619a4698616a3cd7a80cfa6cbe521165bb2faf87
Arthur Monney <monneylobe@gmail.com>
2022-11-02 10:39:33 +0100

    Enable carousel to slide 1 item (#7713)

--- assets/listeners/carousel.js
+++ assets/listeners/carousel.js
@@ -3,7 +3,7 @@ import Carousel from '../components/Carousel/Carousel';
 export default () => {
     findAll(document, '.carousel-widget').forEach((element) => new Carousel(element, {
         slidesVisible: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         loop: true,
     }));
 };
