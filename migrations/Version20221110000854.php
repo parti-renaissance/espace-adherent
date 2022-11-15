d18d2a8cf2b7520e86afa9d13cbfa64abb2a9b7e
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-11-15 09:56:24 +0100

    fix review

--- migrations/Version20221110000854.php
+++ migrations/Version20221110000854.php
@@ -11,13 +11,11 @@ final class Version20221110000854 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE articles ADD json_content LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE articles DROP json_content');
     }
 }
