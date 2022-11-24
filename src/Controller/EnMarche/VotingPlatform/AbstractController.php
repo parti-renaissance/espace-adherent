3e14b4b649ab145256a7e81f6a2045b0c30328fb
Kevin DOH <kevindoh26@gmail.com>
2022-11-24 11:46:34 +0100

    [LPN-1295] move vote platform on RE domain (#7822)
    * [LPN-1295] move vote platform on RE domain
    
    * [LPN-1295] fix review

--- src/Controller/EnMarche/VotingPlatform/AbstractController.php
+++ src/Controller/EnMarche/VotingPlatform/AbstractController.php
@@ -2,31 +2,40 @@
 
 namespace App\Controller\EnMarche\VotingPlatform;
 
+use App\AppCodeEnum;
 use App\Entity\VotingPlatform\Election;
+use App\OAuth\App\AuthAppUrlManager;
 use App\Repository\VotingPlatform\CandidateGroupRepository;
 use App\VotingPlatform\Election\RedirectManager;
 use App\VotingPlatform\Election\VoteCommandProcessor;
 use App\VotingPlatform\Election\VoteCommandStorage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as BaseAbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractController extends BaseAbstractController
 {
-    protected $redirectManager;
-    protected $storage;
-    protected $processor;
-    protected $candidateGroupRepository;
+    protected RedirectManager $redirectManager;
+    protected VoteCommandStorage $storage;
+    protected VoteCommandProcessor $processor;
+    protected CandidateGroupRepository $candidateGroupRepository;
+    private AuthAppUrlManager $appUrlManager;
+    private RequestStack $requestStack;
 
     public function __construct(
         RedirectManager $redirectManager,
         VoteCommandStorage $storage,
         VoteCommandProcessor $processor,
-        CandidateGroupRepository $candidateGroupRepository
+        CandidateGroupRepository $candidateGroupRepository,
+        AuthAppUrlManager $appUrlManager,
+        RequestStack $requestStack
     ) {
         $this->redirectManager = $redirectManager;
         $this->storage = $storage;
         $this->processor = $processor;
         $this->candidateGroupRepository = $candidateGroupRepository;
+        $this->appUrlManager = $appUrlManager;
+        $this->requestStack = $requestStack;
     }
 
     protected function redirectToElectionRoute(string $routeName, Election $election): Response
@@ -36,10 +45,13 @@ abstract class AbstractController extends BaseAbstractController
 
     protected function renderElectionTemplate(string $template, Election $election, array $params = []): Response
     {
+        $appCode = $this->appUrlManager->getAppCodeFromRequest($this->requestStack->getMasterRequest());
+
         return $this->render($template, array_merge($params, [
             'base_layout' => sprintf('voting_platform/_layout_%s.html.twig', $designationType = $election->getDesignationType()),
             'designation_type' => $designationType,
             'election' => $election,
+            'is_renaissance_host' => AppCodeEnum::isRenaissanceApp($appCode),
         ]));
     }
 }
