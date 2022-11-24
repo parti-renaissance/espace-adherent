401c3c0ebc7786edd5dd5961d469e684a22514a2
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-11-24 17:05:39 +0100

    [Autocomplete] fix city initialisation (#7840)

--- assets/components/Address/AddressForm.js
+++ assets/components/Address/AddressForm.js
@@ -5,6 +5,7 @@ export default class AddressForm {
         this._address = address;
         this._postalCode = postalCode;
         this._cityName = cityName;
+        this._cityNameInitialized = false;
         this._country = country;
     }
 
@@ -33,7 +34,10 @@ export default class AddressForm {
             } else if (data.types.includes('route')) {
                 this._address.value += (this._address.value ? ' ' : '') + data.long_name;
             } else if (data.types.includes('locality') || data.types.includes('administrative_area_level_1')) {
-                this._cityName.value = data.long_name;
+                if (!this._cityNameInitialized) {
+                    this._cityName.value = data.long_name;
+                    this._cityNameInitialized = data.types.includes('locality');
+                }
             } else if (data.types.includes('country')) {
                 this._country.value = data.short_name;
             } else if (data.types.includes('postal_code')) {
