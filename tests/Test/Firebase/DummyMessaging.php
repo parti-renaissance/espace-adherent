23ece5aaae956d0f546cb40b4da58563228cf852
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-12-02 12:08:28 +0100

    Fix CI

--- tests/Test/Firebase/DummyMessaging.php
+++ tests/Test/Firebase/DummyMessaging.php
@@ -2,15 +2,13 @@
 
 namespace Tests\App\Test\Firebase;
 
-use Kreait\Firebase\Messaging;
+use Kreait\Firebase\Contract\Messaging;
+use Kreait\Firebase\Exception\Messaging\NotFound;
+use Kreait\Firebase\Messaging\AppInstance;
 use Kreait\Firebase\Messaging\MulticastSendReport;
 
-class DummyMessaging extends Messaging
+class DummyMessaging implements Messaging
 {
-    public function __construct()
-    {
-    }
-
     public function send($message, bool $validateOnly = false): array
     {
         return [];
@@ -29,5 +27,40 @@ class DummyMessaging extends Messaging
     public function subscribeToTopics(iterable $topics, $registrationTokenOrTokens): array
     {
         return [];
+    }
+
+    public function sendAll($messages, bool $validateOnly = false): MulticastSendReport
+    {
+        return MulticastSendReport::withItems([]);
+    }
+
+    public function validate($message): array
+    {
+        return [];
+    }
+
+    public function validateRegistrationTokens($registrationTokenOrTokens): array
+    {
+        return [];
+    }
+
+    public function subscribeToTopic($topic, $registrationTokenOrTokens): array
+    {
+        return [];
+    }
+
+    public function unsubscribeFromTopic($topic, $registrationTokenOrTokens): array
+    {
+        return [];
+    }
+
+    public function unsubscribeFromTopics(array $topics, $registrationTokenOrTokens): array
+    {
+        return [];
+    }
+
+    public function getAppInstance($registrationToken): AppInstance
+    {
+        throw NotFound::becauseTokenNotFound('');
     }
 }
