371e8144ca790be2cff673ecf5de68fa4ddaab61
Arthur Monney <monneylobe@gmail.com>
2022-12-05 15:52:14 +0100

    update article post ui

--- tailwind.config.site.js
+++ tailwind.config.site.js
@@ -100,6 +100,7 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/forms')({ strategy: 'base' }),
+        require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/typography'),
     ],
 };
