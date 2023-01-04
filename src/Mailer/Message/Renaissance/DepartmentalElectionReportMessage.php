7ebffb0d4f30d6e0e715aa19cca073cd74447162
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-01-04 12:20:18 +0100

    [DptElection] Send report election

---
+++ src/Mailer/Message/Renaissance/DepartmentalElectionReportMessage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mailer\Message\Renaissance;
+
+use App\Entity\Adherent;
+use Ramsey\Uuid\Uuid;
+
+final class DepartmentalElectionReportMessage extends AbstractRenaissanceMessage
+{
+    /** @param Adherent[] $adherents */
+    public static function create(array $adherents): self
+    {
+        $adherent = array_shift($adherents);
+
+        $message = new self(
+            Uuid::uuid4(),
+            $adherent->getEmailAddress(),
+            $adherent->getFullName(),
+            'Informations – Report de l’élection du bureau dans votre département'
+        );
+
+        foreach ($adherents as $adherent) {
+            $message->addRecipient($adherent->getEmailAddress(), $adherent->getFullName());
+        }
+
+        return $message;
+    }
+}
