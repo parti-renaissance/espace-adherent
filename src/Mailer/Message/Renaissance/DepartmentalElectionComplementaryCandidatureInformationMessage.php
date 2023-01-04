fd2bf4aaefc7ccdb4dddc60a9b0fd1bf0b9ffa04
kevin <kevindoh26@gmail.com>
2023-01-04 16:45:09 +0100

    send complementary message

---
+++ src/Mailer/Message/Renaissance/DepartmentalElectionComplementaryCandidatureInformationMessage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mailer\Message\Renaissance;
+
+use App\Entity\Adherent;
+use Ramsey\Uuid\Uuid;
+
+class DepartmentalElectionComplementaryCandidatureInformationMessage extends AbstractRenaissanceMessage
+{
+    /** @param Adherent[] $adherents */
+    public function create(array $adherents): self
+    {
+        $adherent = array_shift($adherents);
+
+        $message = new self(
+            Uuid::uuid4(),
+            $adherent->getEmailAddress(),
+            $adherent->getFullName(),
+            'Complément d\'information - Appel à candidatures des bureaux départementaux'
+        );
+
+        foreach ($adherents as $adherent) {
+            $message->addRecipient($adherent->getEmailAddress(), $adherent->getFullName());
+        }
+
+        return $message;
+    }
+}
