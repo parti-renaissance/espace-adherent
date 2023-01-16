e588432cd8c6f86b2d168643b0abef2368d15e92
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-01-16 17:24:10 +0100

    [DptElection] use uuid for election view page

--- src/Controller/Renaissance/LocalElection/DepartmentElectionController.php
+++ src/Controller/Renaissance/LocalElection/DepartmentElectionController.php
@@ -3,32 +3,34 @@
 namespace App\Controller\Renaissance\LocalElection;
 
 use App\Entity\Adherent;
-use App\LocalElection\Manager;
+use App\Entity\VotingPlatform\Designation\Designation;
+use App\Repository\LocalElection\LocalElectionRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/elections-departementales")
+ * @Route("/elections-departementales/{uuid}")
  * @IsGranted("ROLE_ADHERENT")
  */
 class DepartmentElectionController extends AbstractController
 {
+    public function __construct(private readonly LocalElectionRepository $localElectionRepository)
+    {
+    }
+
     /**
      * @Route("", name="app_renaissance_departmental_election_lists", methods="GET")
      */
-    public function candidaturesListAction(Manager $manager): Response
+    public function candidaturesListAction(Designation $designation): Response
     {
-        /** @var Adherent $adherent */
-        $adherent = $this->getUser();
-
-        if (!$localElection = $manager->getLastLocalElection($adherent)) {
+        if (!$localElection = $this->localElectionRepository->findByDesignation($designation)) {
             return $this->redirectToRoute('app_renaissance_homepage');
         }
 
         return $this->render('renaissance/local_election/lists.html.twig', [
-            'designation' => $localElection->getDesignation(),
+            'designation' => $designation,
             'candidacies_groups' => $localElection->getCandidaciesGroups(),
         ]);
     }
@@ -36,17 +38,10 @@ class DepartmentElectionController extends AbstractController
     /**
      * @Route("/reglement-interieur", name="app_renaissance_departmental_election_rules", methods="GET")
      */
-    public function rulesAction(Manager $manager): Response
+    public function rulesAction(Designation $designation): Response
     {
-        /** @var Adherent $adherent */
-        $adherent = $this->getUser();
-
-        if (!$localElection = $manager->getLastLocalElection($adherent)) {
-            return $this->redirectToRoute('app_renaissance_homepage');
-        }
-
         return $this->render('renaissance/local_election/rules.html.twig', [
-            'designation' => $localElection->getDesignation(),
+            'designation' => $designation,
         ]);
     }
 }
--- src/LocalElection/Manager.php
+++ src/LocalElection/Manager.php
@@ -2,31 +2,14 @@
 
 namespace App\LocalElection;
 
-use App\Entity\Adherent;
 use App\Entity\LocalElection\CandidaciesGroup;
-use App\Entity\LocalElection\LocalElection;
-use App\Repository\LocalElection\LocalElectionRepository;
-use App\VotingPlatform\Designation\DesignationTypeEnum;
-use App\VotingPlatform\ElectionManager;
 use League\Flysystem\FilesystemInterface;
 use Ramsey\Uuid\Uuid;
 
 class Manager
 {
-    public function __construct(
-        private readonly LocalElectionRepository $localElectionRepository,
-        private readonly ElectionManager $electionManager,
-        private readonly FilesystemInterface $storage
-    ) {
-    }
-
-    public function getLastLocalElection(Adherent $adherent): ?LocalElection
+    public function __construct(private readonly FilesystemInterface $storage)
     {
-        if ($designations = $this->electionManager->findActiveDesignations($adherent, [DesignationTypeEnum::LOCAL_ELECTION])) {
-            return $this->localElectionRepository->findByDesignation(current($designations));
-        }
-
-        return null;
     }
 
     public function uploadFaithStatementFile(CandidaciesGroup $candidaciesGroup): void
