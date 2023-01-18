6745bb2b1fe2e4a9c359c06162e68a671938bbb7
kevin <kevindoh26@gmail.com>
2023-01-18 13:48:22 +0100

    [LPN-1363] clean code

--- migrations/Version20230117175420.php
+++ migrations/Version20230117175420.php
@@ -1,15 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230117175420 extends AbstractMigration
 {
     public function up(Schema $schema): void
