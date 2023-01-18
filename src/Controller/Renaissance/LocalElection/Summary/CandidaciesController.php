49aeb353375911df483131718b034f342031d5f8
Rémi Gardien <gardien.remi@gmail.com>
2023-01-18 11:21:39 +0100

    Update election summary URL (#8109)
    * Update election summary URL
    
    * Enable in production

--- src/Controller/Renaissance/LocalElection/Summary/CandidaciesController.php
+++ src/Controller/Renaissance/LocalElection/Summary/CandidaciesController.php
@@ -2,22 +2,17 @@
 
 namespace App\Controller\Renaissance\LocalElection\Summary;
 
-use App\Controller\CanaryControllerTrait;
 use App\Entity\LocalElection\LocalElection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/elections-locales/{uuid}", name="app_renaissance_local_election_summary_candidacies", methods={"GET"})
+ * @Route("/elections-assemblees-departementales/{uuid}", name="app_renaissance_local_election_summary_candidacies", methods={"GET"})
  */
 class CandidaciesController extends AbstractController
 {
-    use CanaryControllerTrait;
-
     public function __invoke(LocalElection $localElection)
     {
-        $this->disableInProduction();
-
         return $this->render('renaissance/local_election/summary/candidacies.html.twig', [
             'election' => $localElection,
         ]);
--- src/Controller/Renaissance/LocalElection/Summary/ListController.php
+++ src/Controller/Renaissance/LocalElection/Summary/ListController.php
@@ -2,23 +2,18 @@
 
 namespace App\Controller\Renaissance\LocalElection\Summary;
 
-use App\Controller\CanaryControllerTrait;
 use App\LocalElection\LocalElectionCollection;
 use App\Repository\LocalElection\LocalElectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/elections-locales", name="app_renaissance_local_election_summary_list", methods={"GET"})
+ * @Route("/elections-assemblees-departementales", name="app_renaissance_local_election_summary_list", methods={"GET"})
  */
 class ListController extends AbstractController
 {
-    use CanaryControllerTrait;
-
     public function __invoke(LocalElectionRepository $localElectionRepository)
     {
-        $this->disableInProduction();
-
         return $this->render('renaissance/local_election/summary/list.html.twig', [
             'elections' => new LocalElectionCollection($localElectionRepository->findUpcomingDepartmentElections()),
         ]);
