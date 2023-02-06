531106c40b06f026d470ffcc4a80b3e6058c4562
Kevin DOH <kevindoh26@gmail.com>
2023-02-06 13:54:32 +0100

    [LPN-1410] add new my team member role (#8171)

--- src/MyTeam/RoleEnum.php
+++ src/MyTeam/RoleEnum.php
@@ -6,12 +6,16 @@ use MyCLabs\Enum\Enum;
 
 class RoleEnum extends Enum
 {
+    public const GENERAL_SECRETARY = 'general_secretary';
+    public const TREASURER = 'treasurer';
     public const MOBILIZATION_MANAGER = 'mobilization_manager';
     public const LOGISTICS_MANAGER = 'logistics_manager';
     public const COMMUNICATION_MANAGER = 'communication_manager';
     public const COMPLIANCE_AND_FINANCE_MANAGER = 'compliance_and_finance_manager';
 
     public const LABELS = [
+        self::GENERAL_SECRETARY => 'Secrétaire général',
+        self::TREASURER => 'Trésorier',
         self::MOBILIZATION_MANAGER => 'Responsable mobilisation',
         self::LOGISTICS_MANAGER => 'Responsable logistique',
         self::COMMUNICATION_MANAGER => 'Responsable communication',
@@ -19,6 +23,8 @@ class RoleEnum extends Enum
     ];
 
     public const ALL = [
+        self::GENERAL_SECRETARY,
+        self::TREASURER,
         self::MOBILIZATION_MANAGER,
         self::LOGISTICS_MANAGER,
         self::COMMUNICATION_MANAGER,
