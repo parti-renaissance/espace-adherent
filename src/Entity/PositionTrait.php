4bf6f0b5fe2d35d9de6b02a583b173b7cc3117b2
Rémi Gardien <gardien.remi@gmail.com>
2023-02-08 15:25:49 +0100

    use sortable behavior for position

--- src/Entity/PositionTrait.php
+++ src/Entity/PositionTrait.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Serializer\Annotation as SymfonySerializer;
 
 trait PositionTrait
@@ -11,6 +12,8 @@ trait PositionTrait
      * @var int
      *
      * @ORM\Column(type="smallint", options={"unsigned": true, "default": 0})
+     *
+     * @Gedmo\SortablePosition
      *
      * @SymfonySerializer\Groups({
      *     "formation_read",
