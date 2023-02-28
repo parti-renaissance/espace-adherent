056b46fb044df77d37ba0dfb09b75e261a1f0d8f
Rémi Gardien <gardien.remi@gmail.com>
2023-02-28 16:55:44 +0100

    Fix migration

--- migrations/Version20230228165519.php
+++ migrations/Version20230228165519.php
@@ -5,11 +5,10 @@ namespace Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20230228093017 extends AbstractMigration
+final class Version20230228165519 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE elected_representative ADD last_contribution_date DATETIME DEFAULT NULL');
         $this->addSql('CREATE TABLE elected_representative_contribution (
           id INT UNSIGNED AUTO_INCREMENT NOT NULL,
           elected_representative_id INT UNSIGNED DEFAULT NULL,
@@ -18,14 +17,15 @@ final class Version20230228093017 extends AbstractMigration
           uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\',
           created_at DATETIME NOT NULL,
           updated_at DATETIME NOT NULL,
-          UNIQUE INDEX UNIQ_EA351E15D17F50A6 (uuid),
-          INDEX IDX_EA351E15D38DA5D3 (elected_representative_id),
+          UNIQUE INDEX UNIQ_6F9C7915D17F50A6 (uuid),
+          INDEX IDX_6F9C7915D38DA5D3 (elected_representative_id),
           PRIMARY KEY(id)
         ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE
           elected_representative_contribution
         ADD
-          CONSTRAINT FK_EA351E15D38DA5D3 FOREIGN KEY (elected_representative_id) REFERENCES elected_representative (id)');
+          CONSTRAINT FK_6F9C7915D38DA5D3 FOREIGN KEY (elected_representative_id) REFERENCES elected_representative (id)');
+        $this->addSql('ALTER TABLE elected_representative ADD last_contribution_date DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
