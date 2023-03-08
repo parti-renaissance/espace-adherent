8acd1fe29f0babd12ce868ce1aad70f33f7372d4
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-03-08 16:22:24 +0100

    [MyTeam] update role list (#8282)

--- src/MyTeam/RoleEnum.php
+++ src/MyTeam/RoleEnum.php
@@ -12,6 +12,9 @@ class RoleEnum extends Enum
     public const LOGISTICS_MANAGER = 'logistics_manager';
     public const COMMUNICATION_MANAGER = 'communication_manager';
     public const COMPLIANCE_AND_FINANCE_MANAGER = 'compliance_and_finance_manager';
+    public const HEAD_OF_EUROPEAN_AFFAIRS = 'head_of_european_affairs';
+    public const HEAD_OF_GENDER_EQUALITY = 'head_of_gender_equality';
+    public const HEAD_OF_RELATIONS_AND_TRAINING_OF_ELECTED_OFFICIALS = 'head_of_relations_and_training_of_elected_officials';
 
     public const LABELS = [
         self::GENERAL_SECRETARY => 'Secrétaire général',
@@ -20,14 +23,13 @@ class RoleEnum extends Enum
         self::LOGISTICS_MANAGER => 'Responsable logistique',
         self::COMMUNICATION_MANAGER => 'Responsable communication',
         self::COMPLIANCE_AND_FINANCE_MANAGER => 'Responsables conformité et finance',
+        self::HEAD_OF_GENDER_EQUALITY => 'Responsable Égalité Femme / Homme',
+        self::HEAD_OF_EUROPEAN_AFFAIRS => 'Responsable des questions européennes',
+        self::HEAD_OF_RELATIONS_AND_TRAINING_OF_ELECTED_OFFICIALS => 'Responsable des relations et formation élus',
     ];
 
-    public const ALL = [
-        self::GENERAL_SECRETARY,
-        self::TREASURER,
-        self::MOBILIZATION_MANAGER,
-        self::LOGISTICS_MANAGER,
-        self::COMMUNICATION_MANAGER,
-        self::COMPLIANCE_AND_FINANCE_MANAGER,
-    ];
+    public static function getAll(): array
+    {
+        return array_keys(static::LABELS);
+    }
 }
