4ff76b5cc36519e6584ec85da03c6b1bdb8dbebe
Rémi Gardien <gardien.remi@gmail.com>
2023-03-20 13:19:43 +0100

    Update elected_representative.last_contribution_id in migration (#8341)

--- migrations/Version20230316170530.php
+++ migrations/Version20230316170530.php
@@ -28,6 +28,12 @@ final class Version20230316170530 extends AbstractMigration
           start_date DATETIME DEFAULT NULL,
         ADD
           end_date DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE elected_representative AS er
+          SET er.last_contribution_id = (
+              SELECT MAX(id)
+              FROM elected_representative_contribution
+              WHERE elected_representative_id = er.id
+          )');
     }
 
     public function down(Schema $schema): void
@@ -37,7 +43,13 @@ final class Version20230316170530 extends AbstractMigration
         $this->addSql('ALTER TABLE
           elected_representative
         ADD
-          last_contribution_date DATETIME DEFAULT NULL,
+          last_contribution_date DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE elected_representative AS er
+          INNER JOIN elected_representative_contribution AS erc
+            ON erc.elected_representative_id = er.id
+          SET er.last_contribution_date = erc.created_at');
+        $this->addSql('ALTER TABLE
+          elected_representative
         DROP
           last_contribution_id');
         $this->addSql('ALTER TABLE elected_representative_contribution DROP start_date, DROP end_date');
