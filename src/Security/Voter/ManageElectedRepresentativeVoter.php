b5ff8c15d68762c93fef1fdf1dc75ee43593b08f
Kevin DOH <kevindoh26@gmail.com>
2023-03-30 10:32:18 +0200

    update elected representative author voter (#8408)
    * update elected representative author voter
    
    * fix elected representative security voter

---
+++ src/Security/Voter/ManageElectedRepresentativeVoter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Adherent;
+use App\Entity\ElectedRepresentative\ElectedRepresentative;
+use App\Entity\Geo\Zone;
+use App\Geo\ManagedZoneProvider;
+use App\Scope\ScopeGeneratorResolver;
+
+class ManageElectedRepresentativeVoter extends AbstractAdherentVoter
+{
+    public const PERMISSION = 'MANAGE_ELECTED_REPRESENTATIVE';
+
+    public function __construct(
+        private readonly ScopeGeneratorResolver $scopeGeneratorResolver,
+        private readonly ManagedZoneProvider $managedZoneProvider
+    ) {
+    }
+
+    protected function doVoteOnAttribute(string $attribute, Adherent $adherent, $subject): bool
+    {
+        $scope = $this->scopeGeneratorResolver->generate();
+        $adherent = $scope->getDelegator() ?? $adherent;
+        $zoneIds = array_map(fn (Zone $zone) => $zone->getId(), $scope->getZones());
+
+        /** @var ElectedRepresentative $subject */
+        if (
+            ($subject->getCreatedByAdherent() && $subject->getCreatedByAdherent()->equals($adherent))
+            || ($subject->getUpdatedByAdherent() && $subject->getUpdatedByAdherent()->equals($adherent))
+        ) {
+            return true;
+        }
+
+        $zones = array_merge($subject->getZones()->toArray(), $subject->getAdherent() ? $subject->getAdherent()->getZones()->toArray() : []);
+        foreach ($zones as $zone) {
+            if ($this->managedZoneProvider->zoneBelongsToSome($zone, $zoneIds)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return self::PERMISSION === $attribute && $subject instanceof ElectedRepresentative;
+    }
+}
