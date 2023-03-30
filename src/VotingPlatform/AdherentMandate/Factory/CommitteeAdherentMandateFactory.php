21f51afb4482600a381b0ed797bb9b04b9b2eb80
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-03-30 15:36:20 +0200

    Disable autocreation of supervisor mandates after election

--- src/VotingPlatform/AdherentMandate/Factory/CommitteeAdherentMandateFactory.php
+++ src/VotingPlatform/AdherentMandate/Factory/CommitteeAdherentMandateFactory.php
@@ -2,12 +2,10 @@
 
 namespace App\VotingPlatform\AdherentMandate\Factory;
 
-use App\Admin\Committee\CommitteeAdherentMandateTypeEnum;
 use App\Entity\AdherentMandate\AdherentMandateInterface;
 use App\Entity\AdherentMandate\CommitteeAdherentMandate;
 use App\Entity\VotingPlatform\Candidate;
 use App\Entity\VotingPlatform\Election;
-use App\VotingPlatform\Designation\DesignationTypeEnum;
 
 class CommitteeAdherentMandateFactory implements AdherentMandateFactoryInterface
 {
@@ -16,11 +14,7 @@ class CommitteeAdherentMandateFactory implements AdherentMandateFactoryInterface
         $mandate = new CommitteeAdherentMandate(
             $candidate->getAdherent(),
             $candidate->getGender(),
-            $election->getVoteEndDate(),
-            null,
-            DesignationTypeEnum::COMMITTEE_SUPERVISOR === $election->getDesignationType() ?
-                CommitteeAdherentMandateTypeEnum::TYPE_SUPERVISOR :
-                null
+            $election->getVoteEndDate()
         );
 
         $mandate->setCommittee($election->getElectionEntity()->getCommittee());
@@ -30,6 +24,6 @@ class CommitteeAdherentMandateFactory implements AdherentMandateFactoryInterface
 
     public function supports(Election $election): bool
     {
-        return $election->getDesignation()->isCommitteeTypes();
+        return $election->getDesignation()->isCommitteeAdherentType();
     }
 }
