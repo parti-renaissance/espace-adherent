a9854fadc2b8819c89ba74293b139b3bbb6b52a4
Kevin DOH <kevindoh26@gmail.com>
2023-04-06 17:27:40 +0200

    remove parliamentary mandate (#8461)

--- src/Membership/MandatesEnum.php
+++ src/Membership/MandatesEnum.php
@@ -8,13 +8,12 @@ final class MandatesEnum extends Enum
 {
     private const REGIONAL_COUNCILOR = 'regional_councilor';
     private const DEPARTMENTAL_COUNCILOR = 'departmental_councilor';
-    private const CONSULAR_COUNSELOR = 'consular_conselor';
+    private const CONSULAR_COUNCILOR = 'consular_councilor';
     private const CORSICA_ASSEMBLY_MEMBER = 'corsica_assembly_member';
     private const MAYOR = 'mayor';
     private const MAYOR_ASSISTANT = 'mayor_assistant';
     private const CITY_COUNCILOR = 'city_councilor';
     private const BOROUGH_COUNCILOR = 'borough_councilor';
-    private const PARLIAMENTARY = 'parliamentary';
     private const DEPUTY = 'deputy';
     private const SENATOR = 'senator';
     private const EUROPEAN_DEPUTY = 'european_deputy';
@@ -25,13 +24,12 @@ final class MandatesEnum extends Enum
     public const ALL = [
         self::REGIONAL_COUNCILOR,
         self::DEPARTMENTAL_COUNCILOR,
-        self::CONSULAR_COUNSELOR,
+        self::CONSULAR_COUNCILOR,
         self::CORSICA_ASSEMBLY_MEMBER,
         self::MAYOR,
         self::MAYOR_ASSISTANT,
         self::CITY_COUNCILOR,
         self::BOROUGH_COUNCILOR,
-        self::PARLIAMENTARY,
         self::DEPUTY,
         self::SENATOR,
         self::EUROPEAN_DEPUTY,
@@ -43,13 +41,12 @@ final class MandatesEnum extends Enum
     public const CHOICES = [
         'adherent.mandate.regional_councilor' => self::REGIONAL_COUNCILOR,
         'adherent.mandate.departmental_councilor' => self::DEPARTMENTAL_COUNCILOR,
-        'adherent.mandate.consular_conselor' => self::CONSULAR_COUNSELOR,
+        'adherent.mandate.consular_councilor' => self::CONSULAR_COUNCILOR,
         'adherent.mandate.corsica_assembly_member' => self::CORSICA_ASSEMBLY_MEMBER,
         'adherent.mandate.mayor' => self::MAYOR,
         'adherent.mandate.mayor_assistant' => self::MAYOR_ASSISTANT,
         'adherent.mandate.city_councilor' => self::CITY_COUNCILOR,
         'adherent.mandate.borough_councilor' => self::BOROUGH_COUNCILOR,
-        'adherent.mandate.parliamentary' => self::PARLIAMENTARY,
         'adherent.mandate.deputy' => self::DEPUTY,
         'adherent.mandate.senator' => self::SENATOR,
         'adherent.mandate.european_deputy' => self::EUROPEAN_DEPUTY,
