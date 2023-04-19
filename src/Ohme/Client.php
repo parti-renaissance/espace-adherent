62147464b4c83e48dbc8d07e4a05fee1700f0bfd
Rémi Gardien <gardien.remi@gmail.com>
2023-04-19 10:22:36 +0200

    Fix client query parameters

--- src/Ohme/Client.php
+++ src/Ohme/Client.php
@@ -6,26 +6,31 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Client
 {
-    public function __construct(
-        private readonly HttpClientInterface $client,
-        private readonly string $ohmeClientName,
-        private readonly string $ohmeClientSecret
-    ) {
+    public function __construct(private readonly HttpClientInterface $client)
+    {
     }
 
     public function getContacts(int $limit = 100, int $offset = 0, array $options = []): array
     {
-        return $this->client->request('GET', 'contacts', array_merge($options, [
-            'limit' => $limit,
-            'offset' => $offset,
-        ]))->toArray();
+        $options = [
+            'query' => array_merge($options, [
+                'limit' => $limit,
+                'offset' => $offset,
+            ]),
+        ];
+
+        return $this->client->request('GET', 'contacts', $options)->toArray();
     }
 
     public function getPayments(int $limit = 100, int $offset = 0, array $options = []): array
     {
-        return $this->client->request('GET', 'payments', array_merge($options, [
-            'limit' => $limit,
-            'offset' => $offset,
-        ]))->toArray();
+        $options = [
+            'query' => array_merge($options, [
+                'limit' => $limit,
+                'offset' => $offset,
+            ]),
+        ];
+
+        return $this->client->request('GET', 'payments', $options)->toArray();
     }
 }
