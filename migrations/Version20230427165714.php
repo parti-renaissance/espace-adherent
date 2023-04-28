893cee9696937e9875ec4baee57e619ac4d55a50
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-04-28 10:15:50 +0200

    [Message] update committeeMember check (#8567)
    * [Message] update committeeMember check
    
    * Fix tests

---
+++ migrations/Version20230427165714.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230427165714 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE adherent_message_filters ADD is_committee_member TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE adherent_message_filters DROP is_committee_member');
+    }
+}
