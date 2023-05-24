4246ccbb650538be5d5131227c3edfae1f928ab7
Rémi Gardien <gardien.remi@gmail.com>
2023-05-24 11:09:10 +0200

    Update JME filter group colors (#8663)

--- src/JMEFilter/FilterGroup/ElectedRepresentativeFilterGroup.php
+++ src/JMEFilter/FilterGroup/ElectedRepresentativeFilterGroup.php
@@ -5,5 +5,5 @@ namespace App\JMEFilter\FilterGroup;
 class ElectedRepresentativeFilterGroup extends AbstractFilterGroup
 {
     protected const LABEL = 'Elu';
-    protected const COLOR = '#123458';
+    protected const COLOR = '#2563EB';
 }
--- src/JMEFilter/FilterGroup/MilitantFilterGroup.php
+++ src/JMEFilter/FilterGroup/MilitantFilterGroup.php
@@ -5,5 +5,5 @@ namespace App\JMEFilter\FilterGroup;
 class MilitantFilterGroup extends AbstractFilterGroup
 {
     protected const LABEL = 'Militant';
-    protected const COLOR = '#123457';
+    protected const COLOR = '#0F766E';
 }
