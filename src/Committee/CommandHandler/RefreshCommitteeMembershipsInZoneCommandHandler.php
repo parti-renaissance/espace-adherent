61e987dfa912b27de588df46c606a7eade5fff0a
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-05-26 16:52:36 +0200

    [Committee] fix already existing membership exception

--- src/Committee/CommandHandler/RefreshCommitteeMembershipsInZoneCommandHandler.php
+++ src/Committee/CommandHandler/RefreshCommitteeMembershipsInZoneCommandHandler.php
@@ -10,6 +10,7 @@ use App\Entity\Geo\Zone;
 use App\Repository\AdherentRepository;
 use App\Repository\CommitteeRepository;
 use App\Repository\Geo\ZoneRepository;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
@@ -53,7 +54,14 @@ class RefreshCommitteeMembershipsInZoneCommandHandler implements MessageHandlerI
                         continue;
                     }
 
-                    $this->committeeMembershipManager->followCommittee($adherent, $committee, CommitteeMembershipTriggerEnum::COMMITTEE_EDITION);
+                    try {
+                        $this->committeeMembershipManager->followCommittee($adherent, $committee, CommitteeMembershipTriggerEnum::COMMITTEE_EDITION);
+                    } catch (UniqueConstraintViolationException $e) {
+                        if (!str_contains($e->getMessage(), 'adherent_has_joined_committee')) {
+                            throw $e;
+                        }
+                    }
+
                     $committeeAdherentIds[$committee->getId()][] = $alreadyAssignedAdherentIds[] = $adherent->getId();
                 }
             }
