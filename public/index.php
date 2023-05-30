9241cc70f011dfd95e32dad5c0552be32906cbd2
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-05-30 19:16:05 +0200

    [Config] use var env for TrustedProxies

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ if ($_SERVER['ENABLE_MAINTENANCE']) {
 }
 
 require_once \dirname(__DIR__).'/vendor/autoload_runtime.php';
-require_once \dirname(__DIR__).'/config/trusted_proxies.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
