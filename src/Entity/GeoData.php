fa2155338316c3536d5a9f68456a65094ca21f4f
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-06-20 11:40:35 +0200

    [PHP] replace CrEOF spatial lib (#8795)
    * [PHP] replace CrEOF spatial lib
    
    * Fix namespace

--- src/Entity/GeoData.php
+++ src/Entity/GeoData.php
@@ -2,10 +2,10 @@
 
 namespace App\Entity;
 
-use CrEOF\Spatial\PHP\Types\Geometry\GeometryInterface;
-use CrEOF\Spatial\PHP\Types\Geometry\MultiPolygon;
-use CrEOF\Spatial\PHP\Types\Geometry\Polygon;
 use Doctrine\ORM\Mapping as ORM;
+use LongitudeOne\Spatial\PHP\Types\Geometry\GeometryInterface;
+use LongitudeOne\Spatial\PHP\Types\Geometry\MultiPolygon;
+use LongitudeOne\Spatial\PHP\Types\Geometry\Polygon;
 
 /**
  * @ORM\Entity
