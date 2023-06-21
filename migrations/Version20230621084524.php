dc6617876ba916c37f9e5b17de4acf850754bf3b
Rémi Gardien <gardien.remi@gmail.com>
2023-06-21 10:58:05 +0200

    [Ohme] Payment status are nullable

---
+++ migrations/Version20230621084524.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230621084524 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE elected_representative_payment CHANGE status status VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE elected_representative_payment CHANGE status status VARCHAR(50) NOT NULL');
+    }
+}
