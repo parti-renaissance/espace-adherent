e8a95a75e30b0a5ce9831f6c50e2f752866dd4e6
Rémi Gardien <gardien.remi@gmail.com>
2023-06-23 12:36:21 +0200

    Merge migrations

--- migrations/Version20230623103320.php
+++ migrations/Version20230623103320.php
@@ -5,15 +5,17 @@ namespace Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20230623093912 extends AbstractMigration
+final class Version20230623103320 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE adherent_message_filters ADD is_campus_registered TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE projection_managed_users ADD campus_registered_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE adherent_message_filters DROP is_campus_registered');
+        $this->addSql('ALTER TABLE projection_managed_users DROP campus_registered_at');
     }
 }
