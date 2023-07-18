b4097a7b2a35f1e2c6dd476f8fe6795fc0bf9478
Rémi Gardien <gardien.remi@gmail.com>
2023-07-18 21:36:53 +0200

    Add ER adherent mandates admin

---
+++ src/Admin/ElectedRepresentativeAdherentMandateAdmin.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\ElectedRepresentative\MandateTypeEnum;
+use App\Entity\Geo\Zone;
+use Doctrine\ORM\QueryBuilder;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridInterface;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+
+class ElectedRepresentativeAdherentMandateAdmin extends AbstractAdmin
+{
+    public const SERVICE_ID = 'app.admin.elected_representative_adherent_mandate_admin';
+
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        parent::configureDefaultSortValues($sortValues);
+
+        $sortValues[DatagridInterface::SORT_BY] = 'type';
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form->add('zone', ModelAutocompleteType::class, [
+            'callback' => [$this, 'prepareAutocompleteFilterCallback'],
+            'to_string_callback' => [$this, 'toStringCallback'],
+            'property' => ['name', 'code'],
+            'btn_add' => false,
+        ]);
+    }
+
+    public function toStringCallback(Zone $zone): string
+    {
+        return sprintf(
+            '%s : %s (%s)',
+            $this->getTranslator()->trans('geo_zone.'.$zone->getType()),
+            $zone->getName(),
+            $zone->getCode()
+        );
+    }
+
+    public static function prepareAutocompleteFilterCallback(
+        AbstractAdmin $admin,
+        array $properties,
+        string $value
+    ): void {
+        /** @var QueryBuilder $qb */
+        $qb = $admin->getDatagrid()->getQuery();
+        $alias = $qb->getRootAliases()[0];
+
+        $orx = $qb->expr()->orX();
+        foreach ($properties as $property) {
+            $orx->add($alias.'.'.$property.' LIKE :property_'.$property);
+            $qb->setParameter('property_'.$property, '%'.$value.'%');
+        }
+        $qb->orWhere($orx);
+
+        $request = $admin->getRequest();
+        $mandateType = $request->query->get('mandate_type');
+
+        if ($mandateType && $zoneTypeConditions = (MandateTypeEnum::ZONE_FILTER_BY_MANDATE[$mandateType] ?? [])) {
+            $conditions = [];
+
+            if (\array_key_exists('types', $zoneTypeConditions)) {
+                $conditions[] = sprintf('%s.type IN (:zone_types)', $alias);
+                $qb->setParameter('zone_types', $zoneTypeConditions['types']);
+            }
+
+            if (\array_key_exists('codes', $zoneTypeConditions)) {
+                $conditions[] = sprintf('%s.code IN (:zone_codes)', $alias);
+                $qb->setParameter('zone_codes', $zoneTypeConditions['codes']);
+            }
+
+            $qb->andWhere($qb->expr()->andX(...$conditions));
+        }
+
+        $qb->andWhere(sprintf('%s.active = 1', $alias));
+    }
+}
---
+++ src/Form/Admin/ElectedRepresentativeAdherentMandateType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Admin\ElectedRepresentativeAdherentMandateAdmin;
+use App\Entity\AdherentMandate\ElectedRepresentativeAdherentMandate;
+use App\Entity\ElectedRepresentative\MandateTypeEnum;
+use Sonata\AdminBundle\Model\ModelManagerInterface;
+use Sonata\Form\Type\DatePickerType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ElectedRepresentativeAdherentMandateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mandateType', ChoiceType::class, [
+                'label' => 'Type',
+                'choices' => MandateTypeEnum::CHOICES,
+            ])
+            ->add('zone', AdminZoneAutocompleteType::class, [
+                'label' => 'Zone',
+                'model_manager' => $options['model_manager'],
+                'admin_code' => ElectedRepresentativeAdherentMandateAdmin::SERVICE_ID,
+                'template' => 'admin/adherent/partial/elected_representative_adherent_mandate_autocomplete.html.twig',
+            ])
+            ->add('delegation', TextType::class, [
+                'label' => 'Délégation',
+                'required' => false,
+            ])
+            ->add('beginAt', DatePickerType::class, [
+                'label' => 'Date de début de mandat',
+                'attr' => ['class' => 'width-140'],
+            ])
+            ->add('finishAt', DatePickerType::class, [
+                'label' => 'Date de fin du mandat',
+                'attr' => ['class' => 'width-140'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'data_class' => ElectedRepresentativeAdherentMandate::class,
+            ])
+            ->setRequired('model_manager')
+            ->addAllowedTypes('model_manager', [ModelManagerInterface::class])
+        ;
+    }
+}
