283bd2e95d212e1c801e6b16f1aea536022884e1
Rémi Gardien <gardien.remi@gmail.com>
2023-09-25 12:38:39 +0200

    Add mandate zone filter

--- src/Admin/AdherentElectedRepresentativeAdmin.php
+++ src/Admin/AdherentElectedRepresentativeAdmin.php
@@ -3,9 +3,16 @@
 namespace App\Admin;
 
 use App\Entity\AdherentMandate\ElectedRepresentativeAdherentMandate;
+use App\Entity\Geo\Zone;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Query\Expr;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\AdminBundle\Filter\Model\FilterData;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery;
+use Sonata\DoctrineORMAdminBundle\Filter\CallbackFilter;
 
 class AdherentElectedRepresentativeAdmin extends AbstractAdherentAdmin
 {
@@ -27,6 +34,61 @@ class AdherentElectedRepresentativeAdmin extends AbstractAdherentAdmin
         ;
 
         return $query;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        parent::configureDatagridFilters($filter);
+
+        $filter
+            ->add('adherentMandates', CallbackFilter::class, [
+                'label' => 'Périmètres géographiques des mandats',
+                'field_type' => ModelAutocompleteType::class,
+                'show_filter' => true,
+                'field_options' => [
+                    'class' => Zone::class,
+                    'multiple' => true,
+                    'context' => 'form',
+                    'property' => ['code', 'name'],
+                    'minimum_input_length' => 1,
+                    'items_per_page' => 20,
+                    'req_params' => [
+                        'field' => 'zone',
+                        '_sonata_admin' => ElectedRepresentativeAdherentMandateAdmin::SERVICE_ID,
+                    ],
+                ],
+                'callback' => function (ProxyQuery $qb, string $alias, string $field, FilterData $value) {
+                    if (!$value->hasValue()) {
+                        return false;
+                    }
+
+                    $zones = $value->getValue();
+
+                    if ($zones instanceof Collection) {
+                        $zones = $zones->toArray();
+                    } elseif (!\is_array($zones)) {
+                        $zones = [$zones];
+                    }
+
+                    $ids = array_map(static function (Zone $zone) {
+                        return $zone->getId();
+                    }, $zones);
+
+                    $qb
+                        ->innerJoin('er_adherent_mandate.zone', 'mandate_zone_filter')
+                        ->leftJoin('mandate_zone_filter.parents', 'mandate_zone_parent_filter')
+                        ->andWhere(
+                            $qb->expr()->orX(
+                                $qb->expr()->in('mandate_zone_filter.id', $ids),
+                                $qb->expr()->in('mandate_zone_parent_filter.id', $ids),
+                            )
+                        )
+                    ;
+
+                    return true;
+                },
+            ])
+        ;
     }
 
     protected function configureListFields(ListMapper $list): void
