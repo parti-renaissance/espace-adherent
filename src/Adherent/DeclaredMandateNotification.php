d829d4c440347d15dbf8f27e18cac04b3528fd57
Rémi Gardien <gardien.remi@gmail.com>
2023-09-25 16:48:23 +0200

    Aggregate declared mandate histories (#9134)
    * Aggregate declared mandate histories
    
    * merge arrays for aggregation

---
+++ src/Adherent/DeclaredMandateNotification.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Adherent;
+
+use App\Entity\Adherent;
+
+class DeclaredMandateNotification
+{
+    public function __construct(
+        public readonly Adherent $adherent,
+        public readonly array $addedMandates,
+        public readonly array $removedMandates
+    ) {
+    }
+}
