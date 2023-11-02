35a013bf062833479db6e15cb87f6ad7035366b4
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-11-02 16:34:44 +0100

    [VotingPlatform] save voter dpt or country zone code (#9264)

---
+++ migrations/Version20231102091259.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231102091259 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE voting_platform_vote_result ADD zone_code VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE voting_platform_vote_result DROP zone_code');
+    }
+}
--- src/Address/AddressInterface.php
+++ src/Address/AddressInterface.php
@@ -6,6 +6,21 @@ interface AddressInterface
 {
     public const FRANCE = 'FR';
 
+    public const FRENCH_CODES = [
+        0 => 'FR', // FR
+        '971' => 'GP', // Guadeloupe
+        '972' => 'MQ', // Martinique
+        '973' => 'GF', // Guyane
+        '974' => 'RE', // Réunion
+        '975' => 'PM', // Saint-Pierre-et-Miquelon
+        '976' => 'YT', // Mayotte
+        '977' => 'BL', // Saint-Barthélemy
+        '978' => 'MF', // Saint-Martin
+        '986' => 'WF', // Wallis-et-Futuna
+        '987' => 'PF', // Polynésie
+        '988' => 'NC', // Nouvelle-Calédonie
+    ];
+
     /**
      * Returns the street name.
      */
