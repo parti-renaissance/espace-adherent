177e2495de3caf5de33f72c1e9063f6b69a72d33
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-11-21 17:07:52 +0100

    [Mailchimp] sync adherent on tag update (#9335)

--- src/Adherent/Tag/Handler/RefreshAdherentTagHandler.php
+++ src/Adherent/Tag/Handler/RefreshAdherentTagHandler.php
@@ -4,9 +4,11 @@ namespace App\Adherent\Tag\Handler;
 
 use App\Adherent\Tag\Command\RefreshAdherentTagCommand;
 use App\Adherent\Tag\TagAggregator;
+use App\Mailchimp\Synchronisation\Command\AdherentChangeCommand;
 use App\Repository\AdherentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class RefreshAdherentTagHandler implements MessageHandlerInterface
 {
@@ -14,6 +16,7 @@ class RefreshAdherentTagHandler implements MessageHandlerInterface
         private readonly EntityManagerInterface $entityManager,
         private readonly AdherentRepository $adherentRepository,
         private readonly TagAggregator $tagAggregator,
+        private readonly MessageBusInterface $bus,
     ) {
     }
 
@@ -28,5 +31,7 @@ class RefreshAdherentTagHandler implements MessageHandlerInterface
         $adherent->tags = $this->tagAggregator->getTags($adherent);
 
         $this->entityManager->flush();
+
+        $this->bus->dispatch(new AdherentChangeCommand($adherent->getUuid(), $adherent->getEmailAddress()));
     }
 }
