03cc9dd8a420da19111cee01c44651f6fd9ea383
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-11-30 09:22:54 +0100

    Add design components demo page (#9379)

---
+++ src/Controller/Renaissance/DesignComponentsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Renaissance;
+
+use App\Controller\CanaryControllerTrait;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/design-components', name: 'app_design_components')]
+class DesignComponentsController extends AbstractController
+{
+    use CanaryControllerTrait;
+
+    public function __invoke(): Response
+    {
+        $this->disableInProduction();
+
+        return $this->render('renaissance/design_components.html.twig');
+    }
+}
