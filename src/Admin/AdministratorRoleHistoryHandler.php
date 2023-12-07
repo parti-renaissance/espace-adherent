ac73d3930999631d603f36319cc68392d82978a7
Rémi Gardien <gardien.remi@gmail.com>
2023-12-07 10:49:25 +0100

    [Admin] Keep track of administrator role changes

---
+++ src/Admin/AdministratorRoleHistoryHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\Administrator;
+use App\Entity\Reporting\AdministratorRoleHistory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class AdministratorRoleHistoryHandler
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly Security $security
+    ) {
+    }
+
+    public function handleChanges(Administrator $administrator, array $oldRoles): void
+    {
+        /** @var Administrator $author */
+        $author = $this->security->getUser();
+
+        $newRoles = $administrator->getAdministratorRoleCodes();
+
+        $addedRoles = array_diff($newRoles, $oldRoles);
+        $deletedRoles = array_diff($oldRoles, $newRoles);
+
+        foreach ($addedRoles as $addedRole) {
+            $this->entityManager->persist(
+                AdministratorRoleHistory::createAdd($administrator, $addedRole, $author)
+            );
+        }
+
+        foreach ($deletedRoles as $deletedRole) {
+            $this->entityManager->persist(
+                AdministratorRoleHistory::createRemove($administrator, $deletedRole, $author)
+            );
+        }
+
+        $this->entityManager->flush();
+    }
+}
