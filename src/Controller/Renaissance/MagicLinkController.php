de02397173b306fb5660b7a5146d618ec05ab423
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-12-11 12:22:51 +0100

    [MagicLink] redirect connected user (#9445)

--- src/Controller/Renaissance/MagicLinkController.php
+++ src/Controller/Renaissance/MagicLinkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Renaissance;
 
+use App\Entity\Adherent;
 use App\Mailer\MailerService;
 use App\Mailer\Message\Renaissance\RenaissanceMagicLinkMessage;
 use App\Repository\AdherentRepository;
@@ -54,6 +55,10 @@ class MagicLinkController extends AbstractController
     #[Route(path: '/connexion-avec-un-lien-magique', name: 'app_user_connect_with_magic_link', methods: ['GET', 'POST'])]
     public function connectViaMagicLinkAction(Request $request): Response
     {
+        if ($this->getUser() instanceof Adherent) {
+            return $this->redirectToRoute('app_renaissance_adherent_space');
+        }
+
         return $this->render('security/renaissance_connect_magic_link.html.twig', [
             'expires' => $request->query->get('expires'),
             'user' => $request->query->get('user'),
