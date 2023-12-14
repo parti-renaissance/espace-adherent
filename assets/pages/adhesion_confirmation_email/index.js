c91a6c427708a8d9402b85ac873f2f314062ce6e
Antonin Carlin <antonin.carlin@icloud.com>
2023-12-14 11:34:48 +0100

    feat(funnel): Etape 6

---
+++ assets/pages/adhesion_confirmation_email/index.js
@@ -0,0 +1,5 @@
+import Form from './components/Form';
+
+export default () => {
+    window.Alpine.data('xEmailVerificationForm', Form);
+};
--- src/Controller/Renaissance/Adhesion/V2/ActivateEmailController.php
+++ src/Controller/Renaissance/Adhesion/V2/ActivateEmailController.php
@@ -7,11 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(path: '/v2/adhesion/activation', name: 'app_adhesion_activation', methods: ['GET', 'POST'])]
+#[Route(path: '/v2/adhesion/confirmation-email', methods: ['GET', 'POST'])]
 class ActivateEmailController extends AbstractController
 {
     public function __invoke(Request $request): Response
     {
-        return $this->render('renaissance/adhesion/activation.html.twig');
+        return $this->render('renaissance/adhesion/confirmation_email.html.twig', [
+            'email' => $request->query->get('email'),
+        ]);
     }
 }
