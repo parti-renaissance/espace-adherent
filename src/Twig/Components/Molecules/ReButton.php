f10d8eb3b09a30f64f7871894723e4151ac16171
Antonin <antonin.carlin@icloud.com>
2023-12-18 16:19:59 +0100

    feat: add all loaders icons (#9484)

--- src/Twig/Components/Molecules/ReButton.php
+++ src/Twig/Components/Molecules/ReButton.php
@@ -10,9 +10,46 @@ class ReButton
     /**
      * @var string 'blue'|'green'|'yellow'
      */
-    public string $color = 'blue';
+    public ?string $color = null;
+    public ?string $stroke = null;
     public string $tag = 'button';
     public string $value = '';
     public ?string $icon;
     public ?string $xSyncLoading;
+
+    public function getLoaderType(): string
+    {
+        if (!$this->color) {
+            return 'loading';
+        }
+        if ($this->stroke) {
+            $color = match ($this->color) {
+                'blue', => null,
+                default => 'black',
+            };
+        } else {
+            $color = match ($this->color) {
+                'yellow' => 'brown',
+                default => 'white',
+            };
+        }
+
+        return $color ? 'loading--'.$color : 'loading';
+    }
+
+    public function getDefaultClass(): string
+    {
+        $class = 're-button';
+
+        if ($this->stroke) {
+            $class .= ' re-button--stroke';
+            if ('black' === $this->color) {
+                $class .= ' re-button--stroke--black';
+            }
+        } else {
+            $class .= ' re-button--'.($this->color ?? 'blue');
+        }
+
+        return $class;
+    }
 }
