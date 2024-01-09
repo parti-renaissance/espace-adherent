cebc3e664a6e2a46c4f40af87115edada12bc13b
Rémi Gardien <gardien.remi@gmail.com>
2024-01-09 11:33:03 +0100

    [Donation] Add funnel v2

---
+++ src/Controller/Renaissance/Donation/V2/DonationController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Renaissance\Donation\V2;
+
+use App\Donation\Handler\DonationRequestHandler;
+use App\Donation\Request\DonationRequest;
+use App\Entity\Adherent;
+use App\Form\DonationRequestV2Type;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/v2/don', name: 'app_donation_index', methods: ['GET', 'POST'])]
+class DonationController extends AbstractController
+{
+    private int $step = 0;
+
+    public function __construct(
+        private readonly DonationRequestHandler $donationRequestHandler,
+    ) {
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        /** @var Adherent $adherent */
+        $adherent = $this->getUser();
+
+        $donationRequest = $this->getDonationRequest($request, $adherent);
+
+        $form = $this
+            ->createForm(DonationRequestV2Type::class, $donationRequest)
+            ->handleRequest($request)
+        ;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $donation = $this->donationRequestHandler->handle($donationRequest, $adherent);
+
+            return $this->redirectToRoute('app_payment', ['uuid' => $donation->getUuid()]);
+        }
+
+        return $this->renderForm('renaissance/donation/form.html.twig', [
+            'form' => $form,
+            'step' => $this->step,
+        ]);
+    }
+
+    private function getDonationRequest(Request $request, ?Adherent $currentUser): DonationRequest
+    {
+        $clientIp = $request->getClientIp();
+
+        return $currentUser
+            ? DonationRequest::createFromAdherent($currentUser, $clientIp)
+            : new DonationRequest($clientIp);
+    }
+}
