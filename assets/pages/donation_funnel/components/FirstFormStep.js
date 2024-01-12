cbca39cec434dabc8fd5167a55d8069486ab9465
Antonin <antonin.carlin@icloud.com>
2024-01-12 18:28:02 +0100

    [ADH-Funnels-1022] feat: visuel don (#9592)

--- assets/pages/donation_funnel/components/FirstFormStep.js
+++ assets/pages/donation_funnel/components/FirstFormStep.js
@@ -2,6 +2,19 @@
 
 import CommonFormStep from './CommonFormStep';
 
+function closest(num, arr) {
+    let curr = arr[0];
+    let diff = Math.abs(num - curr);
+    for (let val = 0; val < arr.length; val++) {
+        const newdiff = Math.abs(num - arr[val]);
+        if (newdiff < diff) {
+            diff = newdiff;
+            curr = arr[val];
+        }
+    }
+    return curr;
+}
+
 /**
  * First Step component for funnel
  * @returns {AlpineComponent}
@@ -16,9 +29,55 @@ const FirstForm = () => {
         id: 'step_1',
         submitted: false,
         submittedValues: null,
+        defaultCustomAmount: '',
 
         getTaxTextReduction() {
             return `${(this.amount * 0.66).toFixed(2)} € ${'-1' === this.duration ? '/ mois' : ''}`;
+        },
+
+        handleAmountClick(amount) {
+            this.animateSvg(amount);
+            document.querySelector('#amount_custom').value = '';
+        },
+
+        /**
+         * @param {Event} e
+         */
+        handleCustomFieldChange(e) {
+            const { value } = e.target;
+            this.amount = '' === value ? '60' : value;
+            this.animateSvg(this.amount);
+        },
+
+        animateSvg(value) {
+            let index = this.getAmounts()
+                .indexOf(Number(value));
+            if (-1 === index) {
+                index = this.getAmounts()
+                    .indexOf(closest(Number(value), this.getAmounts()));
+            }
+            console.log(index, value, this.getAmounts());
+
+            const indexesToAnimate = Array.from(Array(index + 1)
+                .keys());
+            document.querySelectorAll('[id^="p_"]')
+                .forEach((el) => {
+                    const elIndex = Number(el.id.split('_')[1]) - 1;
+                    if (indexesToAnimate.includes(elIndex)) return;
+                    el.classList.remove('active');
+                });
+            indexesToAnimate.forEach((i) => {
+                setTimeout(() => {
+                    document.querySelector(`#p_${i + 1}`)
+                        .classList
+                        .add('active');
+                }, i * 100);
+            });
+        },
+
+        init() {
+            this.animateSvg(this.amount);
+            this.defaultCustomAmount = this.getCustomAmount();
         },
 
         getAmounts(duration) {
