bf137cbab52f82ba96b152d4dfc09cbc1f5847f5
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-01-25 16:33:04 +0100

    Add recotisation user pop-up (#9667)
    * Add recotisation user pop-up
    
    * Fix check _target_path

---
+++ src/Twig/Components/ReModal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Twig\AbstractComponentsLogic;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+class ReModal extends AbstractComponentsLogic
+{
+}
