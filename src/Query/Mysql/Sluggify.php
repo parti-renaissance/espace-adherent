06208b00a85631bf748853492750c00b88758578
Rémi Gardien <gardien.remi@gmail.com>
2024-02-26 21:51:28 +0100

    [Admin] Enhance adherent search

---
+++ src/Query/Mysql/Sluggify.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class Sluggify extends FunctionNode
+{
+    private $firstExpression;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->firstExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            "REGEXP_REPLACE(%s, '[^a-zA-Z0-9]+', '')",
+            $this->firstExpression->dispatch($sqlWalker)
+        );
+    }
+}
