b36571165a00bba7f2bf44234f9ea43eccdf67b3
Rémi Gardien <gardien.remi@gmail.com>
2024-02-27 12:05:08 +0100

    Add committee field in edit form

---
+++ src/Form/EventListener/CommitteeMembershipListener.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form\EventListener;
+
+use App\Committee\CommitteeMembershipManager;
+use App\Committee\CommitteeMembershipTriggerEnum;
+use App\Entity\Adherent;
+use App\Entity\Committee;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class CommitteeMembershipListener implements EventSubscriberInterface
+{
+    public function __construct(private readonly CommitteeMembershipManager $committeeMembershipManager)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            FormEvents::POST_SET_DATA => 'onPostSetData',
+            FormEvents::SUBMIT => 'onSubmit',
+        ];
+    }
+
+    public function onPostSetData(FormEvent $event): void
+    {
+        $form = $event->getForm();
+        /** @var Adherent $adherent */
+        $adherent = $event->getData();
+
+        if (
+            !$adherent instanceof Adherent
+            || !($membership = $adherent->getCommitteeV2Membership())
+            || !($committee = $membership->getCommittee())
+        ) {
+            return;
+        }
+
+        $form->get('committee')->setData($committee);
+    }
+
+    public function onSubmit(FormEvent $event): void
+    {
+        $form = $event->getForm();
+        /** @var Adherent $adherent */
+        $adherent = $form->getData();
+
+        if (!$adherent instanceof Adherent) {
+            return;
+        }
+
+        $membershipBefore = $adherent->getCommitteeV2Membership();
+        $committeeBefore = $membershipBefore?->getCommittee();
+
+        /** @var Committee|null $committee */
+        $committee = $form->get('committee')->getData();
+
+        if ($committee && $committeeBefore !== $committee) {
+            $this->committeeMembershipManager->followCommittee($adherent, $committee, CommitteeMembershipTriggerEnum::ADMIN);
+        }
+    }
+}
