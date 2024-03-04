f7af2411146bdffc9ff7053649ea30c702224ce8
Antonin <antonin.carlin@icloud.com>
2024-03-04 13:52:19 +0100

    feat(proxy): style layout

---
+++ assets/pages/proxy_voting_representative/components/FirstFormStep.js
@@ -0,0 +1,94 @@
+/** @typedef  {import('alpinejs').AlpineComponent} AlpineComponent */
+
+import CommonFormStep from './CommonFormStep';
+
+function closest(num, arr) {
+    let curr = arr[0];
+    let diff = Math.abs(num - curr);
+    for (let val = 0; val < arr.length; val += 1) {
+        const newdiff = Math.abs(num - arr[val]);
+        if (newdiff < diff) {
+            diff = newdiff;
+            curr = arr[val];
+        }
+    }
+    return curr;
+}
+
+/**
+ * First Step component for funnel
+ * @returns {AlpineComponent}
+ */
+const FirstForm = () => {
+    const uniqAmounts = [30, 60, 120, 250, 500, 1000];
+    const monthlyAmounts = [5, 10, 20, 30, 60, 100];
+    return ({
+        ...CommonFormStep(),
+        fieldsValid: {},
+        nextStepId: 'step_2',
+        id: 'step_1',
+        submitted: false,
+        submittedValues: null,
+        defaultCustomAmount: '',
+
+        getTaxTextReduction() {
+            return `${(this.amount * 0.34).toFixed(2)} € ${'-1' === this.duration ? '/ mois' : ''}`;
+        },
+
+        handleAmountClick(amount) {
+            this.animateSvg(amount);
+            document.querySelector('#amount_custom').value = '';
+        },
+
+        /**
+         * @param {Event} e
+         */
+        handleCustomFieldChange(e) {
+            const { value } = e.target;
+            this.amount = '' === value ? '60' : value;
+            this.animateSvg(this.amount);
+        },
+
+        animateSvg(value) {
+            let index = this.getAmounts()
+                .indexOf(Number(value));
+            if (-1 === index) {
+                index = this.getAmounts()
+                    .indexOf(closest(Number(value), this.getAmounts()));
+            }
+            const indexesToAnimate = Array.from(Array(index + 1)
+                .keys());
+            document.querySelectorAll('[id^="p_"]')
+                .forEach((el) => {
+                    const elIndex = Number(el.id.split('_')[1]) - 1;
+                    if (indexesToAnimate.includes(elIndex)) return;
+                    el.classList.remove('active');
+                });
+            indexesToAnimate.forEach((i) => {
+                setTimeout(() => {
+                    document.querySelector(`#p_${i + 1}`)
+                        .classList
+                        .add('active');
+                }, i * 100);
+            });
+        },
+
+        init() {
+            this.animateSvg(this.amount);
+            this.defaultCustomAmount = this.getCustomAmount();
+        },
+
+        getAmounts(duration) {
+            return '0' === this.duration ? uniqAmounts : monthlyAmounts;
+        },
+        getCustomAmount() {
+            return uniqAmounts.includes(Number(this.amount)) ? '' : this.amount;
+        },
+        async handleOnSubmit(e, $dispatch) {
+            e.preventDefault();
+            this.handleNextStep();
+        },
+    });
+};
+
+export default FirstForm;
---
+++ assets/pages/proxy_voting_representative/components/ThirdFormStep.js
@@ -0,0 +1,39 @@
+/** @typedef  {import('alpinejs').AlpineComponent} AlpineComponent */
+import '../../../components/Validator/typedef';
+import CommonFormStep from './CommonFormStep';
+
+/**
+ * First Step component for funnel
+ * @return {AlpineComponent}
+ */
+const ThirdForm = () => ({
+    ...CommonFormStep(),
+    id: 'step_3',
+    fieldsValid: {
+        autorisations: false,
+        captcha: false,
+    },
+    captchaToken: null,
+    loading: false,
+    handleOnSubmit(e) {
+        this.loading = true;
+    },
+
+    init() {
+        this.$nextTick(() => {
+            const tokenInput = dom('input[name="frc-captcha-solution"]:last-child');
+
+            if (dom('.frc-captcha')) {
+                friendlyChallenge.autoWidget.opts.doneCallback = (token) => {
+                    this.captchaToken = token;
+                    this.fieldsValid.captcha = true;
+                };
+            } else if (tokenInput && tokenInput.value) {
+                this.captchaToken = tokenInput.value;
+                this.fieldsValid.captcha = true;
+            }
+        });
+    },
+});
+
+export default ThirdForm;
