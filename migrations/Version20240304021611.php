b2a5ea1b18c3f5708db8d4ad28e01fb77a24de7c
Rémi Gardien <gardien.remi@gmail.com>
2024-03-05 16:11:45 +0100

    Add migration

--- migrations/Version20240304021611.php
+++ migrations/Version20240304021611.php
@@ -7,11 +7,6 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20240304021611 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE procuration_v2_elections (
