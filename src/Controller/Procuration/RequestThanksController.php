e6f33e669581400c7b42c341d2cbe9d7010fe757
Rémi Gardien <gardien.remi@gmail.com>
2024-03-12 17:21:37 +0100

    [Procuration v2] Disable canary (#9881)

--- src/Controller/Procuration/RequestThanksController.php
+++ src/Controller/Procuration/RequestThanksController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Procuration;
 
-use App\Controller\CanaryControllerTrait;
 use App\Entity\Procuration\Request as ProcurationRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path: '/mandant/{uuid}/merci', name: 'app_procuration_v2_request_thanks', methods: ['GET'])]
 class RequestThanksController extends AbstractController
 {
-    use CanaryControllerTrait;
-
     public function __invoke(ProcurationRequest $procurationRequest): Response
     {
-        $this->disableInProduction();
-
         return $this->render('procuration_v2/request_thanks.html.twig', [
             'procuration_request' => $procurationRequest,
         ]);
