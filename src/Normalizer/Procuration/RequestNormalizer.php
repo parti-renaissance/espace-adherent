0afc6ff9ef417192f47f92e4c9e83024195b2b82
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-03-22 17:24:45 +0100

    [API] add available proxies count on request endpoint

--- src/Normalizer/Procuration/RequestNormalizer.php
+++ src/Normalizer/Procuration/RequestNormalizer.php
@@ -2,31 +2,37 @@
 
 namespace App\Normalizer\Procuration;
 
-use App\Entity\ProcurationV2\Proxy;
+use App\Entity\ProcurationV2\Request;
+use App\Repository\Procuration\ProxyRepository;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-class ProxyNormalizer implements NormalizerInterface, NormalizerAwareInterface
+class RequestNormalizer implements NormalizerInterface, NormalizerAwareInterface
 {
     use NormalizerAwareTrait;
 
-    private const ALREADY_CALLED = 'PROCURATION_PROXY_NORMALIZER_ALREADY_CALLED';
+    private const ALREADY_CALLED = 'PROCURATION_REQUEST_NORMALIZER_ALREADY_CALLED';
+
+    public function __construct(private readonly ProxyRepository $proxyRepository)
+    {
+    }
 
     public function normalize($object, ?string $format = null, array $context = [])
     {
         $context[self::ALREADY_CALLED] = true;
 
-        $context['groups'] = ['procuration_request_list_proxy'];
+        $data = $this->normalizer->normalize($object, $format, $context);
+        $data['available_proxies_count'] = $this->proxyRepository->countAvailableProxies($object);
 
-        return $this->normalizer->normalize($object, $format, $context);
+        return $data;
     }
 
     public function supportsNormalization($data, ?string $format = null, array $context = []): bool
     {
         return
             empty($context[self::ALREADY_CALLED])
-            && $data instanceof Proxy
+            && $data instanceof Request
             && \in_array('procuration_request_list', $context['groups'] ?? [], true);
     }
 }
--- src/Repository/Procuration/ProxyRepository.php
+++ src/Repository/Procuration/ProxyRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository\Procuration;
 
 use App\Entity\ProcurationV2\Proxy;
+use App\Entity\ProcurationV2\Request;
+use App\Procuration\V2\ProxyStatusEnum;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,5 +13,32 @@ class ProxyRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Proxy::class);
+    }
+
+    public function countAvailableProxies(Request $request): int
+    {
+        if (!$request->votePlace && !$request->customVotePlace) {
+            return 0;
+        }
+
+        $qb = $this->createQueryBuilder('p')
+            ->select('COUNT(DISTINCT p.id)')
+            ->where('p.status = :status')
+            ->setParameter('status', ProxyStatusEnum::PENDING)
+        ;
+
+        if ($request->votePlace) {
+            $qb
+                ->andWhere('p.votePlace = :votePlace')
+                ->setParameter('votePlace', $request->votePlace)
+            ;
+        } else {
+            $qb
+                ->andWhere('p.customVotePlace = :customVotePlace')
+                ->setParameter('customVotePlace', $request->customVotePlace)
+            ;
+        }
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
     }
 }
