bcec36a806228bdd62eec180ad92bf567e363a95
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-03-22 16:11:24 +0100

    [Admin] remove old roles column

---
+++ migrations/Version20240322150932.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240322150932 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE administrators DROP roles');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE administrators ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\'');
+    }
+}
