e4055476adcb1ede783c5d0dc5995421a791ed1e
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-04-03 11:16:07 +0200

    Remove RE old donation flow (#9983)

--- src/Donation/Request/DonationRequest.php
+++ src/Donation/Request/DonationRequest.php
@@ -7,23 +7,18 @@ use App\Donation\DonationSourceEnum;
 use App\Donation\Paybox\PayboxPaymentSubscription;
 use App\Entity\Adherent;
 use App\Entity\Donation;
-use App\Form\Renaissance\Donation\DonationRequestConfirmType;
 use App\Recaptcha\RecaptchaChallengeInterface;
 use App\Recaptcha\RecaptchaChallengeTrait;
-use App\Renaissance\Donation\DonationRequestStateEnum;
 use App\Validator\MaxFiscalYearDonation;
 use App\Validator\MaxMonthDonation;
 use App\Validator\PayboxSubscription as AssertPayboxSubscription;
-use App\Validator\Recaptcha as AssertRecaptcha;
 use App\Validator\UniqueDonationSubscription;
-use App\ValueObject\Genders;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @UniqueDonationSubscription(groups={"Default", "fill_personal_info"})
  * @MaxFiscalYearDonation(groups={"Default", "fill_personal_info"})
  * @MaxMonthDonation(groups={"Default", "choose_donation_amount"})
- * @AssertRecaptcha(api="friendly_captcha", groups={"donation_request_mentions"})
  */
 class DonationRequest implements DonationRequestInterface, RecaptchaChallengeInterface
 {
@@ -38,10 +33,6 @@ class DonationRequest implements DonationRequestInterface, RecaptchaChallengeInt
 
     public const MAX_AMOUNT = 7500;
     public const MAX_AMOUNT_SUBSCRIPTION = 625;
-
-    public const ALERT_AMOUNT = 200;
-
-    private string $state = DonationRequestStateEnum::STATE_DONATION_AMOUNT;
 
     /**
      * @Assert\NotBlank(groups={"Default", "choose_donation_amount"})
@@ -122,16 +113,6 @@ class DonationRequest implements DonationRequestInterface, RecaptchaChallengeInt
 
     public $localDestination = false;
 
-    /**
-     * @Assert\Choice(DonationRequestConfirmType::CONFIRM_DONATION_TYPE_CHOICES, groups={"donation_confirm_type"})
-     */
-    private $confirmDonationType = DonationRequestConfirmType::CONFIRM_DONATION_TYPE_UNIQUE;
-
-    /**
-     * @Assert\Range(min=0, max=7500, groups={"donation_confirm_type"})
-     */
-    private $confirmSubscriptionAmount;
-
     private ?\DateTime $donatedAt = null;
 
     private $type;
@@ -169,16 +150,6 @@ class DonationRequest implements DonationRequestInterface, RecaptchaChallengeInt
         $dto->nationality = $adherent->getNationality();
 
         return $dto;
-    }
-
-    public function getState(): string
-    {
-        return $this->state;
-    }
-
-    public function setState(string $state): void
-    {
-        $this->state = $state;
     }
 
     public function getAmount(): ?float
@@ -276,69 +247,6 @@ class DonationRequest implements DonationRequestInterface, RecaptchaChallengeInt
         $this->duration = $duration ?? PayboxPaymentSubscription::NONE;
     }
 
-    public function retryPayload(array $payload): self
-    {
-        $retry = clone $this;
-
-        if (isset($payload['ge']) && \in_array($payload['ge'], Genders::ALL, true)) {
-            $retry->gender = $payload['ge'];
-        }
-
-        if (isset($payload['ln'])) {
-            $retry->lastName = (string) $payload['ln'];
-        }
-
-        if (isset($payload['fn'])) {
-            $retry->firstName = (string) $payload['fn'];
-        }
-
-        if (isset($payload['em'])) {
-            $retry->emailAddress = (string) $payload['em'];
-        }
-
-        if ($payload['co']) {
-            $retry->address->setCountry((string) $payload['co']);
-        }
-
-        if ($payload['na']) {
-            $retry->nationality = (string) $payload['na'];
-        }
-
-        if (isset($payload['pc'])) {
-            $retry->address->setPostalCode((string) $payload['pc']);
-        }
-
-        if (isset($payload['ci'])) {
-            $retry->address->setCityName((string) $payload['ci']);
-        }
-
-        if (isset($payload['ad'])) {
-            $retry->address->setAddress((string) $payload['ad']);
-        }
-
-        return $retry;
-    }
-
-    public function getConfirmDonationType(): ?string
-    {
-        return $this->confirmDonationType;
-    }
-
-    public function setConfirmDonationType(?string $confirmDonationType): void
-    {
-        $this->confirmDonationType = $confirmDonationType;
-    }
-
-    public function getConfirmSubscriptionAmount(): ?string
-    {
-        return $this->confirmSubscriptionAmount;
-    }
-
-    public function setConfirmSubscriptionAmount(?string $confirmSubscriptionAmount): void
-    {
-        $this->confirmSubscriptionAmount = $confirmSubscriptionAmount;
-    }
-
     public function isSubscription(): bool
     {
         return PayboxPaymentSubscription::NONE !== $this->duration;
@@ -382,16 +290,5 @@ class DonationRequest implements DonationRequestInterface, RecaptchaChallengeInt
     public function setDonatedAt(?\DateTime $donatedAt): void
     {
         $this->donatedAt = $donatedAt;
-    }
-
-    public function updateFromAdherent(Adherent $adherent): void
-    {
-        $this->adherentId = $adherent->getId();
-        $this->gender = $adherent->getGender();
-        $this->firstName = $adherent->getFirstName();
-        $this->lastName = $adherent->getLastName();
-        $this->emailAddress = $adherent->getEmailAddress();
-        $this->address = Address::createFromAddress($adherent->getPostAddress());
-        $this->nationality = $adherent->getNationality();
     }
 }
