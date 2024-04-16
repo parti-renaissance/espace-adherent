142c02be08f47080c416fdf49b8aac80184af611
Rémi Gardien <gardien.remi@gmail.com>
2024-04-16 17:53:16 +0200

    [BDE] Send activation reminder

---
+++ migrations/Version20240416154418.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240416154418 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE adherents ADD activation_reminded_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE adherents DROP activation_reminded_at');
+    }
+}
