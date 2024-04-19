a161510156c6f559b3f2f3a89f235045b35b056e
Rémi Gardien <gardien.remi@gmail.com>
2024-04-19 17:20:01 +0200

    Add new role groups

--- src/Entity/AdministratorRoleGroupEnum.php
+++ src/Entity/AdministratorRoleGroupEnum.php
@@ -4,23 +4,30 @@ namespace App\Entity;
 
 enum AdministratorRoleGroupEnum: string
 {
-    case RE_SITE_WEB = 're_site_web';
-    case COMMUNICATION = 'communication';
-    case POLITIQUE = 'politique';
     case ADHERENTS = 'adherents';
+    case COMMUNICATION = 'communication';
+    case EVENTS = 'events';
     case TERRITOIRES = 'territoires';
+    case MOBILISATION = 'mobilisation';
+    case ELUS = 'elus';
+    case ELECTIONS = 'elections';
+    case ELECTIONS_INTERNES = 'elections_internes';
     case APPLICATION_MOBILE = 'application_mobile';
-    case PHONING = 'phoning';
-    case PORTE_A_PORTE = 'porte_a_porte';
-    case INSTANCES = 'instances';
     case FINANCES = 'finances';
     case TECH = 'tech';
     case IDEES = 'idees';
     case FORMATION = 'formation';
+    case ARCHIVES_A_GARDER = 'archives_a_garder';
+    case ARCHIVES_A_DEPUBLIER = 'archives_a_depublier';
+    case ARCHIVES = 'archives';
     case SOCLE_PROGRAMMATIQUE = 'socle_programmatique';
     case COMMUNAUTES_THEMATIQUES = 'communautes_thematiques';
+
+    // Legacy
+    case RE_SITE_WEB = 're_site_web';
+    case POLITIQUE = 'politique';
+    case PHONING = 'phoning';
+    case PORTE_A_PORTE = 'porte_a_porte';
+    case INSTANCES = 'instances';
     case ELECTIONS_DEPARTEMENTALES = 'elections_departementales';
-    case ARCHIVES = 'archives';
-    case EVENTS = 'events';
-    case ELECTIONS = 'elections';
 }
