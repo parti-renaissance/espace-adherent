cfaec404e83fbbac8428d9ace6dad2b9a892b257
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-05-01 17:01:29 +0200

    [Timeline] inject registered_at date for event items

--- src/JeMengage/Timeline/FeedProcessor/EventProcessor.php
+++ src/JeMengage/Timeline/FeedProcessor/EventProcessor.php
@@ -7,21 +7,28 @@ use App\Entity\Adherent;
 use App\Event\EventCleaner;
 use App\Event\EventVisibilityEnum;
 use App\JeMengage\Timeline\TimelineFeedTypeEnum;
+use App\Repository\EventRegistrationRepository;
 use Symfony\Component\Security\Core\Security;
 
 class EventProcessor extends AbstractFeedProcessor
 {
     private const CONTEXT_CLEANER_ENABLED_KEY = 'event:cleaner_enabled';
 
+    private ?Adherent $currentUser = null;
+
     public function __construct(
         private readonly EventCleaner $eventCleaner,
         private readonly Security $security,
+        private readonly EventRegistrationRepository $eventRegistrationRepository,
     ) {
     }
 
     public function process(array $item, array &$context): array
     {
-        return $this->cleanEventDataIfNeed($item, $context);
+        $item = $this->cleanEventDataIfNeed($item, $context);
+        $item = $this->appendEventRegistrationDate($item);
+
+        return $item;
     }
 
     public function supports(array $item): bool
@@ -34,12 +41,11 @@ class EventProcessor extends AbstractFeedProcessor
         $needClean = $context[self::CONTEXT_CLEANER_ENABLED_KEY] ?? null;
 
         if (null === $needClean) {
-            $user = $this->security->getUser();
+            $user = $this->getCurrentUser();
             $needClean = $context[self::CONTEXT_CLEANER_ENABLED_KEY] =
                 EventVisibilityEnum::isForAdherent($visibility = $item['visibility'] ?? EventVisibilityEnum::ADHERENT_DUES->value)
                 && (
-                    !$user instanceof Adherent
-                    || (EventVisibilityEnum::ADHERENT->value === $visibility && !$user->hasTag(TagEnum::ADHERENT))
+                    (EventVisibilityEnum::ADHERENT->value === $visibility && !$user->hasTag(TagEnum::ADHERENT))
                     || (EventVisibilityEnum::ADHERENT_DUES->value === $visibility && !$user->hasTag(TagEnum::getAdherentYearTag()))
                 );
         }
@@ -50,6 +56,25 @@ class EventProcessor extends AbstractFeedProcessor
         } else {
             $item['object_state'] = 'full';
         }
+
+        return $item;
+    }
+
+    private function getCurrentUser(): Adherent
+    {
+        if ($this->currentUser) {
+            return $this->currentUser;
+        }
+
+        return $this->currentUser = $this->security->getUser();
+    }
+
+    private function appendEventRegistrationDate(array $item): array
+    {
+        $item['user_registered_at'] = $this->eventRegistrationRepository->findAdherentRegistration(
+            $item['objectID'],
+            $this->getCurrentUser()->getUuidAsString()
+        )?->getCreatedAt();
 
         return $item;
     }
