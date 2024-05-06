1fed3199784f16af125b1c0355b1603409ce1a9d
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-05-06 17:50:38 +0200

    [NationalEvent] add additional ticket field (#10185)

---
+++ migrations/Version20240506153531.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240506153531 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE national_event_inscription ADD ticket_custom_detail VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE national_event_inscription DROP ticket_custom_detail');
+    }
+}
