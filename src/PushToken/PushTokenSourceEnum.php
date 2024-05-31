9cf2dd0ded941218360cc9977449c556898987a9
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-05-31 15:23:33 +0200

    [Push] add new token source (#10303)

--- src/PushToken/PushTokenSourceEnum.php
+++ src/PushToken/PushTokenSourceEnum.php
@@ -7,8 +7,10 @@ use MyCLabs\Enum\Enum;
 class PushTokenSourceEnum extends Enum
 {
     public const JE_MARCHE = 'je_marche';
+    public const VOX = 'vox';
 
     public const ALL = [
         self::JE_MARCHE,
+        self::VOX,
     ];
 }
