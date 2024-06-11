426c892b12813534d24f1da608bbb06b70397958
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-06-11 17:24:47 +0200

    [Admin] add candidat hub items admin

---
+++ src/Admin/HubItemAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Admin;
+
+use App\Form\PositionType;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+class HubItemAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->with('', ['class' => 'col-md-6'])
+                ->add('title', null, ['label' => 'Titre'])
+                ->add('url', UrlType::class, ['label' => 'Lien', 'default_protocol' => 'https'])
+                ->add('position', PositionType::class, ['label' => 'Position', 'required' => false])
+            ->end()
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('title', null, [
+                'label' => 'Titre',
+            ])
+            ->add('url', null, ['label' => 'URL'])
+            ->add('position', null, ['label' => 'Position'])
+            ->add('createdAt', null, ['label' => 'Créé le'])
+            ->add('updatedAt', null, ['label' => 'Modifié le'])
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+}
