d28c46dbeeed30209ad0324e1f9d4bfedf99d232
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-06-14 11:56:22 +0200

    [PHP] update phpunit to v11

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require \dirname(__DIR__).'/vendor/autoload.php';
+
+// https://github.com/symfony/symfony/issues/53812
+set_exception_handler([new ErrorHandler(), 'handleException']);
 
 if (file_exists(\dirname(__DIR__).'/config/bootstrap.php')) {
     require \dirname(__DIR__).'/config/bootstrap.php';
