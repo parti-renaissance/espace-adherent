16919cd66eb19ecf26f65922c3d775c542653335
Rémi Gardien <gardien.remi@gmail.com>
2024-06-14 16:53:49 +0200

    Update get available proxy

--- src/Controller/Api/Procuration/GetMatchedProxiesController.php
+++ src/Controller/Api/Procuration/GetMatchedProxiesController.php
@@ -5,12 +5,23 @@ namespace App\Controller\Api\Procuration;
 use ApiPlatform\State\Pagination\PaginatorInterface;
 use App\Entity\ProcurationV2\Request;
 use App\Repository\Procuration\ProxyRepository;
+use App\Repository\Procuration\RoundRepository;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class GetMatchedProxiesController
 {
-    public function __invoke(SymfonyRequest $httpRequest, Request $request, ProxyRepository $repository): PaginatorInterface
+    public function __invoke(SymfonyRequest $httpRequest, Request $request, RoundRepository $roundRepository, ProxyRepository $repository): PaginatorInterface
     {
-        return $repository->findAvailableProxies($request, max(1, $httpRequest->query->getInt('page', 1)));
+        if (empty($data['round']) || !Uuid::isValid($roundUuid = $httpRequest->query->get('round'))) {
+            throw new BadRequestHttpException('Identifiant de tour manquant ou invalide');
+        }
+
+        if (!$round = $roundRepository->findOneByUuid($roundUuid)) {
+            throw new BadRequestHttpException('Le tour n\'existe pas');
+        }
+
+        return $repository->findAvailableProxies($request, $round, max(1, $httpRequest->query->getInt('page', 1)));
     }
 }
