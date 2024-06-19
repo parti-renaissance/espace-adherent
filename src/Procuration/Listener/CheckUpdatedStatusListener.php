2b908a198963cea3cd57b3dc2da4b4c169a48d5c
Rémi Gardien <gardien.remi@gmail.com>
2024-06-19 17:36:54 +0200

    Add listener

--- src/Procuration/Listener/CheckUpdatedStatusListener.php
+++ src/Procuration/Listener/CheckUpdatedStatusListener.php
@@ -4,18 +4,29 @@ namespace App\Procuration\Listener;
 
 use ApiPlatform\Symfony\EventListener\EventPriorities;
 use App\Entity\ProcurationV2\AbstractProcuration;
+use App\Entity\ProcurationV2\AbstractSlot;
+use App\Entity\ProcurationV2\ProxySlot;
+use App\Entity\ProcurationV2\RequestSlot;
+use App\Procuration\V2\ProcurationHandler;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class CheckUpdatedStatusListener implements EventSubscriberInterface
 {
+    public function __construct(private readonly ProcurationHandler $procurationHandler)
+    {
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::REQUEST => ['preDeserialize', EventPriorities::PRE_DESERIALIZE],
+            KernelEvents::VIEW => ['preWrite', EventPriorities::PRE_WRITE],
         ];
     }
 
@@ -39,6 +50,27 @@ class CheckUpdatedStatusListener implements EventSubscriberInterface
                 'status' => 'error',
                 'message' => 'Le statut d\'une demande complétée ne peut pas être modifié.',
             ], Response::HTTP_BAD_REQUEST));
+        }
+    }
+
+    public function preWrite(ViewEvent $event): void
+    {
+        $slot = $event->getControllerResult();
+
+        if (!$slot instanceof AbstractSlot
+            || !$event->getRequest()->isMethod(Request::METHOD_PUT)
+        ) {
+            return;
+        }
+
+        if ($slot instanceof ProxySlot) {
+            $this->procurationHandler->updateProxyStatus($slot->proxy);
+
+            return;
+        }
+
+        if ($slot instanceof RequestSlot) {
+            $this->procurationHandler->updateRequestStatus($slot->request);
         }
     }
 }
