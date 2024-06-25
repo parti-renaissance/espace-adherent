98507e2d5f6860af10f4114a216f159fcd76ec01
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-06-25 15:34:22 +0200

    [Procuration] improve search query by removing SLUGGIFY function (#10455)

--- src/Query/Utils/MultiColumnsSearchHelper.php
+++ src/Query/Utils/MultiColumnsSearchHelper.php
@@ -2,7 +2,6 @@
 
 namespace App\Query\Utils;
 
-use App\Query\Mysql\Sluggify;
 use Doctrine\ORM\QueryBuilder;
 
 class MultiColumnsSearchHelper
@@ -16,33 +15,31 @@ class MultiColumnsSearchHelper
     ): void {
         $conditions = $queryBuilder->expr()->orX();
 
-        $searchCharactersPattern = '/'.Sluggify::REGEXP_PATTERN.'/';
-
         preg_match('/(?<first>[^\s]*)[\s]*(?<last>.*)/', $searchTerm, $tokens);
 
         if (\array_key_exists('first', $tokens) && \array_key_exists('last', $tokens)) {
             foreach ($mainColumns as $coupleColumn) {
-                $conditions->add("SLUGGIFY($coupleColumn[0]) LIKE :search_first_token AND SLUGGIFY($coupleColumn[1]) LIKE :search_last_token");
+                $conditions->add("$coupleColumn[0] LIKE :search_first_token AND $coupleColumn[1] LIKE :search_last_token");
             }
 
             $queryBuilder
-                ->setParameter('search_first_token', '%'.preg_replace($searchCharactersPattern, '', $tokens['first']).'%')
-                ->setParameter('search_last_token', '%'.preg_replace($searchCharactersPattern, '', $tokens['last']).'%')
+                ->setParameter('search_first_token', '%'.$tokens['first'].'%')
+                ->setParameter('search_last_token', '%'.$tokens['last'].'%')
             ;
         } else {
             foreach ($mainColumns as $coupleColumn) {
-                $conditions->add("SLUGGIFY($coupleColumn[0]) LIKE :slug_search");
+                $conditions->add("$coupleColumn[0] LIKE :slug_search");
             }
 
-            $queryBuilder->setParameter('slug_search', '%'.preg_replace($searchCharactersPattern, '', $searchTerm).'%');
+            $queryBuilder->setParameter('slug_search', '%'.$searchTerm.'%');
         }
 
         if ($additionalColumns) {
             foreach ($additionalColumns as $column) {
-                $conditions->add("SLUGGIFY($column) LIKE :slug_search");
+                $conditions->add("$column LIKE :slug_search");
             }
 
-            $queryBuilder->setParameter('slug_search', '%'.preg_replace($searchCharactersPattern, '', $searchTerm).'%');
+            $queryBuilder->setParameter('slug_search', '%'.$searchTerm.'%');
         }
 
         if ($columnStrictSearch) {
