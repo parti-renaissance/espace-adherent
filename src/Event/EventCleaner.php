57bc3f1bc07fcc345a5349738718b38e8f2ae144
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-07-16 16:55:47 +0200

    [Event] expose author data for reserved event

--- src/Event/EventCleaner.php
+++ src/Event/EventCleaner.php
@@ -7,7 +7,7 @@ class EventCleaner
     public function cleanEventData(array $eventData): array
     {
         foreach ($eventData as $key => $value) {
-            if (!\in_array($key, ['name', 'title', 'type', 'objectID', 'uuid', 'slug', 'time_zone', 'begin_at', 'finish_at', 'status', 'visibility', 'image', 'image_url', 'link', 'category'])) {
+            if (!\in_array($key, ['name', 'organizer', 'author', 'title', 'type', 'objectID', 'uuid', 'slug', 'time_zone', 'begin_at', 'finish_at', 'status', 'visibility', 'image', 'image_url', 'link', 'category'])) {
                 $eventData[$key] = null;
             }
         }
