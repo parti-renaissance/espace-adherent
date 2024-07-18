fc7d2bcb397a558d6a82a48fb44cbf181c581498
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-07-18 17:13:06 +0200

    Remove commitment feature (#10539)

--- assets/pages/home_page.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import CommitmentSlideOver from '../components/Home/CommitmentSlideOver';
-
-export default () => createRoot(dom('#modal-wrapper')).render(<CommitmentSlideOver />);
--- src/Controller/Renaissance/HomeController.php
+++ src/Controller/Renaissance/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Renaissance;
 
 use App\Form\Renaissance\NewsletterSubscriptionType;
 use App\Repository\Biography\ExecutiveOfficeMemberRepository;
-use App\Repository\CommitmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path: '/', name: 'app_renaissance_homepage', methods: ['GET'])]
 class HomeController extends AbstractController
 {
-    public function __invoke(
-        CommitmentRepository $commitmentRepository,
-        ExecutiveOfficeMemberRepository $executiveOfficeMemberRepository,
-    ): Response {
+    public function __invoke(ExecutiveOfficeMemberRepository $executiveOfficeMemberRepository): Response
+    {
         return $this->render('renaissance/home.html.twig', [
             'newsletter_form' => $this->createForm(NewsletterSubscriptionType::class, null, ['action' => $this->generateUrl('app_renaissance_newsletter_save')])->createView(),
-            'commitments' => $commitmentRepository->getAllOrdered(),
             'executive_office_members' => $executiveOfficeMemberRepository->findAllPublishedMembers(true),
         ]);
     }
