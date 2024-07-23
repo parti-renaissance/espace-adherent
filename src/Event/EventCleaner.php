27afc97b79bf17b69601bd38e863d41b072752ac
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-07-23 18:48:59 +0200

    [Event] expose reserved event in partial mode (#10548)

--- src/Event/EventCleaner.php
+++ src/Event/EventCleaner.php
@@ -4,10 +4,13 @@ namespace App\Event;
 
 class EventCleaner
 {
-    public function cleanEventData(array $eventData): array
+    private const ALLOWED_KEYS_FOR_DEEP = ['name', 'title', 'type', 'objectID', 'uuid', 'slug', 'status', 'visibility', 'category'];
+    private const ALLOWED_KEYS = ['name', 'organizer', 'author', 'title', 'type', 'objectID', 'uuid', 'slug', 'time_zone', 'begin_at', 'finish_at', 'status', 'visibility', 'image', 'image_url', 'link', 'category'];
+
+    public function cleanEventData(array $eventData, bool $deep = false): array
     {
         foreach ($eventData as $key => $value) {
-            if (!\in_array($key, ['name', 'organizer', 'author', 'title', 'type', 'objectID', 'uuid', 'slug', 'time_zone', 'begin_at', 'finish_at', 'status', 'visibility', 'image', 'image_url', 'link', 'category'])) {
+            if (!\in_array($key, $deep ? self::ALLOWED_KEYS_FOR_DEEP : self::ALLOWED_KEYS)) {
                 $eventData[$key] = null;
             }
         }
