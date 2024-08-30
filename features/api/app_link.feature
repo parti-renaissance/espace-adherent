e863a03f04a6fa773a2852dcf25b95789fdb03f6
Rémi Gardien <gardien.remi@gmail.com>
2024-08-30 08:09:28 +0200

    [API] Add endpoint to retrieve magic link

---
+++ features/api/app_link.feature
@@ -0,0 +1,19 @@
+@api
+@renaissance
+Feature:
+  As a logged-in user
+  I should be able to retrieve magic link with redirections
+
+  Scenario: As a logged-in user I can retrieve magic links based on a key
+    Given I am logged with "carl999@example.fr" via OAuth client "JeMengage Mobile"
+    When I send a "GET" request to "/api/v3/app-link/adhesion"
+    Then the response status code should be 200
+    And the response should be in JSON
+    And print last JSON response
+    And the JSON should be equal to:
+    """
+    {
+        "url": "@string@.isUrl()",
+        "expires_at": "@string@.isDateTime()"
+    }
+    """
---
+++ src/Controller/Api/AppLink/AuthenticatedAppLinkController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Api\AppLink;
+
+use App\AppCodeEnum;
+use App\Controller\Renaissance\Adhesion\AdhesionController;
+use App\Entity\Adherent;
+use App\OAuth\App\AuthAppUrlManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
+
+#[Route(path: '/v3/app-link/{key}', name: 'api_app_link_authenticated', methods: ['GET'])]
+#[Security("is_granted('ROLE_USER')")]
+class AuthenticatedAppLinkController extends AbstractController
+{
+    private const KEYS_TO_ROUTES = [
+        'adhesion' => AdhesionController::ROUTE_NAME,
+    ];
+
+    public function __invoke(
+        Request $request,
+        AuthAppUrlManager $appUrlManager,
+        LoginLinkHandlerInterface $loginLinkHandler,
+        string $key
+    ): JsonResponse {
+        /** @var Adherent $user */
+        $user = $this->getUser();
+
+        if (!\array_key_exists($key, self::KEYS_TO_ROUTES)) {
+            throw new BadRequestHttpException(\sprintf('No route found for key "%s".', $key));
+        }
+
+        $appCode = $appUrlManager->getAppCodeFromRequest($request) ?? AppCodeEnum::RENAISSANCE;
+
+        return $this->json(
+            $loginLinkHandler->createLoginLink(
+                $user,
+                $request,
+                $appCode,
+                $this->generateUrl(self::KEYS_TO_ROUTES[$key])
+            ),
+        );
+    }
+}
