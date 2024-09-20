873d709f3b2534cac640aaa9640510317c843a83
Dimitri <gritsajuk.dimitri@gmail.com>
2024-09-20 17:20:38 +0200

    [SwitchUser] configure VOX redirect from admin domain (#10751)

--- src/Controller/Vox/RedirectAppController.php
+++ src/Controller/Vox/RedirectAppController.php
@@ -10,17 +10,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RedirectAppController extends AbstractController
 {
+    public function __construct(private readonly string $adminRenaissanceHost)
+    {
+    }
+
     public function __invoke(Request $request, AuthAppUrlManager $appUrlManager, ClientRepository $clientRepository): Response
     {
         $currentApp = $appUrlManager->getAppCodeFromRequest($request);
-        $urlGenerator = $appUrlManager->getUrlGenerator($currentApp);
+        $urlGenerator = $appUrlManager->getUrlGenerator($currentApp ?? '');
 
         $client = $clientRepository->getVoxClient();
 
         $redirectUri = current($client->getRedirectUris());
 
         return $this->redirectToRoute('app_front_oauth_authorize', [
-            'app_domain' => $urlGenerator->getAppHost(),
+            'app_domain' => $this->isGranted('ROLE_PREVIOUS_ADMIN') ? $this->adminRenaissanceHost : $urlGenerator?->getAppHost(),
             'response_type' => 'code',
             'client_id' => $client->getUuid(),
             'redirect_uri' => $redirectUri,
