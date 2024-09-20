1d9e4182ec9b165dc46823433c311ffc1af23412
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-09-20 13:11:30 +0200

    [Admin] adjust esCadre auth url for impersonified admin

--- src/Normalizer/JecouteAdherentNormalizer.php
+++ src/Normalizer/JecouteAdherentNormalizer.php
@@ -7,6 +7,7 @@ use App\OAuth\Model\Scope;
 use App\Repository\Jecoute\DataSurveyRepository;
 use App\Repository\OAuth\ClientRepository;
 use App\Security\Voter\DataCornerVoter;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 
@@ -20,6 +21,9 @@ class JecouteAdherentNormalizer extends AdherentNormalizer
         private readonly DataSurveyRepository $dataSurveyRepository,
         private readonly AuthorizationCheckerInterface $authorizationChecker,
         private readonly ClientRepository $clientRepository,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly string $adminRenaissanceHost,
+        private readonly string $userVoxHost,
     ) {
     }
 
@@ -33,7 +37,21 @@ class JecouteAdherentNormalizer extends AdherentNormalizer
         ];
 
         if ($data['cadre_access'] = $this->authorizationChecker->isGranted(DataCornerVoter::DATA_CORNER, $object)) {
-            $data['cadre_auth_path'] = '/oauth/v2/auth?scope='.Scope::JEMENGAGE_ADMIN.'&response_type=code&client_id='.$this->clientRepository->findOneBy(['code' => AppCodeEnum::JEMENGAGE_WEB])->getUuid();
+            if ($object->getAuthAppVersion() < 580) {
+                $data['cadre_auth_path'] = $this->urlGenerator->generate('app_front_oauth_authorize', [
+                    'scope' => Scope::JEMENGAGE_ADMIN,
+                    'response_type' => 'code',
+                    'client_id' => $this->clientRepository->findOneBy(['code' => AppCodeEnum::JEMENGAGE_WEB])->getUuid(),
+                ]);
+            } else {
+                $data['cadre_auth_url'] = $this->urlGenerator->generate('app_front_oauth_authorize', [
+                    'app_domain' => $this->authorizationChecker->isGranted('ROLE_PREVIOUS_ADMIN') ? $this->adminRenaissanceHost : $this->userVoxHost,
+                    'scope' => Scope::JEMENGAGE_ADMIN,
+                    'response_type' => 'code',
+                    'client_id' => $this->clientRepository->findOneBy(['code' => AppCodeEnum::JEMENGAGE_WEB])->getUuid(
+                    ),
+                ], UrlGeneratorInterface::ABSOLUTE_URL);
+            }
         }
 
         return $data;
