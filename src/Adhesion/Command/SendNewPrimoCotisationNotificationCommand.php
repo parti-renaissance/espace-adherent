743920123722c69a4fbed92ed41da28ed170feb9
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-09-24 17:36:30 +0200

    [Adhesion] fix notification command argument

--- src/Adhesion/Command/SendNewPrimoCotisationNotificationCommand.php
+++ src/Adhesion/Command/SendNewPrimoCotisationNotificationCommand.php
@@ -8,8 +8,8 @@ use Ramsey\Uuid\UuidInterface;
 
 class SendNewPrimoCotisationNotificationCommand extends UuidDefaultAsyncMessage implements AsyncNotificationInterface
 {
-    public function __construct(UuidInterface $adherentUuid, public readonly float $amount)
+    public function __construct(UuidInterface $uuid, public readonly float $amount)
     {
-        parent::__construct($adherentUuid);
+        parent::__construct($uuid);
     }
 }
