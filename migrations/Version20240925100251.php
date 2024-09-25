9aaf786727f55bfc944044a3aa67e3ad3aa63991
Dimitri <gritsajuk.dimitri@gmail.com>
2024-09-25 13:30:47 +0200

    [ManagedUser] fix check subscription status (#10777)

---
+++ migrations/Version20240925100251.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240925100251 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE projection_managed_users ADD mailchimp_status VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE projection_managed_users DROP mailchimp_status');
+    }
+}
