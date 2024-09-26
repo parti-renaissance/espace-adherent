3654bcd43bb8abd97cd707161b91cac304110817
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-09-26 16:55:14 +0200

    [ResubscribeEmail] modify email link to redirect to Vox app

--- src/Controller/Vox/RedirectAppController.php
+++ src/Controller/Vox/RedirectAppController.php
@@ -7,11 +7,14 @@ use App\Repository\OAuth\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RedirectAppController extends AbstractController
 {
-    public function __construct(private readonly string $adminRenaissanceHost)
-    {
+    public function __construct(
+        private readonly UrlGeneratorInterface $sfRouterGenerator,
+        private readonly string $adminRenaissanceHost,
+    ) {
     }
 
     public function __invoke(Request $request, AuthAppUrlManager $appUrlManager, ClientRepository $clientRepository): Response
@@ -22,6 +25,14 @@ class RedirectAppController extends AbstractController
         $client = $clientRepository->getVoxClient();
 
         $redirectUri = current($client->getRedirectUris());
+
+        if ($requestedTargetPath = $request->query->get('target_path')) {
+            $requestedRedirectUri = rtrim($this->sfRouterGenerator->generate('vox_app', [], UrlGeneratorInterface::ABSOLUTE_URL), '/').'/'.ltrim($requestedTargetPath, '/');
+
+            if (\in_array($requestedRedirectUri, $client->getRedirectUris())) {
+                $redirectUri = $requestedRedirectUri;
+            }
+        }
 
         return $this->redirectToRoute('app_front_oauth_authorize', [
             'app_domain' => $this->isGranted('ROLE_PREVIOUS_ADMIN') ? $this->adminRenaissanceHost : $urlGenerator?->getAppHost(),
