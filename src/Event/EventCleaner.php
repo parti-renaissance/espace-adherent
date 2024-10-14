0157974b752ea950282519d58f6a8243a59668f8
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-10-14 10:35:26 +0200

    [API] add Event editable default value

--- src/Event/EventCleaner.php
+++ src/Event/EventCleaner.php
@@ -5,7 +5,7 @@ namespace App\Event;
 class EventCleaner
 {
     private const ALLOWED_KEYS_FOR_DEEP = ['name', 'title', 'type', 'objectID', 'uuid', 'slug', 'status', 'visibility', 'category'];
-    private const ALLOWED_KEYS = ['name', 'organizer', 'author', 'title', 'type', 'objectID', 'uuid', 'slug', 'time_zone', 'begin_at', 'finish_at', 'status', 'visibility', 'image', 'image_url', 'link', 'category'];
+    private const ALLOWED_KEYS = ['name', 'organizer', 'author', 'title', 'type', 'objectID', 'uuid', 'slug', 'time_zone', 'begin_at', 'finish_at', 'status', 'visibility', 'image', 'image_url', 'link', 'category', 'editable'];
 
     public function cleanEventData(array $eventData, bool $deep = false): array
     {
