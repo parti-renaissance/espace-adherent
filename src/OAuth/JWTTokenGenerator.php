09baafc482fdae6e06d573a826514cc7d1f23b8b
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-10-17 10:20:37 +0200

    [Security] add API endpoint for generating JWT token

---
+++ src/OAuth/JWTTokenGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\OAuth;
+
+use App\Entity\Adherent;
+use App\Entity\OAuth\Client;
+use Firebase\JWT\JWT;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class JWTTokenGenerator
+{
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function generate(Adherent $user, Client $client): string
+    {
+        $payload = [
+            'email' => $user->getEmailAddress(),
+            'name' => $user->getFullName(),
+            'locale' => 'fr',
+        ];
+
+        if ($user->getImagePath()) {
+            $payload['profilePicture'] = $this->urlGenerator->generate('asset_url', ['path' => $user->getImagePath()], UrlGeneratorInterface::ABSOLUTE_URL);
+        }
+
+        return JWT::encode($payload, $client->getSecret(), 'HS256');
+    }
+}
---
+++ src/Security/Voter/OAuthClientVoter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Adherent;
+use App\Entity\AuthoredInterface;
+use App\Entity\OAuth\Client;
+use Symfony\Component\Security\Core\Security;
+
+class OAuthClientVoter extends AbstractAdherentVoter
+{
+    public const PERMISSION = 'CHECK_OAUTH_CLIENT';
+
+    public function __construct(private readonly Security $security)
+    {
+    }
+
+    /**
+     * @param AuthoredInterface $subject
+     */
+    protected function doVoteOnAttribute(string $attribute, Adherent $adherent, $subject): bool
+    {
+        /** @var Client $subject */
+        foreach ($subject->getRequestedRoles() ?? [] as $role) {
+            $roleParts = explode('|', $role, 2);
+            if (!$this->security->isGranted($roleParts[0], $roleParts[1] ?? $adherent)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return self::PERMISSION === $attribute && $subject instanceof Client;
+    }
+}
