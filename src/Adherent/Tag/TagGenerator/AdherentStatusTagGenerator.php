859a978e2ea84269d51b1ddc241cc93b478ee214
Rémi Gardien <gardien.remi@gmail.com>
2024-10-30 17:39:45 +0100

    Fix adherent status tag generator

--- src/Adherent/Tag/TagGenerator/AdherentStatusTagGenerator.php
+++ src/Adherent/Tag/TagGenerator/AdherentStatusTagGenerator.php
@@ -63,10 +63,10 @@ class AdherentStatusTagGenerator extends AbstractTagGenerator
                 }
             }
 
-            $allYears = array_unique(array_merge(
+            $allYears = array_unique(array_merge(array_keys(
                 $countCotisationByYear,
                 $totalContributionPaymentsByYear
-            ));
+            )));
             unset($allYears[$currentYear]);
 
             foreach ($allYears as $year) {
