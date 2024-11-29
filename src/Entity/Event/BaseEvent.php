c1fde862389db0780426e8e9903477e5f39c47d4
Rémi Gardien <gardien.remi@gmail.com>
2024-11-29 10:23:21 +0100

    Do not check role on public endpoint

--- src/Entity/Event/BaseEvent.php
+++ src/Entity/Event/BaseEvent.php
@@ -82,7 +82,7 @@ use Symfony\Component\Validator\Constraints as Assert;
         ),
         new Get(
             uriTemplate: '/events/{uuid}',
-            security: '!(object.isForAdherent() and !is_granted(\'ROLE_ADHERENT\'))',
+            security: '!object.isForAdherent()',
             provider: EventProvider::class,
         ),
         new Put(
