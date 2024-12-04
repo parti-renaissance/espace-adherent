26a7665dbf84228c25cc7ccbac27de0d2123cef1
Dimitri <gritsajuk.dimitri@gmail.com>
2024-12-04 15:11:36 +0100

    [Election] add ballots endpoint (#11133)

---
+++ src/Controller/Api/VotingPlatform/ElectionBallotsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Api\VotingPlatform;
+
+use App\Entity\VotingPlatform\Designation\Designation;
+use App\Repository\VotingPlatform\ElectionRepository;
+use App\Repository\VotingPlatform\VoteResultRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sonata\Exporter\ExporterInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/v3/designations/{uuid}/ballots.{_format}', name: 'app_designation_get_ballots', requirements: ['uuid' => '%pattern_uuid%', '_format' => 'json|xlsx'], defaults: ['_format' => 'json'], methods: ['GET'])]
+#[Security("is_granted('ROLE_OAUTH_SCOPE_JEMENGAGE_ADMIN') and is_granted('IS_FEATURE_GRANTED', 'designation')")]
+class ElectionBallotsController extends AbstractController
+{
+    public function __invoke(
+        Designation $designation,
+        ElectionRepository $electionRepository,
+        VoteResultRepository $voteResultRepository,
+        ExporterInterface $exporter,
+        string $_format = 'json',
+    ): Response {
+        $ballots = $voteResultRepository->getVotes($designation);
+
+        if ('json' !== $_format) {
+            return $exporter->getResponse($_format, 'Bulletins.xlsx', new \ArrayIterator($ballots));
+        }
+
+        return $this->json($ballots);
+    }
+}
--- src/Controller/Api/VotingPlatform/ElectionVotersListController.php
+++ src/Controller/Api/VotingPlatform/ElectionVotersListController.php
@@ -6,11 +6,13 @@ use App\Entity\VotingPlatform\Designation\Designation;
 use App\Repository\VotingPlatform\ElectionRepository;
 use App\Repository\VotingPlatform\VoterRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sonata\Exporter\ExporterInterface;
+use Sonata\Exporter\Source\IteratorCallbackSourceIterator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/v3/designations/{uuid}/voters', name: 'app_designation_get_voters', methods: ['GET'])]
+#[Route('/v3/designations/{uuid}/voters.{_format}', name: 'app_designation_get_voters', requirements: ['uuid' => '%pattern_uuid%', '_format' => 'json|xlsx'], defaults: ['_format' => 'json'], methods: ['GET'])]
 #[Security("is_granted('ROLE_OAUTH_SCOPE_JEMENGAGE_ADMIN') and is_granted('IS_FEATURE_GRANTED', 'designation')")]
 class ElectionVotersListController extends AbstractController
 {
@@ -18,11 +20,32 @@ class ElectionVotersListController extends AbstractController
         Designation $designation,
         ElectionRepository $electionRepository,
         VoterRepository $voterRepository,
+        ExporterInterface $exporter,
+        string $_format = 'json',
     ): Response {
-        if (!$election = $electionRepository->findOneByDesignation($designation)) {
-            return $this->json([]);
+        $voters = [];
+        if ($election = $electionRepository->findOneByDesignation($designation)) {
+            $voters = $voterRepository->getVotersForElection($election);
         }
 
-        return $this->json($voterRepository->getVotersForElection($election));
+        if ('json' !== $_format) {
+            $voters = array_filter($voters, fn ($voter) => $voter['voted_at']);
+
+            return $exporter->getResponse(
+                $_format,
+                \sprintf('Emargements - %d.xlsx', \count($voters)),
+                new IteratorCallbackSourceIterator(
+                    new \ArrayIterator($voters),
+                    fn ($voter) => [
+                        'Prénom' => $voter['first_name'],
+                        'Nom' => $voter['last_name'],
+                        'Date, heure d\'émargement' => $voter['voted_at'],
+                        'Code postal' => $voter['postal_code'],
+                    ]
+                )
+            );
+        }
+
+        return $this->json($voters);
     }
 }
