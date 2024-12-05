f4f8a81d3c69c5d7d43e90e70c4944f466b029d4
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-12-05 19:10:18 +0100

    [Security] fix logout redirection to app

---
+++ src/Security/Listener/LogoutListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Security\Listener;
+
+use App\Entity\Administrator;
+use App\Repository\OAuth\ClientRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class LogoutListener implements EventSubscriberInterface
+{
+    public function __construct(private readonly ClientRepository $clientRepository)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        if ($event->getToken()?->getUser() instanceof Administrator) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $client = $this->clientRepository->getVoxClient();
+        $redirectUri = $request->query->get('redirect_uri');
+
+        if (!$redirectUri || !\in_array($redirectUri, $client->getRedirectUris())) {
+            $redirectUri = current($client->getRedirectUris());
+        }
+
+        $event->setResponse(new RedirectResponse($redirectUri, Response::HTTP_SEE_OTHER));
+    }
+}
