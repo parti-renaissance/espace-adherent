7a01c6b8386e2daeed7aa08eff8f2b02a80312d4
Dimitri <gritsajuk.dimitri@gmail.com>
2024-12-10 16:59:26 +0100

    [Adhesion] fix scroll on form (#11174)

--- src/Mailer/Message/Renaissance/AbstractRenaissanceMessage.php
+++ src/Mailer/Message/Renaissance/AbstractRenaissanceMessage.php
@@ -6,4 +6,14 @@ use App\Mailer\Message\Message;
 
 abstract class AbstractRenaissanceMessage extends Message implements RenaissanceMessageInterface
 {
+    protected const SENDER_NAME = 'Gabriel Attal';
+    protected const SENDER_EMAIL = 'contact@parti-renaissance.fr';
+
+    protected static function updateSenderInfo(Message $message): Message
+    {
+        $message->setSenderEmail(static::SENDER_EMAIL);
+        $message->setSenderName(static::SENDER_NAME);
+
+        return $message;
+    }
 }
