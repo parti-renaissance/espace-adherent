3fcda959d825e30ce75508e58d51e1062a3e2fdd
Dimitri <gritsajuk.dimitri@gmail.com>
2024-12-16 14:37:33 +0100

    Remove terser-webpack-plugin (#11205)

--- webpack.production.js
+++ webpack.production.js
@@ -1,4 +1,3 @@
-const TerserJSPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
@@ -7,7 +6,7 @@ module.exports = merge(common, {
     mode: 'production',
     optimization: {
         minimizer: [
-            new TerserJSPlugin({}),
+            '...',
             new CssMinimizerPlugin({}),
         ],
     },
