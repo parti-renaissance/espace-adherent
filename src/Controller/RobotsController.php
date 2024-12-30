00c7841d1cc04601ad15fb0d9ab9c5a2fb1b4d37
Dimitri <gritsajuk.dimitri@gmail.com>
2024-12-30 11:28:43 +0100

    [SEO] disallow indexing admin domain (#11243)

---
+++ src/Controller/RobotsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/robots.txt', methods: 'GET')]
+class RobotsController extends AbstractController
+{
+    public function __invoke(Request $request, string $app_domain, string $appEnvironment, string $adminRenaissanceHost): Response
+    {
+        if ('production' !== $appEnvironment || $app_domain === $adminRenaissanceHost) {
+            return new Response("User-agent: *\nDisallow: /", 200, ['Content-Type' => 'text/plain']);
+        }
+
+        return new Response(
+            <<<ROBOTS
+                User-agent: *
+                Disallow: /*callback=/connexion
+                Disallow: /*callback=/adhesion
+                Disallow: /documents-partages/*
+                Disallow: /cdn-cgi/
+
+                Sitemap: https://parti-renaissance.fr/sitemap.xml
+                ROBOTS, 200, ['Content-Type' => 'text/plain']);
+    }
+}
