2275e4c997579c188b7a2cb61a604ed969fe7659
Rémi Gardien <gardien.remi@gmail.com>
2025-01-08 10:26:09 +0100

    [Ohme] Improve import command (#11276)

--- tests/Ohme/DummyClient.php
+++ tests/Ohme/DummyClient.php
@@ -27,7 +27,7 @@ class DummyClient implements ClientInterface
         ];
     }
 
-    public function getPayments(int $limit = 100, int $offset = 0, array $options = []): array
+    public function getPayments(array $options = []): array
     {
         return [
             'status' => 200,
