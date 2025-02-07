f441c299be821c0207001729308df585db875ba6
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2025-02-07 18:00:27 +0100

    [Event] add live event page with player

--- src/Controller/Renaissance/LiveStreamController.php
+++ src/Controller/Renaissance/LiveStreamController.php
@@ -3,18 +3,20 @@
 namespace App\Controller\Renaissance;
 
 use App\Entity\Adherent;
+use App\Entity\Event\Event;
 use App\Entity\LiveStream;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_USER')]
-#[Route('/live-stream/{uuid}', name: 'app_live_stream', methods: ['GET'])]
 class LiveStreamController extends AbstractController
 {
-    public function __invoke(LiveStream $liveStream, UserInterface $user): Response
+    #[IsGranted('ROLE_USER')]
+    #[Route('/live-stream/{uuid}', name: 'app_live_stream', methods: ['GET'])]
+    public function liveStreamAction(LiveStream $liveStream, UserInterface $user): Response
     {
         /** @var Adherent $user */
         if (!$user->hasActiveMembership()) {
@@ -25,6 +27,14 @@ class LiveStreamController extends AbstractController
 
         return $this->render('renaissance/live_stream.html.twig', [
             'live_stream' => $liveStream,
+        ]);
+    }
+
+    #[Route('/live-event/{slug}', name: 'app_live_event', methods: ['GET'])]
+    public function liveEventAction(Request $request, Event $event): Response
+    {
+        return $this->render('renaissance/live_event.html.twig', [
+            'event' => $event,
         ]);
     }
 }
