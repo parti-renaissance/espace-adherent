ac7609920e995dc09757eb2d0500f84bcd7fc7bb
Rémi Gardien <gardien.remi@gmail.com>
2025-02-13 12:05:32 +0100

    Add referral entity

---
+++ src/Admin/ReferralAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Route\RouteCollectionInterface;
+
+class ReferralAdmin extends AbstractAdmin
+{
+    protected function configureRoutes(RouteCollectionInterface $collection): void
+    {
+        $collection->clearExcept(['list']);
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('referrer', null, [
+                'label' => 'Adhérent',
+                'template' => 'admin/referral/list_referrer.html.twig',
+            ])
+            ->add('emailAddress', null, [
+                'label' => 'Email',
+            ])
+            ->add('firstName', null, [
+                'label' => 'Prénom',
+            ])
+        ;
+    }
+}
---
+++ src/Entity/Referral.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReferralRepository;
+use Doctrine\ORM\Mapping as ORM;
+use libphonenumber\PhoneNumber;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+#[ORM\Entity(repositoryClass: ReferralRepository::class)]
+class Referral
+{
+    use EntityIdentityTrait;
+    use EntityTimestampableTrait;
+    use EntityNullablePostAddressTrait;
+
+    #[ORM\Column]
+    public ?string $emailAddress = null;
+
+    #[ORM\Column(length: 50)]
+    public ?string $firstName = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    public ?string $lastName = null;
+
+    #[ORM\Column(length: 2, nullable: true)]
+    public ?string $nationality = null;
+
+    #[ORM\Column(type: 'phone_number', nullable: true)]
+    public ?PhoneNumber $phone = null;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    public ?\DateTimeInterface $birthdate = null;
+
+    #[ORM\JoinColumn(nullable: true, onDelete: 'SET NULL')]
+    #[ORM\ManyToOne(targetEntity: Adherent::class)]
+    public ?Adherent $referrer = null;
+
+    #[ORM\JoinColumn(nullable: true, onDelete: 'SET NULL')]
+    #[ORM\ManyToOne(targetEntity: Adherent::class)]
+    public ?Adherent $referee = null;
+
+    public function __construct(?UuidInterface $uuid = null)
+    {
+        $this->uuid = $uuid ?? Uuid::uuid4();
+    }
+
+    public function __toString()
+    {
+        return $this->emailAddress;
+    }
+}
