6b6a11393166ab40bb8f26843fb15f7b72c4c402
Rémi <remi@Remis-MacBook-Pro.local>
2025-03-19 16:47:49 +0100

    [Referral] Add timestamps and labels

--- src/Entity/EntityTimestampableTrait.php
+++ src/Entity/EntityTimestampableTrait.php
@@ -12,12 +12,12 @@ trait EntityTimestampableTrait
      * @var \DateTimeInterface
      */
     #[Gedmo\Timestampable(on: 'create')]
-    #[Groups(['jecoute_news_read', 'jecoute_news_read_dc', 'email_template_read', 'email_template_list_read', 'riposte_list_read', 'riposte_read', 'phoning_campaign_read', 'message_read_list', 'pap_building_history', 'pap_campaign_history_read_list', 'pap_campaign_replies_list', 'event_read', 'event_list_read', 'survey_list_dc', 'committee:list', 'document_read', 'national_event_inscription:webhook', 'procuration_request_read', 'procuration_request_list', 'procuration_proxy_list', 'procuration_matched_proxy', 'action_read', 'action_read_list', 'adherent_elect_read', 'tax_receipt:list', 'designation_list', 'designation_read'])]
+    #[Groups(['jecoute_news_read', 'jecoute_news_read_dc', 'email_template_read', 'email_template_list_read', 'riposte_list_read', 'riposte_read', 'phoning_campaign_read', 'message_read_list', 'pap_building_history', 'pap_campaign_history_read_list', 'pap_campaign_replies_list', 'event_read', 'event_list_read', 'survey_list_dc', 'committee:list', 'document_read', 'national_event_inscription:webhook', 'procuration_request_read', 'procuration_request_list', 'procuration_proxy_list', 'procuration_matched_proxy', 'action_read', 'action_read_list', 'adherent_elect_read', 'tax_receipt:list', 'designation_list', 'designation_read', 'referral_read'])]
     #[ORM\Column(type: 'datetime')]
     protected $createdAt;
 
     #[Gedmo\Timestampable(on: 'update')]
-    #[Groups(['phoning_campaign_read', 'committee:list', 'national_event_inscription:webhook', 'action_read', 'action_read_list'])]
+    #[Groups(['phoning_campaign_read', 'committee:list', 'national_event_inscription:webhook', 'action_read', 'action_read_list', 'referral_read'])]
     #[ORM\Column(type: 'datetime')]
     protected $updatedAt;
 
---
+++ src/Normalizer/ReferralNormalizer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Normalizer;
+
+use App\Entity\Referral;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ReferralNormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    public function __construct(private readonly TranslatorInterface $translator)
+    {
+    }
+
+    public function normalize($object, $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    {
+        $data = $this->normalizer->normalize($object, $format, $context + [__CLASS__ => true]);
+
+        $data['type_label'] = $data['type'] ? $this->translator->trans('referral.type.'.$data['type']) : null;
+        $data['mode_label'] = $data['mode'] ? $this->translator->trans('referral.mode.'.$data['mode']) : null;
+        $data['status_label'] = $data['status'] ? $this->translator->trans('referral.status.'.$data['status']) : null;
+
+        return $data;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            '*' => true,
+            Referral::class => false,
+        ];
+    }
+
+    public function supportsNormalization($data, $format = null, array $context = []): bool
+    {
+        return !isset($context[__CLASS__]) && $data instanceof Referral;
+    }
+}
