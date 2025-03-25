ab7c1e7cbdfd780c595576c24d6d58bc4673537f
Rémi Gardien <gardien.remi@gmail.com>
2025-03-25 14:54:29 +0100

    [Referral] Add statistics endpoint (#11634)
    Co-authored-by: Rémi <remi@remis-mbp.home>

---
+++ src/Controller/Api/Referral/GetStatisticsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Api\Referral;
+
+use App\Entity\Adherent;
+use App\Repository\ReferralRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('RENAISSANCE_ADHERENT')]
+#[Route(path: '/v3/referrals/statistics', name: 'api_get_referral_statistics', methods: ['GET'])]
+class GetStatisticsController extends AbstractController
+{
+    public function __invoke(ReferralRepository $referralRepository): Response
+    {
+        /** @var Adherent $user */
+        $user = $this->getUser();
+
+        return $this->json($referralRepository->getStatistics($user));
+    }
+}
