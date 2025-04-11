7292fb37ab423a94e3ff5d8498823609edc8d5d1
Rémi <remi@remis-mbp.home>
2025-04-11 15:22:27 +0200

    Fix review

--- src/Mailer/Message/Renaissance/EventReminderMessage.php
+++ src/Mailer/Message/Renaissance/EventReminderMessage.php
@@ -18,15 +18,11 @@ class EventReminderMessage extends AbstractRenaissanceMessage
             '[Rappel] Participation à un événement',
             [
                 'target_firstname' => $eventRegistration->getFirstName(),
-                'event_hour' => \sprintf(
-                    '%sh%s',
-                    static::formatDate($event->getLocalBeginAt(), 'HH'),
-                    static::formatDate($event->getLocalBeginAt(), 'mm')
-                ),
+                'event_hour' => static::formatDate($event->getLocalBeginAt(), 'HH\'h\'mm'),
                 'event_name' => $event->getName(),
                 'event_address' => $event->getInlineFormattedAddress(),
                 'event_description' => $event->getDescription(),
-                'animator_firstname' => $event->getAuthor()?->getFirstname(),
+                'animator_firstname' => $event->getAuthor()?->getFirstName(),
                 'event_slug' => $eventLink,
             ],
         );
