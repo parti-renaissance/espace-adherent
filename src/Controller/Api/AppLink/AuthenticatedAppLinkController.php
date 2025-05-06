38887113bc9e39c036bb5f5e2ee05114e4673df3
Dimitri <gritsajuk.dimitri@gmail.com>
2025-05-06 16:16:15 +0200

    [Session] avoid create session for cadre access on impersonification (#11813)

--- src/Controller/Api/AppLink/AuthenticatedAppLinkController.php
+++ src/Controller/Api/AppLink/AuthenticatedAppLinkController.php
@@ -3,13 +3,16 @@
 namespace App\Controller\Api\AppLink;
 
 use App\Controller\Renaissance\Adhesion\AdhesionController;
+use App\Entity\Adherent;
+use App\OAuth\Model\Scope;
+use App\Repository\OAuth\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
 
@@ -29,10 +32,20 @@ class AuthenticatedAppLinkController extends AbstractController
         'utm_campaign',
     ];
 
-    public function __invoke(Request $request, string $userVoxHost, LoginLinkHandlerInterface $loginLinkHandler, string $key, UserInterface $user, UrlGeneratorInterface $urlGenerator): JsonResponse
+    public function __construct(
+        private readonly string $adminRenaissanceHost,
+        private readonly ClientRepository $clientRepository,
+    ) {
+    }
+
+    public function __invoke(Request $request, string $userVoxHost, LoginLinkHandlerInterface $loginLinkHandler, string $key, #[CurrentUser] Adherent $user, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
         if (!\array_key_exists($key, self::KEYS_TO_ROUTES)) {
             throw new BadRequestHttpException(\sprintf('No route found for key "%s".', $key));
+        }
+
+        if ('cadre' === $key && $this->isGranted(Scope::generateRole(Scope::IMPERSONATOR))) {
+            return $this->json($this->getCadreLink());
         }
 
         $context = $urlGenerator->getContext();
@@ -68,5 +81,21 @@ class AuthenticatedAppLinkController extends AbstractController
         }
 
         return [$routeName, $parameters];
+    }
+
+    private function getCadreLink(): array
+    {
+        $client = $this->clientRepository->getCadreClient();
+
+        return [
+            'url' => $this->generateUrl('app_front_oauth_authorize', [
+                'app_domain' => $this->adminRenaissanceHost,
+                'response_type' => 'code',
+                'client_id' => $client->getUuid()->toString(),
+                'redirect_uri' => $client->getRedirectUris()[0],
+                'scope' => implode(' ', $client->getSupportedScopes()),
+            ], UrlGeneratorInterface::ABSOLUTE_URL),
+            'expires_at' => (new \DateTimeImmutable())->add(new \DateInterval('PT10M')),
+        ];
     }
 }
