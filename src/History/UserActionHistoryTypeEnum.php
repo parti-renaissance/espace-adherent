fe7f588db147123b11e38d302d020f5721477d5c
Rémi <remi@remis-mbp.home>
2025-05-14 11:07:27 +0200

    [Agora] Add history logs for privileges

--- src/History/UserActionHistoryTypeEnum.php
+++ src/History/UserActionHistoryTypeEnum.php
@@ -21,4 +21,8 @@ enum UserActionHistoryTypeEnum: string
     case DELEGATED_ACCESS_REMOVE = 'delegated_access_remove';
     case AGORA_MEMBERSHIP_ADD = 'agora_membership_add';
     case AGORA_MEMBERSHIP_REMOVE = 'agora_membership_remove';
+    case AGORA_PRESIDENT_ADD = 'agora_president_add';
+    case AGORA_PRESIDENT_REMOVE = 'agora_president_remove';
+    case AGORA_GENERAL_SECRETARY_ADD = 'agora_general_secretary_add';
+    case AGORA_GENERAL_SECRETARY_REMOVE = 'agora_general_secretary_remove';
 }
