ccb97dd1901d32241b89aae262b885c6036a6656
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2025-07-21 14:12:48 +0200

    [NationalEvent] check payment status on background

---
+++ src/NationalEvent/Payment/Direct.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\NationalEvent\Payment;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class Direct
+{
+    public function __construct(
+        private readonly HttpClientInterface $ogoneClient,
+        private readonly string $ogonePspId,
+        private readonly string $ogoneUserId,
+        private readonly string $ogoneUserPwd,
+    ) {
+    }
+
+    public function getStatus(string $orderId): string
+    {
+        return $this->ogoneClient->request('POST', 'querydirect.asp', [
+            'body' => [
+                'PSPID' => $this->ogonePspId,
+                'USERID' => $this->ogoneUserId,
+                'PSWD' => $this->ogoneUserPwd,
+                'ORDERID' => $orderId,
+            ],
+        ])->getContent(false);
+    }
+}
