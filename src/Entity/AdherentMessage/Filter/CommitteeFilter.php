f3afbcf45c4ccc630fba6319a1a56b79c1e3369b
Rémi Gardien <gardien.remi@gmail.com>
2025-07-28 14:21:38 +0200

    Add missing normalization groups for committee (#12150)
    Co-authored-by: Rémi <remi@remis-mbp.home>

--- src/Entity/AdherentMessage/Filter/CommitteeFilter.php
+++ src/Entity/AdherentMessage/Filter/CommitteeFilter.php
@@ -4,12 +4,14 @@ namespace App\Entity\AdherentMessage\Filter;
 
 use App\Entity\Committee;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity]
 class CommitteeFilter extends AbstractUserFilter
 {
     #[Assert\NotBlank]
+    #[Groups(['adherent_message_update_filter', 'adherent_message_read_filter'])]
     #[ORM\JoinColumn(onDelete: 'SET NULL')]
     #[ORM\ManyToOne(targetEntity: Committee::class)]
     private ?Committee $committee = null;
