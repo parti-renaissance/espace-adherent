099a83abec5c16c9f6a5aca0baa73ba1398b48cf
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2025-08-29 16:52:56 +0200

    Remove unused subscription webhook

--- src/Subscription/SubscriptionHandler.php
+++ src/Subscription/SubscriptionHandler.php
@@ -4,62 +4,20 @@ namespace App\Subscription;
 
 use App\Entity\Adherent;
 use App\History\EmailSubscriptionHistoryHandler;
-use App\Repository\AdherentRepository;
-use App\Repository\NewsletterSubscriptionRepository;
 use App\Repository\SubscriptionTypeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class SubscriptionHandler
 {
-    public const ACTION_TYPE_SUBSCRIBE = 'subscribe';
-    public const ACTION_TYPE_UNSUBSCRIBE = 'unsubscribe';
-    public const ACTION_TYPES = [
-        self::ACTION_TYPE_SUBSCRIBE,
-        self::ACTION_TYPE_UNSUBSCRIBE,
-    ];
-
     public function __construct(
         private readonly EntityManagerInterface $em,
         private readonly EmailSubscriptionHistoryHandler $subscriptionHistoryHandler,
-        private readonly AdherentRepository $adherentRepository,
         private readonly SubscriptionTypeRepository $subscriptionTypeRepository,
-        private readonly NewsletterSubscriptionRepository $newsletterSubscriptionRepository,
     ) {
     }
 
-    public function changeSubscription(string $type, string $email, string $listId): void
+    public function addDefaultTypesToAdherent(Adherent $adherent, bool $allowEmailNotifications, bool $allowMobileNotifications): void
     {
-        if (!\in_array($type, self::ACTION_TYPES)) {
-            throw new \InvalidArgumentException('Action is not authorised.');
-        }
-
-        $adherent = $this->adherentRepository->findOneByEmail($email);
-        if ($adherent) {
-            $subscriptionType = $this->subscriptionTypeRepository->findOneByExternalId($listId);
-            if (!$subscriptionType) {
-                throw new \RuntimeException(\sprintf('There is no subscription type with external service id "%s".', $listId));
-            }
-
-            $hasSubscription = $adherent->hasSubscriptionType($subscriptionType->getCode());
-            if (self::ACTION_TYPE_SUBSCRIBE === $type && !$hasSubscription) {
-                $adherent->addSubscriptionType($subscriptionType);
-            } elseif (self::ACTION_TYPE_UNSUBSCRIBE === $type && $hasSubscription) {
-                $adherent->removeSubscriptionType($subscriptionType);
-            }
-        } elseif (($newsletterSubscription = $this->newsletterSubscriptionRepository->findOneByEmail($email))
-            && self::ACTION_TYPE_UNSUBSCRIBE === $type) {
-            // Newsletter subscription will remain in the table but with filled `deleted_at`field
-            $this->em->remove($newsletterSubscription);
-        }
-
-        $this->em->flush();
-    }
-
-    public function addDefaultTypesToAdherent(
-        Adherent $adherent,
-        bool $allowEmailNotifications,
-        bool $allowMobileNotifications,
-    ): void {
         $this->handleUpdateSubscription($adherent, array_merge(
             $allowEmailNotifications ? $this->getEmailDefaultTypes($adherent) : [],
             $allowMobileNotifications ? SubscriptionTypeEnum::DEFAULT_MOBILE_TYPES : []
