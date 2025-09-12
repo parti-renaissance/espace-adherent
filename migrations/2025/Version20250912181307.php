b9cc10cb03734fea63d65e9508d479f1dc896321
Dimitri <gritsajuk.dimitri@gmail.com>
2025-09-12 20:24:22 +0200

    [NationalEvent] add bracelet field (#12303)

---
+++ migrations/2025/Version20250912181307.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250912181307 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE
+          national_event_inscription
+        ADD
+          ticket_bracelet VARCHAR(255) DEFAULT NULL,
+        CHANGE
+          ticket_custom_detail_color ticket_bracelet_color VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE
+          national_event_inscription
+        ADD
+          ticket_custom_detail_color VARCHAR(255) DEFAULT NULL,
+        DROP
+          ticket_bracelet,
+        DROP
+          ticket_bracelet_color');
+    }
+}
