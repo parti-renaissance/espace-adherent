15c697b487134dd65fd7c5eddcbab9b534a3a9a0
ivdami <231574100+ivdami@users.noreply.github.com>
2025-10-28 12:26:54 +0100

    Fix deprecations

--- src/Repository/CommitteeMembershipRepository.php
+++ src/Repository/CommitteeMembershipRepository.php
@@ -114,7 +114,7 @@ class CommitteeMembershipRepository extends ServiceEntityRepository
             $qb
                 ->innerJoin('cm.adherent', 'adherent')
                 ->andWhere('adherent.tags LIKE :adherent_tag')
-                ->setParameter('adherent_tag', ($adherentRenaissance ? TagEnum::ADHERENT : TagEnum::SYMPATHISANT).'%')
+                ->setParameter('adherent_tag', ($adherentRenaissance ? TagEnum::ADHERENT : TagEnum::SYMPATHISANT).':%')
             ;
         }
 
