ada62dbbe8c7b9881be7f7e9fa0fe96dbabbe437
ivdami <231574100+ivdami@users.noreply.github.com>
2025-11-18 09:28:35 +0100

    feat(api/event): filter by region if any event for current dpt (#12518)

---
+++ src/Api/Provider/EventsFallbackProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Api\Provider;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Api\Filter\EventsDepartmentFilter;
+use App\Entity\Event\Event;
+use App\Entity\Geo\Zone;
+use App\Repository\Geo\ZoneRepository;
+
+class EventsFallbackProvider implements ProviderInterface
+{
+    public const CONTEXT_KEY = 'event_region_fallback';
+
+    public function __construct(
+        private readonly ProviderInterface $decorated,
+        private readonly ZoneRepository $zoneRepository,
+    ) {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        $filters = $context['filters'] ?? [];
+        $dptCode = $filters[EventsDepartmentFilter::PROPERTY_NAME] ?? null;
+
+        if (\is_array($dptCode)) {
+            $dptCode = current($dptCode);
+        }
+
+        $result = $this->decorated->provide($operation, $uriVariables, $context);
+
+        /** @var Event[] $events */
+        $events = iterator_to_array($result);
+
+        $hasLocal = false;
+        foreach ($events as $event) {
+            if (!$event->isNational()) {
+                $hasLocal = true;
+                break;
+            }
+        }
+
+        if ($hasLocal) {
+            return $result;
+        }
+
+        $zone = $this->zoneRepository->findOneByCode($dptCode);
+        $region = $zone?->getParentsOfType(Zone::REGION)[0] ?? null;
+
+        if (!$region) {
+            return $events;
+        }
+
+        $contextFallback = $context;
+        $contextFallback['filters'] = $filters;
+        $contextFallback['filters'][EventsDepartmentFilter::PROPERTY_NAME] = $region->getTypeCode();
+
+        $context['request']->attributes->set(self::CONTEXT_KEY, true);
+
+        return $this->decorated->provide($operation, $uriVariables, $contextFallback);
+    }
+}
--- src/Api/Serializer/EventGroupsContextBuilder.php
+++ src/Api/Serializer/EventGroupsContextBuilder.php
@@ -3,6 +3,7 @@
 namespace App\Api\Serializer;
 
 use ApiPlatform\State\SerializerContextBuilderInterface;
+use App\Api\Provider\EventsFallbackProvider;
 use App\Entity\Event\Event;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,10 +18,15 @@ class EventGroupsContextBuilder implements SerializerContextBuilderInterface
         $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
         $resourceClass = $context['resource_class'] ?? null;
 
-        if (Event::class !== $resourceClass
-            || Request::METHOD_PUT !== $request->getMethod()
-            || $normalization
-        ) {
+        if (Event::class !== $resourceClass) {
+            return $context;
+        }
+
+        if ($request->attributes->get(EventsFallbackProvider::CONTEXT_KEY)) {
+            $context['groups'][] = 'event_list_read:region';
+        }
+
+        if (Request::METHOD_PUT !== $request->getMethod() || $normalization) {
             return $context;
         }
 
