067dcfcd9586629b992223ee53a6ed850a39cdea
ivdami <231574100+ivdami@users.noreply.github.com>
2026-01-20 15:44:19 +0100

    chore(IPN): change command queue (#12824)

--- src/Donation/Command/ReceivePayboxIpnResponseCommand.php
+++ src/Donation/Command/ReceivePayboxIpnResponseCommand.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Donation\Command;
 
-use App\Messenger\Message\AsynchronousMessageInterface;
+use App\Messenger\Message\SequentialMessageInterface;
 
-class ReceivePayboxIpnResponseCommand implements AsynchronousMessageInterface
+class ReceivePayboxIpnResponseCommand implements SequentialMessageInterface
 {
     public function __construct(public readonly array $payload)
     {
