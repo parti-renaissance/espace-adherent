87b301c49c5495636f4e604f7f64afb75b4a7114
ivdami <231574100+ivdami@users.noreply.github.com>
2026-01-21 11:01:36 +0100

    Revert "chore(IPN): change command queue (#12824)"
    This reverts commit 067dcfcd9586629b992223ee53a6ed850a39cdea.

--- src/Donation/Command/ReceivePayboxIpnResponseCommand.php
+++ src/Donation/Command/ReceivePayboxIpnResponseCommand.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Donation\Command;
 
-use App\Messenger\Message\SequentialMessageInterface;
+use App\Messenger\Message\AsynchronousMessageInterface;
 
-class ReceivePayboxIpnResponseCommand implements SequentialMessageInterface
+class ReceivePayboxIpnResponseCommand implements AsynchronousMessageInterface
 {
     public function __construct(public readonly array $payload)
     {
