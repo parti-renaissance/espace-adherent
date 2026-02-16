8225e0f11a15f5aee88fba29b0648392c048858c
ivdami <231574100+ivdami@users.noreply.github.com>
2026-02-16 09:57:56 +0100

    feat(donation): update major donator pdf

--- src/Form/DonationRequestType.php
+++ src/Form/DonationRequestType.php
@@ -18,30 +18,32 @@ class DonationRequestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('amount', AmountType::class, [
-                'error_bubbling' => false,
-            ])
-            ->add('duration', HiddenType::class, [
-                'error_bubbling' => false,
-            ])
-            ->add('localDestination', CheckboxType::class)
-
+            ->add('amount', AmountType::class, ['error_bubbling' => false])
+            ->add('duration', HiddenType::class, ['error_bubbling' => false])
             ->add('gender', GenderCivilityType::class)
             ->add('firstName', TextType::class)
             ->add('lastName', TextType::class)
             ->add('emailAddress', EmailType::class)
             ->add('nationality', ReCountryType::class)
             ->add('address', AutocompleteAddressType::class, ['with_additional_address' => true])
-
             ->add('autorisations', RequiredCheckboxType::class)
         ;
+
+        if (!$options['major_donator']) {
+            $builder->add('localDestination', CheckboxType::class);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => DonationRequest::class,
-        ]);
+        $resolver
+            ->setDefaults([
+                'data_class' => DonationRequest::class,
+                'major_donator' => false,
+            ])
+            ->setDefined('major_donator')
+            ->setAllowedTypes('major_donator', 'bool')
+        ;
     }
 
     public function getBlockPrefix(): string
