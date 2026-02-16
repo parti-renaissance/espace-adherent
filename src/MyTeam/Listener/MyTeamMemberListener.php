539f4012a53d35cef884043289dd4fa0cce278dc
ivdami <231574100+ivdami@users.noreply.github.com>
2026-02-17 00:36:55 +0100

    feat(team): auto delegate custom role feature (#12921)

--- src/MyTeam/Listener/MyTeamMemberListener.php
+++ src/MyTeam/Listener/MyTeamMemberListener.php
@@ -2,29 +2,61 @@
 
 declare(strict_types=1);
 
-namespace App\MyTeam\Api\Listener;
+namespace App\MyTeam\Listener;
 
 use ApiPlatform\Symfony\EventListener\EventPriorities;
 use App\Entity\MyTeam\Member;
 use App\MyTeam\DelegatedAccessManager;
+use App\Scope\FeatureEnum;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-class PostWriteMyTeamMemberListener implements EventSubscriberInterface
+class MyTeamMemberListener implements EventSubscriberInterface
 {
-    private DelegatedAccessManager $delegatedAccessManager;
-
-    public function __construct(DelegatedAccessManager $delegatedAccessManager)
+    public function __construct(private readonly DelegatedAccessManager $delegatedAccessManager)
     {
-        $this->delegatedAccessManager = $delegatedAccessManager;
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::VIEW => ['updateDelegatedAccess', EventPriorities::POST_WRITE],
+            KernelEvents::VIEW => [
+                ['updateMemberScopeFeatures', EventPriorities::PRE_WRITE],
+                ['updateDelegatedAccess', EventPriorities::POST_WRITE],
+            ],
         ];
+    }
+
+    public function updateMemberScopeFeatures(ViewEvent $event): void
+    {
+        $request = $event->getRequest();
+        if (Member::class !== $request->attributes->get('_api_resource_class')) {
+            return;
+        }
+
+        /** @var Member $member */
+        $member = $event->getControllerResult();
+        if (!$member instanceof Member) {
+            return;
+        }
+
+        if ('_api_/v3/my_team_members/{uuid}_delete' === $request->attributes->get('_api_operation_name')) {
+            return;
+        }
+
+        $features = $member->getScopeFeatures();
+        if (\in_array(FeatureEnum::MY_TEAM, $features, true)) {
+            if (!\in_array(FeatureEnum::MY_TEAM_CUSTOM_ROLE, $features, true)) {
+                $features[] = FeatureEnum::MY_TEAM_CUSTOM_ROLE;
+            }
+        } else {
+            $key = array_search(FeatureEnum::MY_TEAM_CUSTOM_ROLE, $features, true);
+            if (false !== $key) {
+                unset($features[$key]);
+            }
+        }
+        $member->setScopeFeatures(array_values($features));
     }
 
     public function updateDelegatedAccess(ViewEvent $event): void
