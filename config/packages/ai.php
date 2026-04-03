74fcf4cada1f44b255cd4a6e1e7c165d75d4e6a4
ivdami <231574100+ivdami@users.noreply.github.com>
2026-04-03 14:39:01 +0200

    feat(chatbot): replace API Gemini by VertexAI (#13117)

--- config/packages/ai.php
+++ config/packages/ai.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 return static function (Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('ai', [
         'platform' => [
-            'gemini' => [
-                'api_key' => '%env(GEMINI_API_KEY)%',
+            'vertexai' => [
+                'project_id' => '%env(VERTEX_AI_PROJECT_ID)%',
+                'location' => '%env(VERTEX_AI_LOCATION)%',
             ],
         ],
         'agent' => [
             'default' => [
-                'platform' => 'ai.platform.gemini',
+                'platform' => 'ai.platform.vertexai',
                 'model' => [
                     'name' => 'gemini-2.5-flash-lite',
                     'options' => [
