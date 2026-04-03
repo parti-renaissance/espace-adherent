347b09f115b9c393b1a8a0296e01fd5450854696
ivdami <231574100+ivdami@users.noreply.github.com>
2026-04-03 11:19:25 +0200

    feat(publication): reset sender on duplication

--- src/Controller/Api/AdherentMessage/DuplicateMessageController.php
+++ src/Controller/Api/AdherentMessage/DuplicateMessageController.php
@@ -6,21 +6,16 @@ namespace App\Controller\Api\AdherentMessage;
 
 use App\AdherentMessage\AdherentMessageManager;
 use App\Entity\AdherentMessage\AdherentMessage;
-use App\Normalizer\ImageExposeNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class DuplicateMessageController extends AbstractController
 {
-    public function __invoke(AdherentMessageManager $manager, AdherentMessage $data): Response
+    public function __invoke(AdherentMessageManager $manager, AdherentMessage $data): JsonResponse
     {
         $clone = $manager->duplicate($data);
 
-        return $this->json(
-            $clone,
-            Response::HTTP_CREATED,
-            [],
-            ['groups' => ['message_read', ImageExposeNormalizer::NORMALIZATION_GROUP]]
-        );
+        return new JsonResponse(['uuid' => $clone->getUuid()->toString()], Response::HTTP_CREATED);
     }
 }
