3b9fc5daaa6a6db8467a6117274c81a9db3e52a7
ivdami <231574100+ivdami@users.noreply.github.com>
2026-05-13 14:43:10 +0200

    fix(publication): fix duplication by using existing stats object (#13289)

--- src/AdherentMessage/Handler/UpdatePublicationsStatsHandler.php
+++ src/AdherentMessage/Handler/UpdatePublicationsStatsHandler.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\AdherentMessage\Handler;
 
 use App\Entity\AdherentMessage\AdherentMessage;
+use App\Entity\AdherentMessage\PublicationStatistics;
 use App\JeMengage\Hit\Stats\AggregatorInterface;
 use App\JeMengage\Hit\TargetTypeEnum;
 use App\Mailchimp\Campaign\Report\Command\SyncReportCommand;
+use App\Repository\AdherentMessage\PublicationStatisticsRepository;
 use App\Repository\AdherentMessageRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -18,6 +20,7 @@ class UpdatePublicationsStatsHandler
     public function __construct(
         private readonly AggregatorInterface $aggregator,
         private readonly AdherentMessageRepository $adherentMessageRepository,
+        private readonly PublicationStatisticsRepository $publicationStatisticsRepository,
         private readonly EntityManagerInterface $entityManager,
     ) {
     }
@@ -37,9 +40,12 @@ class UpdatePublicationsStatsHandler
             return;
         }
 
-        $statsObject = $adherentMessage->getStatistics();
+        $statsObject = $this->publicationStatisticsRepository->findOneByMessage($adherentMessage)
+            ?? new PublicationStatistics($adherentMessage);
+
         $statsObject->refresh($this->aggregator->getStats(TargetTypeEnum::Publication, $adherentMessage->getUuid()));
 
+        $this->entityManager->persist($statsObject);
         $this->entityManager->flush();
     }
 }
