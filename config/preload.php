1c5c51d61b166d94580072aefaa678119d525550
ivdami <231574100+ivdami@users.noreply.github.com>
2026-05-18 11:10:18 +0200

    chore(Preload): set time limit to 0

--- config/preload.php
+++ config/preload.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+set_time_limit(0);
+
 if (file_exists(\dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require \dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
