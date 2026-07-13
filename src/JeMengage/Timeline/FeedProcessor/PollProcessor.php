0b0bef548b064f360c96658fe672266834b49880
Alex-renaissance <alexandre.damasceno@parti-renaissance.fr>
2026-07-13 17:34:50 +0200

    feat(Timeline): change data structure timeline poll

--- src/JeMengage/Timeline/FeedProcessor/PollProcessor.php
+++ src/JeMengage/Timeline/FeedProcessor/PollProcessor.php
@@ -4,27 +4,41 @@ declare(strict_types=1);
 
 namespace App\JeMengage\Timeline\FeedProcessor;
 
+use App\Api\Serializer\PrivatePublicContextBuilder;
 use App\Entity\Adherent;
 use App\JeMengage\Timeline\TimelineFeedTypeEnum;
+use App\Repository\Poll\PollRepository;
 use App\Repository\Poll\VoteRepository;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Uid\Uuid;
 
 class PollProcessor extends AbstractFeedProcessor
 {
-    public function __construct(private readonly VoteRepository $voteRepository)
-    {
+    public function __construct(
+        private readonly PollRepository $pollRepository,
+        private readonly VoteRepository $voteRepository,
+        private readonly NormalizerInterface $normalizer,
+    ) {
     }
 
+    /**
+     * @throws ExceptionInterface
+     */
     public function process(array $item, Adherent $user): array
     {
-        $vote = $this->voteRepository->findAdherentVote(Uuid::fromString($item['objectID']), $user);
+        $poll = $this->pollRepository->findOneByUuid($item['objectID']);
 
-        $item['user_registered_at'] = $vote?->getCreatedAt();
+        if (null === $poll) {
+            return $item;
+        }
 
-        $item['poll'] = [
-            'question' => $item['title'] ?? null,
-            'has_voted' => null !== $vote,
-        ];
+        $item['user_registered_at'] = $this->voteRepository->findAdherentVote(Uuid::fromString($item['objectID']), $user)?->getCreatedAt();
+
+        $item['poll'] = $this->normalizer->normalize($poll, null, [
+            'groups' => ['poll_read'],
+            PrivatePublicContextBuilder::CONTEXT_KEY => PrivatePublicContextBuilder::CONTEXT_PUBLIC_CONNECTED_USER,
+        ]);
 
         return $item;
     }
